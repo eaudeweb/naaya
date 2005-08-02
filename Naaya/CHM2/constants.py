7d5dc71955534210cfc39ba52c2560a026c4f721
Miruna Badescu <miruna.badescu@eaudeweb.ro>
2005-08-02 09:28:01 +0000

    Initial import of the Naaya suite of products (ex EWPublisher) - CHM specific modules

---
+++ Naaya/CHM2/constants.py
@@ -0,0 +1,34 @@
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Initial Owner of the Original Code is European Environment
+# Agency (EEA).  Portions created by Finsiel Romania are
+# Copyright (C) European Environment Agency.  All
+# Rights Reserved.
+#
+# Authors:
+#
+# Cornel Nitu, Finsiel Romania
+# Dragos Chirila, Finsiel Romania
+
+#Python imports
+from os.path import join
+
+#Zope imports
+
+#Product imports
+import Globals
+
+CHM2_PRODUCT_NAME = 'CHM2'
+CHM2_PRODUCT_PATH = Globals.package_home(globals())
+
+PERMISSION_ADD_CHMSITE = 'CHM2 - Add CHM Site objects'
+
+METATYPE_CHMSITE = 'CHM Site'
