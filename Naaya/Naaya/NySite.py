ff0be18a96b0264a7858ba890b14641c294af0ab
Dragos Chiriac <dragos.chiriac@eaudeweb.ro>
2005-08-24 13:02:29 +0000

    fix external search

--- Naaya/Naaya/NySite.py
+++ Naaya/Naaya/NySite.py
@@ -733,6 +733,8 @@ class NySite(CookieCrumbler, LocalPropertyManager, Folder,
     security.declarePublic('external_search')
     def external_search(self, search_param=''):
         """ """
+        if isinstance(search_param, unicode):
+            search_param = search_param.encode('utf-8')
         curr_lang = self.gl_get_selected_language()
         #transtab = string.maketrans('/','_')
         #unused='\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf'
