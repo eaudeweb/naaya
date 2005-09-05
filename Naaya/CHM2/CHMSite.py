54a1cdb6de776ee5ef476d3a695f507a1185f19e
Dragos Chiriac <dragos.chiriac@eaudeweb.ro>
2005-09-05 16:33:42 +0000

    modify the URL management page to handle multiple objects for the same link

--- Naaya/CHM2/CHMSite.py
+++ Naaya/CHM2/CHMSite.py
@@ -232,18 +232,22 @@ class CHMSite(NySite):
         #list urls
         for x in self.query_objects_ex(meta_type=METATYPE_NYURL):
             p_value = x.locator
-            if url_struct.has_key(p_value):
-                url_struct[p_value][1].append(x.getParentNode())
-            else:
-                url_struct[p_value] = [x, [x.getParentNode()]]
+            if isinstance(p_value, unicode): p_value = p_value.encode('utf-8')
+            if p_value != '':
+                if url_struct.has_key(p_value):
+                    url_struct[p_value].append((x, x.getParentNode()))
+                else:
+                    url_struct[p_value] = [(x, x.getParentNode())]
         #list other
         for x in self.query_objects_ex(meta_type=self.get_content_urls().keys()):
             for y in self.get_content_urls()[x.meta_type]:
                 p_value = getattr(x, y)
-                if url_struct.has_key(p_value):
-                    url_struct[p_value][1].append(x.getParentNode())
-                else:
-                    url_struct[p_value] = [x, [x.getParentNode()]]
+                if isinstance(p_value, unicode): p_value = p_value.encode('utf-8')
+                if p_value != '':
+                    if url_struct.has_key(p_value):
+                        url_struct[p_value].append((x, x.getParentNode()))
+                    else:
+                        url_struct[p_value] = [(x, x.getParentNode())]
         return url_struct
 
     def getAllLatestUploads(self):
