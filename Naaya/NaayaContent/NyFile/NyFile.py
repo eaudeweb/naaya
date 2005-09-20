609a3674a5c3af53a6c103e40f7ad588c71a9700
Dragos Chiriac <dragos.chiriac@eaudeweb.ro>
2005-09-20 09:28:05 +0000

    import stuff

--- Naaya/NaayaContent/NyFile/NyFile.py
+++ Naaya/NaayaContent/NyFile/NyFile.py
@@ -94,30 +94,37 @@ def addNyFile(self, id='', title='', description='', coverage='', keywords='', s
             self.setSession('referer', self.absolute_url())
             REQUEST.RESPONSE.redirect('%s/note_html' % self.getSitePath())
 
-def importNyFile(self, id, attrs, content, properties, discussion, objects):
+def importNyFile(self, param, id, attrs, content, properties, discussion, objects):
     #this method is called during the import process
-    addNyFile(self, id=id,
-        sortorder=attrs['sortorder'].encode('utf-8'),
-        source='file', file=self.utBase64Decode(attrs['file'].encode('utf-8')),
-        downloadfilename=attrs['downloadfilename'].encode('utf-8'),
-        contributor=attrs['contributor'].encode('utf-8'),
-        discussion=abs(int(attrs['discussion'].encode('utf-8'))))
-    ob = self._getOb(id)
-    #set the real content_type and precondition
-    ob.content_type = attrs['content_type'].encode('utf-8')
-    ob.precondition = attrs['precondition'].encode('utf-8')
-    ob._p_changed = 1
-    for property, langs in properties.items():
-        for lang in langs:
-            ob._setLocalPropValue(property, lang, langs[lang])
-    ob.approveThis(abs(int(attrs['approved'].encode('utf-8'))))
-    ob.setReleaseDate(attrs['releasedate'].encode('utf-8'))
-    ob.checkThis(attrs['validation_status'].encode('utf-8'),
-        attrs['validation_comment'].encode('utf-8'),
-        attrs['validation_by'].encode('utf-8'),
-        attrs['validation_date'].encode('utf-8'))
-    ob.import_comments(discussion)
-    self.recatalogNyObject(ob)
+    try: param = abs(int(param))
+    except: param = 0
+    if param in [0, 1]:
+        if param == 1:
+            #delete the object if exists
+            try: self.manage_delObjects([id])
+            except: pass
+        addNyFile(self, id=id,
+            sortorder=attrs['sortorder'].encode('utf-8'),
+            source='file', file=self.utBase64Decode(attrs['file'].encode('utf-8')),
+            downloadfilename=attrs['downloadfilename'].encode('utf-8'),
+            contributor=attrs['contributor'].encode('utf-8'),
+            discussion=abs(int(attrs['discussion'].encode('utf-8'))))
+        ob = self._getOb(id)
+        #set the real content_type and precondition
+        ob.content_type = attrs['content_type'].encode('utf-8')
+        ob.precondition = attrs['precondition'].encode('utf-8')
+        ob._p_changed = 1
+        for property, langs in properties.items():
+            for lang in langs:
+                ob._setLocalPropValue(property, lang, langs[lang])
+        ob.approveThis(abs(int(attrs['approved'].encode('utf-8'))))
+        ob.setReleaseDate(attrs['releasedate'].encode('utf-8'))
+        ob.checkThis(attrs['validation_status'].encode('utf-8'),
+            attrs['validation_comment'].encode('utf-8'),
+            attrs['validation_by'].encode('utf-8'),
+            attrs['validation_date'].encode('utf-8'))
+        ob.import_comments(discussion)
+        self.recatalogNyObject(ob)
 
 class NyFile(NyAttributes, file_item, NyItem, NyVersioning, NyCheckControl, NyValidation):
     """ """
