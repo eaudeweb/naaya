7ef83f435e239fbf3b4c1594c7956a24f30073c8
Dragos Chiriac <dragos.chiriac@eaudeweb.ro>
2005-10-26 14:29:01 +0000

    CHM2 installation package for windows: initial import

---
+++ Naaya/chm2_installation_package_windows/CHM2/WizardData.h
@@ -0,0 +1,46 @@
+// WizardData.h: interface for the CWizardData class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_WIZARDDATA_H__43ED010B_6D44_4951_86F6_0E3816AF10CD__INCLUDED_)
+#define AFX_WIZARDDATA_H__43ED010B_6D44_4951_86F6_0E3816AF10CD__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CWizardData  
+{
+public:
+	CWizardData();
+	virtual ~CWizardData();
+
+public:
+	BOOL m_bIsCurrentUserAdministrator;
+
+	// InstallPath page
+	CString m_strPath;
+    unsigned __int64 m_n64SpaceRequired;
+	CString m_strSpaceRequired;
+
+	// Parameters page
+	CString m_strFullHostName;
+	CString m_strHostIPAddress;
+	CString m_strUsername;
+	CString m_strPassword;
+	CString m_strAdministratorEmail;
+
+	// Ports page
+	int m_nZopeHTTPPort;
+	int m_nZopeFTPPort;
+	int m_nZopeWEBDAVPort;
+
+	// Run page
+	int m_nRunAsService;
+
+	// Other
+    unsigned __int64 m_n64TotalSize;
+    unsigned __int64 m_n64PieceSize;
+};
+
+#endif // !defined(AFX_WIZARDDATA_H__43ED010B_6D44_4951_86F6_0E3816AF10CD__INCLUDED_)
