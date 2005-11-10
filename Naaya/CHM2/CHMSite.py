6bce842d621538434ac43e6c528cf6b656744b2b
Dragos Chiriac <dragos.chiriac@eaudeweb.ro>
2005-11-10 13:34:48 +0000

    EC CHM site in the network portals list

--- Naaya/CHM2/CHMSite.py
+++ Naaya/CHM2/CHMSite.py
@@ -100,6 +100,8 @@ class CHMSite(NySite):
         self.keywords_glossary = ID_GLOSSARY_KEYWORDS
         self.coverage_glossary = ID_GLOSSARY_COVERAGE
         self._p_changed = 1
+        #add EC CHM to network portals list
+        self.admin_addnetworkportal('EC CHM', 'http://biodiversity-chm.eea.eu.int/')
 
     #objects getters
     def getLinkChecker(self): return self._getOb(ID_LINKCHECKER, None)
