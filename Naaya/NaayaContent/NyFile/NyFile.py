6dc56565180ae39e80060ad559b8266b65f6ec34
Alec Ghica <alec.ghica@eaudeweb.ro>
2006-02-06 09:13:15 +0000

    work in progress for forms validation (mandatory fields)

--- Naaya/NaayaContent/NyFile/NyFile.py
+++ Naaya/NaayaContent/NyFile/NyFile.py
@@ -49,7 +49,7 @@ DESCRIPTION_OBJECT = 'This is Naaya File type.'
 PREFIX_OBJECT = 'file'
 PROPERTIES_OBJECT = {
     'id':               (0, '', ''),
-    'title':            (1, MUST_BE_NONEMPTY, 'Title must have a value.'),
+    'title':            (0, '', ''),
     'description':      (0, '', ''),
     'coverage':         (0, '', ''),
     'keywords':         (0, '', ''),
