97f2e20769bcf463a6ec492b9c73864ca3cc281b
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2006-06-06 13:13:26 +0000

    xliff export KeyError bug fixed

--- eggs/Naaya/Naaya/NaayaCore/TranslationsTool/TranslationsTool.py
+++ eggs/Naaya/Naaya/NaayaCore/TranslationsTool/TranslationsTool.py
@@ -173,4 +173,96 @@ class TranslationsTool(MessageCatalog):
                 d.append(x)
         return d
 
+    security.declareProtected('Manage messages', 'xliff_export')
+    def xliff_export(self, x, export_all=1, REQUEST=None, RESPONSE=None):
+        """ Exports the content of the message catalog to an XLIFF file
+        """
+        orglang = self._default_language
+        from DateTime import DateTime
+        from Products.Localizer.MessageCatalog import md5text
+        from cgi import escape
+        r = []
+        # alias for append function. For optimization purposes
+        r_append = r.append
+        # Generate the XLIFF file header
+        RESPONSE.setHeader('Content-Type', 'text/xml; charset=UTF-8')
+        RESPONSE.setHeader('Content-Disposition',
+                           'attachment; filename="%s_%s_%s.xlf"' % (self.id,
+                                                                    orglang,
+                                                                    x))
+
+        r_append('<?xml version="1.0" encoding="UTF-8"?>')
+        # Version 1.1 of the DTD is not yet available - use version 1.0
+        r_append('<!DOCTYPE xliff SYSTEM "http://www.oasis-open.org/committees/xliff/documents/xliff.dtd">')
+        # Force a UTF-8 char in the start
+        r_append(u'<!-- XLIFF Format Copyright \xa9 OASIS Open 2001-2003 -->')
+        r_append('<xliff version="1.0">')
+        r_append('<file')
+        r_append('original="/%s"' % self.absolute_url(1))
+        r_append('product-name="Localizer"')
+        r_append('product-version="1.1.x"')
+        r_append('datatype="plaintext"')
+        r_append('source-language="%s"' % orglang)
+        r_append('target-language="%s"' % x)
+        r_append('date="%s"' % DateTime().HTML4())
+        r_append('>')
+        r_append('<header>')
+#       r_append('<phase-group>')
+#       r_append('<phase ')
+#       r_append('phase-name="%s"' % REQUEST.get('phase_name', ''))
+#       r_append('process-name="Export"')
+#       r_append('tool="Localizer"')
+#       r_append('date="%s"' % DateTime().HTML4())
+#       r_append('company-name="%s"' % REQUEST.get('company_name', ''))
+#       r_append('job-id="%s"' % REQUEST.get('job_id', ''))
+#       r_append('contact-name="%s"' % REQUEST.get('contact_name', ''))
+#       r_append('contact-email="%s"' % REQUEST.get('contact_email', ''))
+#       r_append('/>')
+#       r_append('</phase-group>')
+        r_append('</header>')
+        r_append('<body>')
+
+        # Get the messages, and perhaps its translations.
+        d = {}
+        for msgkey, transunit in self._messages.items():
+            # if export_all=1 export all messages otherwise export
+            # only untranslated messages
+            try:
+                tr_unit = transunit[x]
+            except KeyError:
+                tr_unit = ''
+
+            if int(export_all) == 1 or (int(export_all) == 0 and tr_unit == ''):
+                d[msgkey] = tr_unit
+            else:
+                d[msgkey] = ""
+
+            if d[msgkey] == "":
+                d[msgkey] = msgkey
+
+        # Generate sorted msgids to simplify diffs
+        dkeys = d.keys()
+        dkeys.sort()
+        for msgkey in dkeys:
+            transunit = self._messages[msgkey]
+            r_append('<trans-unit id="%s">' % md5text(msgkey))
+            r_append(' <source>%s</source>' % escape(msgkey))
+            r_append(' <target>%s</target>' % escape(d[msgkey]))
+            if transunit.has_key('note') and transunit['note']:
+                r_append(' <note>%s</note>' % escape(transunit['note']))
+            r_append('</trans-unit>')
+
+        r_append('</body>')
+        r_append('</file>')
+        r_append('</xliff>')
+
+        r2 = []
+        for x in r:
+            if isinstance(x, unicode):
+                r2.append(x.encode('utf-8'))
+            else:
+                r2.append(x)
+
+        return '\r\n'.join(r2)
+
 InitializeClass(TranslationsTool)
