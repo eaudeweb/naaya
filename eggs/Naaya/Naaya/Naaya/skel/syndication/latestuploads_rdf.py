a4cd8e83a19e5359034271759bcc9448819b6aa4
Dragos Chiriac <dragos.chiriac@eaudeweb.ro>
2006-06-14 12:41:04 +0000

    latest uploads script: do not show items that the current user doesn't have the right to access

--- eggs/Naaya/Naaya/Naaya/skel/syndication/latestuploads_rdf.py
+++ eggs/Naaya/Naaya/Naaya/skel/syndication/latestuploads_rdf.py
@@ -1,3 +1,12 @@
 howmany = -1
-if context.numberofitems != 0: howmany = context.numberofitems
-return context.getCatalogedObjects(meta_type=context.get_meta_types(), approved=1, howmany=howmany, path=['/'.join(x.getPhysicalPath()) for x in context.getMainTopics()])
+if context.numberofitems != 0: howmany = context.numberofitems + 10
+#dirty trick: we retrieve with 10 more results and we hope that eliminating
+#protected items will have at least 'numberofitems' number of items
+#otherwise will be fewer
+r = []
+for x in context.getCatalogedObjects(meta_type=context.get_meta_types(), approved=1, howmany=howmany, path=['/'.join(x.getPhysicalPath()) for x in context.getMainTopics()]):
+    if x.checkPermissionView():
+        r.append(x)
+        if howmany != -1 and len(r)>context.numberofitems:
+            break
+return r
