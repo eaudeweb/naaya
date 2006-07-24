147c02e9cce8b0e6014905e38a8b479cbd92aaf4
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2006-07-24 13:01:08 +0000

    convert dtml to zpt

--- Naaya/NaayaLinkChecker/LogEntry.py
+++ Naaya/NaayaLinkChecker/LogEntry.py
@@ -21,14 +21,19 @@
 # $Id: LogEntry.py 864 2003-12-08 17:14:01Z finrocvs $
 #
 
+#Zope imports
 from OFS.SimpleItem import SimpleItem
-from Globals import DTMLFile
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from Globals import InitializeClass
+from AccessControl.Permissions import view
+from AccessControl import ClassSecurityInfo
 
+#Product's related imports
 from Utils import UtilsManager
 
 def manage_addLogEntry(self, user, date_create, url_list):
     """Add a Language"""
-    id = 'log_' + str(self.umGenRandomKey(8))
+    id = 'log_%s' % self.umGenRandomKey(8)
     logentry = LogEntry(id, user, date_create, url_list)
     self._setObject(id, logentry)
 
@@ -41,15 +46,18 @@ class LogEntry(SimpleItem, UtilsManager):
     manage_options = (
         {'label': 'View', 'action': 'index_html',},)
 
+    security = ClassSecurityInfo()
+
     def __init__(self, id, user, date_create, url_list):
         """Constructor"""
         self.id = id
-        self.title = 'Log Entry at ' + self.umFormatDateTimeToString(date_create)
+        self.title = 'Log Entry at %s' % self.umFormatDateTimeToString(date_create)
         self.user = user
         self.date_create = date_create
         self.url_list = url_list
         UtilsManager.__dict__['__init__'](self)
 
+    security.declareProtected(view, 'index_html')
+    index_html = PageTemplateFile('zpt/LogEntry_index', globals())
 
-    ########################################
-    index_html = DTMLFile('dtml/LogEntry_index', globals())
+InitializeClass(LogEntry)
