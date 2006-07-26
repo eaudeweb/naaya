6b2e52a7b3167abbde38be81199f49f84713eb13
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2006-07-26 08:45:00 +0000

    license updated remove the unnecessary DTMLMethods

--- eggs/Naaya/Naaya/NaayaLinkChecker/MyURLopener.py
+++ eggs/Naaya/Naaya/NaayaLinkChecker/MyURLopener.py
@@ -1,22 +1,23 @@
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
+#The contents of this file are subject to the Mozilla Public
+#License Version 1.1 (the "License"); you may not use this file
+#except in compliance with the License. You may obtain a copy of
+#the License at http://www.mozilla.org/MPL/
 #
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
+#Software distributed under the License is distributed on an "AS
+#IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+#implied. See the License for the specific language governing
+#rights and limitations under the License.
 #
-# Portions created by EEA are
-# Copyright (C) European Environment Agency.  All
-# Rights Reserved.
+#The Original Code is "LinkChecker"
 #
-# Contributor:
-# Cornel Nitu, Finsiel Romania
+#The Initial Owner of the Original Code is European Environment
+#Agency (EEA).  Portions created by Finsiel Romania are
+#Copyright (C) 2006 by European Environment Agency.  All
+#Rights Reserved.
 #
-# $Id: MyURLopener.py 864 2003-12-08 17:14:01Z finrocvs $
-#
+#Contributor(s):
+#  Original Code: Cornel Nitu (Finsiel Romania)
+
 
 import urllib
 
