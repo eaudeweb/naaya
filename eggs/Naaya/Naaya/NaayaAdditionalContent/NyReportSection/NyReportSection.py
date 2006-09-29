eb499c27ae9ac0948007cd050103afc92023d744
Alec Ghica <alec.ghica@eaudeweb.ro>
2006-09-29 10:03:10 +0000

    - work in progress

--- eggs/Naaya/Naaya/NaayaAdditionalContent/NyReportSection/NyReportSection.py
+++ eggs/Naaya/Naaya/NaayaAdditionalContent/NyReportSection/NyReportSection.py
@@ -78,7 +78,7 @@ def addNyReportSection(self, id='', title='', description='', coverage='', keywo
     #check mandatory fiels
     l_referer = ''
     if REQUEST is not None: l_referer = REQUEST['HTTP_REFERER'].split('/')[-1]
-    if not(l_referer == 'reportsection_manage_add' or l_referer.find('reportsection_manage_add') != -1) and REQUEST:
+    if not(l_referer == 'manage_addNyReportSection_html' or l_referer.find('manage_addNyReportSection_html') != -1) and REQUEST:
         r = self.getSite().check_pluggable_item_properties(METATYPE_OBJECT, id=id, title=title, \
             description=description, coverage=coverage, keywords=keywords, sortorder=sortorder, \
             releasedate=releasedate, discussion=discussion)
@@ -108,7 +108,7 @@ def addNyReportSection(self, id='', title='', description='', coverage='', keywo
         self.notifyFolderMaintainer(self, ob)
         #redirect if case
         if REQUEST is not None:
-            if l_referer == 'reportsection_manage_add' or l_referer.find('reportsection_manage_add') != -1:
+            if l_referer == 'manage_addNyReportSection_html' or l_referer.find('manage_addNyReportSection_html') != -1:
                 return self.manage_main(self, REQUEST, update_menu=1)
             elif l_referer == 'reportsection_add_html':
                 self.setSession('referer', self.absolute_url())
