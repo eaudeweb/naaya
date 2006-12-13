c054634bd0acec46b949844c810559d769afea01
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2006-12-13 10:37:10 +0000

    notify multiple administrators - improvements

--- Naaya/NaayaCore/EmailTool/EmailTool.py
+++ Naaya/NaayaCore/EmailTool/EmailTool.py
@@ -117,8 +117,7 @@ class EmailTool(Folder):
     def sendEmail(self, p_content, p_to, p_from, p_subject):
         #sends a generic email
         if not isinstance(p_to, list):
-            p_to = p_to.split(', ')
-        print p_to
+            p_to = [e.strip() for e in p_to.split(',') if e.strip()!='']
         try:
             if self.mail_server_name and self.mail_server_port and p_to:
                 l_message = self.__create_email(p_content, self.__build_addresses(p_to), p_from, p_subject)
