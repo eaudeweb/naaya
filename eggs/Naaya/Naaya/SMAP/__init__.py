ddf48072e412508109a22e3fbc2f97b3a57447d8
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2007-02-26 18:29:20 +0000

    integration with GeoMapTool

--- eggs/Naaya/Naaya/SMAP/__init__.py
+++ eggs/Naaya/Naaya/SMAP/__init__.py
@@ -28,7 +28,7 @@ from tools.constants import *
 from Products.NaayaCore.constants import *
 import SMAPSite
 from tools.SyncerTool import SyncerTool
-
+from Products.NaayaCore.GeoMapTool import GeoMapTool
 def initialize(context):
     """ """
 
@@ -52,7 +52,21 @@ def initialize(context):
         icon = 'tools/SyncerTool/www/SyncerTool.gif'
         )
 
+    context.registerClass(
+        GeoMapTool.GeoMapTool,
+        permission = PERMISSION_ADD_NAAYACORE_TOOL,
+        constructors = (
+                GeoMapTool.manage_addGeoMapTool,
+                ),
+        icon = '../NaayaCore/GeoMapTool/www/GeoMapTool.gif'
+        )
+
+
 misc_ = {
+    'GeoMapTool.gif':ImageFile('../NaayaCore/GeoMapTool/www/GeoMapTool.gif', globals()),
+    'dnt_rgis.swf': ImageFile('../NaayaCore/GeoMapTool/www/dnt_rgis.swf', globals()),
+    'dntp_rgis.swf': ImageFile('../NaayaCore/GeoMapTool/www/dntp_rgis.swf', globals()),
+    'bounding.xml': ImageFile('../NaayaCore/GeoMapTool/www/bounding.xml', globals()),
     'SyncerTool.gif':ImageFile('tools/SyncerTool/www/SyncerTool.gif', globals()),
     'Site.gif':ImageFile('www/Site.gif', globals()),
     'print.gif':ImageFile('www/print.gif', globals()),
