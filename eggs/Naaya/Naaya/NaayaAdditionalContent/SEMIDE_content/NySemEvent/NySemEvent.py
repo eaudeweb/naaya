177d65745fe51f94a8c0002abbc9a40e64b674a5
Alec Ghica <alec.ghica@eaudeweb.ro>
2007-03-27 10:44:45 +0000

    - import updated for empty translations

--- eggs/Naaya/Naaya/NaayaAdditionalContent/SEMIDE_content/NySemEvent/NySemEvent.py
+++ eggs/Naaya/Naaya/NaayaAdditionalContent/SEMIDE_content/NySemEvent/NySemEvent.py
@@ -201,8 +201,7 @@ def importNySemEvent(self, param, id, attrs, content, properties, discussion, ob
                 discussion=abs(int(attrs['discussion'].encode('utf-8'))))
             ob = self._getOb(id)
             for property, langs in properties.items():
-                for lang in langs:
-                    ob._setLocalPropValue(property, lang, langs[lang])
+                [ ob._setLocalPropValue(property, lang, langs[lang]) for lang in langs if langs[lang]!='' ]
             ob.approveThis(approved=abs(int(attrs['approved'].encode('utf-8'))),
                 approved_by=self.utEmptyToNone(attrs['approved_by'].encode('utf-8')))
             if attrs['releasedate'].encode('utf-8') != '':
