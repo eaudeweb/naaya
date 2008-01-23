9007bfc18d2e30372abbc329d217892f0e139661
ciupicri <ciupicri@eaudeweb.ro>
2008-01-23 19:18:31 +0000

    Added render method to ChartTool.

--- eggs/Naaya/Naaya/NaayaChart/ChartTool.py
+++ eggs/Naaya/Naaya/NaayaChart/ChartTool.py
@@ -1,5 +1,6 @@
 # Python imports
 from os.path import dirname, join
+import urllib
 
 # Zope imports
 from OFS.Folder import Folder
@@ -7,6 +8,7 @@ from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 
 # Product imports
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Products.LocalFS.LocalFS import manage_addLocalFS
 
 
@@ -38,6 +40,8 @@ class ChartTool(Folder):
         {'label': 'View', 'action': 'index_html'},
     )
 
+    xmlswfchart = PageTemplateFile('zpt/xmlswfchart.zpt', globals())
+
     def __init__(self, id, **kw):
         Folder.__init__(self, id)
         self.manage_changeProperties(**kw)
@@ -46,5 +50,16 @@ class ChartTool(Folder):
     def loadXmlSwfChartsFiles(self):
         """Load the XML/SWF Charts files"""
         self.manage_addProduct['LocalFS'].manage_addLocalFS('XmlSwfCharts', '', join(dirname(__file__), 'XmlSwfCharts'))
+
+    def render(self, data_url):
+        """Render chart
+
+            @param data_url: URL of the XML data source
+        """
+        charts_url = '%s/charts.swf?%s' % \
+                        (self.XmlSwfCharts.absolute_url(),
+                         urllib.urlencode({'library_path': self.XmlSwfCharts.absolute_url() + '/' + 'charts_library',
+                                           'xml_source': data_url}))
+        return self.xmlswfchart(charts_url=charts_url)
 
 InitializeClass(ChartTool)
