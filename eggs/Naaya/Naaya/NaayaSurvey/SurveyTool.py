878630e77cecdac0b3bf3dfc6ddbb6b84fe9a0eb
ciupicri <ciupicri@eaudeweb.ro>
2008-03-07 12:21:45 +0000

    SurveyTool: made configuration procedures methods of the SurveyTool class to be able to run them anytime after the SurveyTool was added.

--- eggs/Naaya/Naaya/NaayaSurvey/SurveyTool.py
+++ eggs/Naaya/Naaya/NaayaSurvey/SurveyTool.py
@@ -45,70 +45,6 @@ from MegaSurvey import MegaSurvey
 from constants import *
 from statistics.constants import PERMISSION_ADD_STATISTIC
 
-def configure_catalog(catalog_tool):
-    """Configure  catalog tool:
-        - add a survey_template index for the getSurveyTemplateId method
-
-        @param catalog_tool: catalog tool
-    """
-
-    zLOG.LOG('NaayaSurvey', zLOG.INFO,
-             'Configuring catalog tool %s' % (catalog_tool.absolute_url(1)))
-    try:
-        if 'survey_template' not in catalog_tool.indexes():
-            catalog_tool.addIndex(name='survey_template', type='FieldIndex',
-                                  extra={'indexed_attrs': 'getSurveyTemplateId'})
-        if 'respondent' not in catalog_tool.indexes():
-            catalog_tool.addIndex('respondent', 'FieldIndex')
-    except CatalogError:
-        err = sys.exc_info()
-        zLOG.LOG('Naaya Survey Tool', zLOG.ERROR,
-                 'Could not add catalog index survey_template', error=err)
-
-def configure_email_notifications(site):
-    """Configure email notifications for surveys:
-        - configure email template
-
-        @param site: Naaya Site
-    """
-
-    zLOG.LOG('NaayaSurvey', zLOG.INFO,
-             'Configuring email notifications on site %s' % (site.absolute_url(1)))
-    email_tool = site.getEmailTool()
-    for template, title in [('email_survey_answer.txt', 'Survey answered')]:
-        id = template[:-4] # without .txt
-        f = open(os.path.join(os.path.dirname(__file__),
-                              'templates', template), 'r')
-        content = f.read()
-        f.close()
-        t_ob = email_tool._getOb(id, None)
-        if t_ob is None:
-            email_tool.manage_addEmailTemplate(id, title, content)
-        else:
-            t_ob.manageProperties(title=title, body=content)
-
-def configure_security(site):
-    """Configure security for surveys:
-        - Manager + Administrator: add/manage survey templates
-        - Contributor: add/manage questionnaires (surveys)
-        - Anonymous + Authenticated: add answer (take survey) & view reports
-
-        @param site: Naaya site
-    """
-
-    zLOG.LOG('NaayaSurvey', zLOG.INFO,
-             'Configuring security for surveys on site %s' % (site.absolute_url(1)))
-    site.manage_permission(PERMISSION_MANAGE_SURVEYTEMPLATE, ('Manager', 'Administrator'), acquire=0)
-    site.manage_permission(PERMISSION_ADD_SURVEYTEMPLATE, ('Manager', 'Administrator'), acquire=0)
-    site.manage_permission(PERMISSION_ADD_WIDGETS, ('Manager', 'Administrator'), acquire=0)
-    site.manage_permission(PERMISSION_ADD_STATISTIC, ('Manager', 'Administrator'), acquire=0)
-
-    site.manage_permission(PERMISSION_ADD_QUESTIONNAIRE, ('Manager', 'Administrator', 'Contributor', ), acquire=0)
-    site.manage_permission(PERMISSION_ADD_MEGASURVEY, ('Manager', 'Administrator', 'Contributor', ), acquire=0)
-
-    site.manage_permission(PERMISSION_ADD_ANSWER, ('Anonymous', 'Authenticated'), acquire=0)
-    site.manage_permission(PERMISSION_VIEW_REPORTS, ('Anonymous', 'Authenticated'), acquire=0)
-    site.manage_permission(PERMISSION_VIEW_ANSWERS, ('Manager', 'Administrator'), acquire=0)
 
 def manage_addSurveyTool(context, REQUEST=None):
     """ZMI method that creates an object of this type."""
@@ -116,13 +52,7 @@ def manage_addSurveyTool(context, REQUEST=None):
     ob = SurveyTool(portal_id, title="Portal Survey Manager")
     context._setObject(portal_id, ob)
     ob = context._getOb(portal_id)
-    ob.manage_updatePortlets()
-
-    configure_catalog(ob.getSite().getCatalogTool())
-    configure_email_notifications(ob.getSite())
-    ob.getSite().searchable_content.append(SurveyQuestionnaire.meta_type)
-    ob.getSite().searchable_content.append(MegaSurvey.meta_type)
-    configure_security(ob.getSite())
+    ob.manage_configureSite()
 
     if REQUEST:
         return context.manage_main(context, REQUEST, update_menu=1)
@@ -174,6 +104,110 @@ class SurveyTool(Folder):
         self.manage_changeProperties(**kwargs)
 
     #
+    # Methods for configuring the Naaya Site for the Naaya Survey Product
+    #
+    security.declareProtected(PERMISSION_ADMINISTRATE, 'manage_configureCatalog')
+    def manage_configureCatalog(self):
+        """Configure  catalog tool:
+            - add a survey_template index for the getSurveyTemplateId method
+        """
+        catalog_tool = self.getSite().getCatalogTool()
+        zLOG.LOG('NaayaSurvey', zLOG.INFO,
+                 'Configuring catalog tool %s' % (catalog_tool.absolute_url(1)))
+        try:
+            if 'survey_template' not in catalog_tool.indexes():
+                catalog_tool.addIndex(name='survey_template', type='FieldIndex',
+                                      extra={'indexed_attrs': 'getSurveyTemplateId'})
+            if 'respondent' not in catalog_tool.indexes():
+                catalog_tool.addIndex('respondent', 'FieldIndex')
+        except CatalogError:
+            err = sys.exc_info()
+            zLOG.LOG('Naaya Survey Tool', zLOG.ERROR,
+                     'Could not configure catalog index survey_template', error=err)
+
+    security.declareProtected(PERMISSION_ADMINISTRATE, 'manage_configureEmailNotifications')
+    def manage_configureEmailNotifications(self):
+        """Configure email notifications for surveys:
+            - configure email template
+        """
+        site = self.getSite()
+        zLOG.LOG('NaayaSurvey', zLOG.INFO,
+                 'Configuring email notifications on site %s' % (site.absolute_url(1)))
+        email_tool = site.getEmailTool()
+        for template, title in [('email_survey_answer.txt', 'Survey answered')]:
+            id = template[:-4] # without .txt
+            f = open(os.path.join(os.path.dirname(__file__),
+                                  'templates', template), 'r')
+            content = f.read()
+            f.close()
+            t_ob = email_tool._getOb(id, None)
+            if t_ob is None:
+                email_tool.manage_addEmailTemplate(id, title, content)
+            else:
+                t_ob.manageProperties(title=title, body=content)
+
+    security.declareProtected(PERMISSION_ADMINISTRATE, 'manage_configureSecurity')
+    def manage_configureSecurity(self):
+        """Configure security for surveys:
+            - Manager + Administrator: add/manage survey templates
+            - Contributor: add/manage questionnaires (surveys)
+            - Anonymous + Authenticated: add answer (take survey) & view reports
+        """
+        site = self.getSite()
+        zLOG.LOG('NaayaSurvey', zLOG.INFO,
+                 'Configuring security for surveys on site %s' % (site.absolute_url(1)))
+        site.manage_permission(PERMISSION_MANAGE_SURVEYTEMPLATE, ('Manager', 'Administrator'), acquire=0)
+        site.manage_permission(PERMISSION_ADD_SURVEYTEMPLATE, ('Manager', 'Administrator'), acquire=0)
+        site.manage_permission(PERMISSION_ADD_WIDGETS, ('Manager', 'Administrator'), acquire=0)
+        site.manage_permission(PERMISSION_ADD_STATISTIC, ('Manager', 'Administrator'), acquire=0)
+
+        site.manage_permission(PERMISSION_ADD_QUESTIONNAIRE, ('Manager', 'Administrator', 'Contributor', ), acquire=0)
+        site.manage_permission(PERMISSION_ADD_MEGASURVEY, ('Manager', 'Administrator', 'Contributor', ), acquire=0)
+
+        site.manage_permission(PERMISSION_ADD_ANSWER, ('Anonymous', 'Authenticated'), acquire=0)
+        site.manage_permission(PERMISSION_VIEW_REPORTS, ('Anonymous', 'Authenticated'), acquire=0)
+        site.manage_permission(PERMISSION_VIEW_ANSWERS, ('Manager', 'Administrator'), acquire=0)
+
+    # Add tool specific portlets
+    security.declareProtected(PERMISSION_ADMINISTRATE, 'manage_updatePortlets')
+    def manage_updatePortlets(self, REQUEST=None, **kwargs):
+        """Add admininstration portlet"""
+        ptool = getattr(self, 'portal_portlets', None)
+        if not ptool:
+            zLOG.LOG('NaayaSurvey', zLOG.ERROR,
+                     'Could not add administration portlet for portal_survey')
+            return
+        zLOG.LOG('NaayaSurvey', zLOG.INFO,
+                 'Adding administration portlet to portal_portlets %s' % (ptool.absolute_url(1)))
+        #
+        # Admin portlet
+        #
+        portlet_id = 'portlet_admin_survey'
+        portlet_title = 'Portal Survey Manager'
+        if portlet_id not in ptool.objectIds():
+            ptool.addPortlet(portlet_id, portlet_title, portlettype=100)
+        portlet_ob = ptool._getOb(portlet_id)
+        # Set portlet content
+        text = PageTemplateFile('portlets/%s' % portlet_id, globals())
+        text = text.read()
+        portlet_ob.pt_edit(text=text, content_type='text/html')
+        # Add special property in order to be found by generic template
+        if not portlet_ob.hasProperty('show_in_form'):
+            portlet_ob.manage_addProperty('show_in_form',
+                                          'admin_centre_html','string')
+        portlet_ob.manage_changeProperties(show_in_form='admin_centre_html')
+
+    security.declareProtected(PERMISSION_ADMINISTRATE, 'manage_configureSite')
+    def manage_configureSite(self, REQUEST=None):
+        """Configures the Naaya Site for the Naaya Survey Product"""
+        self.manage_configureCatalog()
+        self.manage_configureEmailNotifications()
+        self.manage_configureSecurity()
+        self.manage_updatePortlets()
+        self.getSite().searchable_content.append(SurveyQuestionnaire.meta_type)
+        self.getSite().searchable_content.append(MegaSurvey.meta_type)
+
+    #
     # global methods used in naaya site context
     #
 
@@ -195,32 +229,6 @@ class SurveyTool(Folder):
             if getattr(doc, '_object_del_language', None):
                 doc._object_del_language(language)
 
-    # Add tool specific portlets
-    security.declareProtected(PERMISSION_ADMINISTRATE, 'manage_updatePortlets')
-    def manage_updatePortlets(self, REQUEST=None, **kwargs):
-        # Add admininstration portlet
-        ptool = getattr(self, 'portal_portlets', None)
-        if not ptool:
-            zLOG.LOG('NaayaSurvey', zLOG.ERROR,
-                     'Could not add administration portlet for portal_survey')
-            return
-        #
-        # Admin portlet
-        #
-        portlet_id = 'portlet_admin_survey'
-        portlet_title = 'Portal Survey Manager'
-        if portlet_id not in ptool.objectIds():
-            ptool.addPortlet(portlet_id, portlet_title, portlettype=100)
-        portlet_ob = ptool._getOb(portlet_id)
-        # Set portlet content
-        text = PageTemplateFile('portlets/%s' % portlet_id, globals())
-        text = text.read()
-        portlet_ob.pt_edit(text=text, content_type='text/html')
-        # Add special property in order to be found by generic template
-        if not portlet_ob.hasProperty('show_in_form'):
-            portlet_ob.manage_addProperty('show_in_form',
-                                          'admin_centre_html','string')
-        portlet_ob.manage_changeProperties(show_in_form='admin_centre_html')
 
     #
     # Edit methods
