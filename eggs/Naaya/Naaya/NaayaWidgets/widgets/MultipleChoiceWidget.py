bea73366eca74ed330a0fe9a0cdc835f809a7029
ciupicri <ciupicri@eaudeweb.ro>
2008-03-25 11:45:14 +0000

    Factored out common part from multiple choice widget manage ZPTs.

--- eggs/Naaya/Naaya/NaayaWidgets/widgets/MultipleChoiceWidget.py
+++ eggs/Naaya/Naaya/NaayaWidgets/widgets/MultipleChoiceWidget.py
@@ -1,3 +1,6 @@
+# Zope imports
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+
 # Products imports
 from Products.Localizer.LocalPropertyManager import LocalProperty
 from Products.NaayaWidgets.Widget import Widget
@@ -8,6 +11,8 @@ class MultipleChoiceWidget(Widget):
 
     # Local properties
     choices = LocalProperty('choices')
+
+    multiplechoice_render_meth = PageTemplateFile('zpt/widget_multiplechoice', globals())
 
     def __init__(self, id, lang=None, **kwargs):
         self.set_localproperty('choices', 'lines', lang)
