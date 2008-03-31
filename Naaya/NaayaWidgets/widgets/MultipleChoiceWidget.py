cf5ca916766ac7816633f1315e51d1a535829c76
ciupicri <ciupicri@eaudeweb.ro>
2008-03-31 10:47:29 +0000

    partial BUG FIX #1401 - initial implementation for radio question extra choice

--- Naaya/NaayaWidgets/widgets/MultipleChoiceWidget.py
+++ Naaya/NaayaWidgets/widgets/MultipleChoiceWidget.py
@@ -2,11 +2,16 @@
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
 # Products imports
+from AccessControl import ClassSecurityInfo
+from AccessControl.Permissions import view
+from Globals import InitializeClass
 from Products.Localizer.LocalPropertyManager import LocalProperty
 from Products.NaayaWidgets.Widget import Widget
 
 class MultipleChoiceWidget(Widget):
     """Abstract class"""
+
+    security = ClassSecurityInfo()
 
     # Local properties
     choices = LocalProperty('choices')
@@ -17,3 +22,10 @@ class MultipleChoiceWidget(Widget):
     def __init__(self, id, lang=None, **kwargs):
         self.set_localproperty('choices', 'lines', lang)
         Widget.__init__(self, id, lang, **kwargs)
+
+    security.declareProtected(view, 'getChoices')
+    def getChoices(self):
+        """ """
+        return self.choices
+
+InitializeClass(MultipleChoiceWidget)
