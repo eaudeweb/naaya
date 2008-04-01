e81e9f99b0129309279371cb4ee1abd6abeecec7
ciupicri <ciupicri@eaudeweb.ro>
2008-04-01 15:51:32 +0000

    Improved "testMegaSurvey.py" (added more tests).

--- Naaya/NaayaSurvey/tests/testMegaSurvey.py
+++ Naaya/NaayaSurvey/tests/testMegaSurvey.py
@@ -3,12 +3,14 @@ from unittest import TestSuite, makeSuite
 
 # Zope imports
 from Testing import ZopeTestCase
+from DateTime import DateTime
 
 # Naaya imports
 from Products.Naaya.tests.NaayaTestCase import NaayaTestCase
 
 # Survey imports
 from Products.NaayaSurvey.MegaSurvey import manage_addMegaSurvey
+from Products.NaayaSurvey.SurveyQuestionnaire import SurveyQuestionnaireException
 from Products.NaayaSurvey.SurveyReport import SurveyReport
 from Products.NaayaSurvey.SurveyTool import SurveyTool, manage_addSurveyTool
 from Products.NaayaWidgets.widgets import AVAILABLE_WIDGETS
@@ -25,20 +27,49 @@ class MegaSurveyTestCase(NaayaTestCase):
         id = manage_addMegaSurvey(self.getPortal(), title='Testing survey')
         self.survey = self.getPortal()._getOb(id)
 
+    def beforeTearDown(self):
+        self.logout()
+
     def testAddQuestions(self):
         """Add questions"""
         for widget_class in AVAILABLE_WIDGETS:
-            id = self.survey.addWidget(title='A question', meta_type=widget_class.meta_type)
+            title = 'A question'
+            id = self.survey.addWidget(title=title, meta_type=widget_class.meta_type)
             w = self.survey._getOb(id, None)
             self.assertNotEqual(w, None)
             self.assert_(isinstance(w, widget_class))
+            self.assertEqual(w.getLocalProperty('title', self.getPortal().gl_get_selected_language()), title)
 
     def testAddReport(self):
         """Add report"""
-        id = self.survey.addReport(title='A report')
+        title = 'A report'
+        id = self.survey.addReport(title=title)
         report = self.survey._getOb(id, None)
         self.assertNotEqual(report, None)
         self.assert_(isinstance(report, SurveyReport))
+        self.assertEqual(report.getLocalProperty('title', self.getPortal().gl_get_selected_language()), title)
+
+    def testGenerateFullReport(self):
+        """Generate full report"""
+        title = 'Full report'
+        id = self.survey.generateFullReport(title=title)
+        report = self.survey._getOb(id, None)
+        self.assertNotEqual(report, None)
+        self.assert_(isinstance(report, SurveyReport))
+        self.assertEqual(report.getLocalProperty('title', self.getPortal().gl_get_selected_language()), title)
+        self.assertEqual(len(report.getStatistics()), 0) # 0 questions -> 0 statistics
+
+    def testTakingSurvey(self):
+        """Test taking a survey"""
+        answer = self.survey.getMyAnswer()
+        self.assertEqual(answer, None)
+
+        self.survey.expirationdate = DateTime() + 1
+        self.survey.addSurveyAnswer(notify_respondent=False)
+
+        self.survey.expirationdate = DateTime() - 1
+        self.assertRaises(SurveyQuestionnaireException, self.survey.addSurveyAnswer, notify_respondent=False)
+
 
 def test_suite():
     suite = TestSuite()
