a4fda404a8a124fa3f04560ff576f9ebc40c3956
Miruna Badescu <miruna.badescu@eaudeweb.ro>
2008-04-24 17:58:59 +0000

    SMAP nyexp export

--- eggs/Naaya/Naaya/NaayaBase/NyImportExport.py
+++ eggs/Naaya/Naaya/NaayaBase/NyImportExport.py
@@ -105,21 +105,24 @@ class NyImportExport:
         ra('<?xml version="1.0" encoding="utf-8"?>')
         ra('<export xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="%s">' % self.nyexp_schema)
         for x in self.objectValues('Naaya Folder'):
-            ra(x.export_this_tag())
-            ra(x.export_this_body())
-            if x.publicinterface:
-                l_index = x._getOb('index', None)
-                if l_index is not None:
-                    ra('<![CDATA[%s]]>' % l_index.document_src())
-            for y in x.objectValues('Naaya Folder'):
+            if x.id == 'about':
+                ra(x.export_this(folderish=1, all_levels=0))
+            elif x.id not in ['ptk', 'ptk-cd']:
                 ra(x.export_this_tag())
                 ra(x.export_this_body())
                 if x.publicinterface:
                     l_index = x._getOb('index', None)
                     if l_index is not None:
                         ra('<![CDATA[%s]]>' % l_index.document_src())
+                for y in x.objectValues('Naaya Folder'):
+                    ra(x.export_this_tag())
+                    ra(x.export_this_body())
+                    if x.publicinterface:
+                        l_index = x._getOb('index', None)
+                        if l_index is not None:
+                            ra('<![CDATA[%s]]>' % l_index.document_src())
+                    ra('</ob>')
                 ra('</ob>')
-            ra('</ob>')
         ra('</export>')
         self.REQUEST.RESPONSE.setHeader('Content-Type', 'text/xml')
         self.REQUEST.RESPONSE.setHeader('Content-Disposition', 'attachment;filename=%s.nyexp' % self.id)
