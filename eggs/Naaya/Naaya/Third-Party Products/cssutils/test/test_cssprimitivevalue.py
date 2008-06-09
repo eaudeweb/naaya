cff7c3e099d57527b353141a6b782a55c05649e8
David Batranu <david.batranu@eaudeweb.ro>
2008-06-09 12:52:26 +0000

    import of the cssutils 0.51 package for use with naayaUpdater

---
+++ eggs/Naaya/Naaya/Third-Party Products/cssutils/test/test_cssprimitivevalue.py
@@ -0,0 +1,17 @@
+__version__ = '0.4'
+
+import unittest
+import xml.dom
+
+from cssutils.cssprimitivevalue import *
+
+
+class CSSPrimitiveValueTestCase(unittest.TestCase):
+
+    def test_(self):
+        p = PrimitiveValue()
+
+    
+        
+if __name__ == '__main__':
+    unittest.main() 
