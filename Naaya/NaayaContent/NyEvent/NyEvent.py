c720db9de78d530935c30fb5ec784c6160a98ae2
David Batranu <david.batranu@eaudeweb.ro>
2008-06-25 16:44:10 +0000

    - implemented map portal control

--- Naaya/NaayaContent/NyEvent/NyEvent.py
+++ Naaya/NaayaContent/NyEvent/NyEvent.py
@@ -424,12 +424,14 @@ class NyEvent(NyAttributes, event_item, NyItem, NyCheckControl):
                 self.version.updateDynamicProperties(self.processDynamicProperties(METATYPE_OBJECT, REQUEST, kwargs), lang)
             if discussion: self.open_for_comments()
             else: self.close_for_comments()
+
             self._p_changed = 1
             self.recatalogNyObject(self)
             #log date
             contributor = self.REQUEST.AUTHENTICATED_USER.getUserName()
             auth_tool = self.getAuthenticationTool()
             auth_tool.changeLastPost(contributor)
+
             if REQUEST:
                 self.setSessionInfo([MESSAGE_SAVEDCHANGES % self.utGetTodayDate()])
                 REQUEST.RESPONSE.redirect('%s/edit_html?lang=%s' % (self.absolute_url(), lang))
