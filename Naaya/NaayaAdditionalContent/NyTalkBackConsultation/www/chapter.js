b49362ee6aab7bc64e68455f283ac34b07fbe372
Alex Morega <alex@grep.ro>
2008-09-26 11:54:56 +0000

    javascript add comment

--- Naaya/NaayaAdditionalContent/NyTalkBackConsultation/www/chapter.js
+++ Naaya/NaayaAdditionalContent/NyTalkBackConsultation/www/chapter.js
@@ -1,3 +1,28 @@
+function comment_add(comment_link) {
+    $.get(comment_link.href, function(data){
+        $('div.talkback-comment_floating_box').remove();
+
+        var top = $(comment_link).attr('offsetTop') - 50;
+        var left = $(comment_link).attr('offsetLeft') + 200;
+
+        var comment_box = $('<div class="talkback-comment_floating_box"></div>').css({
+            position: 'absolute',
+            top: top,
+            left: left,
+            'background-color': 'white',
+            border: '2px solid green',
+            padding: '10px'
+        }).append($('div.talkback-add_comment_form', data));
+        $('form', comment_box).append(
+            $('<input type="submit" value="cancel" />').click(function() {
+                try { comment_box.remove(); }
+                catch(e) {} return false;
+            })
+        ).attr('action', $(comment_link).attr('href') + '/' + $('form', comment_box).attr('action'));
+        comment_box.insertAfter(comment_link);
+    });
+}
+
 function comment_buttons_display(comments_visible) {
     $('a.talkback-show_comments').css({display: (comments_visible ? 'none' : 'block')});
     $('a.talkback-hide_comments').css({display: (comments_visible ? 'block' : 'none')});
@@ -20,5 +45,14 @@ $(document).ready(function() {setTimeout(function(){
     $('a.talkback-hide_comments').click(hide_comments);
     
     $('div.talkback-show_hide_comments').css({display: 'block'});
+
+    $('a.talkback-add_comment').each(function() {
+        var n_comments = String($('fieldset', $(this).parent()).length);
+        var text = (n_comments == 1 ? 'comment' : 'comments');
+        $(this).text(n_comments + ' ' + text);
+    }).click(function() {
+        try { comment_add(this); }
+        catch(e) {} return false;
+    });
 }, 0); });
 
