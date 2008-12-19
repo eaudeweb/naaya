2825afde05b88256a066935d39410c753c988877
David Batranu <david.batranu@eaudeweb.ro>
2008-12-19 15:45:36 +0000

    - Added a new permission for the Consultation (Naaya - View Consultation answers) - Changed explanatory text when adding a review and no questions are defined - Hidden the 'View answers' button if the user doesn't have the view answers permission

--- Naaya/NaayaAdditionalContent/NyConsultation/NyConsultation.py
+++ Naaya/NaayaAdditionalContent/NyConsultation/NyConsultation.py
@@ -74,7 +74,7 @@ manage_addNyConsultation_html.action = 'addNyConsultation'
 
 consultation_add_html = PageTemplateFile('zpt/consultation_add', globals())
 
-def addNyConsultation(self, id='', title='', description='', sortorder='', start_date='', end_date='', public_registration='', 
+def addNyConsultation(self, id='', title='', description='', sortorder='', start_date='', end_date='', public_registration='',
                             allow_file='',line_comments='', contributor=None, releasedate='', lang=None, REQUEST=None, **kwargs):
     """
     Create a Naaya Consultation type of object.
@@ -135,7 +135,7 @@ def addNyConsultation(self, id='', title='', description='', sortorder='', start
         if REQUEST is not None:
             self.setSessionErrors(r)
             self.set_pluggable_item_session(METATYPE_OBJECT, id=id, title=title, description=description, \
-                sortorder=sortorder, releasedate=releasedate, start_date=start_date, end_date=end_date, 
+                sortorder=sortorder, releasedate=releasedate, start_date=start_date, end_date=end_date,
                 allow_file=allow_file, line_comments=line_comments, public_registration=public_registration, lang=lang)
             REQUEST.RESPONSE.redirect('%s/consultation_add_html' % self.absolute_url())
         else:
@@ -174,23 +174,23 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
 
     security.declarePrivate('save_properties')
     def save_properties(self, title, description, sortorder, start_date, end_date, public_registration, allow_file, line_comments, releasedate, lang):
-        
+
         self._setLocalPropValue('title', lang, title)
         self._setLocalPropValue('description', lang, description)
-        
+
         if not hasattr(self, 'imageContainer'):
             self.imageContainer = NyImageContainer(self, True)
-        
+
         if start_date:
             self.start_date = self.utConvertStringToDateTimeObj(start_date)
         else:
             self.start_date = self.utGetTodayDate()
-            
+
         if end_date:
             self.end_date = self.utConvertStringToDateTimeObj(end_date)
         else:
             self.end_date = self.utGetTodayDate()
-            
+
         self.sortorder = sortorder
         self.releasedate = releasedate
         self.public_registration = public_registration
@@ -201,32 +201,32 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     def saveProperties(self, title='', description='', sortorder='', start_date='', end_date='',
                        public_registration='', allow_file='', line_comments='', file='', lang='', REQUEST=None):
         """ """
-        
+
         if not title:
             self.setSession('title', title)
             self.setSession('description', description)
             self.setSessionErrors(['The Title field must have a value.'])
             return REQUEST.RESPONSE.redirect('%s/edit_html?lang=%s' % (self.absolute_url(), lang))
-        
+
         if file and not file.read():
             self.setSession('title', title)
             self.setSession('description', description)
             self.setSessionErrors(['File must not be empty'])
             return REQUEST.RESPONSE.redirect('%s/edit_html?lang=%s' % (self.absolute_url(), lang))
-        
+
         exfile = self.get_exfile()
-        
+
         if file and exfile:
             exfile.saveUpload(file=file, lang=lang)
             downloadfilename=file.filename
             exfile._setLocalPropValue('downloadfilename', lang, downloadfilename)
         elif file and not exfile:
             addNyExFile(self, title=title, file=file, lang=lang, source='file')
-            
+
         releasedate = self.releasedate
         self.updateRequestRoleStatus(public_registration, lang)
         self.save_properties(title, description, sortorder, start_date, end_date, public_registration, allow_file, line_comments, releasedate, lang)
-            
+
         if REQUEST:
             self.setSessionInfo([MESSAGE_SAVEDCHANGES % self.utGetTodayDate()])
             REQUEST.RESPONSE.redirect('%s/edit_html?lang=%s' % (self.absolute_url(), lang))
@@ -239,15 +239,15 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     security.declareProtected(PERMISSION_MANAGE_CONSULTATION, 'checkReviewerRole')
     def checkReviewerRole(self):
         """
-        Checks if the 'Reviewer' role exists, 
+        Checks if the 'Reviewer' role exists,
         creates and adds review permissions if it doesn't exist
         """
-        
-        
+
+
         auth_tool = self.getAuthenticationTool()
         roles = auth_tool.list_all_roles()
         PERMISSION_GROUP = 'Review content'
-        
+
         if PERMISSION_GROUP not in auth_tool.listPermissions().keys():
             auth_tool.addPermission(PERMISSION_GROUP, 'Allow posting reviews/comments to consultation objects.', [PERMISSION_REVIEW_CONSULTATION])
         else:
@@ -255,7 +255,7 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
             if PERMISSION_REVIEW_CONSULTATION not in permissions:
                 permissions.append(PERMISSION_REVIEW_CONSULTATION)
                 auth_tool.editPermission(PERMISSION_GROUP, 'Allow posting reviews/comments to consultation objects.', permissions)
-        
+
         if 'Reviewer' not in roles:
             auth_tool.addRole('Reviewer', [PERMISSION_GROUP])
         else:
@@ -263,12 +263,13 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
             if PERMISSION_GROUP not in role_permissions:
                 role_permissions.append(PERMISSION_GROUP)
                 auth_tool.editRole('Reviewer', role_permissions)
-        
+
         #give permissions to administrators
         admin_permissions = self.permissionsOfRole('Administrator')
         site = self.getSite()
         if PERMISSION_MANAGE_CONSULTATION not in admin_permissions:
             site.manage_permission(PERMISSION_MANAGE_CONSULTATION, ('Administrator', ), acquire=1)
+            site.manage_permission(PERMISSION_VIEW_CONSULTATION, ('Administrator', ), acquire=1)
             site.manage_permission(PERMISSION_REVIEW_CONSULTATION, ('Administrator', ), acquire=1)
 
 
@@ -364,12 +365,12 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     security.declareProtected(view, 'get_exfile')
     def get_exfile(self):
         """ Returns the first ExFile in the Consultation, there should be only one. """
-        
+
         try:
             exfile = self.objectValues(['Naaya Extended File'])[0]
         except IndexError:
             exfile = None
-            
+
         return exfile
 
     security.declareProtected(view, 'get_exfile_url')
@@ -381,33 +382,33 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     security.declareProtected(view, 'check_exfile_for_lang')
     def check_exfile_for_lang(self, lang):
         """ Checks if there is a file uploaded for the given language. """
-       
+
         return self.get_exfile().getFileItem(lang).size > 0
 
     security.declareProtected(view, 'get_exfile_langs')
     def get_exfile_langs(self):
         """ Returns the languages for witch NyExFile contains files. """
-        
+
         return [language for language in self.getSite().gl_get_languages_map() if self.check_exfile_for_lang(language['id'])]
 
     security.declareProtected(view, 'get_start_date')
     def get_start_date(self):
         """ Returns the start date in dd/mm/yyyy string format. """
-        
+
         return self.utConvertDateTimeObjToString(self.start_date)
 
     security.declareProtected(view, 'get_end_date')
     def get_end_date(self):
         """ Returns the end date in dd/mm/yyyy string format. """
-        
+
         return self.utConvertDateTimeObjToString(self.end_date)
-    
+
     security.declareProtected(view, 'get_days_left')
     def get_days_left(self):
         """ Returns the remaining days for the consultation or the number of days before it starts """
-        
+
         today = self.utGetTodayDate().earliestTime()
-        
+
         if self.start_date.lessThanEqualTo(today):
             return (1, int(str(self.end_date - today).split('.')[0]))
         else:
@@ -416,7 +417,7 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     security.declareProtected(view_management_screens, 'manage_options')
     def manage_options(self):
         """ """
-        
+
         l_options = (NyContainer.manage_options[0],)
         l_options += ({'label': 'View', 'action': 'index_html'},) + NyContainer.manage_options[3:8]
         return l_options
@@ -424,32 +425,32 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     security.declareProtected(view, 'check_contributor_review')
     def check_contributor_review(self, contributor='', REQUEST=None):
         """ Returns True if user already posted a comment """
-        
+
         if not contributor and REQUEST:
             contributor = REQUEST.AUTHENTICATED_USER.getUserName()
-            
+
         return contributor in [review.contributor for review in self.objectValues(['Consultation Review'])]
 
     security.declareProtected(PERMISSION_REVIEW_CONSULTATION, 'addConsultationReview')
     def addConsultationReview(self, contributor_name='', file='', REQUEST=None, **kwargs):
         """ """
-        
+
         contributor = REQUEST.AUTHENTICATED_USER.getUserName()
-        
+
         if not contributor_name:
             self.setSession('contributor_name', contributor_name)
             self.setSessionErrors(['Fill in the name field.'])
             return REQUEST.RESPONSE.redirect(self.absolute_url() + '/review_add_html')
-        
+
         if REQUEST:
             kwargs.update(REQUEST.form)
-            
+
         if self.allow_file != '1':
             file = ''
-        
+
         if self.line_comments != '1':
             kwargs['adt_comment'] = {}
-        
+
         days = self.get_days_left()
         if days[0] == 1 and days[1] > 0 and self.get_exfile():
             if not self.check_contributor_review(contributor):
@@ -466,12 +467,12 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     def checkConsultationUser(self):
         """
         Checks if the user is logged in and has reviewer rights:
-        0 if user is anonymous, 
+        0 if user is anonymous,
         1 if user has reviewer role
         2 if user doesn't have reviewer role
         """
         review_check = self.checkPermissionReviewConsultation()
-        
+
         if self.isAnonymousUser(): return 0
         elif review_check: return 1
         elif not review_check: return 2
@@ -488,6 +489,12 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
         """
         return self.checkPermission(PERMISSION_MANAGE_CONSULTATION)
 
+    def checkPermissionViewConsultation(self):
+        """
+        Check for managing the Consultation.
+        """
+        return self.checkPermission(PERMISSION_VIEW_CONSULTATION)
+
     security.declareProtected(view, 'get_reviews')
     def get_reviews(self):
         """ Returns a list with all the Consultation Review objects """
@@ -495,7 +502,7 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
 
     def count_line_comments(self):
         """ Returns the total count of line comments in all reviews """
-        
+
         reviews = self.get_reviews()
         count = 0
         if reviews:
@@ -506,7 +513,7 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
 
     def count_additional_files(self):
         """ Returns the total count of additional files in all reviews """
-        
+
         reviews = self.get_reviews()
         count = 0
         if reviews:
@@ -517,14 +524,14 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
 
     def count_question_answers(self):
         """ Returns (question_id, answer_count) """
-        
+
         reviews = self.get_reviews()
         questions = self.get_questions()
         count = {}
-        
+
         for qid, q in questions:
             count[qid] = 0
-            
+
         for review in reviews:
             for qid, answer in review.answers:
                 if answer:
@@ -535,13 +542,13 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     def addQuestion(self, question_body, lang, sortorder, REQUEST=None):
         """ """
         id = 'q%s' % self.utGenRandomId(4)
-        
+
         try: sortorder = int(sortorder)
         except: sortorder = 100
-        
+
         if not self.questions.has_key(id): id = id
         else: id = id + self.utGenRandomId()
-        
+
         self.questions[id] = question_item(question_body, lang, sortorder)
         self._p_changed = 1
         return REQUEST.RESPONSE.redirect('%s/manage_questions_html' % (self.absolute_url(), ))
@@ -604,30 +611,30 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
         """ """
         cns_ratings = [x.title for x in self.getRateLists()]
         return cns_ratings
-    
+
     security.declareProtected(view, 'getQuestionsReviews')
     def getQuestionsReviews(self):
         """
-        Returns a dictionary with question ids as keys and 
+        Returns a dictionary with question ids as keys and
         a list of reviews that answered to that question as values.
         {qid: [rating1, rating2, ]}
         """
-        
-        
+
+
         reviews = self.get_reviews()
         questions = self.get_questions()
         qs_rev = {}
-        
+
         for qid, q in questions:
             qs_rev[qid] = []
-            
+
         for review in reviews:
             for qid, answer in review.answers:
                 if answer:
                     qs_rev[qid].append(review.getId())
         return qs_rev
 
-    security.declareProtected(view, 'getReviewById')
+    security.declareProtected(PERMISSION_VIEW_CONSULTATION, 'getReviewById')
     def getReviewById(self, rid):
         """ """
         reviews = self.get_reviews()
@@ -636,25 +643,25 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
                 return review
 
     #reviews sorting
-    security.declareProtected(view, 'sort_cns_reviews')
+    security.declareProtected(PERMISSION_VIEW_CONSULTATION, 'sort_cns_reviews')
     def sort_cns_reviews(self, by='Date'):
         """ Selects whitch method to use for sorting """
-        
+
         if by == 'Date':
             return self.utSortObjsListByMethod(self.get_reviews(), 'get_review_date', 1)
         else:
             return self.get_reviews_sorted(by)
 
-    security.declareProtected(view, 'get_reviews_sorted')
+    security.declareProtected(PERMISSION_VIEW_CONSULTATION, 'get_reviews_sorted')
     def get_reviews_sorted(self, rating=''):
         """ Returns the list of reviews sorted by rating value for the given rating """
-        
+
         if rating not in self.getRatingTitles(): return self.sort_reviews_page()
         reviews = [x for x in self.get_reviews()]
         reviews.sort(lambda x, y: cmp(int(y.getRatings()[rating]), int(x.getRatings()[rating])))
         return reviews
 
-    security.declareProtected(view, 'sort_reviews_page')
+    security.declareProtected(PERMISSION_VIEW_CONSULTATION, 'sort_reviews_page')
     def sort_reviews_page(self, by='Date', REQUEST=None):
         """ Open the reviews page sorted acording to 'by' """
         if REQUEST is not None:
@@ -687,7 +694,7 @@ class NyConsultation(NyAttributes, Implicit, NyProperties, BTreeFolder2, NyConta
     security.declareProtected(PERMISSION_MANAGE_CONSULTATION, 'manage_questions_html')
     manage_questions_html = PageTemplateFile('zpt/manage_questions', globals())
 
-    security.declareProtected(view, 'reviews_index_html')
+    security.declareProtected(PERMISSION_VIEW_CONSULTATION, 'reviews_index_html')
     reviews_index_html = PageTemplateFile('zpt/reviews_index', globals())
 
     security.declareProtected(view, 'index_html')
