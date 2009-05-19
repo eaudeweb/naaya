d16f7e1138c9a7be01c4b75727312e6ce0ddc7d5
David Batranu <david.batranu@eaudeweb.ro>
2009-05-19 16:20:11 +0000

    Make logo change when the language changes #19

--- eggs/Naaya/Naaya/Naaya/NySite.py
+++ eggs/Naaya/Naaya/Naaya/NySite.py
@@ -121,6 +121,7 @@ class NySite(CookieCrumbler, LocalPropertyManager, Folder,
     networkportals_manager):
     """ """
     display_contributor = "on"
+    default_logo = ''
 
     meta_type = METATYPE_NYSITE
     icon = 'misc_/Naaya/Site.gif'
@@ -171,6 +172,7 @@ class NySite(CookieCrumbler, LocalPropertyManager, Folder,
         self.recaptcha_public_key = ''
         self.recaptcha_private_key = ''
         self.display_contributor = "on"
+        self.default_logo = ''
         # The email address (must exist) from which the email tool sends mails
         self.mail_address_from = ''
         self.administrator_email = ''
@@ -1923,67 +1925,135 @@ class NySite(CookieCrumbler, LocalPropertyManager, Folder,
             REQUEST.RESPONSE.redirect('%s/admin_metadata_html?lang=%s' % (self.absolute_url(), lang))
 
     security.declarePublic('hasLeftLogo')
-    def hasLeftLogo(self):
+    def hasLeftLogo(self, lang=''):
         """ Returns true if the left logo image from the portal_layout tool is not empty
             and therefore telling if this image should be displayed in the standard header.
             Since older versions of Naaya used 'logo' as the image ID, this is the second choice
         """
         layout_tool = self.getLayoutTool()
-        logo = layout_tool._getOb('logo.gif', None)
-        if not logo:
-            logo = layout_tool._getOb('logo', None)
+        if not lang: lang = self.gl_get_selected_language()
+        logo = layout_tool._getOb('logo_%s.gif' % lang, None)
         if logo and logo.size:
             return True
         return False
 
+    security.declarePublic('leftLogo')
+    def leftLogo(self, lang='', REQUEST=None):
+        """Returns the left logo corresponding to the site language"""
+        layout_tool = self.getLayoutTool()
+        if not lang: lang = self.gl_get_selected_language()
+        logo = layout_tool._getOb('logo_%s.gif' % lang, None)
+        if REQUEST:
+            REQUEST.RESPONSE.redirect(logo.absolute_url())
+        return logo
+
+    security.declarePublic('leftLogo')
+    def leftLogoUrl(self, lang=''):
+        """Returns the left logo url corresponding to the site language"""
+        layout_tool = self.getLayoutTool()
+        if not lang: lang = self.gl_get_selected_language()
+        logo = layout_tool._getOb('logo_%s.gif' % lang, None)
+        if logo and logo.size:
+            return logo.absolute_url()
+
     security.declarePublic('hasRightLogo')
-    def hasRightLogo(self):
+    def hasRightLogo(self, lang=''):
         """ Returns true if the right logo image from the portal_layout tool is not empty
             and therefore telling if this image should be displayed in the standard header.
             Since older versions of Naaya used 'logobis' as the image ID, this is the second choice
         """
         layout_tool = self.getLayoutTool()
-        logo = layout_tool._getOb('logobis.gif', None)
-        if not logo:
-            logo = layout_tool._getOb('logobis', None)
+        if not lang: lang = self.gl_get_selected_language()
+        logo = layout_tool._getOb('logobis_%s.gif' % lang, None)
         if logo and logo.size:
             return True
         return False
 
+    security.declarePublic('rightLogo')
+    def rightLogo(self, lang='', REQUEST=None):
+        """Returns the right logo corresponding to the site language"""
+        layout_tool = self.getLayoutTool()
+        if not lang: lang = self.gl_get_selected_language()
+        logo = layout_tool._getOb('logobis_%s.gif' % lang, None)
+        if REQUEST:
+            REQUEST.RESPONSE.redirect(logo.absolute_url())
+        return logo
+
+    security.declarePublic('leftLogo')
+    def rightLogoUrl(self, lang=''):
+        """Returns the right logo url corresponding to the site language"""
+        layout_tool = self.getLayoutTool()
+        if not lang: lang = self.gl_get_selected_language()
+        logo = layout_tool._getOb('logobis_%s.gif' % lang, None)
+        if logo and logo.size:
+            return logo.absolute_url()
+
+    security.declarePublic('defaultLeftLogoUrl')
+    def defaultLeftLogoUrl(self):
+        """Returns the url corresponding to the default left site logo"""
+        default_logo = self.leftLogoUrl(lang=self.default_logo)
+        if not default_logo:
+            logo = self.getLayoutTool()._getOb('logo.gif', None)
+            if logo is None:
+                logo = self.getLayoutTool()._getOb('logo', None)
+            if logo and logo.size:
+                return logo.absolute_url()
+        else:
+            return default_logo
+
+    security.declarePublic('defaultRightLogoUrl')
+    def defaultRightLogoUrl(self):
+        """Returns the url corresponding to the default right site logo"""
+        default_logo = self.rightLogoUrl(lang=self.default_logo)
+        if not default_logo:
+            logo = self.getLayoutTool()._getOb('logobis.gif', None)
+            if logo is None:
+                logo = self.getLayoutTool()._getOb('logobis', None)
+            if logo and logo.size:
+                return logo.absolute_url()
+        else:
+            return default_logo
+
     security.declareProtected(PERMISSION_PUBLISH_OBJECTS, 'admin_logos')
-    def admin_logos(self, logo='', logobis='', del_leftlogo='', del_rightlogo='', REQUEST=None):
+    def admin_logos(self, logo='', logobis='', del_leftlogo='', del_rightlogo='', default_lang='', lang='', REQUEST=None):
         """ Allows changing and deleting the left and right logos for a Naaya site.
             Left and right logos are independent of the layout chosen and are images called
             'logo.gif' and 'logobis.gif' (or, for older versions of Naaya, 'logo' and 'logobis'
         """
-        left_logo_ob = self.getLayoutTool()._getOb('logo.gif', None)
-        if left_logo_ob is None:
-            left_logo_ob = self.getLayoutTool()._getOb('logo', None)
+        layouttool = self.getLayoutTool()
+        if not lang: lang = self.gl_get_selected_language()
 
-        right_logo_ob = self.getLayoutTool()._getOb('logobis.gif', None)
-        if right_logo_ob is None:
-            right_logo_ob = self.getLayoutTool()._getOb('logobis', None)
-
+        left_logo_id = 'logo_%s.gif' % lang
+        left_logo = layouttool._getOb(left_logo_id, None)
+        if left_logo is None:
+            layouttool.manage_addImage(id=left_logo_id, file='', title='Site logo')
+            left_logo = layouttool._getOb(left_logo_id)
+        if logo != '' and hasattr(logo, 'filename') and logo.filename != '':
+            content = logo.read()
+            if content != '':
+                left_logo.update_data(data=content)
+                left_logo._p_changed = 1
         if del_leftlogo:
-            left_logo_ob.update_data(data='')
-            left_logo_ob._p_changed = 1
+            left_logo.update_data(data='')
+            left_logo._p_changed = 1
+
+        right_logo_id = 'logobis_%s.gif' % lang
+        right_logo = layouttool._getOb(right_logo_id, None)
+        if right_logo is None:
+            layouttool.manage_addImage(id=right_logo_id, file='', title='Site logo (right side)')
+            right_logo = layouttool._getOb(right_logo_id)
+        if logobis != '' and hasattr(logobis, 'filename') and logobis.filename != '':
+            content = logobis.read()
+            if content != '':
+                right_logo.update_data(data=content)
+                right_logo._p_changed = 1
         if del_rightlogo:
-            right_logo_ob.update_data(data='')
-            right_logo_ob._p_changed = 1
-        if logo != '':
-            if hasattr(logo, 'filename'):
-                if logo.filename != '':
-                    content = logo.read()
-                    if content != '':
-                        left_logo_ob.update_data(data=content)
-                        left_logo_ob._p_changed = 1
-        if logobis != '':
-            if hasattr(logobis, 'filename'):
-                if logobis.filename != '':
-                    content = logobis.read()
-                    if content != '':
-                        right_logo_ob.update_data(data=content)
-                        right_logo_ob._p_changed = 1
+            right_logo.update_data(data='')
+            right_logo._p_changed = 1
+
+        if default_lang:
+            self.default_logo = default_lang
+
         if REQUEST:
             self.setSessionInfo([MESSAGE_SAVEDCHANGES % self.utGetTodayDate()])
             REQUEST.RESPONSE.redirect('%s/admin_logos_html' % self.absolute_url())
