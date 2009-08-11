004e6958af34ba83030c2faf47c0690110c4182e
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2009-08-11 10:53:48 +0000

    cleanup code

---
+++ Naaya/MeetingRegistration/MeetingRegistration.py
@@ -0,0 +1,60 @@
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Initial Owner of the Original Code is European Environment
+# Agency (EEA).  Portions created by Finsiel Romania are
+# Copyright (C) European Environment Agency.  All
+# Rights Reserved.
+#
+# Authors:
+#
+# Alex Morega, Eau de Web
+# Cornel Nitu, Eau de Web
+# Valentin Dumitru, Eau de Web
+
+from OFS.Folder import Folder
+from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
+from AccessControl.Permissions import view
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+
+from utilities.Slugify import slugify
+
+manage_addRegistration_html = PageTemplateFile('zpt/meeting_registration/add', globals())
+def manage_addRegistration(self, id='', title='', REQUEST=None):
+    """ """
+    if not id:
+        id = slugify(title)
+    ob = MeetingRegistration(id, title)
+    self._setObject(id, ob)
+    ob = self._getOb(id)
+    if REQUEST:
+        REQUEST.RESPONSE.redirect(self.absolute_url())
+
+class MeetingRegistration(Folder):
+    """ """
+
+    meta_type = 'Meeting registration'
+    product_name = 'MeetingRegistration'
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, id, title):
+        """ constructor """
+        self.id = id
+        self.title = title
+
+    security.declareProtected(view, 'index_html')
+    index_html = PageTemplateFile('zpt/meeting_registration/index', globals())
+
+    security.declareProtected(view, 'edit_html')
+    edit = PageTemplateFile('zpt/meeting_registration/edit', globals())
+
+InitializeClass(MeetingRegistration)
