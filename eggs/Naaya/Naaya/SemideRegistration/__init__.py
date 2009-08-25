c8c8dbf95cd095ad9c44e6d37878aaa56961855c
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2009-08-25 09:01:22 +0000

    initial import (registration forms for SEMIDE)

---
+++ eggs/Naaya/Naaya/SemideRegistration/__init__.py
@@ -0,0 +1,32 @@
+from App.ImageFile import ImageFile
+
+from Products.Naaya import register_content
+import SemideRegistration
+from utilities.StaticServe import StaticServeFromZip
+
+ADD_PERMISSION = 'Add Semide Registration'
+
+# Register as a folder content type
+register_content(
+    module=SemideRegistration,
+    klass=SemideRegistration.SemideRegistration,
+    module_methods={'manage_add_registration': ADD_PERMISSION, 'add_registration': ADD_PERMISSION},
+    klass_methods={},
+    add_method=('add_registration', ADD_PERMISSION),
+)
+
+
+def initialize(context):
+    context.registerClass(
+                          SemideRegistration.SemideRegistration,
+                          permission = ADD_PERMISSION,
+                          constructors=(
+                                    SemideRegistration.add_registration,
+                                    SemideRegistration.manage_add_registration),
+                          icon='www/SemideRegistration.gif',
+                          )
+
+misc_ = {
+    'tinymce': StaticServeFromZip('', 'www/tinymce_3_2_5.zip', globals()),
+    'tinymceconfig.js': ImageFile('www/tinymceconfig.js', globals()),
+    }
