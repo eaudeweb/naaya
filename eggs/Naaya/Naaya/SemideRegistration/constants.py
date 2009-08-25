760a666c8e54d1d36e9ed54e0bfba56de47c1962
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2009-08-25 13:56:39 +0000

    forms validators added

--- eggs/Naaya/Naaya/SemideRegistration/constants.py
+++ eggs/Naaya/Naaya/SemideRegistration/constants.py
@@ -1,5 +1,7 @@
 #constants
 PART_MANDATORY_FIELDS = ['last_name', 'country', 'phone_number', 'email', 'passport_no', 'passport_expire']
 PRESS_MANDATORY_FIELDS = ['last_name', 'country', 'phone_number', 'email', 'passport_no', 'passport_expire']
+AUTH_MANDATORY_FIELDS = ['registration_no']
+
 DATE_FIELDS = ['passport_expire', 'arrival_date', 'departure_date']
 TIME_FIELDS = ['arrival_time', 'departure_time']
