02188bb1a935c7a28f6e9f1b46bc31213e25532b
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2009-08-26 16:19:50 +0000

    If set to "international", the constructor is forced to treat ambiguous dates as "days before month before year"

--- eggs/Naaya/Naaya/SemideRegistration/BaseParticipant.py
+++ eggs/Naaya/Naaya/SemideRegistration/BaseParticipant.py
@@ -26,20 +26,20 @@ class BaseParticipant(SimpleItem):
         self.phone_number = phone_number
         self.fax_number = fax_number
         if arrival_date:
-            self.arrival_date = DateTime(arrival_date)
+            self.arrival_date = DateTime(arrival_date, datefmt='international')
         else:
             self.arrival_date = ''
         self.arrival_from = arrival_from
         self.arrival_flight = arrival_flight
         self.arrival_time = arrival_time
         if departure_date:
-            self.departure_date = DateTime(departure_date)
+            self.departure_date = DateTime(departure_date, datefmt='international')
         else:
             self.departure_date = ''
         self.departure_flight = departure_flight
         self.departure_time = departure_time
         self.is_journalist = is_journalist
-        self.registration_date = DateTime()
+        self.registration_date = DateTime(datefmt='international')
 
     security.declareProtected(constants.VIEW_PERMISSION, 'edit')
     def edit(self, first_name, last_name, email, country, passport_no, passport_expire, phone_number, \
@@ -55,14 +55,14 @@ class BaseParticipant(SimpleItem):
         self.phone_number = phone_number
         self.fax_number = fax_number
         if arrival_date:
-            self.arrival_date = DateTime(arrival_date)
+            self.arrival_date = DateTime(arrival_date, datefmt='international')
         else:
             self.arrival_date = ''
         self.arrival_from = arrival_from
         self.arrival_flight = arrival_flight
         self.arrival_time = arrival_time
         if departure_date:
-            self.departure_date = DateTime(departure_date)
+            self.departure_date = DateTime(departure_date, datefmt='international')
         else:
             self.departure_date = ''
         self.departure_flight = departure_flight
