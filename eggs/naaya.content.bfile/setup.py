6924d06b403b9a2e15af43c91576030fe6d0934d
Alex Morega <alex@grep.ro>
2009-09-16 13:37:12 +0000

    initial import of naaya.content.bfile egg

---
+++ eggs/naaya.content.bfile/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='naaya.content.bfile',
+      version=version,
+      description="",
+      long_description=open("README.txt").read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['naaya', 'naaya.content'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
