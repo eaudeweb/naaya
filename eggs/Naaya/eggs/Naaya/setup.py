1ea15b29bbe7cc4af36218445bfc5af172f6ffa0
Alin Voinea <alin.voinea@eaudeweb.ro>
2009-10-26 14:16:26 +0000

    - Update mediafile flash player to flowplayer.

--- eggs/Naaya/eggs/Naaya/setup.py
+++ eggs/Naaya/eggs/Naaya/setup.py
@@ -3,9 +3,12 @@ from setuptools import setup, find_packages
 setup(name='Naaya',
       version='1.0dev',
       author='Eau de Web',
-      email='office@eaudeweb.ro',
+      author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      )
+      install_requires=[
+          'naaya.flowplayer',
+      ]
+)
