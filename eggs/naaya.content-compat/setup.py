2a5b5804e7082ca4e36ceacf88701ba97b55b9a9
David Batranu <david.batranu@eaudeweb.ro>
2009-10-26 15:51:54 +0000

    - Simplified setup.py to follow Naaya model; - Removed Manifest.in; - Removed external getversion.py.
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content-compat@13375 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content-compat/setup.py
+++ eggs/naaya.content-compat/setup.py
@@ -1,33 +1,11 @@
 from setuptools import setup, find_packages
-import os
-from getversion import get_version
-
-version = get_version()
 
 setup(name='naaya.content-compat',
-      version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='GPL',
-      packages=['Products'],
-      namespace_packages=['Products'],
+      version='1.0dev',
+      author='Eau de Web',
+      email='office@eaudeweb.ro',
+      url='http://naaya.eaudeweb.ro',
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
       )
