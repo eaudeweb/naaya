fec3d8062595eec22d655e2b8ad1fc0bd850a8e4
Alex Morega <alex@grep.ro>
2009-10-27 09:37:40 +0000

    added dependencies to `install_requires`

--- eggs/Naaya/eggs/Naaya/setup.py
+++ eggs/Naaya/eggs/Naaya/setup.py
@@ -10,5 +10,14 @@ setup(name='Naaya',
       zip_safe=False,
       install_requires=[
           'naaya.flowplayer',
+          'Products.TextIndexNG3 >= 3.2.11',
+          'itools == 0.20.6',
+              # for Windows, download and install DLLs from
+              # http://naaya.eaudeweb.ro/eggshop/glib-dlls.zip
+          'feedparser >= 4.1',
+          'BeautifulSoup >= 3.0.7a',
+          'twill >= 0.9',
+          'simplejson >= 2.0.9',
+          'vobject >= 0.8.1c',
       ]
 )
