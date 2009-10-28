32c7168219caa1a2094e45776ee3b11fe767d228
David Batranu <david.batranu@eaudeweb.ro>
2009-10-28 14:27:16 +0000

    - Deleted Manifest.in; - Updated setup.py; - Removed external requirement.

--- eggs/naaya.groupware/setup.py
+++ eggs/naaya.groupware/setup.py
@@ -1,33 +1,14 @@
 from setuptools import setup, find_packages
-import os
-from getversion import get_version
-
-version = get_version()
 
 setup(name='naaya.groupware',
-      version=version,
-      description="Naaya Groupware",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='naaya groupware',
-      author='David Batranu',
-      author_email='david@eaudeweb.ro',
-      url='',
-      license='MPL',
-      packages=['naaya', 'Products'],
-      namespace_packages=['naaya', 'Products'],
+      version='1.0dev',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      url='http://naaya.eaudeweb.ro',
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+          'Naaya',
+      ]
+)
