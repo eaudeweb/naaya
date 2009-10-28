f327a37c1729963bc463fb4eae858f71a46f3f8c
David Batranu <david.batranu@eaudeweb.ro>
2009-10-28 14:33:56 +0000

    - Deleted Manifest.in; - Updated setup.py; - Removed external requirement.

--- eggs/Naaya/eggs/naaya.content.bfile/setup.py
+++ eggs/Naaya/eggs/naaya.content.bfile/setup.py
@@ -1,32 +1,14 @@
 from setuptools import setup, find_packages
-import os
-from getversion import get_version
-
-version = get_version()
 
 setup(name='naaya.content.bfile',
-      version=version,
-      description="",
-      long_description=open("README.txt").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='GPL',
-      packages=['naaya'],
-      namespace_packages=['naaya'],
+      version='1.0dev',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      url='http://naaya.eaudeweb.ro',
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+          'Naaya',
+      ]
+)
