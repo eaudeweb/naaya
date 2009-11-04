997f1ec153dd6b3c66d15e7ae1b6828e722dc85e
Alin Voinea <alin.voinea@eaudeweb.ro>
2009-11-04 13:06:22 +0000

    - Refatoring media converter and conversion logging in order to avoid UNKNOWN conversion errors.

--- eggs/Naaya/eggs/Naaya/naaya/content/mediafile/converters/MediaConverter.py
+++ eggs/Naaya/eggs/Naaya/naaya/content/mediafile/converters/MediaConverter.py
@@ -20,68 +20,128 @@
 
 # Python imports
 import os
-import sys
-from popen2 import popen3, popen4 # TODO Python 2.5: use the new subprocess module
-from threading import Thread
-
-# Zope imports
-from zLOG import LOG, WARNING, DEBUG
+import logging
 import subprocess
-
+import threading
+logger = logging.getLogger('mediafile.converters')
+#
+# Media converter
+#
 class MediaConverterError(Exception):
     """Media Convertor Error"""
     pass
 
-class MediaConverter(Thread):
-    """ Media Converter
+class MediaConverter(object):
+    """ Convert any video file to FlashVideoFile (.flv) using ffmpeg and index
+    is with flvtool2.
     """
-    def __init__(self, finput, foutput, fdone):
-        self.finput = finput
-        self.foutput = foutput
-        if sys.platform == 'win32':
-            self.fdone = os.path.basename(fdone)
+    def __init__(self, fin, fout, fdone, flog):
+        self.fin = fin
+        self.fout = fout
+        self.fdone = fdone
+
+        self.log = open(flog, 'w')
+        self.exit_code = 0
+        self.process = None
+
+    def step_1(self):
+        """ Convert video to flv
+        """
+        logger.debug('Conversion step 1')
+
+        if not can_convert():
+            return self.finish('Can not convert (are tools available?)')
+
+        cmd = ["ffmpeg", "-y", "-v", "0", "-benchmark", "-i", self.fin, "-ar",
+               "22050", "-s", "320x240", "-b", "500k", "-f", "flv", self.fout]
+        self.process = subprocess.Popen(cmd, stdout=self.log, stderr=self.log)
+        return self.step_2()
+
+    def step_2(self):
+        """ Wait for step 1 to finish and launch step 3 or exit
+        """
+        logger.debug('Conversion step 2')
+
+        self.exit_code = self.process.poll()
+        if self.exit_code is None:
+            # Not finished yet
+            timer = threading.Timer(5, self.step_2)
+            timer.start()
+            return None
+
+        if self.exit_code != 0:
+            return self.finish('Exit code %s' % self.exit_code)
+
+        self.process = None
+        return self.step_3()
+
+    def step_3(self):
+        """ Update video index using flvtool2 or finish
+        """
+        logger.debug('Conversion step 3')
+
+        if not can_index():
+            logger.debug("Can not index video (is flvtool2 installed?)")
+            return self.finish()
+
+        cmd = ["flvtool2", "-U", self.fout]
+        self.process = subprocess.Popen(cmd, stdout=self.log, stderr=self.log)
+        return self.step_4()
+
+    def step_4(self):
+        """ Wait for step 3 and finish
+        """
+        logger.debug('Conversion step 4')
+
+        self.exit_code = self.process.poll()
+        if self.exit_code is None:
+            # Not finished yet
+            timer = threading.Timer(5, self.step_4)
+            timer.start()
+            return None
+
+        if self.exit_code != 0:
+            logger.exception('An error occured while indexing video file.')
+        return self.finish()
+
+    def finish(self, error=None):
+        """ If no error rename output to done and cleanup
+        """
+        if error:
+            error = 'MediaConverterError: %s' % error
+            logger.exception(error)
+            self.log.write(error)
+
+            # Cleanup output file
+            try:
+                os.unlink(self.fout)
+            except Exception, err:
+                logger.exception(err)
         else:
-            self.fdone = fdone
-        Thread.__init__(self)
-    
-    def execute(self, cmd):
-        """ Execute fs cmd"""
-        LOG('NaayaContent.NyMediaFile.convertors.MediaConverter', DEBUG, 'executing command: %s' % cmd)
-        return os.popen4(cmd)
+            # Cleanup input file
+            try:
+                os.unlink(self.fin)
+            except Exception, err:
+                logger.exception(err)
+
+            # Rename output file to done file
+            try:
+                os.rename(self.fout, self.fdone)
+            except Exception, err:
+                logger.exception(err)
+                self.log.write(
+                    'MediaConverterError: Could not finish conversion %s' % err)
+
+        # Close log
+        self.log.close()
 
     def run(self):
-        """Converts media to flash video (flv) files"""
-        if sys.platform == 'win32':
-            # TODO: Improve (fix) conversion on Win Platform
-            cmd = "ffmpeg -y -v 0 " \
-                    "-benchmark " \
-                    "-i %(in)s " \
-                    "-ar 22050 -s 320x240 -b 500k -f flv " \
-                    "%(out)s > %(done)s.log " \
-                    "&& del %(in)s "
-            if _is_flvtool2_available:
-                cmd += "&& flvtool2 -U %(out)s >> %(done)s.log "
-            else:
-                LOG('NaayaContent.NyMediaFile.convertors.MediaConverter', WARNING, 'can not update FLV with an onMetaTag event, because flvtool2 is not available')
-            cmd += "&& rename %(out)s %(done)s"
-        else:
-            cmd = "ffmpeg -y -v 0 " \
-                    "-benchmark " \
-                    "-i %(in)s " \
-                    "-ar 22050 -s 320x240 -b 500k -f flv " \
-                    "%(out)s > %(done)s.log " \
-                    "&& rm %(in)s "
-            if _is_flvtool2_available:
-                cmd += "&& flvtool2 -U %(out)s >> %(done)s.log "
-            else:
-                LOG('NaayaContent.NyMediaFile.convertors.MediaConverter', WARNING, 'can not update FLV with an onMetaTag event, because flvtool2 is not available')
-            cmd += "&& mv %(out)s %(done)s"
-        cmd = cmd % {"in": self.finput,
-                     "out": self.foutput,
-                     "done": self.fdone}
-        return self.execute(cmd)
-
-
+        """ Run converter step by step
+        """
+        return self.step_1()
+#
+# Private interface
+#
 def _check_ffmpeg():
     """Checks if ffmpeg is available.
 
@@ -89,13 +149,18 @@ def _check_ffmpeg():
         a MediaConverterError exception will be raised.
     """
     process = subprocess.Popen(["ffmpeg", "-h"],
-                               shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout = process.stdout.read()
     process.wait()
-    if process.returncode!=1:
-        raise MediaConverterError('could not run ffmpeg: "ffmpeg -h" has exited with code %s' % (process.returncode, ))
+
+    if process.returncode != 1:
+        raise MediaConverterError(
+            'could not run ffmpeg: "ffmpeg -h" has exited with code %s' % (
+                process.returncode, ))
+
     if "--enable-libmp3lame" not in stdout:
-        raise MediaConverterError('ffmpeg was not compiled with --enable-libmp3lame')
+        raise MediaConverterError(
+            'ffmpeg was not compiled with --enable-libmp3lame')
 
 def _check_flvtool2():
     """Checks if flvtool2 is available.
@@ -103,75 +168,78 @@ def _check_flvtool2():
         If flvtool2 is not installed a MediaConverterError exception will be raised.
     """
     process = subprocess.Popen(["flvtool2", "-H"],
-                               shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout = process.stdout.read()
-    process.wait()
-    if process.returncode:
-        raise MediaConverterError('could not run flvtool2: "flvtool2 -H" has exited with code %s' % (process.returncode, ))
 
-_is_ffmepg_available = _is_flvtool2_available = False
+    process.wait()
+
+    if process.returncode:
+        raise MediaConverterError(
+            'could not run flvtool2: "flvtool2 -H" has exited with code %s' % (
+                process.returncode, ))
+#
+# Private variables
+#
+_is_ffmepg_available = False
 try:
     _check_ffmpeg()
+except MediaConverterError, media_err:
+    logger.exception(media_err)
+else:
     _is_ffmepg_available = True
-    _check_flvtool2()
-    _is_flvtool2_available = True
-except MediaConverterError, ex:
-    LOG('NaayaContent.NyMediaFile.convertors', WARNING, "%s" % (ex, ))
 
+_is_flvtool2_available = False
+try:
+    _check_flvtool2()
+except MediaConverterError, media_err:
+    logger.exception(media_err)
+else:
+    _is_flvtool2_available = True
+
+#
+# Public interface
+#
 def can_convert():
+    """ Is ffmpeg installed?
+    """
     return _is_ffmepg_available
+
+def can_index():
+    """ Is flvtool2 installed?
+    """
+    return _is_flvtool2_available
+
+def media2flv(finput, suffix=""):
+    """ Convert media to flv and add metadata
+    """
+    if not can_convert():
+        return "Can not convert (are tools available?)"
+
+    fin = finput + suffix
+    tcv = finput + ".tcv" # to convert
+    cvd = finput + ".cvd" # converted
+    os.rename(fin, tcv)
+    media_converter = MediaConverter(tcv, cvd, finput, finput + '.log')
+    media_converter.run()
 
 def get_conversion_errors(fpath, suffix=".log"):
     """ Open error file and parse it for errors
     """
     error_path = fpath + suffix
+
+    # If flv file exists conversion is done
     if os.path.isfile(fpath):
-        # Clean log files
-        os.unlink(error_path)
         return ""
-    
+
     try:
         error_file = open(error_path, "r")
     except IOError:
         return ""
-    
+
     for error in error_file.readlines():
-        # flvtool2 errors
-        if "ERROR:" in error:
+        if "MediaConverterError:" in error:
             error_file.close()
             return error
-    
-        # ffmpeg errors
-        # TODO: Better error catch algorithm
-        err_str = fpath + ".tcv:"
-        if err_str in error:
-            error_file.close()
-            error = error.replace(err_str, "ERROR:", 1)
-            return error
-    
+
     error_file.close()
     return ""
-
-def media2flv(finput, suffix=""):
-    """ Convert media to flv and add metadata
-    """
-    fin = finput + suffix
-    # Check for available server tools
-    if not can_convert():
-        return "Can not convert (are tools available?)"
-    
-    tcv = finput + ".tcv" # to convert
-    cvd = finput + ".cvd" # converted
-    os.rename(fin, tcv)
-    
-    conv = MediaConverter(tcv, cvd, finput)
-    conv.start()
-    conv.join()
-    return ""
-
-if __name__ == "__main__":
-    argv = sys.argv
-    if(len(argv)>1):
-        filepath = sys.argv[1]
-    else:
-        filepath = "video.flv"
