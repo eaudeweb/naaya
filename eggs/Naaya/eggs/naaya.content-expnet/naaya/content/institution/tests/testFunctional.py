f4d114a72028c098a4415a0276b24edc5f7ac980
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2009-11-11 11:16:43 +0000

    search test for expert tests for institution

--- eggs/Naaya/eggs/naaya.content-expnet/naaya/content/institution/tests/testFunctional.py
+++ eggs/Naaya/eggs/naaya.content-expnet/naaya/content/institution/tests/testFunctional.py
@@ -17,130 +17,122 @@ def setUp_EW_test_data(portal):
 def tearDown_EW_test_data(portal):
     portal.portal_portlets.manage_delObjects(['countries'])
 
-class NyExpertFunctionalTestCase(NaayaFunctionalTestCase):
+class NyInstitutionFunctionalTestCase(NaayaFunctionalTestCase):
     """ TestCase for NaayaContent object """
 
     def afterSetUp(self):
-        self.install_content_type('Naaya Expert')
+        self.install_content_type('Naaya Institution')
         setUp_EW_test_data(self.portal)
         from Products.Naaya.NyFolder import addNyFolder
-        from naaya.content.expert.expert_item import addNyExpert
+        from naaya.content.institution.institution_item import addNyInstitution
         addNyFolder(self.portal, 'myfolder', contributor='contributor', submitted=1)
-        addNyExpert(self.portal.myfolder, id='myexpert', name='My expert',
-            surname='Knowitall', submitted=1, contributor='contributor')
+        addNyInstitution(self.portal.myfolder, id='myinstitution', title="My Institution",
+                        submitted=1, contributor='contributor')
         transaction.commit()
 
     def beforeTearDown(self):
         self.portal.manage_delObjects(['myfolder'])
         tearDown_EW_test_data(self.portal)
-        self.remove_content_type('Naaya Expert')
+        self.remove_content_type('Naaya Institution')
         transaction.commit()
 
     def test_add(self):
-        self.browser_do_login('contributor', 'contributor')
-        self.browser.go('http://localhost/portal/myfolder/expert_add_html')
-        self.failUnless('<h1>Submit Expert</h1>' in self.browser.get_html())
+        self.browser_do_login('admin', '')
+        self.browser.go('http://localhost/portal/myfolder/institution_add_html')
+        self.failUnless('<h1>Submit Institution</h1>' in self.browser.get_html())
         form = self.browser.get_form('frmAdd')
-        expected_controls = set([
-            'name:utf8:ustring', 'surname:utf8:ustring', 'lang',
-            'sortorder:utf8:ustring', 'email:utf8:ustring','instant_messaging:utf8:ustring',
-            'ref_lang:utf8:ustring', 'phone:utf8:ustring','mobile:utf8:ustring',
-        ])
-        found_controls = set(c.name for c in form.controls)
-        self.failUnless(expected_controls.issubset(found_controls),
-            'Missing form controls: %s' % repr(expected_controls - found_controls))
 
         self.failUnlessEqual(form['sortorder:utf8:ustring'], '100')
 
-        self.browser.clicked(form, self.browser.get_form_field(form, 'name'))
-        form['name:utf8:ustring'] = 'mister expert'
-        form['surname:utf8:ustring'] = 'knowitall'
-        form['email:utf8:ustring'] = 'expert@example.com'
+        self.browser.clicked(form, self.browser.get_form_field(form, 'title'))
+        form['title:utf8:ustring'] = 'My New Institution'
 
         self.browser.submit()
         html = self.browser.get_html()
 
-        self.failUnless('The administrator will analyze your request and you will be notified about the result shortly.' in html)
+        self.failUnless('Item added' in html)
 
-        expert_id = (set(self.portal.myfolder.objectIds()) - set(['myexpert'])).pop()
-        self.portal.myfolder[expert_id].approveThis()
+        institution_id = (set(self.portal.myfolder.objectIds()) - set(['myinstitution'])).pop()
+        self.portal.myfolder[institution_id].approveThis()
 
-        self.browser.go('http://localhost/portal/myfolder/' + expert_id)
+        self.browser.go('http://localhost/portal/myfolder/' + institution_id)
         html = self.browser.get_html()
-        self.failUnless(re.search(r'<h1>.*mister expert.*</h1>', html, re.DOTALL))
-        self.failUnless('expert at example.com' in html)
+        self.failUnless(re.search(r'<h1>.*My New Institution.*</h1>', html, re.DOTALL))
 
         self.browser_do_logout()
 
     def test_add_error(self):
         self.browser_do_login('contributor', 'contributor')
-        self.browser.go('http://localhost/portal/myfolder/expert_add_html')
+        self.browser.go('http://localhost/portal/myfolder/institution_add_html')
 
         #The object should not be added if the mandatory fields are not all filled
         form = self.browser.get_form('frmAdd')
-        self.browser.clicked(form, self.browser.get_form_field(form, 'name'))
-        form['name:utf8:ustring'] = 'mister expert'
+        self.browser.clicked(form, self.browser.get_form_field(form, 'title'))
         self.browser.submit()
         html = self.browser.get_html()
         self.failUnless('The form contains errors' in html)
-        self.failUnless('Value required for "Surname"' in html)
-
-        form = self.browser.get_form('frmAdd')
-        self.browser.clicked(form, self.browser.get_form_field(form, 'name'))
-        form['surname:utf8:ustring'] = 'knowitall'
-        form['name:utf8:ustring'] = ''
-        self.browser.submit()
-        html = self.browser.get_html()
-        self.failUnless('The form contains errors' in html)
-        self.failUnless('Value required for "Name"' in html)
+        self.failUnless('Value required for "Title"' in html)
 
         self.browser_do_logout()
 
     def test_edit(self):
         self.browser_do_login('admin', '')
 
-        self.browser.go('http://localhost/portal/myfolder/myexpert/edit_html')
+        self.browser.go('http://localhost/portal/myfolder/myinstitution/edit_html')
         form = self.browser.get_form('frmEdit')
-        self.failUnlessEqual(form['name:utf8:ustring'], 'My expert')
-        form['name:utf8:ustring'] = 'new_expert_name'
-        form['surname:utf8:ustring'] = 'his_surname'
+        self.failUnlessEqual(form['title:utf8:ustring'], 'My Institution')
+        form['title:utf8:ustring'] = 'New Institution Name'
         self.browser.clicked(form, form.find_control('saveProperties:method'))
         self.browser.submit()
 
-        self.failUnlessEqual(self.portal.myfolder.myexpert.name, 'new_expert_name')
+        self.failUnlessEqual(self.portal.myfolder.myinstitution.title, 'New Institution Name')
 
         html = self.browser.get_html()
-        self.failUnless('<h1>Edit Expert</h1>' in html)
+        self.failUnless('<h1>Edit Institution</h1>' in html)
 
         self.browser_do_logout()
 
     def test_edit_error(self):
         self.browser_do_login('admin', '')
-        self.browser.go('http://localhost/portal/myfolder/myexpert/edit_html')
+        self.browser.go('http://localhost/portal/myfolder/myinstitution/edit_html')
 
         #The object properties should not be saved if the mandatory fields are not all filled
         form = self.browser.get_form('frmEdit')
-        form['name:utf8:ustring'] = ''
+        form['title:utf8:ustring'] = ''
         self.browser.clicked(form, form.find_control('saveProperties:method'))
         self.browser.submit()
 
         html = self.browser.get_html()
         self.failUnless('The form contains errors' in html)
-        self.failUnless('Value required for "Name"' in html)
+        self.failUnless('Value required for "Title"' in html)
 
-        form = self.browser.get_form('frmEdit')
-        form['name:utf8:ustring'] = 'new_expert_name'
-        form['surname:utf8:ustring'] = ''
-        self.browser.clicked(form, form.find_control('saveProperties:method'))
+        self.browser_do_logout()
+
+    def test_search(self):
+        self.browser_do_login('admin', '')
+        self.browser.go('http://localhost/portal/myfolder/institutions_list')
+
+        #Find the institution added
+        form = self.browser.get_form('frmSearch')
+        form['q'] = 'My Institution'
+        self.browser.clicked(form, form.find_control('search'))
         self.browser.submit()
 
         html = self.browser.get_html()
-        self.failUnless('The form contains errors' in html)
-        self.failUnless('Value required for "Surname"' in html)
+        self.failUnless('My Institution</a></h4>' in html)
+
+        #Fail to find a nonexistend string
+        form = self.browser.get_form('frmSearch')
+        form['q'] = 'No results'
+        self.browser.clicked(form, form.find_control('search'))
+        self.browser.submit()
+
+        html = self.browser.get_html()
+        self.failUnless('No institutions found for this query.' in html)
 
         self.browser_do_logout()
 
 def test_suite():
     suite = TestSuite()
-    suite.addTest(makeSuite(NyExpertFunctionalTestCase))
+    suite.addTest(makeSuite(NyInstitutionFunctionalTestCase))
     return suite
