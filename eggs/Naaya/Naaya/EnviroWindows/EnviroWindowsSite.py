ed8045d6b59269755921595eec75a9bc17bebae8
andredor <andredor@eaudeweb.ro>
2009-11-18 12:53:44 +0000

    Added objects listing portlet for EnviroWindows sites.

--- eggs/Naaya/Naaya/EnviroWindows/EnviroWindowsSite.py
+++ eggs/Naaya/Naaya/EnviroWindows/EnviroWindowsSite.py
@@ -1151,6 +1151,10 @@ text-decoration: underline;
     ew_print_css = ImageFile('www/ew_print.css', globals())
     ew_style_css = ImageFile('www/ew_style.css', globals())
 
+    security.declareProtected(view, 'folder_listing')
+    folder_listing = PageTemplateFile(
+       'zpt/folder_listing', globals())
+
 InitializeClass(EnviroWindowsSite)
 
 #
