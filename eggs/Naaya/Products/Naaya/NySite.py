9657ece625b04eff8b445964514bd01ab990d3fe
David Batranu <david.batranu@eaudeweb.ro>
2009-11-23 15:23:13 +0000

    Fixing sitemap RuntimeError (#180)
    - Added checks for getPublishedFolders and getPublishedObjects;
    - Added two new utils functions.

--- eggs/Naaya/Products/Naaya/NySite.py
+++ eggs/Naaya/Products/Naaya/NySite.py
@@ -99,6 +99,7 @@ from NyFolder import NyFolder, folder_add_html, addNyFolder, importNyFolder
 from Products.NaayaCore.NotificationTool.Subscriber import Subscriber
 from Products.NaayaBase.gtranslate import translate, translate_url
 from NyFolderBase import NyFolderBase
+from naaya.core.utils import call_method
 
 #reCaptcha
 from Products.NaayaCore.managers import recaptcha_utils
@@ -1448,7 +1449,9 @@ class NySite(CookieCrumbler, LocalPropertyManager, Folder,
                 ppath = ''
             else:
                 ppath = parent.absolute_url(1)
-            return folder_ob.getPublishedFolders(), folder_ob.getPublishedObjects(), ppath
+            pub_folders = call_method(folder_ob, 'getPublishedFolders', [])
+            pub_objects = call_method(folder_ob, 'getPublishedObjects', [])
+            return pub_folders, pub_objects, ppath
         return [x for x in self.objectValues(self.get_naaya_containers_metatypes()) if x.approved == 1 and x.submitted==1], [], ''
 
     def getFolderContent(self, folder_path):
@@ -1467,14 +1470,14 @@ class NySite(CookieCrumbler, LocalPropertyManager, Folder,
         #site map core
         l_tree = []
         if root is self: l_folders = [x for x in root.objectValues(self.get_naaya_containers_metatypes()) if x.approved == 1 and x.submitted==1]
-        else: l_folders = root.getPublishedFolders()
+        else: l_folders = call_method(root, 'getPublishedFolders', [])
         l_folders = self.utSortObjsListByAttr(l_folders, 'sortorder', sort_order)
         for l_folder in l_folders:
             if (len(l_folder.objectValues(self.get_naaya_containers_metatypes())) > 0) or ((len(l_folder.getObjects()) > 0) and showitems==1):
                 if l_folder.absolute_url(1) in expand or 'all' in expand:
                     l_tree.append((l_folder, 0, depth))
                     if showitems:
-                        for l_item in l_folder.getPublishedObjects():
+                        for l_item in call_method(l_folder, 'getPublishedObjects', []):
                             l_tree.append((l_item, -1, depth+1))
                     l_tree.extend(self.__getSiteMap(l_folder, showitems, expand, depth+1, sort_order))
                 else:
