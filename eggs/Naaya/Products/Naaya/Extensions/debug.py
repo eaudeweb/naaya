81f9d4c57be622f56a72abca776afd699d3942ba
Alex Morega <alex@grep.ro>
2009-11-30 15:59:25 +0000

    the "debug" external method can open up a non-pdb interactive prompt

--- eggs/Naaya/Products/Naaya/Extensions/debug.py
+++ eggs/Naaya/Products/Naaya/Extensions/debug.py
@@ -1,6 +1,15 @@
 def debug(self, REQUEST, RESPONSE):
     RESPONSE.setHeader('Content-Type', 'text/plain')
-    RESPONSE.write('invoking pdb...\n')
+    RESPONSE.write('invoking pdb.set_trace() ...\n')
     RESPONSE.flush()
     import pdb; pdb.set_trace()
+    RESPONSE.write('done.\n')
+
+def interact(self, REQUEST, RESPONSE):
+    RESPONSE.setHeader('Content-Type', 'text/plain')
+    RESPONSE.write('invoking code.interact() ...\n')
+    RESPONSE.flush()
+    import code; code.interact(local={'self': self,
+                                      'REQUEST': REQUEST,
+                                      'RESPONSE': RESPONSE})
     RESPONSE.write('done.\n')
