a9d685f40d3d0f5942ddb931786226e841267b3f
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2009-12-11 16:34:06 +0000

    use utils.make_id Requires NaayaCore.managers.utils at revision 13772

--- eggs/Naaya/naaya/content/url/url_item.py
+++ eggs/Naaya/naaya/content/url/url_item.py
@@ -46,6 +46,7 @@ from Products.NaayaBase.NyAttributes import NyAttributes
 from Products.NaayaBase.NyValidation import NyValidation
 from Products.NaayaBase.NyCheckControl import NyCheckControl
 from Products.NaayaBase.NyContentType import NyContentData
+from Products.NaayaCore.managers.utils import make_id
 
 from interfaces import INyURL
 
@@ -98,10 +99,7 @@ def url_add_html(self, REQUEST=None, RESPONSE=None):
     return self.getFormsTool().getContent({'here': self, 'kind': config['meta_type'], 'action': 'addNyURL', 'form_helper': form_helper}, 'url_add')
 
 def _create_NyURL_object(parent, id, contributor):
-    i = 0
-    while parent._getOb(id, None):
-        i += 1
-        id = '%s-%u' % (id, i)
+    id = make_id(parent, id=id, prefix='url')
     ob = NyURL(id, contributor)
     parent.gl_add_languages(ob)
     parent._setObject(id, ob)
@@ -122,9 +120,7 @@ def addNyURL(self, id='', REQUEST=None, contributor=None, **kwargs):
     schema_raw_data.setdefault('locator', '')
     _contact_word = schema_raw_data.get('contact_word', '')
 
-    id = self.utCleanupId(id)
-    if not id: id = self.utGenObjectId(schema_raw_data.get('title', ''))
-    if not id: id = 'url' + self.utGenRandomId(5)
+    id = make_id(self, id=id, title=schema_raw_data.get('title', ''), prefix='url')
     if contributor is None: contributor = self.REQUEST.AUTHENTICATED_USER.getUserName()
 
     ob = _create_NyURL_object(self, id, contributor)
