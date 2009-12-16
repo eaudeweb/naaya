ce585a09f39ba3911d596a8a7ad6bec70b8b8d77
David Batranu <david.batranu@eaudeweb.ro>
2009-12-16 13:47:37 +0000

    Merging EditorTool branch back into trunk

--- eggs/Naaya/Products/NaayaCore/EditorTool/EditorTool.py
+++ eggs/Naaya/Products/NaayaCore/EditorTool/EditorTool.py
@@ -8,7 +8,6 @@
 #implied. See the License for the specific language governing
 #rights and limitations under the License.
 #
-#The Original Code is "GeoMapTool"
 #
 #The Initial Owner of the Original Code is European Environment
 #Agency (EEA). Portions created by Eau de Web are Copyright (C)
@@ -17,183 +16,281 @@
 #Contributor(s):
 #  Original Code:
 #        Cornel Nitu (Eau de Web)
-
+# Cristian Romanescu (Eau de Web)
 """
 This is a core tool of the Naaya CMF.
 Every portal B{must} have an object of this type inside.
 """
-
 #Python imports
+import copy
 from ConfigParser import ConfigParser
 from os.path import join, dirname
+import simplejson as json 
 
 #Zope imports
-from Globals import InitializeClass
 from AccessControl import ClassSecurityInfo
-from AccessControl.Permissions import view_management_screens, view
+from App.ImageFile import ImageFile
+from Globals import InitializeClass
 from OFS.Folder import Folder
-from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from Products.LocalFS.LocalFS import manage_addLocalFS
+
 #Product imports
-from Products.NaayaCore.constants import *
-import tinyMCEUtils
+from Products.NaayaCore.constants import ID_EDITORTOOL, TITLE_EDITORTOOL, \
+METATYPE_EDITORTOOL
+from Products.NaayaCore.EditorTool.utilities import parse_css_margin, \
+parse_css_border_width, strip_unit
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from naaya.core.StaticServe import StaticServeFromZip
 
 
 def manage_addEditorTool(self, REQUEST=None):
-    """
-    ZMI method that creates an object of this type.
-    """
+    """ ZMI method that creates an EditorTool object instance into the portal"""
     ob = EditorTool(ID_EDITORTOOL, TITLE_EDITORTOOL)
     self._setObject(ID_EDITORTOOL, ob)
-    self._getOb(ID_EDITORTOOL).loadDefaultData()
     if REQUEST:
         return self.manage_main(self, REQUEST, update_menu=1)
 
-class EditorTool(Folder):
+
+def loadConfig(section='tinymce'):
+    """ Loads the default configuration from *config.ini*.
+    The configuration is loaded from the *config.ini* file.
+    `section` Section to load from config.ini. You can use config.ini
+    to create multiple templates for the editor and name them differently.
+    Returns an dictionary with tinymce configuration options
     """
-    Class that implements the tool.
+    ret = {}
+    config = ConfigParser()
+    config.read(join(dirname(__file__), 'config.ini'))
+    if config.has_section(section):
+        for option in config.options(section):
+            ret[option.strip()] = config.get(section, option).strip()
+    return ret
+
+configuration = loadConfig() #Global configuration loaded from *config.ini*
+
+
+class EditorTool(Folder):
+    """ **EditorTool** installs itself into the portal and provides rich text 
+    editing capabilities attachable via to HTML input elements such as textarea.
+    It uses *TinyMCE* (http://tinymce.moxiecode.com/) editor.
+    Please see README.txt located into this module for notes
     """
 
     meta_type = METATYPE_EDITORTOOL
     icon = 'misc_/NaayaCore/EditorTool.gif'
-
-    manage_options = (
-        Folder.manage_options
-    )
-
+    manage_options = (Folder.manage_options)
     security = ClassSecurityInfo()
 
+
     def __init__(self, id, title):
-        """
-        Initialize variables.
-        """
+        """ Initialize variables"""
         self.id = id
         self.title = title
-        self.configuration = {}
+
 
     def getConfiguration(self):
-        """ return configuration """
-        return self.configuration
+        """ Return current default configuration """
+        return configuration
 
-    def _getTinyMCEInstance(self):
-        """ return the TinyMCE instance object we intend to use """
-        return self._getOb('tinymce')
-
-    def _getTinyMCELanguages(self):
-        """ return the list of languages that TinyMCE could handle """
-        return ['da', 'en', 'es', 'fr', 'it', 'ro', 'ru']
-
-    def _getTinyMCEDefaultLang(self):
-        return 'en'
-
-    def _getTinyMCELang(self, lang=None):
-        """Return the language that should be used by TinyMCE.
-
-            If the desired language is unsupported by TinyMCE,
-            another language will be chosen.
-            @param lang: language to use; default is the language of the portal
-        """
-        if lang is None: lang = self.gl_get_selected_language()
-        if lang not in self._getTinyMCELanguages():
-            lang = self._getTinyMCEDefaultLang()
-        return lang
 
     def includeLibs(self, lang=None):
-        """Return HTML code that includes the TinyMCE JavaScript libraries.
-
-            @param lang: language to use; default is the language of the portal
+        """ Returns HTML code that includes required JavaScript libraries.
+        Parameters:
+            `lang` 
+                **Not used**
         """
-        tinymce = self._getTinyMCEInstance()
-        doc_url = self.REQUEST['URLPATH1'].lstrip('/')
-        js = []
-        jsappend = js.append
-        jsappend('<script type="text/javascript" src="%s/%s"></script>' % (tinymce.jscripts.tiny_mce.absolute_url(), 'tiny_mce_gzip.js'))
-        jsappend('<script type="text/javascript">')
-        jsappend('tinyMCE_GZ.init({')
-        jsappend('page_name: "%s/getTinyMCEJavaScript",' % self.absolute_url())
-        jsappend('plugins: "%s",' %  ','.join(self.configuration['plugins']))
-        jsappend('themes: "%s",' %  ','.join(self.configuration['theme']))
-        jsappend('languages: "%s"' % self._getTinyMCELang(lang))
-        jsappend('});')
-        jsappend('</script>')
-        jsappend('<script type="text/javascript">')
-        jsappend('nyFileBrowserCallBack = getNyFileBrowserCallBack("%s")' % (doc_url,))
-        jsappend('</script>')
-        return '\n'.join(js)
+        return '<script language="JavaScript" \
+src="%(parent_url)s/tinymce/jscripts/tiny_mce/jquery.tinymce.js"></script>'\
+    % {'parent_url': self.absolute_url()}
+
 
     def render(self, element, lang=None, image_support=False, extra_options={}):
         """Return the HTML necessary to run the TinyMCE.
-
-            @param element: name of the HTML element that will be converted to TinyMCE;
-                            the element can be any kind, e.g. textarea or div
-            @param lang: language to use; default is the language of the portal
-            @param image_support: if True, the user can add images
+        Parameters:
+            `element` 
+                `id` of HTML element that editor will is attached to.
+            `lang` 
+                **Not used** 
+            `image_support` 
+                **No longer used** In order to disable images. 
+                Use extra_options, see below. 
+            `extra_options` 
+                Extra options you can pass to tinyMCE editor. See `config.ini` 
+                for further reference. You can pass any option from there
+                to override default one. 
+                `extra_options['config_template']` Loads one of the defined 
+                templates from `config.ini`. Default is 'tinymce'. 
+                Also you can use 'tinymce_noimage' to disable image insertion.
         """
-        doc_url = self.REQUEST['URLPATH1'].lstrip('/')
-        lang = self._getTinyMCELang(lang)
-        params = []
-        params.append('elements:"%s"' % element)
-        params.append('language:"%s"' % lang)
-        params.append('server_url:"%s"' % self.REQUEST.get('BASE0', ''))
-        if self.isRTL(lang):
-            params.append('directionality:"rtl"')
-        # TODO for Python 2.4: use generator comprehension
-        if image_support:
-            params.extend([('%s: "%s"' % (k, ','.join(v))) for k, v in self.configuration.items()])
+        doc_url = self.REQUEST['URLPATH1']
+        if extra_options.has_key('config_template'):
+            template = extra_options['config_template']
+            cfg = loadConfig(template)
         else:
-            params.extend(['%s: "%s"' % (k, ','.join(v)) for k, v in self.configuration.items()
-                                                                    if k != "theme_advanced_buttons2"])
-            L = list(self.configuration['theme_advanced_buttons2'])
-            L.remove('image')
-            params.append('theme_advanced_buttons2: "%s"' % ','.join(L))
-        doc = self.restrictedTraverse(doc_url)
-        if not doc.imageContainer.relative:
-            params.append('document_base_url:"%s/"' % self.getSitePath())
-        params.extend([('%s: "%s"' % (k, ','.join(v))) for k, v in extra_options.items()])
-        return """<script type="text/javascript">tinyMCE.init({%s});</script>""" % (',\n'.join(params), )
+            cfg = copy.copy(configuration)
+        cfg.update({
+            'select_image_url' : '%s/select_image?document=%s' \
+                    % (self.absolute_url(), doc_url),
+            'edit_image_url' : '%s/prepare_image?mode=edit&document=%s' % (self.absolute_url(), doc_url),
+            'link_popup_url' : '%s/select_link' % self.absolute_url(),
+            'element_id': element,
+            'script_url' : '%s/tinymce/jscripts/tiny_mce/tiny_mce.js' \
+                % self.absolute_url(),
+        })
+        cfg.update(extra_options)
+        return "<script type=\"text/javascript\">\
+$().ready(function() {$('#%s').tinymce(%s);})\
+</script>" % (element, json.dumps(cfg, indent=2))
 
-    security.declarePublic('getTinyMCEJavaScript')
-    def getTinyMCEJavaScript(self, REQUEST):
-        """Return all the TinyMCE JavaScript code.
 
-            The purpose of this function is to minimize the number of HTTP requests.
-            It's needed by tinyMCE_GZ.
+    def get_preview_img(self, REQUEST=None):
+        """ Compute src attribute for the preview image. Uploads image if
+        requested.
+        Returns the URL to the image.
         """
-        isJS = REQUEST.get('js', '') == 'true'
-        languages = REQUEST['languages'].split(',')
-        themes = REQUEST['themes'].split(',')
-        plugins = REQUEST['plugins'].split(',')
-        custom_files = [join('Naaya', 'file_browser_callback.js'),
-                        join('Naaya', 'urlconverter_callback.js'),
-                        join('Naaya', 'jscripts', 'select_relative_link.js'),
-                        join('Naaya', 'jscripts', 'select_image.js')]
-        content = tinyMCEUtils.getCompressedJavaScript(isJS,
-                                                       languages,
-                                                       themes,
-                                                       plugins,
-                                                       custom_files)
-        REQUEST.RESPONSE.enableHTTPCompression(REQUEST)
-        REQUEST.RESPONSE.setHeader('Content-type', 'application/x-javascript')
-        REQUEST.RESPONSE.write(content)
+        url = ''
+        if REQUEST.form.has_key('url'):
+            url = REQUEST.form['url']
+        if REQUEST.form.has_key('mode'):
+            mode = REQUEST.form['mode']
+            if mode == 'upload':
+                url = self._upload_image(REQUEST)
 
-    security.declarePrivate('loadDefaultData')
-    def loadDefaultData(self):
-        """Loads the default TinyMCE configuration.
+        if not url.startswith('http'):
+            url = '%s/%s' % (self.getEnclosingDocument(REQUEST).absolute_url(), url)
+        return url
 
-            The configuration is loaded from the "config.ini" file.
+
+    def _upload_image(self, REQUEST=None):
+        """ Upload an image into the document container. """
+        if REQUEST:
+            if REQUEST.form.has_key('file'):
+                file = REQUEST.form['file']
+                document = self.getEnclosingDocument(REQUEST)
+                if document:
+                    imageContainer = document.imageContainer;
+                    uploaded = imageContainer.uploadImage(file, None)
+                    return uploaded.absolute_url()
+        else:
+            print 'no image to upload'
+
+
+    def enumerateImages(self, source, REQUEST=None):
+        """ Retrieve the list of images depending on the source.
+        Return a list of ``Image`` objects
         """
-        manage_addLocalFS(self, 'tinymce', '', join(dirname(__file__), 'tinymce'))
+        ret = []
+        if source == 'document':
+            document = self.getEnclosingDocument(REQUEST)
+            if document:
+                ret = document.imageContainer.getImages()
+        if source == 'website':
+            site = self.getSite()
+            ret = site.imageContainer.getImages()
+        if source == 'album':
+            album_url = REQUEST.form['album']
+            album = self.restrictedTraverse(album_url)
+            ret = album.getObjects()
+        return ret
 
-        config = ConfigParser()
-        config.read(join(dirname(__file__), 'config.ini'))
-        for section in config.sections():
-            for option in config.options(section):
-                self.configuration[option] = [i.strip() for i in config.get(section, option).split(',')]
 
-    security.declareProtected(view_management_screens, 'index_html')
-    index_html = PageTemplateFile('zpt/editor_test', globals())
+    def enumeratePhotoAlbums(self, REQUEST=None):
+        """
+        Lookup photo galleries from the site.
+        Return a list with all photo galleries available within the site.
+        """
+        ctool = self.getCatalogTool()
+        return [x.getObject() \
+            for x in ctool.search({'meta_type' : 'Naaya Photo Folder'})]
 
-    security.declarePublic('style_css')
-    style_css = PageTemplateFile('zpt/editor_css', globals())
+
+    def getEnclosingDocument(self, REQUEST):
+        """
+        Return the enclosing document where this editor tool 
+        instance is located.
+        `REQUEST` 
+            Http request that **must** contain the 'document' parameter
+        Return enclosing document object
+        """
+        if REQUEST.form.has_key('document'):
+            document_url = REQUEST.form['document']
+            return self.restrictedTraverse(document_url)
+        return None
+
+
+    def prepare_image_styles(self, REQUEST=None):
+        """ Parse `REQUEST` and retrieve image attributes to set them into
+        preview. This method is called when user selects the image into editor
+        and then presses image button to adjust its settings.
+        Return an dictionary with found attributes such as border, margin etc.
+        """
+        ret = {
+               'title' : self.get_request_param(REQUEST, 'title'),
+               'alignment' : self.get_request_param(REQUEST, 'left'),
+               'css_alignment' : '', 'margin' : '', 'border_preview' : '',
+               'width_preview' : '', 'height_preview' : '', 'border' : '',
+               'width' : '', 'height' : '', 'h_margin' : '', 'v_margin' : '',
+        }
+        if REQUEST.form.has_key('alignment'):
+            alignment = REQUEST.form['alignment']
+            if alignment:
+                if alignment == 'left' or alignment == 'right':
+                    ret['css_alignment'] = 'float: %s' % alignment
+                else:
+                    ret['css_alignment'] = 'vertical-align: %s' % alignment
+        if REQUEST.form.has_key('margin'):
+            ret['margin'] = 'margin: %s' % REQUEST.form['margin']
+        if REQUEST.form.has_key('border'):
+            ret['border_preview'] = 'border: %s' % REQUEST.form['border']
+            ret['border'] = parse_css_border_width(REQUEST.form['border'])
+        if REQUEST.form.has_key('width'):
+            ret['width_preview'] = 'width: %s' % REQUEST.form['width']
+            ret['width'] = strip_unit(REQUEST.form['width'])
+        if REQUEST.form.has_key('height'):
+            ret['height_preview'] = 'height: %s' % REQUEST.form['height']
+            ret['height'] = strip_unit(REQUEST.form['height'])
+        if REQUEST and REQUEST.form.has_key('margin'):
+            margins = parse_css_margin(REQUEST.form['margin'])
+            ret['h_margin'] = margins[0]
+            ret['v_margin'] = margins[1]
+        return ret
+
+    def get_request_param(self, REQUEST, name, default=''):
+        """ Safely retrieve an parameter from request
+        Parameters:
+            `REQUEST`
+                Http request
+            `name`
+                Name of the parameter
+            `default`
+                Default value to return if parameter not found. If not specified
+                is empty string.
+        Return the parameter or default if none found. 
+        """
+        if self.REQUEST.has_key(name):
+            return self.REQUEST.form[name]
+        return default
+
+
+    def isImageContainer(self, document):
+        """ Verifies document if is image container or inherits the 
+        imageContainer of NySite.
+        Parameters:
+            `document` 
+                Document to be checked.
+        Return ``true`` if document has its own instance of `imageContainer`
+        """
+        return self.getSite().imageContainer != document.imageContainer
+
+
+    image_js = ImageFile('www/image.js', globals())
+    link_js = ImageFile('www/link.js', globals())
+    image_css = ImageFile('www/image.css', globals())
+    tinymce = \
+        StaticServeFromZip('tinymce', 'www/tinymce_3_2_7_jquery.zip', globals())
+    tinymce_naaya \
+        = StaticServeFromZip('Naaya', 'www/tinymce_naaya.zip', globals())
+    select_image = PageTemplateFile('zpt/select_image', globals())
+    prepare_image = PageTemplateFile('zpt/prepare_image', globals())
+    select_link = PageTemplateFile('zpt/select_link', globals())
 
 InitializeClass(EditorTool)
