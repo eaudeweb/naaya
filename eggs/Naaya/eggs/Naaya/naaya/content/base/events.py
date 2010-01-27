55d2ef94275ac349dfaa7cbcf1271f2eae3bb841
Alex Morega <alex@grep.ro>
2010-01-27 13:03:38 +0000

    refactor "object moved" event; don't fail when site path changed

--- eggs/Naaya/eggs/Naaya/naaya/content/base/events.py
+++ eggs/Naaya/eggs/Naaya/naaya/content/base/events.py
@@ -30,27 +30,35 @@ class NyContentObjectMovedEvent(object):
     """ Naaya content object has been moved/renamed """
     implements(INyContentObjectMovedEvent)
 
-    def __init__(self, obj, event):
-        new_pp = obj.getPhysicalPath()
-        new_parent_pp = event.newParent.getPhysicalPath()
-        old_parent_pp = event.oldParent.getPhysicalPath()
-        assert new_pp[:len(new_parent_pp)] == new_parent_pp
-        assert new_pp[len(new_parent_pp)] == event.newName
-        old_pp = (old_parent_pp + (event.oldName,) +
-                  new_pp[len(new_parent_pp)+1:])
-
-        site = obj.getSite()
-        site_pp = site.getPhysicalPath()
-        assert old_pp[:len(site_pp)] == site_pp
-
-        self.old_site_path = '/'.join(old_pp[len(site_pp):])
-        self.new_site_path = '/'.join(new_pp[len(site_pp):])
-        self.context = obj
-        self.zope_event = event
+    def __init__(self, context, zope_event, old_site_path, new_site_path):
+        self.context = context
+        self.zope_event = zope_event
+        self.old_site_path = old_site_path
+        self.new_site_path = new_site_path
 
 @adapter(INyContentObject, IObjectMovedEvent)
 def notify_content_object_moved(obj, event):
     if event.oldParent is None or event.newParent is None:
         return # this is actually an added/removed event; skip it...
 
-    notify(NyContentObjectMovedEvent(obj, event))
+
+    new_pp = obj.getPhysicalPath()
+    new_parent_pp = event.newParent.getPhysicalPath()
+    old_parent_pp = event.oldParent.getPhysicalPath()
+    assert new_pp[:len(new_parent_pp)] == new_parent_pp
+    assert new_pp[len(new_parent_pp)] == event.newName
+    old_pp = (old_parent_pp + (event.oldName,) +
+              new_pp[len(new_parent_pp)+1:])
+
+    site = obj.getSite()
+    site_pp = site.getPhysicalPath()
+    if old_pp[:len(site_pp)] != site_pp:
+        # old site path and new site path are different
+        # object moved between sites, or the site itself is being moved
+        # ignore this event
+        return
+
+    old_site_path = '/'.join(old_pp[len(site_pp):])
+    new_site_path = '/'.join(new_pp[len(site_pp):])
+
+    notify(NyContentObjectMovedEvent(obj, event, old_site_path, new_site_path))
