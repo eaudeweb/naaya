2e0881fe06fea1f2bbd03d5e3b33a0c319ff555e
David Batranu <david.batranu@eaudeweb.ro>
2010-02-12 09:51:55 +0000

    Ticket #165 - Directory
        - Adding `Details` page.
    
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.groupware@14117 a0171fae-0946-0410-966d-b1775066cd26

--- naaya/groupware/directory.py
+++ naaya/groupware/directory.py
@@ -29,6 +29,9 @@ class Directory(Implicit, Item):
         return self.index_html(user_list=user_list)
 
     def get_users(self):
+        if not hasattr(self, '_v_external_user_cache'):
+            self._v_external_user_cache = {}
+        cache = self._v_external_user_cache
         users = []
 
         portal = self.getSite()
@@ -38,6 +41,7 @@ class Directory(Implicit, Item):
         for source in acl_tool.getSources():
             if not isinstance(source, plugLDAPUserFolder):
                 continue
+
             acl_folder = source.getUserFolder()
             individual_users = source.getUsersRoles(acl_folder).keys()
 
@@ -45,12 +49,20 @@ class Directory(Implicit, Item):
             for group_id in source.get_groups_roles_map().keys():
                 group_users.extend(source.group_member_ids(group_id))
 
-            for userid in (individual_users + group_users):
-                user_dict = source._get_user_by_uid(userid,
-                                                    source.getUserFolder())
+            combined_users = individual_users + group_users
+            for userid in combined_users:
+
+                if cache.get(userid, {}):
+                    user_dict = cache[userid]
+                else:
+                    user_dict = source._get_user_by_uid(userid,
+                                                        source.getUserFolder())
+                    cache[userid] = user_dict
+
                 user_roles = self.get_external_user_roles(source, userid)
                 user_dict['access_level'] = \
                          self.get_user_access_level(user_roles)
+
                 external_users.append(user_dict)
 
         for user in local_users:
@@ -61,6 +73,8 @@ class Directory(Implicit, Item):
                 'lastname': user.lastname,
                 'email': user.email,
                 'access_level': self.get_user_access_level(user_roles),
+                'organisation': 'N/A',
+                'postal_address': 'N/A',
             })
 
         for user in external_users:
@@ -70,7 +84,10 @@ class Directory(Implicit, Item):
                 'lastname': user['sn'],
                 'email': user['mail'],
                 'access_level': user['access_level'],
+                'organisation': user.get('o', 'N/A'),
+                'postal_address': user.get('postalAddress', 'N/A'),
             })
+        self._v_user_cache = users
         return users
 
     def get_local_user_roles(self, userob):
@@ -79,8 +96,12 @@ class Directory(Implicit, Item):
         return acl_tool.getUserRoles(userob)
 
     def get_external_user_roles(self, source, userid):
+        if not hasattr(self, '_v_individual_users_roles'):
+            self._v_individual_users_roles = None
         source_uf = source.getUserFolder()
-        individual_ldap_users = source.getUsersRoles(source_uf)
+        if not self._v_individual_users_roles:
+            self._v_individual_users_roles = source.getUsersRoles(source_uf)
+        individual_ldap_users = self._v_individual_users_roles
         group_to_role_mapping = source.get_groups_roles_map()
         site_id = self.getSite().getId()
         roles = []
@@ -96,6 +117,14 @@ class Directory(Implicit, Item):
                     roles.append(role)
         return roles
 
+    def get_user_info(self, userid):
+        if not hasattr(self, '_v_user_cache'):
+            self.get_users()
+        for user in self._v_user_cache:
+            if user['userid'] != userid:
+                continue
+            return user
+
     def get_user_access_level(self, roles):
         access_level = 'N/A'
         if 'Contributor' in roles:
@@ -109,5 +138,6 @@ class Directory(Implicit, Item):
         return access_level
 
     index_html = PageTemplateFile('zpt/directory_index', globals())
+    user_details = PageTemplateFile('zpt/directory_user_details', globals())
 
 InitializeClass(Directory)
