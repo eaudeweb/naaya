9e4ccc6209375a492deae1ed6c2f03c99bbcaf26
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2010-02-16 12:41:33 +0000

    "filter events" portlet added
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content-sdo@14149 a0171fae-0946-0410-966d-b1775066cd26

--- naaya/content/infofolder/portlets.py
+++ naaya/content/infofolder/portlets.py
@@ -37,4 +37,19 @@ class InfoFolderSearchPortlet(object):
     template = NaayaPageTemplateFile('zpt/infofolder_search_portlet', globals(),
                                      'naaya.content-sdo.infofolder.infofolder_search_portlet')
 
+class InfoFolderEventsPortlet(object):
+    interface.implements(INyPortlet)
+    component.adapts(INySite)
+
+    title = 'sd-online events filter'
+
+    def __init__(self, site):
+        self.site = site
+
+    def __call__(self, context, position):
+        macro = self.site.getPortletsTool()._get_macro(position)
+        return self.template.__of__(context)(macro=macro)
+
+    template = NaayaPageTemplateFile('zpt/events_filter', globals(),
+                                     'naaya.content-sdo.infofolder.events_filter')
 
