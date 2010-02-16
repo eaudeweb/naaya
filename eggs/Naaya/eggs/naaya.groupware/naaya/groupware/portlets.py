234a19518901a6038f0a23aa5d970ebed33e8900
David Batranu <david.batranu@eaudeweb.ro>
2010-02-16 14:01:45 +0000

    Ticket #165 - Layout
        - adding `portlet_navigation` for old blue-bar links;
        - using EIONET dropdowns in the blue bar;
        - updating `Help` link.

---
+++ eggs/Naaya/eggs/naaya.groupware/naaya/groupware/portlets.py
@@ -0,0 +1,22 @@
+from zope import interface
+from zope import component
+
+from Products.Naaya.interfaces import INySite
+from Products.NaayaCore.PortletsTool.interfaces import INyPortlet
+from Products.NaayaCore.FormsTool.NaayaTemplate import NaayaPageTemplateFile
+
+class NavigationPortlet(object):
+    interface.implements(INyPortlet)
+    component.adapts(INySite)
+
+    title = 'Navigation'
+
+    def __init__(self, site):
+        self.site = site
+
+    def __call__(self, context, position):
+        macro = self.site.getPortletsTool()._get_macro(position)
+        return self.template.__of__(context)(macro=macro)
+
+    template = NaayaPageTemplateFile('zpt/navigation_portlet', globals(),
+                                     'naaya.groupware.navigation_portlet')
