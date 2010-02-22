80df777334b32f7e6dcc436ccf0f674f67c558a2
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2010-02-22 14:31:10 +0000

    bugfix with geolocation submit_site_portlet import releasedate
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content-sdo@14176 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content-sdo/naaya/content/infofolder/portlets.py
+++ eggs/naaya.content-sdo/naaya/content/infofolder/portlets.py
@@ -53,3 +53,19 @@ class InfoFolderEventsPortlet(object):
     template = NaayaPageTemplateFile('zpt/events_filter', globals(),
                                      'naaya.content-sdo.infofolder.events_filter')
 
+class InfoFolderSubmitSitePortlet(object):
+    interface.implements(INyPortlet)
+    component.adapts(INySite)
+
+    title = 'sd-online submit your site'
+
+    def __init__(self, site):
+        self.site = site
+
+    def __call__(self, context, position):
+        macro = self.site.getPortletsTool()._get_macro(position)
+        return self.template.__of__(context)(macro=macro)
+
+    template = NaayaPageTemplateFile('zpt/submit_site_portlet', globals(),
+                                     'naaya.content-sdo.infofolder.submit_site_portlet')
+
