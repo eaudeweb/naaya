4468b2fbeb1be67582bd3746122f86e12746c14e
Alex Morega <alex@grep.ro>
2010-03-01 10:09:48 +0000

    cleanup mail testing code

--- eggs/Naaya/Products/NaayaCore/EmailTool/tests/testEmail.py
+++ eggs/Naaya/Products/NaayaCore/EmailTool/tests/testEmail.py
@@ -4,33 +4,38 @@ from Products.Naaya.tests.NaayaTestCase import FunctionalTestCase
 class EmailTestCase(FunctionalTestCase):
     def test_mail(self):
         self.layer.mail_divert.clear_log()
-        
-        self.portal.getEmailTool().sendEmail('test_content', 'test_to', 'test_from', 'test_subject')
-        
+
+        self.portal.getEmailTool().sendEmail('test_content',
+            'test_to@example.com', 'test_from@example.com', 'test_subject')
+
         smtp_log = self.layer.mail_divert.log
-        
+
         self.failUnlessEqual(len(smtp_log), 3)
         self.failUnlessEqual(smtp_log[0][0], 'init')
         self.failUnlessEqual(smtp_log[1][0], 'sendmail')
         self.failUnlessEqual(smtp_log[2][0], 'quit')
-        
-        self.failUnlessEqual(smtp_log[1][1]['from'], 'test_from')
-        self.failUnlessEqual(smtp_log[1][1]['to'], ['test_to'])
+
+        self.failUnlessEqual(smtp_log[1][1]['from'], 'test_from@example.com')
+        self.failUnlessEqual(smtp_log[1][1]['to'], ['test_to@example.com'])
         self.failUnless('Subject: test_subject\n' in smtp_log[1][1]['message'])
         self.failUnless('test_content' in smtp_log[1][1]['message'])
-        self.failUnless('Content-Type: text/plain;' in smtp_log[1][1]['message'])
-    
+        self.failUnless('Content-Type: text/plain;' in
+                        smtp_log[1][1]['message'])
+
     def test_mail_from(self):
         self.layer.mail_divert.clear_log()
-        
-        self.portal.getEmailTool().sendEmail('test_content', 'test_to_1, test_to_2', 'test_from', 'test_subject')
-        
+
+        self.portal.getEmailTool().sendEmail('test_content',
+            'test_to_1@example.com, test_to_2@example.com',
+            'test_from@example.com', 'test_subject')
+
         smtp_log = self.layer.mail_divert.log
+        self.assertEqual(len(smtp_log), 3)
         to = smtp_log[1][1]['to']
-        
+
         self.failUnlessEqual(len(to), 2)
-        self.failUnless('test_to_1' in to)
-        self.failUnless('test_to_2' in to)
+        self.failUnless('test_to_1@example.com' in to)
+        self.failUnless('test_to_2@example.com' in to)
 
 def test_suite():
     suite = TestSuite()
