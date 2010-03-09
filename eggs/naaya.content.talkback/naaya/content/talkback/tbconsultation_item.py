15f18f5fa3fb1ed1cd5352d6af80b6548da98760
Alex Morega <alex@grep.ro>
2010-03-09 15:16:24 +0000

    various small fixes
    - proper footer on edit page
    - send non-list parameters when invoking editor
    - properly close <a id=...> tag
    - strip message in test code

--- eggs/naaya.content.talkback/naaya/content/talkback/tbconsultation_item.py
+++ eggs/naaya.content.talkback/naaya/content/talkback/tbconsultation_item.py
@@ -568,16 +568,16 @@ class NyTalkBackConsultation(NyRoleManager,
     security.declareProtected(view, 'custom_editor')
     def custom_editor(self, editor_tool, lang, dom_id):
         extra_options = {
-            'content_css': [self.absolute_url() +
-                            '/misc_/NaayaContent/tb-editor.css'],
-            'theme_advanced_buttons1': [
+            'content_css': self.absolute_url() +
+                            '/misc_/NaayaContent/tb-editor.css',
+            'theme_advanced_buttons1':
                 'bold,italic,underline,strikethrough,sub,sup,forecolor,'
                     'backcolor,removeformat,separator,'
                 'bullist,numlist,separator,'
                 'justifyleft,justifycenter,justifyright,justifyfull,separator,'
                 'link,unlink,hr,image,separator,'
-                'pastetext,pasteword,cleanup,code,help'],
-            'theme_advanced_buttons2': [''],
+                'pastetext,pasteword,cleanup,code,help',
+            'theme_advanced_buttons2': '',
         }
         return editor_tool.render(dom_id, lang, image_support=True,
                                   extra_options=extra_options)
