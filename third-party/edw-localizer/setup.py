427cb80d91be173fdafac36abe7a0179c79c50bb
Alex Morega <alex@grep.ro>
2010-05-28 15:16:37 +0000

    repackaging of localizer

---
+++ third-party/edw-localizer/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='edw-localizer',
+      version='1.1.1',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      packages=find_packages(),
+      zip_safe=False,
+)
