0b58ee00be0b8aefd4579a6bc61602022263b625
Alex Morega <alex@grep.ro>
2010-05-31 09:30:53 +0000

    forgot to update setup.py

--- eggs/Naaya/third-party/edw-localizer/setup.py
+++ eggs/Naaya/third-party/edw-localizer/setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='edw-localizer',
-      version='1.1.1',
+      version='1.2.2',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
+      include_package_data=True,
       zip_safe=False,
 )
