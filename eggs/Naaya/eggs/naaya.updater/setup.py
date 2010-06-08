6ed03f17d8adcf9f008426a196d37b5ad5d9218c
Alex Morega <alex@grep.ro>
2010-06-08 09:02:51 +0000

    skeleton for naaya.updater package

---
+++ eggs/Naaya/eggs/naaya.updater/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='naaya.updater',
+      version='1.0dev',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+)
