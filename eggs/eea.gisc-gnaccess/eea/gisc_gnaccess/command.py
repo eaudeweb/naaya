5feba6cad1ca4530844b9358ef18bb9b65abbfd0
andredor <andredor@eaudeweb.ro>
2010-06-08 12:37:27 +0000

    Generates JSON files for jstree to display.

--- eggs/eea.gisc-gnaccess/eea/gisc_gnaccess/command.py
+++ eggs/eea.gisc-gnaccess/eea/gisc_gnaccess/command.py
@@ -14,12 +14,23 @@ from xml_parser import get_title, get_points_of_contact, get_distribution_info
 from unicode_csv import UnicodeWriter
 
 def config_directive(declaration):
-    declaration.expect(dict, names=['report_points_of_contact', 'report_distribution_info'])
+    declaration.expect(dict, names=['report_points_of_contact', 'report_distribution_info',
+                                     'organisation_icon', 'document_icon', 'role_icon', 'email_icon', 'url_icon'])
     report_points_of_contact = declaration.string('report_points_of_contact', None)
     report_distribution_info = declaration.string('report_distribution_info', None)
+    organisation_icon = declaration.string('organisation_icon', None)
+    document_icon = declaration.string('document_icon', None)
+    role_icon = declaration.string('role_icon', None)
+    email_icon = declaration.string('email_icon', None)
+    url_icon = declaration.string('url_icon', None)
     def callback():
         declaration.registry['report_points_of_contact'] = report_points_of_contact
         declaration.registry['report_distribution_info'] = report_distribution_info
+        declaration.registry['organisation_icon'] = organisation_icon
+        declaration.registry['document_icon'] = document_icon
+        declaration.registry['role_icon'] = role_icon
+        declaration.registry['email_icon'] = email_icon
+        declaration.registry['url_icon'] = url_icon
     declaration.action(callback)
 
 def print_settings(settings, level=0):
@@ -69,40 +80,68 @@ def dump_points_of_contact_report(context, db_statement):
     gen = gen_metadata(db_statement)
     metadatas = [m for m in gen if m['isTemplate'] == 'n']
     organisations = {}
+    organisations2 = {}
     for m in metadatas:
         id = str(m['id'])
         title = get_title(m['data'])
         contacts = get_points_of_contact(m['data'])
         for c in contacts:
-            if c['organisation'] not in organisations:
-                organisations[c['organisation']] = []
-            organisations[c['organisation']].append({
-                'id': id,
-                'title': title,
-                'email': c['email'],
-            })
+            org_name = c['organisation']
+            if type(c['email']) == type([]):
+                emails = c['email']
+            else:
+                emails = [c['email']]
+            role = c.get('role', '')
+            
+            if org_name not in organisations:
+                organisations[org_name] = {}
+            if title not in organisations[org_name]:
+                organisations[org_name][title] = {}
+            if role not in organisations[org_name][title]:
+                organisations[org_name][title][role] = []
+            organisations[org_name][title][role].extend(emails)
+    
+    organisation_icon = context.registry['organisation_icon'] 
+    document_icon = context.registry['document_icon']
+    role_icon = context.registry['role_icon']
+    email_icon = context.registry['email_icon']
+    jstree = []
+    for org, v in organisations.iteritems():
+        doc_tree = []
+        for title, v2 in v.iteritems():
+            role_tree = []
+            for role, emails in v2.iteritems():
+                email_tree = []
+                for email in emails:
+                    email_tree.append({'data': {'title': email, 'icon': email_icon, 'attributes': {'href': 'mailto:'+email}}})
+                role_tree.append({'data': {'title': role, 'icon': role_icon}, 'children': email_tree})
+            if len(role_tree) == 1 and role_tree[0]['data']['title'] == '':
+                email_tree = role_tree[0]['children']
+                doc_tree.append({'data': {'title': title, 'icon': document_icon}, 'children': email_tree})
+            else:
+                doc_tree.append({'data': {'title': title, 'icon': document_icon}, 'children': role_tree})
+        jstree.append({'data': {'title': org, 'icon': organisation_icon}, 'state': 'open', 'children': doc_tree})
+
     with output_file(context, 'report_points_of_contact') as f:
-        writer = UnicodeWriter(f)
-        writer.writerow(['Organisation', 'Id', 'Title', 'Email'])
-        for k, v in organisations.iteritems():
-            o = v[0]
-            id, title, email = o['id'], o['title'], o['email']
-            writer.writerow([k, id, title, email])
-            for o in v[1:]:
-                id, title, email = o['id'], o['title'], o['email']
-                writer.writerow(['', id, title, email])
+        f.write(json.dumps(jstree))
 
 def dump_distribution_info_report(context, db_statement):
     gen = gen_metadata(db_statement)
     metadatas = [m for m in gen if m['isTemplate'] == 'n']
+
+    document_icon = context.registry['document_icon']
+    url_icon = context.registry['url_icon']   
+    jstree = []
+    for m in metadatas:
+        title = get_title(m['data'])
+        url_list = get_distribution_info(m['data'])
+        url_tree = []
+        for url in url_list:
+            url_tree.append({'data': {'title': url, 'icon': url_icon, 'attributes': {'href': url}}})
+        jstree.append({'data': {'title': title, 'icon': document_icon}, 'children': url_tree})
+
     with output_file(context, 'report_distribution_info') as f:
-        writer = UnicodeWriter(f)
-        writer.writerow(['Id', 'Title', 'Distibution url'])
-        for m in metadatas:
-            id = str(m['id'])
-            title = get_title(m['data'])
-            url = get_distribution_info(m['data'])
-            writer.writerow([id, title, url])
+        f.write(json.dumps(jstree))
 
 def main():
     usage = "usage: %prog path/to/config.yaml <print_settings|dump_harvesting|report_points_of_contact|report_distribution_info>"
