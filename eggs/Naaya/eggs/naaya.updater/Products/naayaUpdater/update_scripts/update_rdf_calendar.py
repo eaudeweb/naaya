b6838818333ece1ceb7c0cf5dd8398d5b46181aa
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-06-21 10:03:58 +0000

    Delete resource_interval index in favor of start_date and end_date

--- eggs/Naaya/eggs/naaya.updater/Products/naayaUpdater/update_scripts/update_rdf_calendar.py
+++ eggs/Naaya/eggs/naaya.updater/Products/naayaUpdater/update_scripts/update_rdf_calendar.py
@@ -16,8 +16,8 @@ except:
 class UpdateRDFCalendar(UpdateScript):
     id = 'update_rdf_calendar'
     title = 'Update RDF Calendar'
-    description = 'Replaces local RDFSummary objects with Python Script, creat\
-                  e DateRangeIndex in portal_catalog'
+    description = ('Replaces local RDFSummary objects with Python Script, crea'
+                  'te DateIndexes in portal_catalog')
     authors = ['Alexandru Plugaru', ]
     security = ClassSecurityInfo()
 
@@ -25,22 +25,27 @@ class UpdateRDFCalendar(UpdateScript):
     def _update(self, portal):
         rdf_calendars = portal.objectValues([RDFCalendar.meta_type, ])
         if rdf_calendar_available and len(rdf_calendars):
-            #adding range index to catalog
-            class Empty(object):
+            try: #deleteing DateRangeIndex if availalble
+                portal.getCatalogTool().delIndex('resource_interval')
+                self.log.debug('Deleted resource_interval (DateRangeIndex) fro'
+                               'm portal_catalog')
+            except:
                 pass
-            extra = Empty() #Extra has to be an object.. see DateRangeIndex
-            extra.since_field = 'start_date'
-            extra.until_field = 'end_date'
-            portal.getCatalogTool().addIndex('resource_interval',
-                                             'DateRangeIndex', extra=extra)
-            self.log.debug('Added resource_interval (DateRangeIndex) to portal_catalog')
+            #adding start_end and end_date indexes
+            portal.getCatalogTool().addIndex('start_date', 'DateIndex')
+            self.log.debug('Added start_date (DateIndex) to portal_catalog')
+
+            portal.getCatalogTool().addIndex('end_date', 'DateIndex')
+            self.log.debug('Added end_date (DateIndex) to portal_catalog')
+
             portal_path = self.get_portal_path(portal)
             script_content = open(portal_path +
                     '/skel/others/local_events.py', 'r').read()
             for rdfcalendar_ob in rdf_calendars:
                 try:
                     rdfcalendar_ob.manage_delObjects(['self_events', ])
-                    self.log.debug('Removed self_events (RDFSummary) to RDFCalendar')
+                    self.log.debug('Removed self_events (RDFSummary) to\
+                                   RDFCalendar')
                 except:
                     break
                 #adding local_events Script (Python) from Naaya skel
@@ -48,5 +53,6 @@ class UpdateRDFCalendar(UpdateScript):
                 local_events_ob = rdfcalendar_ob._getOb('local_events')
                 local_events_ob._params = 'year=None, month=None, day=None'
                 local_events_ob.write(script_content)
-                self.log.debug('Added local_events (Python Script) to RDFCalendar')
+                self.log.debug('Added local_events (Python Script) to RDFCalen'
+                               'dar')
         return True
