feaddac257b26d0146fc2dd37a9cf3c75ddebd0c
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-06-22 06:12:02 +0000

    Fixed rdf_calendar_utils test, and end_date search

--- eggs/Naaya/eggs/Naaya/Products/NaayaCore/managers/tests/test_rdf_calendar_utils.py
+++ eggs/Naaya/eggs/Naaya/Products/NaayaCore/managers/tests/test_rdf_calendar_utils.py
@@ -11,13 +11,14 @@ class TestCase(NaayaFunctionalTestCase):
                    start_date='12/12/2010', end_date='13/12/2010', lang='en')
         addNyEvent(self.portal.info, id='event1_fr', title='event1_fr',
                    start_date='12/12/2010', end_date='12/12/2010', lang='fr')
+        addNyEvent(self.portal.info, id='event2', title='event2',
+                   start_date='28/11/2010', end_date='02/12/2010', lang='fr')
 
     def test_rdf_cataloged_items(self):
         items = self.portal.rdf_cataloged_items(config['meta_type'],
                                                 year=2010, month=12)
-        self.assertEqual(len(items), 2)
-        self.assertEqual(items[0]['title'], u'event1')
-
+        self.assertEqual(len(items), 3)
+        
         #Overriding field
         items = self.portal.rdf_cataloged_items(config['meta_type'], {
             'startdate': 'releasedate',
