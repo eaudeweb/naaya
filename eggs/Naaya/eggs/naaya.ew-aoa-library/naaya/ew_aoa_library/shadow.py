b61c3c969409685c34119d3eb4c58130cebfcea3
Alex Morega <alex@grep.ro>
2010-07-02 11:34:23 +0000

    update to reflect changes in AoA Library survey

--- eggs/Naaya/eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/shadow.py
+++ eggs/Naaya/eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/shadow.py
@@ -34,82 +34,49 @@ def extract_singleselect(answer, widget_name):
         return widget.choices[datamodel]
 
 geo_type_map = {
-    'Global': {
-        ('Green economy',):         'symbol825',
-        ('Water',):                 'symbol814',
-        ('Green economy', 'Water'): 'symbol851',
-    },
-    'Regional/Transboundary': {
-        ('Green economy',):         'symbol862',
-        ('Water',):                 'symbol817',
-        ('Green economy', 'Water'): 'symbol987',
-    },
-    'National/Local': {
-        ('Green economy',):         'symbol268',
-        ('Water',):                 'symbol256',
-        ('Green economy', 'Water'): 'symbol166',
-    },
+    0: 'symbol825', # green economy
+    1: 'symbol814', # water
+    2: 'symbol851', # green economy and water
 }
 
+
 def extract_geo_type(answer):
-    themes = tuple(extract_multipleselect(answer, 'w_theme3082'))
-    theme_coverage = extract_singleselect(answer, 'w_theme-coverage')
+    themes_number = answer.get('w_theme')
 
     try:
-        return geo_type_map[theme_coverage][themes]
+        return geo_type_map[themes_number]
     except KeyError:
         return None
 
-widgetidmap = {
-    'theme': 'w_theme',
-    'geo_location': 'w_theme1115',
-    'water_resorces': 'w_2-if-water-specify-which-following-topics-are',
-    'water_resource_management': 'w_water-resource-management-specify-which-he',
-    'green_economy': 'w_if-green-economy-specify-which-following-topics',
-    'resource_efficiency': 'w_resource-efficiency-specify-which-following',
-
-    'assessment_location': 'w_source-ie-location-assessment',
-    'assessment_name': 'w_name-assessment',
-    'body_conducting_assessment': 'w_name-body-conducting-assessment',
-    'year': 'w_year-which-assessment-has-been-published',
-    'url': 'w_url',
-    'uploaded_file': 'w_if-not-available-please-upload-assessment',
-    'specific_scope': 'w_assembled-selected-knowledge-ie-specify-if',
-    'formal': 'w_formal-ie-specify-if-assessment-has-defined-scope',
-    'publically_available': 'w_publicly-available-ie-specify-if-assessment',
-    'other_criteria': 'w_are-there-other-criteria-basis-which-you-would',
-    'priority_main_criterion': 'w_if-yes-specify-main-criterion',
-    'name': 'w_name',
-    'email': 'w_email',
-    'organisation': 'w_organisation',
-}
-
-public_widgets = set(widgetidmap[x] for x in (
-    'theme',
-    'geo_location',
-    'water_resorces',
-    'water_resource_management',
-    'green_economy',
-    'resource_efficiency',
-
-    'assessment_location',
-    'assessment_name',
-    'body_conducting_assessment',
-    'year',
-    'url',
-    'uploaded_file',
-    'specific_scope',
-    'formal',
-    'publically_available',
-    'other_criteria',
-    'priority_main_criterion',
-))
+public_widgets = set([
+    'w_assessment-name',
+    'w_assessment-upload',
+    'w_assessment-url',
+    'w_assessment-year',
+    'w_body-conducting-assessment',
+    'w_green-economy',
+    'w_green-economy-topics',
+    'w_information-about-data-uploader',
+    'w_is-formal',
+    'w_location',
+    'w_main-criterion',
+    'w_other-criteria',
+    'w_publicly-available',
+    'w_registration-form-virtual-library',
+    'w_resource-efficiency-topics',
+    'w_specific-scope-or-purpose',
+    'w_theme',
+    'w_theme-coverage',
+    'w_water-related-ecosystem',
+    'w_water-resource-management-topics',
+    'w_water-resources-topics',
+])
 
 def extract_survey_answer_data(answer):
     attrs = {
         'id': answer.getId(),
         'title': answer.get('w_name-assessment'),
-        'geo_location': answer.get('w_theme1115'),
+        'geo_location': answer.get('w_location'),
         'uploader': answer.get('w_15-information-about-data-uploader'),
         'geo_type': extract_geo_type(answer),
         'description': "",
