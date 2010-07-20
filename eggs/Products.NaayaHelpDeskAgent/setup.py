5372c3b7b79a1f5b3e9b74b0d00315e635ccc3a7
Alin Voinea <alin.voinea@eaudeweb.ro>
2010-07-20 16:31:37 +0000

    Egg structure for Products.NaayaHelpDeskAgent

---
+++ eggs/Products.NaayaHelpDeskAgent/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='Products.NaayaHelpDeskAgent',
+      version=version,
+      description="Naaya Help Desk Agent",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='naaya helpdesk agent',
+      author='Alex Morega',
+      author_email='alex.morega@eaudeweb.ro',
+      url='http://naaya.eaudeweb.ro',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
