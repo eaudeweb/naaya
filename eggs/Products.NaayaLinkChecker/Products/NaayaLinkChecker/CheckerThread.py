49a763a3e3a7f572e2a34c64b8a511dba85107a4
Alin Voinea <alin.voinea@eaudeweb.ro>
2010-07-20 16:44:55 +0000

    Products.NaayaLinkChecker to egg

---
+++ eggs/Products.NaayaLinkChecker/Products/NaayaLinkChecker/CheckerThread.py
@@ -0,0 +1,98 @@
+#The contents of this file are subject to the Mozilla Public
+#License Version 1.1 (the "License"); you may not use this file
+#except in compliance with the License. You may obtain a copy of
+#the License at http://www.mozilla.org/MPL/
+#
+#Software distributed under the License is distributed on an "AS
+#IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+#implied. See the License for the specific language governing
+#rights and limitations under the License.
+#
+#The Initial Owner of the Original Code is European Environment
+#Agency (EEA).  Portions created by Finsiel Romania are
+#Copyright (C) 2006 by European Environment Agency.  All
+#Rights Reserved.
+#
+#Contributor(s):
+#  Original Code: Cornel Nitu (Finsiel Romania)
+#    (svn blame): Cristian Ciupitu (Eau de Web)
+
+from httplib import InvalidURL
+from Queue import Empty
+import socket
+from threading import Thread
+import time
+from types import *
+
+from MyURLopener import MyURLopener
+
+class CheckerThread(Thread):
+    """Thread for checking URLs.
+
+        URLs with unsupported protocols will be considered OK.
+    """
+
+    def __init__(self, urls, logresults, proxy):
+        """CheckerThread constructor.
+
+            @param urls: a queue of URLs to check
+            @type urls: Queue of strings
+            @param proxy: proxy
+        """
+        Thread.__init__(self)
+        self.urls = urls
+        self.logresults = logresults
+        self.proxy = proxy
+
+    def _isSupported(self, url):
+        """Returns True if the protocol from url is supported, False otherwise"""
+        for i in 'http:', 'https:', 'ftp', 'gopher:':
+            if url.startswith(i):
+                return True
+        return False
+
+    def run(self):
+        while True:
+            try:
+                url = self.urls.get_nowait()
+                if not self._isSupported(url):
+                    self.logresults[url] = 'OK'
+                    continue
+            except Empty:
+                break
+            try:
+                result = self.readhtml(url)
+            except AttributeError:
+                #in Python 2.3 socket doesn't have sslerror attribute
+                result = "SSL error."
+            self.logresults[url] = str(result)
+
+    def readhtml(self, url):
+        file = MyURLopener()
+        if self.proxy != '':
+            file.proxies['http'] = self.proxy
+        socket.setdefaulttimeout(5)
+        try:
+            file.open(url)
+            file.close()
+            return 'OK'
+        except IOError, msg:
+            msg = self.sanitize(msg)
+            return msg
+        except socket.timeout:
+            return "Attempted connect timed out."
+        except socket.sslerror, err:
+            return "SSL error: " + err
+        except InvalidURL:
+            return "Invalid URL."
+
+    def sanitize(self, msg):
+        if isinstance(IOError, ClassType) and isinstance(msg, IOError):
+            # Do the other branch recursively
+            msg.args = self.sanitize(msg.args)
+        elif isinstance(msg, TupleType):
+            if len(msg) >= 4 and msg[0] == 'http error' and isinstance(msg[3], InstanceType):
+                # Remove the Message instance -- it may contain
+                # a file object which prevents pickling.
+                msg = str(msg[1]) + ': ' + str(msg[2])
+        return msg
