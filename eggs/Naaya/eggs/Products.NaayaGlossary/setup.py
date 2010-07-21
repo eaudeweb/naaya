bb540025b21c2e2014488dc0a2539d5cf3c017d4
Alin Voinea <alin.voinea@eaudeweb.ro>
2010-07-21 09:30:26 +0000

    Prepare release 1.1

--- eggs/Naaya/eggs/Products.NaayaGlossary/setup.py
+++ eggs/Naaya/eggs/Products.NaayaGlossary/setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+NAME = 'Products.NaayaGlossary'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
 
-setup(name='Products.NaayaGlossary',
-      version=version,
+setup(name=NAME,
+      version=VERSION,
       description="Naaya Glossary",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
@@ -14,10 +16,10 @@ setup(name='Products.NaayaGlossary',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='naaya glossary',
-      author='Alec Ghica',
-      author_email='alec@eaudeweb.ro',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
-      license='GPL',
+      license='MPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
