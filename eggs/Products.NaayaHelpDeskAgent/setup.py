e14d9397bf86f2f4760daf47ff2c7f45cd8ae44e
Alin Voinea <alin.voinea@eaudeweb.ro>
2010-07-21 09:41:37 +0000

    Prepare release 2.0

--- eggs/Products.NaayaHelpDeskAgent/setup.py
+++ eggs/Products.NaayaHelpDeskAgent/setup.py
@@ -1,23 +1,26 @@
+""" Installer
+"""
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+NAME = 'Products.NaayaHelpDeskAgent'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
 
-setup(name='Products.NaayaHelpDeskAgent',
-      version=version,
+setup(name=NAME,
+      version=VERSION,
       description="Naaya Help Desk Agent",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='naaya helpdesk agent',
-      author='Alex Morega',
-      author_email='alex.morega@eaudeweb.ro',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
-      license='GPL',
+      license='MPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
