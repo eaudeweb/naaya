3f3cb8604afece1faed132625cb338ce41f66daf
Alin Voinea <alin.voinea@eaudeweb.ro>
2010-07-21 09:52:13 +0000

    Prepare release 1.1
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.NaayaThesaurus@14912 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.NaayaThesaurus/setup.py
+++ eggs/Products.NaayaThesaurus/setup.py
@@ -1,23 +1,26 @@
+""" Installer
+"""
 from setuptools import setup, find_packages
 import os
 
-version = '0.1'
+NAME = 'Products.NaayaThesaurus'
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
 
-setup(name='Products.NaayaThesaurus',
-      version=version,
+setup(name=NAME,
+      version=VERSION,
       description="Naaya Thesaurus",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='naaya thesaurus',
-      author='Alec Ghica',
-      author_email='alec@eaudeweb.ro',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
-      license='GPL',
+      license='MPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
       include_package_data=True,
