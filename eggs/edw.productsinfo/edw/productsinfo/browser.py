944d36bcfd7759f1e71018eb5a183b35e3110adb
Alin Voinea <alin.voinea@eaudeweb.ro>
2010-07-29 14:00:53 +0000

    Added more views
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/edw.productsinfo@15018 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/edw.productsinfo/edw/productsinfo/browser.py
+++ eggs/edw.productsinfo/edw/productsinfo/browser.py
@@ -4,29 +4,27 @@ import simplejson as json
 from zope.publisher.browser import BrowserPage
 from zope.publisher.browser import NotFound
 
-class View(BrowserPage):
+class Info(BrowserPage):
     """Provides information about installed products
     in Json format, required manager authentication
     """
-
-    @property
-    def token(self):
-        """ Authentication token
+    def check_token(self, token):
+        """ Authentication token check
         """
-        return getattr(self.context, 'edw-productsinfo-key', None)
-
-    def publishTraverse(self, request, name):
-        token = self.token
-        if not token:
-            return json.dumps({
+        key = getattr(self.context, 'edw-productsinfo-key', None)
+        if not key:
+            return {
                 'error': 'Unauthorized: Token not set on this instance'
-            })
+            }
 
-        if self.token != name:
-            return json.dumps({
+        if key != token:
+            return {
                 'error': 'Invalid authentication token'
-            })
+            }
+        return {}
 
+
+    def json(self):
         products=[]
         products_folder = self.context.Control_Panel.Products
         for product_name in products_folder.objectIds():
@@ -38,5 +36,23 @@ class View(BrowserPage):
                 })
         return json.dumps(products)
 
+    def publishTraverse(self, request, name):
+        error = self.check_token(name)
+        if error:
+            return json.dumps(error)
+        return self.json()
+
     def __call__(self, **kwargs):
         raise NotFound(self.context, 'products.info', self.request)
+
+class Portals(Info):
+    """ Provides information about portals
+    """
+    def json(self):
+        return json.dumps({'error': 'Not implemented error'})
+
+class Errors(Info):
+    """ Provides portal errors_log
+    """
+    def json(self):
+        return json.dumps({'error': 'Not implemented error'})
