571bae72e6e75e766c8a02f2c3f5a4eaa1bea229
andredor <andredor@eaudeweb.ro>
2010-07-29 14:21:26 +0000

    Fix for meeting permissions. Everything except folder listing permissions.
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.meeting@15020 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content.meeting/naaya/content/meeting/__init__.py
+++ eggs/naaya.content.meeting/naaya/content/meeting/__init__.py
@@ -1,6 +1,11 @@
+#roles used in the meeting
 WAITING_ROLE = 'Meeting Waiting List'
 PARTICIPANT_ROLE = 'Meeting Participant'
 ADMINISTRATOR_ROLE = 'Administrator'
+
+#meeting specific permissions
+PERMISSION_PARTICIPATE_IN_MEETING = 'Naaya - Meeting - Participate in the meeting'
+PERMISSION_ADMIN_MEETING = 'Naaya - Meeting - Administrate the meeting'
 
 from meeting import NyMeeting
 
