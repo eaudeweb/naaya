68a71dd4adf4263ead8545c6f851255106fa153f
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-08-04 10:15:17 +0000

    Added some documentation

--- eggs/Naaya/third-party/edw-zmibespin/setup.py
+++ eggs/Naaya/third-party/edw-zmibespin/setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
-import sys, os
-
+import os
 version = '1.0'
 
-setup(name='zmibespin',
-      version=version,
-      description="Make every textarea in the Zope2 ZMI a bespin editor",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='Python Javascript Bespin',
-      author='Alexandru Plugaru',
-      author_email='alexandru.plugaru@gmail.com',
-      url='http://github.com/humanfromearth/zmibespin',
-      license='MPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      )
+setup(
+    name='zmibespin',
+    version=version,
+    description="Make every textarea in the Zope2 ZMI a bespin editor",
+    long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
+    classifiers=['Framework :: Zope2'],
+    keywords='Python Javascript Bespin',
+    author='Alexandru Plugaru',
+    author_email='alexandru.plugaru@gmail.com',
+    url='http://github.com/humanfromearth/zmibespin',
+    license='MPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires="Zope2"
+)
