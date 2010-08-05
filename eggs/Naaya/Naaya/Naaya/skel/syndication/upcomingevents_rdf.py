5fb864fb2c4d4b5d3f05465adfd425b8cd447cb3
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-08-05 13:03:42 +0000

    Moving old sources

--- eggs/Naaya/Naaya/Naaya/skel/syndication/upcomingevents_rdf.py
+++
@@ -1,5 +0,0 @@
-r, d = [], context.utGetTodayDate()
-ra = r.append
-for e in context.getCatalogedObjectsCheckView(meta_type=context.get_constant('METATYPE_NYEVENT'), approved=1):
-    if e.start_date > d: ra(e)
-return r
