3c924d52951367268eab916ecd113d213884f7cd
Alex Morega <alex@grep.ro>
2010-08-09 09:34:18 +0000

    initial extfile 2.0.2 package import

---
+++ third-party/edw-extfile/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='edw-extfile',
+      version='2.0.2',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+)
