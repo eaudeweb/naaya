ca865003c001f8afa825cc26494f8c4d163c9947
Alex Morega <alex@grep.ro>
2010-08-09 15:36:28 +0000

    switch to nose-based testing

--- eggs/Naaya/eggs/Naaya/Products/NaayaCore/EmailTool/tests/testEmail.py
+++ eggs/Naaya/eggs/Naaya/Products/NaayaCore/EmailTool/tests/testEmail.py
@@ -1,14 +1,44 @@
 from unittest import TestSuite, makeSuite
 from Products.Naaya.tests.NaayaTestCase import FunctionalTestCase
 
-class EmailTestCase(FunctionalTestCase):
-    def test_mail(self):
-        self.layer.mail_divert.clear_log()
+def divert_mail():
+    from Products.NaayaCore.EmailTool import EmailTool
 
+    class smtplib_replacement(object):
+        class SMTP:
+            def __init__(s, server, port):
+                mail_log.append( ('init', {}) )
+
+            def sendmail(s, from_addr, to_addr, message):
+                mail_log.append( ('sendmail',
+                                  {'from': from_addr,
+                                   'to': to_addr,
+                                   'message': message}) )
+
+            def quit(s):
+                mail_log.append( ('quit', {}) )
+
+    _orig_smtplib = EmailTool.smtplib
+    EmailTool.smtplib = smtplib_replacement
+    mail_log = []
+
+    def restore():
+        EmailTool.smtplib = _orig_smtplib
+
+    return mail_log, restore
+
+class EmailTestCase(FunctionalTestCase):
+    def setUp(self):
+        self.mail_log, self._restore_mail = divert_mail()
+
+    def tearDown(self):
+        self._restore_mail()
+
+    def test_mail(self):
         self.portal.getEmailTool().sendEmail('test_content',
             'test_to@example.com', 'test_from@example.com', 'test_subject')
 
-        smtp_log = self.layer.mail_divert.log
+        smtp_log = self.mail_log
 
         self.failUnlessEqual(len(smtp_log), 3)
         self.failUnlessEqual(smtp_log[0][0], 'init')
@@ -23,13 +53,11 @@ class EmailTestCase(FunctionalTestCase):
                         smtp_log[1][1]['message'])
 
     def test_mail_from(self):
-        self.layer.mail_divert.clear_log()
-
         self.portal.getEmailTool().sendEmail('test_content',
             'test_to_1@example.com, test_to_2@example.com',
             'test_from@example.com', 'test_subject')
 
-        smtp_log = self.layer.mail_divert.log
+        smtp_log = self.mail_log
         self.assertEqual(len(smtp_log), 3)
         to = smtp_log[1][1]['to']
 
