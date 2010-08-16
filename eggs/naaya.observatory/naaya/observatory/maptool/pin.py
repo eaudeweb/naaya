b1fd063c4f9279ae6e13f4ed0967ecadd2c063fd
andredor <andredor@eaudeweb.ro>
2010-08-16 08:22:37 +0000

    Added cluster icons with push pin count:
    - the cluster icons are generated from the rating icons and the number icons
    - the pins and comments are kept in a btreefolder named observatory in the site

---
+++ eggs/naaya.observatory/naaya/observatory/maptool/pin.py
@@ -0,0 +1,71 @@
+from random import randint
+
+from OFS.SimpleItem import SimpleItem
+from persistent.list import PersistentList
+
+def addPushPin(parent, catalog, index, latitude, longitude):
+    id = 'pin_%d' % index
+    pin = PushPin(id, latitude, longitude)
+    parent._setOb(id, pin)
+    catalog.catalog_object(pin, uid=id)
+
+def resetPushPinRating(parent, catalog, index):
+    id = 'pin_%d' % index
+    pin_idx = catalog._catalog.indexes['id_pin']._index
+    f = {'id_pin': id}
+
+    # getting the unique data record ids
+    brains = catalog(f)
+    dict_rids = {}
+    rids = []
+    for b in brains:
+        rid = b.data_record_id_
+        if rid not in dict_rids:
+            dict_rids[rid] = 1
+            rids.append(rid)
+
+    ratings = [catalog.getobject(rid) for rid in rids]
+    for r in ratings:
+        assert r.id_pin == id
+    sum_ratings = sum(r.rating for r in ratings)
+    pin = parent._getOb(id)
+    pin.averageRating = sum_ratings / len(ratings)
+
+    # recatalog pin
+    catalog.uncatalog_object(id)
+    catalog.catalog_object(pin, uid=id)
+
+def removePushPin(parent, catalog, index):
+    id = 'pin_%d' % index
+    pin = parent._getOb(id)
+    catalog.uncatalog_object(id)
+    parent._delOb(id)
+
+class PushPin(SimpleItem):
+    def __init__(self, id, latitude, longitude):
+        self._setId(id)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.averageRating = None
+
+
+def ratePushPin(parent, catalog, index, pin_index, type, rating):
+    id = 'rating_%d' % index
+    id_pin = 'pin_%d' % pin_index
+    rating = PinRate(id, id_pin, type, rating)
+    parent._setOb(id, rating)
+    catalog.catalog_object(rating, uid=id)
+    resetPushPinRating(parent, catalog, pin_index)
+
+def removeRating(parent, catalog, index):
+    id = 'rating_%d' % index
+    catalog.uncatalog_object(id)
+    parent._delOb(id)
+
+class PinRate(SimpleItem):
+    def __init__(self, id, id_pin, type, rating):
+        self._setId(id)
+        self.id_pin = id_pin
+        self.type = type
+        self.rating = rating
+
