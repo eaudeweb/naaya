409059d2a924c74818cb95f92e188e11646dc785
andredor <andredor@eaudeweb.ro>
2010-08-23 14:33:17 +0000

    Per type clustering and image changes from bogdan.

--- eggs/Naaya/eggs/naaya.observatory/naaya/observatory/contentratings/interfaces.py
+++
@@ -1,7 +0,0 @@
-from zope.interface import Interface
-
-class IObservatoryRatable(Interface):
-    """Marker interface that promises that an implementing object may be
-    rated by users.
-    """
-
