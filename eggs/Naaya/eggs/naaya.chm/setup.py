16586ea494e925c75146dbb4863b61d6c5dd00fe
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-08-24 09:38:32 +0000

    Added dependencies to egg

--- eggs/Naaya/eggs/naaya.chm/setup.py
+++ eggs/Naaya/eggs/naaya.chm/setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.chm',
-      version='2.3dev',
-      author='Eau de Web',
-      author_email='office@eaudeweb.ro',
-      url='http://naaya.eaudeweb.ro',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'Naaya',
-          'naaya.photoarchive',
-      ]
+    version='2.3dev',
+    author='Eau de Web',
+    author_email='office@eaudeweb.ro',
+    url='http://naaya.eaudeweb.ro',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Naaya',
+        'naaya.photoarchive',
+        'Products.NaayaLinkChecker',
+        'Products.NaayaNetRepository',
+        'Products.NaayaHelpDeskAgent',
+        'Products.NaayaGlossary',
+        'Products.NaayaCalendar',
+        'Products.NaayaForum'
+    ]
 )
