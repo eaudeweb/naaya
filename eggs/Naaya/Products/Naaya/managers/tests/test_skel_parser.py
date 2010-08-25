03017728530e2e660b4128a2b0453b7a12b2c83a
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2010-08-25 11:50:36 +0000

    Skel parser major refactoring to reduce hardcoding

--- eggs/Naaya/Products/Naaya/managers/tests/test_skel_parser.py
+++ eggs/Naaya/Products/Naaya/managers/tests/test_skel_parser.py
@@ -1,22 +1,3 @@
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# The Initial Owner of the Original Code is European Environment
-# Agency (EEA).  Portions created by Eau de Web are
-# Copyright (C) European Environment Agency.  All
-# Rights Reserved.
-#
-# Authors:
-#
-# Eau de Web
-
 # Python imports
 from unittest import TestSuite, makeSuite
 from os.path import join, dirname
@@ -26,16 +7,20 @@ import Globals
 
 # Naaya imports
 from Products.Naaya.tests.NaayaTestCase import NaayaTestCase
-from Products.Naaya.managers.skel_parser import skel_parser
+from Products.Naaya.managers.skel_parser import skel_parser, TAG_MAPPING
 
 class SkelTestCase(NaayaTestCase):
-    
+
     def test_parser(self):
-        skel_path = join(dirname(__file__), '../../skel')
-        skel_content = self.portal.futRead(join(skel_path, 'skel.xml'), 'r')
+        TAG_MAPPING['testnode'] = {'type': 'list'}
+        skel_content = open(join(dirname(__file__), 'skel.xml'), 'r').read()
+        sk, error = skel_parser().parse('Garbage content')
+        self.assertNotEqual(error, '')
+        self.assertEqual(sk, None)
         sk, error = skel_parser().parse(skel_content)
         self.assertEqual(error, '')
         self.assertNotEqual(sk.root, None)
+        self.assertEqual(len(sk.root.testnode), 4)
         self.assertFalse(isinstance(sk.root.forms, list))
         self.assertTrue(isinstance(sk.root.forms.forms, list))
         self.assertTrue(len(sk.root.forms.forms) > 1)
@@ -50,7 +35,7 @@ class SkelTestCase(NaayaTestCase):
             for permission in role.permissions:
                 self.assertNotEqual(permission.name, None)
                 self.assertNotEqual(permission.name, '')
-    
+
 def test_suite():
     suite = TestSuite()
     suite.addTest(makeSuite(SkelTestCase))
