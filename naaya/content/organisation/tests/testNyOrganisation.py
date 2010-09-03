b7a6688e2a44dc215d4e8595073b1506b501e7f1
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2010-09-03 10:24:12 +0000

    Test fixes
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content-expnet@15321 a0171fae-0946-0410-966d-b1775066cd26

--- naaya/content/organisation/tests/testNyOrganisation.py
+++ naaya/content/organisation/tests/testNyOrganisation.py
@@ -9,11 +9,11 @@ def add_selection_list(portal, id, values):
     for key, value in values.iteritems():
         portal.portal_portlets[id].manage_add_item(key, value)
 
-def setUp_EW_test_data(portal):
+def setUp_EW_data(portal):
     add_selection_list(portal, 'countries',
         dict((k,k) for k in ["Egypt", "Lybia", "Tunisia"]))
 
-def tearDown_EW_test_data(portal):
+def tearDown_EW_data(portal):
     portal.portal_portlets.manage_delObjects(['countries'])
 
 class TestNyOrganisation(NaayaTestCase):
@@ -21,14 +21,14 @@ class TestNyOrganisation(NaayaTestCase):
 
     def afterSetUp(self):
         self.install_content_type('Naaya Organisation')
-        setUp_EW_test_data(self.portal)
+        setUp_EW_data(self.portal)
         NyFolder.addNyFolder(self.app.portal, "test_folder")
         pass
 
 
     def beforeTearDown(self):
         self.app.portal.manage_delObjects("test_folder")
-        tearDown_EW_test_data(self.portal)
+        tearDown_EW_data(self.portal)
         self.remove_content_type('Naaya Organisation')
 
 
