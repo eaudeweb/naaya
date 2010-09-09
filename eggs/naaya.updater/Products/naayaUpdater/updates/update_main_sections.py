2411cd64474795c49bef3daf446d32e52ef26c85
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-09-09 14:25:09 +0000

    Added mainsections update script

--- eggs/naaya.updater/Products/naayaUpdater/updates/update_main_sections.py
+++ eggs/naaya.updater/Products/naayaUpdater/updates/update_main_sections.py
@@ -1,94 +1,28 @@
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# The Initial Owner of the Original Code is European Environment
-# Agency (EEA). Portions created by Eau de Web are
-# Copyright (C) European Environment Agency.  All
-# Rights Reserved.
-#
-# Authors:
-#
-# David Batranu, Eau de Web
-
-
-#Python imports
-import traceback
-from os.path import join
-
-#Zope imports
-from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from AccessControl import ClassSecurityInfo
-from AccessControl.Permissions import view_management_screens
-from OFS.Folder import Folder
-
-#Naaya imports
-from Products.naayaUpdater.updates import UpdateScript, PRIORITY
-from Products.Naaya.managers.skel_parser import skel_parser
+import os
+from Products.naayaUpdater.updates import UpdateScript
+from Products.NaayaCore.PortletsTool.Portlet import addPortlet
+from naaya.core.utils import path_in_site
 
 class UpdateMainSections(UpdateScript):
     """ Update portlet_maincategories  """
     title = 'Update main sections portlet'
-    creation_date = 'Jan 20, 2010'
-    authors = ['David Batranu']
-    description = 'Renames portlet_mainsections so that the new-style portlet will be used. Also updates ep_collapse/ep_expand images.'
-    security = ClassSecurityInfo()
+    creation_date = 'Sep 6, 2010'
+    authors = ['Alexandru Plugaru']
+    description = 'Adds mainsections_settings dict to NySite for configuration\
+                  of the navigation. Also a little cleanup of the current\
+                    maintopics values '
 
-    security.declarePrivate('_update')
     def _update(self, portal):
-        self.update_portlet(portal, 'portlet_maincategories')
-        self.update_images(portal, ['ep_expand.gif', 'ep_collapse.gif'])
+        portal.maintopics_settings = {
+            'expanded': True,
+            'persistent': True,
+            'expand_levels': 1,
+            'max_levels': 1
+        }
+        maintopics = []
+        old_maintopics = portal.utSortObjsListByAttr(filter(lambda x: x is not None, map(lambda f, x: f(x, None), (portal.utGetObject,)*len(portal.maintopics), portal.maintopics)), 'sortorder', 0)
+        for path_ob in old_maintopics:
+            maintopics.append(path_in_site(path_ob))
+        portal.maintopics = maintopics
+        portal._p_changed = True
         return True
-
-    def update_portlet(self, portal, portlet_id):
-        ptool = portal.getPortletsTool()
-        if portlet_id not in ptool.objectIds():
-            self.log.debug('%s does not exist in %s' % (portlet_id, portal.absolute_url(1)))
-            return
-        new_portlet_id = '%s_old-style' % portlet_id
-        self.rename_object(ptool, portlet_id, new_portlet_id)
-
-    def rename_object(self, container, old_id, new_id):
-        try:
-            container.manage_renameObjects([old_id], [new_id])
-            self.log.debug('Renamed %s to %s' % (old_id, new_id))
-        except Exception, e:
-            self.log.error('Rename failed - "%s"' % str(e))
-            self.log.error(traceback.format_exc())
-
-    def get_schemes(self, portal):
-        ltool = portal.getLayoutTool()
-        for skin in ltool.objectValues('Naaya Skin'):
-            for scheme in skin.objectValues('Naaya Scheme'):
-                yield scheme
-
-    def update_images(self, portal, images):
-        for scheme in self.get_schemes(portal):
-            for image in images:
-                if image in scheme.objectIds():
-                    self.update_image(scheme[image], self.get_image_file(portal, image))
-
-    def get_skin_and_scheme_id(self, portal_path):
-        skel = open(join(portal_path, 'skel', 'skel.xml'), 'rb')
-        skel_handler, error = skel_parser().parse(skel.read())
-        skel.close()
-        skin = skel_handler.root.layout.default_skin_id
-        scheme = skel_handler.root.layout.default_scheme_id
-        return skin, scheme
-
-    def get_image_file(self, portal, image_id):
-        portal_path = self.get_portal_path(portal)
-        skin_id, scheme_id = self.get_skin_and_scheme_id(portal_path)
-        fs_layout = join(portal_path, 'skel', 'layout')
-        fs_image = join(fs_layout, skin_id, scheme_id, image_id)
-        return open(fs_image, 'rb').read()
-
-    def update_image(self, image_ob, new_image_data):
-        image_ob.update_data(new_image_data)
-        self.log.debug('Updated %s' % image_ob.absolute_url(1))
