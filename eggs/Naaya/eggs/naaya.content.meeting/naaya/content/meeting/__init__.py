fcf78684394f62e1549243f620f075f5280390f9
andredor <andredor@eaudeweb.ro>
2010-09-09 13:14:08 +0000

    Fix for #380 security issue:
    - all objects inside the meeting are restricted except agenda (if not empty)
    - if agenda and other pointers do not exits.. error on add/edit meeting
    - added update script to make all objects in current meetings restricted
    - items are restricted to participants (includes just participants, waiting list and administrators)
    - added test to check the agenda change permisstions

--- eggs/Naaya/eggs/naaya.content.meeting/naaya/content/meeting/__init__.py
+++ eggs/Naaya/eggs/naaya.content.meeting/naaya/content/meeting/__init__.py
@@ -2,6 +2,7 @@
 WAITING_ROLE = 'Meeting Waiting List'
 PARTICIPANT_ROLE = 'Meeting Participant'
 ADMINISTRATOR_ROLE = 'Administrator'
+MANAGER_ROLE = 'Manager'
 
 #meeting specific permissions
 PERMISSION_PARTICIPATE_IN_MEETING = 'Naaya - Meeting - Participate in the meeting'
