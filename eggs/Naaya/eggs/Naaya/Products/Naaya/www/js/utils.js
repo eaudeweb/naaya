f28371d16a75311bb9140467c625f23b112f3470
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-09-15 09:48:55 +0000

    Added linkify function

--- eggs/Naaya/eggs/Naaya/Products/Naaya/www/js/utils.js
+++ eggs/Naaya/eggs/Naaya/Products/Naaya/www/js/utils.js
@@ -30,3 +30,19 @@ function gettext(msgid) {
        return (typeof(value) == 'string') ? value : value[0];
     }
 }
+
+function Linkify(inputText) {
+    //URLs starting with http://, https://, or ftp://
+    var replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    var replacedText = inputText.replace(replacePattern1, '<a href="$1">$1</a>');
+
+    //URLs starting with www. (without // before it, or it'd re-link the ones done above)
+    var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    var replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2">$2</a>');
+
+    //Change email addresses to mailto:: links
+    var replacePattern3 = /(^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-\.]+?\.[a-zA-Z]{2,6}$)/gim;
+    var replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText
+}
