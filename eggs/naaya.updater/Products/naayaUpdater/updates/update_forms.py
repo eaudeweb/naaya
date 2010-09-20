ce2e9bab35921462258e21b9c71474a4e36aa32f
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2010-09-20 11:56:22 +0000

    remove references to an nonexistent file: misc_/naayaUpdater/jquery-1.3.2.min.js; update_forms repaired
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.updater@15390 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.updater/Products/naayaUpdater/updates/update_forms.py
+++ eggs/naaya.updater/Products/naayaUpdater/updates/update_forms.py
@@ -24,6 +24,7 @@ import re
 from os.path import join
 
 #Zope imports
+import zLOG
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
 #Naaya imports
@@ -46,53 +47,59 @@ class UpdateForms(UpdateScript):
 
         return True
 
-    def get_report(self, forms, all_forms=False, portals='', p_action='', REQUEST=None):
+    def get_report(self, forms, portals='', exclude=False, REQUEST=None):
         if not REQUEST.has_key('show_report'): # TODO: remove this
             return
 
         report = {}
-        forms_list = convertLinesToList(forms)
+        forms = convertLinesToList(forms)
         portals_list = self.getPortals()
         portals_custom = []
 
-        for portal_id in portals.split(','):
-            portals_custom.append(portal_id.strip())
+        if portals.strip():
+            for portal_id in portals.split(','):
+                portals_custom.append(portal_id.strip())
 
         for portal in portals_list:
-            if p_action == 'ep' and portal.id in portals_custom: continue
-            elif p_action != 'ep' and portal.id not in portals_custom: continue
+            if portals_custom:
+                if exclude and portal.id in portals_custom: continue
+                elif not exclude and portal.id not in portals_custom: continue
 
             portal_path = '/'.join(portal.getPhysicalPath()[1:])
-            if all_forms:
-                forms_list = portal.portal_forms.objectIds()
-            else:
+            portal_forms = portal.portal_forms
+            if forms:
                 forms_list_tmp = []
-                for form_line in forms_list: #Search in ZMI and FS for template patterns
+                for form_line in forms: #Search in ZMI and FS for template patterns
                     for form_id in self.find_templates(re.compile(form_line), portal):
                         if form_id not in forms_list_tmp: forms_list_tmp.append(form_id)
                 forms_list = forms_list_tmp
                 del(forms_list_tmp)
+            else:
+                forms_list = portal_forms.objectIds()
             deltas = []
+
             for form_id in forms_list:
-                form_path = '%s/portal_forms/%s' % (portal_path, form_id)
-                form_fs = self.get_fs_template(form_id, portal)
-                form_zmi_ob = self.get_zmi_template(form_path)
-                if form_fs and form_zmi_ob:
-                    form_zmi = get_template_content(form_zmi_ob)
-                    t1 = normalize_template(form_fs)
-                    t2 = normalize_template(form_zmi)
+                try:
+                    form_fs = portal_forms._default_form(form_id)
+                except KeyError, exc_error:
+                    zLOG.LOG('Naaya Updater', zLOG.ERROR, '%s: %s' % (portal.id, exc_error))
+                    continue
+                form_zmi = portal_forms._getOb(form_id)
+                if form_fs and form_zmi:
+                    t1 = normalize_template(form_fs._text)
+                    t2 = normalize_template(form_zmi._text)
                     delta = {
-                        'physical_path': '/'.join(form_zmi_ob.getPhysicalPath()[1:]),
-                        'absolute_url': form_zmi_ob.absolute_url(),
-                        'id': form_zmi_ob.getId(),
-                        'title': form_zmi_ob.title_or_id(),
+                        'physical_path': '/'.join(form_zmi.getPhysicalPath()[1:]),
+                        'absolute_url': form_zmi.absolute_url(),
+                        'id': form_zmi.getId(),
+                        'title': form_zmi.title_or_id(),
                     }
                     if t1 == t2:
                         delta['result'] = 'identical'
                     else:
                         delta['result'] = 'different'
-    
-                        delta['diff'] = html_diff(form_fs, form_zmi)
+
+                        delta['diff'] = html_diff(form_fs._text, form_zmi._text)
                     deltas.append(delta)
 
             if len(deltas) > 0:
@@ -124,7 +131,7 @@ class UpdateForms(UpdateScript):
             form_ob = self.get_zmi_template(form_path)
             form_ob.aq_parent.manage_delObjects([form_id])
         return REQUEST.RESPONSE.redirect(self.absolute_url() + '/manage_update')
-        
+
     def get_zmi_template(self, path):
         """
             return a ZMI template object given the path
@@ -159,22 +166,16 @@ class UpdateForms(UpdateScript):
         skel_handler, error = skel_parser().parse(readFile(join(portal_path, 'skel', 'skel.xml'), 'r'))
         if skel_handler.root.forms is not None:
             return [f.id for f in skel_handler.root.forms.forms]
-            
+
     def list_pluggable_templates(self, portal):
         """ get filesystem templates from pluggable content """
         return [ tpl for meta_type in portal.get_pluggable_metatypes() for tpl in portal.get_pluggable_item(meta_type).get('forms', None) ]
-        
+
     def find_templates(self, pattern, portal):
         """ Find templates based on a rexp pattern from FS and ZMI """
-        fs_matches = []
-        fs_forms = self.list_fs_templates(portal) + self.list_fs_templates(NySite_module) + self.list_pluggable_templates(portal)
-        for fs_tpl in fs_forms:
-            if pattern.match(fs_tpl):
-                fs_matches.append(fs_tpl)
-
         portal_forms = getattr(portal, 'portal_forms', None)
         forms_list = portal_forms.objectValues([Template.meta_type ,])
         for zmi_tpl in forms_list:
-            if zmi_tpl.id in fs_matches:
-                yield zmi_tpl.id
+            if pattern.match(zmi_tpl.getId()):
+                yield zmi_tpl.getId()
     manage_update = PageTemplateFile('zpt/update_forms', globals())
