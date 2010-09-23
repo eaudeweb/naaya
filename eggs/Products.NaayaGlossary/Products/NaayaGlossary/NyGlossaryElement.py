0a435e15f6486b051f657147eabda7be8c7f1315
simiamih <simiamih@a0171fae-0946-0410-966d-b1775066cd26>
2010-09-23 12:18:59 +0000

    Deleted manage_* methods in NaayaGlossary, added INyCatalogAware by configure.zcml (internship edw)
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.NaayaGlossary@15417 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.NaayaGlossary/Products/NaayaGlossary/NyGlossaryElement.py
+++ eggs/Products.NaayaGlossary/Products/NaayaGlossary/NyGlossaryElement.py
@@ -99,18 +99,6 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
         self.cu_recatalog_object(self)
         if REQUEST: return REQUEST.RESPONSE.redirect('properties_html?save=ok')
 
-    security.declarePrivate("manage_afterAdd")
-    def manage_afterAdd(self, item, container):
-        """ this method is called, whenever _setObject in ObjectManager gets called """
-        SimpleItem.inheritedAttribute('manage_afterAdd')(self, item, container)
-        self.cu_catalog_object(self)
-
-    security.declarePrivate("manage_beforeDelete")
-    def manage_beforeDelete(self, item, container):
-        """ this method is called, when the object is deleted """
-        SimpleItem.inheritedAttribute('manage_beforeDelete')(self, item, container)
-        self.cu_uncatalog_object(self)
-
     security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'approveElement')
     def approveElement(self, REQUEST=None):
         """ used for approval link in basket of approvals"""
