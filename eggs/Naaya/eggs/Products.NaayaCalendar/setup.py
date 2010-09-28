6ba2b63567d79d5760e0b8f7650320591ac8cac0
Alex Morega <alex@grep.ro>
2010-09-28 10:14:59 +0000

    remove setup.py noise and fix license

--- eggs/Naaya/eggs/Products.NaayaCalendar/setup.py
+++ eggs/Naaya/eggs/Products.NaayaCalendar/setup.py
@@ -1,35 +1,14 @@
 """ Installer
 """
 from setuptools import setup, find_packages
-import os
 
-NAME = 'Products.NaayaCalendar'
-PATH = NAME.split('.') + ['version.txt']
-VERSION = open(os.path.join(*PATH)).read().strip()
-
-setup(name=NAME,
-      version=VERSION,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='naaya calendar',
+setup(name='Products.NaayaCalendar',
+      version='1.1',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
+      license='MPL',
+      packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+  )
