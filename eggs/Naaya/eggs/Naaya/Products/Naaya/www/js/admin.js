9692be8b6efa5916783f42f709be20e224419a41
andredor <andredor@eaudeweb.ro>
2010-10-04 07:25:12 +0000

    All forms with ajax and success messages for assigning roles

--- eggs/Naaya/eggs/Naaya/Products/Naaya/www/js/admin.js
+++ eggs/Naaya/eggs/Naaya/Products/Naaya/www/js/admin.js
@@ -43,9 +43,13 @@ $(document).ready(function(){
             self.autocomplete('search', '   ');
         });
     });
-    add_onclick_second_tabs();
-    add_onclick_sort_links();
-    add_onclick_group_links();
+    ldap_onclick_second_tabs();
+    ldap_onclick_sort_links();
+    ldap_onclick_group_links();
+    ldap_user_roles_revoke_form();
+    ldap_group_roles_revoke_form();
+    ldap_user_search_form();
+    ldap_group_roles_assign_form();
 });
 function toggleLoader(){
     $('.ajax-loader').toggle();
@@ -80,6 +84,7 @@ function createKey(key){
 /**
  * Functions for LDAPUserFolder
 */
+
 function select_second_tab(tabid) {
     $('.second_tab_set a.current_sub').removeClass('current_sub');
     if (tabid) {
@@ -87,64 +92,42 @@ function select_second_tab(tabid) {
     }
 }
 
-function add_onclick_sort_links() {
-    $('.sort_link').click(function() {
-        show_ldap_users_fieldset($(this).attr('href'));
-        return false;
-    });
+function ldap_show_section(data) {
+    var html = $('#section_parent', $(data)).html();
+    $('#section_parent').html(html);
 }
 
-function add_onclick_second_tabs() {
-    $('.second_tab').click(function() {
-        show_ldap_section($(this).attr('id'), $(this).attr('href'));
-        return false;
-    });
+function ldap_ajax_section_manage_all() {
+    ldap_onclick_sort_links();
+    ldap_onclick_group_links();
+    ldap_user_roles_revoke_form();
+    ldap_group_roles_revoke_form();
 }
 
-function add_onclick_group_links() {
-    $('.group_link').click(function() {
-        show_ldap_section('', $(this).attr('href'));
-        return false;
-    });
+function ldap_ajax_section_assign_to_users() {
+    ldap_user_search_form();
 }
 
-function ajax_user_search_form() {
-    $('#frmRoles').ajaxForm({
-    beforeSubmit: function() {
-        $('#waiting_for_search_results').show();
-        $('#search_results_parent').hide();
-    },
-    success: function(data) {
-        var html = $("#search_results_parent", $(data)).html();
-        $('#search_results_parent').html(html);
-
-        $('#waiting_for_search_results').hide();
-        $('#search_results_parent').show();
-    },
-    error: function() {
-        $('#error_on_search_results').show();
-        $('#waiting_for_search_results').hide();
-    }
-    });
+function ldap_ajax_section_assign_to_groups() {
+    ldap_group_roles_assign_form();
 }
 
-function show_ldap_section(tabid, url) {
+function ldap_refresh_section(tabid, url) {
     select_second_tab(tabid);
-
     $('#section_wating_response').show();
     $('#section_parent').hide();
 
     $.ajax({
         url: url,
         success: function(data) {
-            var html = $("#section_parent", $(data)).html();
-            $('#section_parent').html(html);
+            ldap_show_section(data);
 
             if (tabid == 'link_manage_all') {
-                add_onclick_sort_links();
-                add_onclick_group_links();
+                ldap_ajax_section_manage_all();
             } else if (tabid == 'link_assign_to_users') {
-                ajax_user_search_form();
+                ldap_ajax_section_assign_to_users();
+            } else if (tabid == 'link_assign_to_groups') {
+                ldap_ajax_section_assign_to_groups();
             }
 
             $('#section_wating_response').hide();
@@ -157,25 +140,174 @@ function show_ldap_section(tabid, url) {
     });
 }
 
-function show_ldap_users_fieldset(url) {
-    $('#users_roles_wating_response').show();
+function ldap_onclick_second_tabs() {
+    $('.second_tab').click(function() {
+        ldap_refresh_section($(this).attr('id'), $(this).attr('href'));
+        return false;
+    });
+}
+
+function ldap_onclick_sort_links() {
+    $('.sort_link').click(function() {
+        ldap_refresh_users_fieldset($(this).attr('href'));
+        return false;
+    });
+}
+
+function ldap_onclick_group_links() {
+    $('.group_link').click(function() {
+        ldap_refresh_section('', $(this).attr('href'));
+        return false;
+    });
+}
+
+function ldap_show_users_fieldset(data) {
+    var html = $('#users_field', $(data)).html();
+    $('#users_field').html(html);
+
+    ldap_onclick_sort_links();
+    ldap_user_roles_revoke_form();
+}
+
+function ldap_show_groups_fieldset(data) {
+    var html = $('#groups_field', $(data)).html();
+    $('#groups_field').html(html);
+
+    ldap_group_roles_revoke_form();
+}
+
+function ldap_refresh_users_fieldset(url) {
+    $('#users_roles_waiting_response').show();
 
     $.ajax({
         url: url,
         success: function(data) {
-            var html = $("#users_field", $(data)).html();
-            $('#users_field').html(html);
+            ldap_show_users_fieldset(data);
 
-            add_onclick_sort_links();
-
-            $('#users_roles_wating_response').hide();
+            $('#users_roles_waiting_response').hide();
         },
         error: function() {
-            $('#users_roles_wating_response').hide();
+            $('#users_roles_waiting_response').hide();
             $('#users_roles_error_response').show();
         }
     });
 }
+
+function ldap_user_roles_revoke_form() {
+    $('#users_table').ajaxForm({
+    beforeSubmit: function() {
+        $('#users_roles_waiting_response').show();
+    },
+    success: function(data) {
+            ldap_show_users_fieldset(data);
+
+            $('#users_roles_waiting_response').hide();
+    },
+    error: function() {
+        $('#users_roles_waiting_response').hide();
+        $('#users_roles_error_response').show();
+    }
+    });
+}
+
+function ldap_group_roles_revoke_form() {
+    $('#groups_table').ajaxForm({
+    beforeSubmit: function() {
+        $('#groups_roles_waiting_response').show();
+    },
+    success: function(data) {
+            ldap_show_groups_fieldset(data);
+
+            $('#groups_roles_waiting_response').hide();
+    },
+    error: function() {
+        $('#groups_roles_waiting_response').hide();
+        $('#groups_roles_error_response').show();
+    }
+    });
+}
+
+function ldap_show_user_search_results(data) {
+    var html = $('#search_results_parent', $(data)).html();
+    $('#search_results_parent').html(html);
+
+    ldap_user_roles_assign_form();
+}
+
+function ldap_user_search_form() {
+    $('#frmRoles').ajaxForm({
+    beforeSubmit: function() {
+        $('#waiting_for_search_results').show();
+        $('#search_results_parent').hide();
+    },
+    success: function(data) {
+        ldap_show_user_search_results(data);
+
+        $('#waiting_for_search_results').hide();
+        $('#search_results_parent').show();
+    },
+    error: function() {
+        $('#error_on_search_results').show();
+        $('#waiting_for_search_results').hide();
+    }
+    });
+}
+
+function ldap_user_roles_assign_form() {
+    $('#search_results_form').ajaxForm({
+    beforeSubmit: function() {
+        var roles = $('#portal_roles').val();
+        if (!roles) {
+            $('#portal_roles_empty').show();
+            return false;
+        } else {
+            $('#portal_roles_empty').hide();
+        }
+        $('#assign_users_waiting_response').show();
+    },
+    success: function(data) {
+        $('#assign_users_waiting_response').hide();
+        $('#assign_users_success_response').show();
+    },
+    error: function() {
+        $('#assign_users_waiting_response').hide();
+        $('#assign_users_error_response').show();
+    }
+    });
+}
+
+function ldap_group_roles_assign_form() {
+    $('#group_roles_form').ajaxForm({
+    beforeSubmit: function() {
+        var group = $('#ldap_group').val();
+        var roles = $('#portal_roles').val();
+        if (!group) {
+            $('#ldap_group_empty').show();
+        } else {
+            $('#ldap_group_empty').hide();
+        }
+        if (!roles) {
+            $('#portal_roles_empty').show();
+        } else {
+            $('#portal_roles_empty').hide();
+        }
+        if (!group || !roles) {
+            return false;
+        }
+        $('#assign_groups_waiting_response').show();
+    },
+    success: function(data) {
+        $('#assign_groups_waiting_response').hide();
+        $('#assign_groups_success_response').show();
+    },
+    error: function() {
+        $('#assign_groups_waiting_response').hide();
+        $('#assign_groups_error_response').show();
+    }
+    });
+}
+
+
 /**
  * End Functions for LDAPUserFolder
 */
