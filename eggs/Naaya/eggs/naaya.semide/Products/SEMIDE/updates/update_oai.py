4e6d73d64fec13c06ecdb4fb34c954e72990d4f4
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-10-08 14:44:59 +0000

    Update oai indexes

--- eggs/Naaya/eggs/naaya.semide/Products/SEMIDE/updates/update_oai.py
+++ eggs/Naaya/eggs/naaya.semide/Products/SEMIDE/updates/update_oai.py
@@ -3,13 +3,16 @@ from Products.naayaUpdater.updates import UpdateScript
 class UpdateOAI(UpdateScript):
     """  """
     id = 'update_oai'
-    title='Update OAI connection url'
-    description='The connection url to the SQLAlchemy must be editable'
+    title='Update OAI Server Catalog indexes'
+    description='Update current indexes of the OAIServer\'s ZCatalog'
 
     def _update(self, portal):
         """ """
-        if hasattr(portal, 'oai'):
-            portal.oai.connection_url = 'mysql+mysqldb://semide:semide@localhost/%s_oai?charset=utf8&use_unicode=1' % portal.id
-            delattr(portal.oai, 'sqlalchemy')
-            self._p_changed = True
+        if hasattr(portal, 'zoai'):
+            catalog = portal.zoai.getCatalog()
+            catalog.manage_delIndex(catalog.indexes())
+            catalog.manage_delColumn(catalog.schema())
+            portal.zoai.add_indexes(catalog)
+            portal.zoai.add_metadata(catalog)
+            portal.zoai.update(True)
         return True
