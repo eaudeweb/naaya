d40e6d716f6793266320cf8af6c2b403d1d83a3b
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-10-18 13:32:28 +0000

    Added CRUD tests for core Naaya Content Types.

--- eggs/naaya.selenium/setup.py
+++ eggs/naaya.selenium/setup.py
@@ -2,26 +2,22 @@ from setuptools import setup, find_packages
 import os
 
 version = '0.1'
-
-setup(name='naaya.selenium',
-      version=version,
-      description="a Selenium functional test suite for Naaya",
-      long_description=open("README.rst").read(),
-      classifiers=[
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Eaudeweb',
-      author_email='office@eaudeweb.ro',
-      url='http://naaya.eaudeweb.ro/',
-      license='MPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['naaya'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'selenium',
-          'wsgiref',
-          # -*- Extra requirements: -*-
-      ],
-      )
+setup(
+    name='naaya.selenium',
+    version=version,
+    description="A selenium functional test suite for Naaya",
+    long_description=open("README.rst").read(),
+    author='Eaudeweb',
+    author_email='office@eaudeweb.ro',
+    url='http://naaya.eaudeweb.ro/',
+    license='MPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['naaya'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'selenium',
+        'wsgiref',
+    ],
+)
