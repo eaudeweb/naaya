c71abf5a6c7a09e7bb6931384efd44cd6bb7745d
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2010-10-19 12:32:26 +0000

    Fixed latest comments
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.semide@15602 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.semide/Products/SEMIDE/SEMIDESite.py
+++ eggs/naaya.semide/Products/SEMIDE/SEMIDESite.py
@@ -2452,9 +2452,10 @@ class SEMIDESite(NySite, ProfileMeta, export_pdf, SemideZip, Cacheable):
 
         objs_list = self.getCatalogedObjects(has_comments=1)
         for ob in objs_list:
-            for comm_ob in ob.get_comments_list():
-                l_comm_ob = DummyComment(ob, comm_ob, comm_ob.date)
-                comm_list.append(l_comm_ob)
+            if hasattr(ob, 'get_comments_list'):
+                for comm_ob in ob.get_comments_list():
+                    l_comm_ob = DummyComment(ob, comm_ob, comm_ob.releasedate)
+                    comm_list.append(l_comm_ob)
         return self.utSortObjsListByAttr(comm_list, 'comm_date')[:res_number]
 
     #add main topics
