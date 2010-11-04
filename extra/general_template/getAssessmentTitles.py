85e5456cec5789eaf0e9b99b646d876ec563e1a0
Alex Morega <alex@grep.ro>
2010-11-04 14:53:28 +0000

    get survey objects using relative traversal
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.ew-aoa-library@15746 a0171fae-0946-0410-966d-b1775066cd26

--- extra/general_template/getAssessmentTitles.py
+++ extra/general_template/getAssessmentTitles.py
@@ -1,17 +1,15 @@
-#answers = []
-#the_survey = getattr(container.getSite().tools.virtual_library, 'bibliography-details-each-assessment')
-#for x in the_survey.objectValues('Naaya Survey Answer'):
-#  answers.append(getattr(x, 'w_assessment-name'))
-#return answers
+#the_template = container.getSite()['tools']['general_template']['general-template']
+#the_library = container.getSite()['tools']['virtual_library']['bibliography-details-each-assessment']
+the_template = context
+the_library = container.aq_parent.aq_parent['virtual_library']['bibliography-details-each-assessment']
 
 answers = []
 the_user = container.REQUEST.AUTHENTICATED_USER.getUserName()
-the_template = getattr(container.getSite().tools.general_template, 'general-template')
 template_answers = []
 for template_answer in the_template.objectValues('Naaya Survey Answer'):
     if getattr(template_answer, 'respondent') == the_user:
         template_answers.append(getattr(template_answer, 'w_q1-name-assessment-report'))
-the_library = getattr(container.getSite().tools.virtual_library, 'bibliography-details-each-assessment')
+
 for x in the_library.objectValues('Naaya Survey Answer'):
     if getattr(x, 'w_assessment-name') not in template_answers:
         answers.append(getattr(x, 'w_assessment-name'))
