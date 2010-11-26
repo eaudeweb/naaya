005d07dcfb3afcfeed66d6b2996f258270075770
andredor <andredor@eaudeweb.ro>
2010-11-26 15:09:54 +0000

    Testing redone for user management

--- eggs/Naaya/Products/NaayaCore/AuthenticationTool/tests/test_user_management.py
+++ eggs/Naaya/Products/NaayaCore/AuthenticationTool/tests/test_user_management.py
@@ -1,54 +1,178 @@
-from unittest import TestSuite, makeSuite
-from Products.Naaya.tests import NaayaTestCase
+import transaction
+from AccessControl.Permissions import view
+from AccessControl.Permission import Permission
 
-class NaayaUserManagementTests(NaayaTestCase.NaayaTestCase):
-    def afterSetUp(self):
-        self.login()
+from Products.Naaya.tests.NaayaFunctionalTestCase import NaayaFunctionalTestCase
 
-    def beforeTearDown(self):
-        self.logout()
+class AuthTestSetup(NaayaFunctionalTestCase):
+    def setUp(self):
+        super(AuthTestSetup, self).setUp()
+        self.auth_tool = self.portal.getAuthenticationTool()
 
-    def test_userManagement(self):
-        """ Add, Find, Edit and Delete a User"""
-        usr_name = 'test2user'
-        usr_pwd = 'test-user-password'
-        usr_mail = 'test@test-email.com'
+    def tearDown(self):
+        del self.auth_tool
+        super(AuthTestSetup, self).tearDown()
 
-        #Add user.
-        self._portal().getAuthenticationTool().manage_addUser(name=usr_name,
-                    password=usr_pwd, confirm=usr_pwd, firstname=usr_name,
-                    lastname=usr_name, email=usr_mail)
+class BasicTests(AuthTestSetup):
+    def test_user_CRUD(self):
+        """ Create, Read, Update and Delete a User"""
+        user_name = 'test2user'
+        user_password = 'test-user-password'
+        user_email = 'test@test-email.com'
+        user_email2 = 'changed@test-user-email.com'
 
-        #get user object
-        usr_obj = self._portal().getAuthenticationTool().getUser(usr_name)
-        self.assertEqual(usr_obj.name, usr_name)
+        #Create user
+        self.auth_tool.manage_addUser(name=user_name,
+                                      password=user_password,
+                                      confirm=user_password,
+                                      firstname=user_name,
+                                      lastname=user_name,
+                                      email=user_email)
 
-        #change user email
-        self._portal().getAuthenticationTool().manage_changeUser(name=usr_name,
-                        password=usr_pwd, confirm=usr_pwd,
-                        email='changed@test-user-email.com',
-                        firstname=usr_name, lastname=usr_name)
-        self.assertEqual(usr_obj.email, 'changed@test-user-email.com')
+        #Read user object
+        user_obj = self.auth_tool.getUser(user_name)
+        self.assertEqual(user_obj.name, user_name)
 
-        #add user roles
-        self._portal().getAuthenticationTool().manage_addUsersRoles(
-            name=usr_obj.name,
-            roles=['Administrator', 'Manager', 'Contributor'])
-        self.assertEqual(usr_obj.roles, ['Administrator', 'Manager',
-                                         'Contributor'])
+        #Update user
+        self.auth_tool.manage_changeUser(name=user_name,
+                                         password=user_password,
+                                         confirm=user_password,
+                                         email=user_email2,
+                                         firstname=user_name,
+                                         lastname=user_name)
+        self.assertEqual(user_obj.email, user_email2)
 
-        #revoke user roles
-        self._portal().getAuthenticationTool().manage_revokeUserRole(
-            user=usr_obj.name, location='')
-        self.assertEqual(usr_obj.roles, [])
+        #Delete user
+        self.auth_tool.manage_delUsers(names=[user_obj.name])
+        self.assertEqual(self.auth_tool.getUser(user_name), None)
 
-        #delete user
-        self._portal().getAuthenticationTool().manage_delUsers(
-            names=[usr_obj.name])
-        self.assertEqual(self._portal().getAuthenticationTool().getUser(
-                                                            usr_name), None)
 
-def test_suite():
-    suite = TestSuite()
-    suite.addTest(makeSuite(NaayaUserManagementTests))
-    return suite
+class UserAuthTestSetup(AuthTestSetup):
+    def setUp(self):
+        super(UserAuthTestSetup, self).setUp()
+        self.user_name = 'test2user'
+        self.user_password = 'test-user-password'
+        self.user_email = 'test@test-email.com'
+
+        #Create user
+        self.auth_tool._doAddUser(name=self.user_name,
+                                  password=self.user_password,
+                                  roles=[],
+                                  domains=[],
+                                  firstname=self.user_name,
+                                  lastname=self.user_name,
+                                  email=self.user_email)
+
+        #Read user object
+        self.user_obj = self.auth_tool.getUser(self.user_name)
+
+        transaction.commit()
+
+    def tearDown(self):
+        del self.user_obj
+
+        #Delete user
+        self.portal.acl_users._doDelUsers([self.user_name])
+
+        del self.user_email
+        del self.user_password
+        del self.user_name
+
+        transaction.commit()
+        super(UserAuthTestSetup, self).tearDown()
+
+class UserTests(UserAuthTestSetup):
+    def test_portal_roles(self):
+        roles = ['Administrator', 'Manager', 'Contributor']
+
+        #Add user roles
+        self.auth_tool.manage_addUsersRoles(name=self.user_obj.name,
+                                            roles=roles)
+        self.assertEqual(self.user_obj.roles, roles)
+
+        #Revoke user roles
+        self.auth_tool.manage_revokeUserRole(user=self.user_obj.name,
+                                             location='')
+        self.assertEqual(self.user_obj.roles, [])
+
+
+class UserWithRolesTestSetup(UserAuthTestSetup):
+    def setUp(self):
+        super(UserWithRolesTestSetup, self).setUp()
+        roles = ['Administrator', 'Manager', 'Contributor']
+        self.auth_tool.manage_addUsersRoles(name=self.user_obj.name,
+                                            roles=roles,
+                                            location='')
+
+        transaction.commit()
+
+        self.browser_do_login(self.user_name, self.user_password)
+
+    def tearDown(self):
+        self.browser_do_logout()
+
+        self.auth_tool.manage_revokeUserRole(user=self.user_obj.name,
+                                             location='')
+        transaction.commit()
+
+        super(UserWithRolesTestSetup, self).tearDown()
+
+class UserWithRolesTests(UserWithRolesTestSetup):
+    def test_user_cant_view_site(self):
+        portal_url = 'http://localhost/portal'
+
+        self.browser.go(portal_url)
+        self.assertEqual(portal_url, self.browser.get_url())
+
+    def test_user_can_view_folder(self):
+        folder_url = 'http://localhost/portal/info'
+
+        self.browser.go(folder_url)
+        self.assertEqual(folder_url, self.browser.get_url())
+
+
+class UserWithRolesOnlyOnFolderTestSetup(UserAuthTestSetup):
+    def setUp(self):
+        super(UserWithRolesOnlyOnFolderTestSetup, self).setUp()
+
+        # get&save roles with view
+        view_perm = Permission(view, (), self.portal)
+        self.site_roles_with_view = view_perm.getRoles()
+        view_perm.setRoles(('Manager'))
+
+        roles = ['Administrator', 'Manager', 'Contributor']
+        self.auth_tool.manage_addUsersRoles(name=self.user_obj.name,
+                                            roles=roles,
+                                            location='/portal/info')
+
+        transaction.commit()
+
+        self.browser_do_login(self.user_name, self.user_password)
+
+    def tearDown(self):
+        self.browser_do_logout()
+
+        self.auth_tool.manage_revokeUserRole(user=self.user_obj.name,
+                                             location='/portal/info')
+
+        # reset portal roles with view
+        view_perm = Permission(view, (), self.portal)
+        view_perm.setRoles(self.site_roles_with_view)
+
+        transaction.commit()
+
+        super(UserWithRolesOnlyOnFolderTestSetup, self).tearDown()
+
+class UserWithRolesOnlyOnFolderTests(UserWithRolesOnlyOnFolderTestSetup):
+    def test_user_cant_view_site(self):
+        portal_url = 'http://localhost/portal'
+
+        self.browser.go(portal_url)
+        self.assertNotEqual(portal_url, self.browser.get_url())
+
+    def test_user_can_view_folder(self):
+        folder_url = 'http://localhost/portal/info'
+
+        self.browser.go(folder_url)
+        self.assertEqual(folder_url, self.browser.get_url())
+
