ae8c54d2a06c2c38c6927c9859bbbeec846b6d48
andredor <andredor@eaudeweb.ro>
2011-01-11 09:27:57 +0000

    Layout fixes from Bogdan

--- eggs/Naaya/eggs/Products.NaayaSurvey/Products/NaayaSurvey/www/survey.js
+++ eggs/Naaya/eggs/Products.NaayaSurvey/Products/NaayaSurvey/www/survey.js
@@ -30,3 +30,22 @@ $(document).ready(function(){
     }
     });
 })
+
+$(function () {
+  var msie6 = $.browser == 'msie' && $.browser.version < 7;
+  if (!msie6) {
+    var top = parseFloat($('.survey-box').css('margin-top').replace(/auto/, 0));
+    $(window).scroll(function (event) {
+      // what the y position of the scroll is
+      var y = $(this).scrollTop();
+      // whether that's below the form
+      if (y >= top) {
+        // if so, ad the fixed class
+        $('.survey-box').addClass('fixed');
+      } else {
+        // otherwise remove it
+        $('.survey-box').removeClass('fixed');
+      }
+    });
+  }
+});
