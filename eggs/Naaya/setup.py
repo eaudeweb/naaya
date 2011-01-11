cbace385bd586afaf3810a07330dad2b88b5fc5d
Alex Morega <alex@grep.ro>
2011-01-11 10:00:14 +0000

    released 2.10.12, new version 2011.01
    The versioning scheme has changed to be easy to read.

--- eggs/Naaya/setup.py
+++ eggs/Naaya/setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='2.10.12', # December 2010
+      version='2011.01', # Janyary 2011
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
