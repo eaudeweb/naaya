5c15599810e7f53737e2142db8dbb971cbd82347
andredor <andredor@eaudeweb.ro>
2011-01-21 15:55:28 +0000

    Match if name is localized
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.ew-aoa-library@16049 a0171fae-0946-0410-966d-b1775066cd26

--- extra/general_template/getAssessmentTitles.py
+++ extra/general_template/getAssessmentTitles.py
@@ -3,15 +3,44 @@
 the_template = context
 the_library = container.aq_parent.aq_parent['virtual_library']['bibliography-details-each-assessment']
 
-answers = []
-the_user = container.REQUEST.AUTHENTICATED_USER.getUserName()
-template_answers = []
-for template_answer in the_template.objectValues('Naaya Survey Answer'):
-    if getattr(template_answer, 'respondent') == the_user:
-        template_answers.append(getattr(template_answer, 'w_q1-name-assessment-report'))
+def get_all_values(answer, widget_id):
+    ret = []
 
-for x in the_library.objectValues('Naaya Survey Answer'):
-    if getattr(x, 'w_assessment-name') not in template_answers:
-        answers.append(getattr(x, 'w_assessment-name'))
-answers.sort()
-return answers
+    value = answer.get(widget_id, default=None)
+    if not isinstance(value, basestring):
+        for lang, lang_value in value.items():
+            if lang_value:
+                ret.append(lang_value)
+    else:
+        ret.append(value)
+
+    return ret
+
+posible_answers = []
+the_user = container.REQUEST.AUTHENTICATED_USER.getUserName()
+answered = []
+for template_answer in the_template.objectValues('Naaya Survey Answer'):
+    if template_answer.respondent != the_user:
+        continue
+
+    answered.append(template_answer.get('w_q1-name-assessment-report'))
+
+lang = the_library.gl_get_selected_language()
+for library_answer in the_library.objectValues('Naaya Survey Answer'):
+    if library_answer.is_draft():
+        continue
+
+    values = get_all_values(library_answer, 'w_assessment-name')
+    values = [value for value in values if value]
+    answered_values = [value for value in values if value in answered]
+    if answered_values:
+        continue
+
+    value = library_answer.get('w_assessment-name', lang=lang)
+    if not value:
+        value = values[0]
+
+    posible_answers.append(value)
+
+posible_answers.sort()
+return posible_answers
