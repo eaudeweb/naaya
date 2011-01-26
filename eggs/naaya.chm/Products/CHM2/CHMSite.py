8c0b04b39d730f362006583704a1cba2fc93b713
andredor <andredor@eaudeweb.ro>
2011-01-26 16:54:26 +0000

    Revert "CHM (#472): administrator should see the contributor and release date of objects"
    This reverts commit 06403f46edeee95e4a1266915a547d69a87f905f.
    
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.chm@16110 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.chm/Products/CHM2/CHMSite.py
+++ eggs/naaya.chm/Products/CHM2/CHMSite.py
@@ -17,7 +17,6 @@ from Products.NaayaBase.constants import *
 from Products.NaayaContent import *
 from Products.Naaya.constants import *
 from Products.NaayaCore.constants import *
-from Products.NaayaCore.FormsTool.NaayaTemplate import NaayaPageTemplateFile as nptf
 from Products.Naaya.NySite import NySite
 from Products.NaayaCore.managers.utils import utils
 from Products.NaayaLinkChecker.LinkChecker import manage_addLinkChecker
@@ -1039,6 +1038,5 @@ class CHMSite(NySite):
                 return False
 
     chm_common_css = ImageFile('www/chm_common.css', globals())
-    folder_listing = nptf('zpt/folder_listing', globals(), 'chm_folder_listing')
 
 InitializeClass(CHMSite)
