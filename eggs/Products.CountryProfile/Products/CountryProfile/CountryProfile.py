8b29a35ae6ac0e7433d1e2dd2d636157da227c64
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-02-22 13:16:29 +0000

    Added posibility to modify mysql connect settings
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.CountryProfile@16170 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.CountryProfile/Products/CountryProfile/CountryProfile.py
+++ eggs/Products.CountryProfile/Products/CountryProfile/CountryProfile.py
@@ -28,16 +28,51 @@ class CountryProfile(SimpleItem):
     icon = 'meta_type.gif'
     security = ClassSecurityInfo()
 
+    manage_options = (
+        SimpleItem.manage_options + (
+            {'label' : 'Properties', 'action' :'manage_edit_html'},
+        )
+    )
+
     def __init__(self, id):
         self.id = id
+        self.mysql_connection = {}
+        self.mysql_connection['host'] = 'localhost'
+        self.mysql_connection['name'] = 'medwis'
+        self.mysql_connection['user'] = 'cornel'
+        self.mysql_connection['pass'] = 'cornel'
 
     def open_dbconnection(self):
         """ Create and return a MySQL connection object """
         conn = MySQLConnector()
-        conn.open('localhost', 'medwis', 'cornel', 'cornel')
+        conn.open(self.mysql_connection['host'], self.mysql_connection['name'],
+                  self.mysql_connection['user'], self.mysql_connection['pass'])
         return conn
 
     _index = PageTemplateFile('zpt/index', globals())
+
+    security.declareProtected(view_management_screens, 'manage_edit_html')
+    manage_edit_html = PageTemplateFile('zpt/manage_edit', globals())
+
+    security.declareProtected(view_management_screens, 'manageProperties')
+    def manageProperties(self, REQUEST=None, **kwargs):
+        """ """
+        if not self.checkPermissionEditObject():
+            raise EXCEPTION_NOTAUTHORIZED, EXCEPTION_NOTAUTHORIZED_MSG
+
+        if REQUEST is not None:
+            params = dict(REQUEST.form)
+        else:
+            params = kwargs
+        self.mysql_connection = {}
+        self.mysql_connection['host'] = params.pop('mysql_host')
+        self.mysql_connection['name'] = params.pop('mysql_name')
+        self.mysql_connection['user'] = params.pop('mysql_user')
+        self.mysql_connection['pass'] = params.pop('mysql_pass')
+
+        self._p_changed = True
+        self.recatalogNyObject(self)
+        if REQUEST: REQUEST.RESPONSE.redirect('manage_edit_html?save=ok')
 
     security.declareProtected(view, 'index_html')
     def index_html(self, REQUEST):
