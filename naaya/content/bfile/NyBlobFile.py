d5fb805874450b39a082aa4015aeaf426295f6c2
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-02-28 15:25:10 +0000

    (#524) Added support for quick view of zip and pdf files. Also file viewers can be registered through zope component
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.bfile@16219 a0171fae-0946-0410-966d-b1775066cd26

--- naaya/content/bfile/NyBlobFile.py
+++ naaya/content/bfile/NyBlobFile.py
@@ -1,28 +1,12 @@
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# The Initial Owner of the Original Code is European Environment
-# Agency (EEA).  Portions created by Eau de Web are
-# Copyright (C) European Environment Agency.  All
-# Rights Reserved.
-#
-# Authors:
-#
-# Alex Morega, Eau de Web
-# David Batranu, Eau de Web
-
+"""Storage and provider of blob files"""
 import urllib
 
 from ZODB.blob import Blob
 from Persistence import Persistent
 from ZPublisher.Iterators import filestream_iterator
+from zope.interface import implements
+
+from interfaces import INyBlobFile
 
 from naaya.core.utils import mimetype_from_filename
 
@@ -30,9 +14,9 @@ COPY_BLOCK_SIZE = 65536 # 64KB
 DEFAULT_MIMETYPE = 'application/octet-stream'
 
 class NyBlobFile(Persistent):
-    """
-    Naaya container for files stored using ZODB Blob
-    """
+    """Naaya container for files stored using ZODB Blob"""
+
+    implements(INyBlobFile)
 
     def __init__(self, **kwargs):
         kwargs.setdefault('filename', None)
--- naaya/content/bfile/interfaces.py
+++ naaya/content/bfile/interfaces.py
@@ -1,1 +1,1 @@
-from zope.interface import Interfaceclass INyBFile(Interface):    """ Naaya Blob File content type """
+from zope.interface import Interfaceclass INyBFile(Interface):    """ Naaya Blob File content type """class INyBlobFile(Interface):    """ Blob file storage """class IBFileContentViewer(Interface):    """ Content viewer """
