c972a2a9007f7859b8506dd62a27ecc08fb161d1
andredor <andredor@eaudeweb.ro>
2011-03-18 16:11:55 +0000

    Fix hyperlinks in editor tool (#555)

--- eggs/Naaya/Products/NaayaCore/EditorTool/www/link.js
+++ eggs/Naaya/Products/NaayaCore/EditorTool/www/link.js
@@ -61,7 +61,7 @@ function insert_link() {
 function setAllAttribs(link) {
 	var inst = tinyMCEPopup.editor;
 	var dom = tinyMCEPopup.editor.dom;
-	var val = inst.settings.site_url + '/' + getCtrlValue('url');
+	var val = getCtrlValue('url');
 	dom.setAttrib(link, 'href', val);
 	var title = getCtrlValue('title');
 	if(title != '') {
