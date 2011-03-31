eafe7b80d849f2c4de8f42e784cc059c57aaf3d0
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2011-03-31 11:58:09 +0000

    Custom XML parser for europa.eu feeds

---
+++ eggs/Naaya/eggs/naaya.wise/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='naaya.wise',
+    version='0.1dev',
+    author='Eau de Web',
+    author_email='office@eaudeweb.ro',
+    url='http://naaya.eaudeweb.ro',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Naaya',
+        'lxml',
+    ]
+)
