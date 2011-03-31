7d38bbc6c2ef57887c224008b3d420445e1368a3
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-03-31 08:36:41 +0000

    Fixed periodic notifications in NaayaForum
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.NaayaForum@16409 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.NaayaForum/Products/NaayaForum/subscribers.py
+++ eggs/Products.NaayaForum/Products/NaayaForum/subscribers.py
@@ -1,17 +1,32 @@
-from interfaces import INyForumObjectAddEvent, INyForumObjectEditEvent
 from OFS.interfaces import IObjectWillBeAddedEvent
+from interfaces import INyForumObjectAddEvent, INyForumObjectEditEvent
+
+from naaya.core.zope2util import path_in_site
+from Products.NaayaCore.NotificationTool.constants import LOG_TYPES
 
 def handle_forum_object_add(event):
     obj = event.context
+    portal = obj.getSite()
     contributor = event.contributor
-    notification_tool = obj.getSite().getNotificationTool()
+    notification_tool = portal.getNotificationTool()
     notification_tool.notify_instant(obj, contributor)
+
+    if obj.approved: #Create log entry
+        action_logger = portal.getActionLogger()
+        action_logger.create(type=LOG_TYPES['created'],
+                             contributor=contributor, path=path_in_site(obj))
 
 def handle_forum_object_edit(event):
     obj = event.context
+    portal = obj.getSite()
     contributor = event.contributor
-    notification_tool = obj.getSite().getNotificationTool()
+    notification_tool = portal.getNotificationTool()
     notification_tool.notify_instant(obj, contributor, ob_edited=True)
+
+    if obj.approved: #Create log entry
+        action_logger = portal.getActionLogger()
+        action_logger.create(type=LOG_TYPES['modified'],
+                             contributor=contributor, path=path_in_site(obj))
 
 def handle_forum_deletion(ob, event):
     """The Forum will be moved/removed"""
