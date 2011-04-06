7820eadcaf2acc3a6d3bfde6788494ce4798541b
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-04-06 13:52:01 +0000

    Zope Catalog patched to use adapted objects for indexing

--- eggs/Naaya/eggs/Naaya/Products/NaayaCore/CatalogTool/interfaces.py
+++ eggs/Naaya/eggs/Naaya/Products/NaayaCore/CatalogTool/interfaces.py
@@ -2,3 +2,6 @@ from zope import interface
 
 class INyCatalogAware(interface.Interface):
     """ Object can be indexed in the Naaya catalog """
+
+class INyCatalogIndexing(interface.Interface):
+    """ Adapts objects that are about to be indexed (getattr, mainly) """
