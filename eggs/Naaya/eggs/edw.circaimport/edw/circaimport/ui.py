35f14b4133af314006b94ac096790fd5553f10c3
andredor <andredor@eaudeweb.ro>
2011-04-14 14:59:43 +0000

    Migration script for CIRCA notification subscriptions

--- eggs/Naaya/eggs/edw.circaimport/edw/circaimport/ui.py
+++ eggs/Naaya/eggs/edw.circaimport/edw/circaimport/ui.py
@@ -1,11 +1,15 @@
 from zope.publisher.browser import BrowserPage
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
+from naaya.core.zope2util import relative_object_path
+
 upload_prefix = None
 
 import_zpt = PageTemplateFile('import.zpt', globals())
 import_result_zpt = PageTemplateFile('import_result.zpt', globals())
 import_roles_zpt = PageTemplateFile('import_roles.zpt', globals())
+import_notifications_zpt = PageTemplateFile('import_notifications.zpt',
+                                            globals())
 
 class ImportFromCirca_html(BrowserPage):
     def __call__(self):
@@ -56,3 +60,51 @@ class ImportRolesFromCirca(BrowserPage):
 
         return import_roles_zpt.__of__(ctx)(user_2_role=user_2_role,
                                             group_2_role=group_2_role)
+
+
+class ImportNotificationsFromCirca(BrowserPage):
+    def __call__(self):
+        if upload_prefix is None:
+            return "upload prefix not configured"
+
+        ctx = self.context.aq_inner # because self subclasses from Explicit
+        if self.request['REQUEST_METHOD'] == 'GET':
+            return import_notifications_zpt.__of__(ctx)()
+
+        from csv_extract import get_notifications_mapping
+        name = self.request.form['filename']
+        notifications, not_matched = get_notifications_mapping(
+                                                upload_prefix + '/' + name)
+
+        subscriptions = []
+        errors = []
+        notif_tool = ctx.getNotificationTool()
+        auth_tool = ctx.getAuthenticationTool()
+        for user_id, paths in notifications.items():
+            user = auth_tool.get_user_with_userid(user_id)
+            if user is None:
+                errors.append('User not found: %s' % user_id)
+                continue
+
+            for path in paths:
+                try:
+                    ob = ctx.getSite().unrestrictedTraverse(path.strip('/'))
+                except KeyError, msg:
+                    errors.append("Couldn't find object at path: %s" % path)
+                    continue
+
+                location = relative_object_path(ob, ctx.getSite())
+                try:
+                    notif_tool.add_account_subscription(user_id,
+                                                        location,
+                                                        'instant', 'en')
+                except ValueError, msg:
+                    errors.append(msg)
+                    continue
+
+                subscriptions.append({'user_id': user_id,
+                                      'path': path})
+
+        return import_notifications_zpt.__of__(ctx)(subscriptions=subscriptions,
+                                                    not_matched=not_matched,
+                                                    errors=errors)
