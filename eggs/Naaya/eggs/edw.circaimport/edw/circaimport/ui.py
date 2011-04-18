911378dbefc45491a9b272e9c996567ea83eacb1
andredor <andredor@eaudeweb.ro>
2011-04-18 14:40:56 +0000

    Add script to migrate CIRCA ACLs to Forum

--- eggs/Naaya/eggs/edw.circaimport/edw/circaimport/ui.py
+++ eggs/Naaya/eggs/edw.circaimport/edw/circaimport/ui.py
@@ -1,5 +1,7 @@
 from zope.publisher.browser import BrowserPage
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from AccessControl.Permissions import view
+from AccessControl.Permission import Permission
 
 from naaya.core.zope2util import relative_object_path
 
@@ -10,6 +12,7 @@ import_result_zpt = PageTemplateFile('import_result.zpt', globals())
 import_roles_zpt = PageTemplateFile('import_roles.zpt', globals())
 import_notifications_zpt = PageTemplateFile('import_notifications.zpt',
                                             globals())
+import_acls_zpt = PageTemplateFile('import_acls.zpt', globals())
 
 class ImportFromCirca_html(BrowserPage):
     def __call__(self):
@@ -91,7 +94,7 @@ class ImportNotificationsFromCirca(BrowserPage):
             for path in paths:
                 try:
                     ob = ctx.getSite().unrestrictedTraverse(path.strip('/'))
-                except KeyError, msg:
+                except KeyError:
                     errors.append("Couldn't find object at path: %s" % path)
                     continue
 
@@ -110,3 +113,89 @@ class ImportNotificationsFromCirca(BrowserPage):
         return import_notifications_zpt.__of__(ctx)(subscriptions=subscriptions,
                                                     not_matched=not_matched,
                                                     errors=errors)
+
+
+class ImportACLsFromCirca(BrowserPage):
+    def __call__(self):
+        if upload_prefix is None:
+            return "upload prefix not configured"
+
+        ctx = self.context.aq_inner # because self subclasses from Explicit
+        if self.request['REQUEST_METHOD'] == 'GET':
+            return import_acls_zpt.__of__(ctx)()
+
+        auth_tool = ctx.getAuthenticationTool()
+        def set_acl_for_user(ob, user):
+            ldap_source_title = auth_tool.getUserSource(user)
+            location = relative_object_path(ob, ctx.getSite())
+            for source in auth_tool.getSources():
+                if source.title == ldap_source_title:
+                    ldap_source = source
+                    ldap_source.addUserRoles(str(user), ['Viewer'], location)
+                    break
+
+        def set_acl_for_roles(ob, roles):
+            permission_object = Permission(view, (), ob)
+            current_roles = permission_object.getRoles()
+            is_tuple = isinstance(current_roles, tuple)
+            current_roles = list(current_roles)
+            new_roles = set(roles + current_roles)
+            if is_tuple:
+                new_roles = tuple(new_roles)
+            else:
+                new_roles = list(new_roles)
+            permission_object.setRoles(new_roles)
+
+        def get_role(circa_profile):
+            ROLES_MAPPING = {} # waiting for feedback on this
+            DEFAULT_ROLE = 'Viewer'
+
+            if circa_profile in ROLES_MAPPING:
+                return ROLES_MAPPING[circa_profile]
+            return DEFAULT_ROLE
+
+        from acl_extract import get_acl_mapping
+        name = self.request.form['filename']
+
+        fd = open(upload_prefix + '/' + name, 'rb')
+        acls, not_matched = get_acl_mapping(fd)
+        fd.close()
+
+        users_map, roles_map = {}, {}
+        errors = []
+        for path, values in acls.items():
+            try:
+                ob = ctx.getSite().unrestrictedTraverse(path.strip('/'))
+            except KeyError:
+                errors.append("Couldn't find object at path: %s" % path)
+                continue
+
+            # add users acls
+            userids = [val[:-6] for val in values if val.endswith('@circa')]
+            non_userids = [val for val in values if not val.endswith('@circa')]
+            for user_id in userids:
+                user = auth_tool.get_user_with_userid(user_id)
+                if user is None:
+                    errors.append('User not found: %s' % user_id)
+                    continue
+                set_acl_for_user(ob, user)
+                if path not in users_map:
+                    users_map[path] = []
+                users_map[path].append(user_id)
+
+            # add roles acls
+            roles = [val[2:] for val in non_userids if val.startswith('__')]
+            roles = list(set(map(get_role, roles)))
+            if roles:
+                set_acl_for_roles(ob, roles)
+                roles_map[path] = roles
+
+            # not matched values
+            nonvals = [val for val in non_userids if not val.startswith('__')]
+            if nonvals:
+                errors.append('Not matched user or profile for %s' % nonvals)
+
+        return import_acls_zpt.__of__(ctx)(users_map=users_map,
+                                            roles_map=roles_map,
+                                            errors=errors,
+                                            not_matched=not_matched)
