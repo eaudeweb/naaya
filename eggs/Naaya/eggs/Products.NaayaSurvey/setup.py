d3376dd52aa214174fbc40545ebb446ae10a1d99
Alex Morega <alex@grep.ro>
2011-04-28 14:23:13 +0000

    tag version 1.2.2

--- eggs/Naaya/eggs/Products.NaayaSurvey/setup.py
+++ eggs/Naaya/eggs/Products.NaayaSurvey/setup.py
@@ -1,32 +1,16 @@
 from setuptools import setup, find_packages
-import os
 
-NAME = 'Products.NaayaSurvey'
-PATH = NAME.split('.') + ['version.txt']
-VERSION = open(os.path.join(*PATH)).read().strip()
-
-setup(name=NAME,
-      version=VERSION,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='naaya survey',
-      author='EaudeWeb',
-      author_email='office@eaudeweb.ro',
-      url='http://naaya.eaudeweb.ro/',
-      license='MPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.NaayaWidgets',
-          # -*- Extra requirements: -*-
-      ],
-      )
+setup(
+    name='Products.NaayaSurvey',
+    version="1.2.2",
+    author='EaudeWeb',
+    author_email='office@eaudeweb.ro',
+    url='http://naaya.eaudeweb.ro/',
+    license='MPL',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Products.NaayaWidgets',
+    ],
+)
