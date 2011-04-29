62a37b2783fb64189b3b466b5329ae94b5ff64b6
Alex Morega <alex@grep.ro>
2011-04-29 15:48:50 +0000

    html2text - trim with ellipsis

---
+++ eggs/Naaya/Products/NaayaCore/managers/tests/test_html2text.py
@@ -0,0 +1,26 @@
+import unittest
+from Products.NaayaCore.managers.utils import html2text
+
+class Html2TextTest(unittest.TestCase):
+    def test_blank(self):
+        self.assertEqual(html2text(""), "")
+
+    def test_strip_tags(self):
+        self.assertEqual(html2text('<input type="text">'), "")
+        self.assertEqual(html2text('<script>alert(1)</script>'), "alert(1)")
+
+    def test_trim(self):
+        self.assertEqual(html2text("<b>hello</b> world!", trim_length=4),
+                         "hell")
+        self.assertEqual(html2text("blah blah some text", trim_length=4),
+                         "blah")
+
+    def test_strip_whitespace(self):
+        self.assertEqual(html2text(' something   '), 'something')
+
+    def test_ellipsis(self):
+        self.assertEqual(html2text("some long text here", trim_length=12),
+                         "some long te")
+        self.assertEqual(html2text("some long text here",
+                                   trim_length=12, ellipsis=True),
+                         u"some long \u2026")
