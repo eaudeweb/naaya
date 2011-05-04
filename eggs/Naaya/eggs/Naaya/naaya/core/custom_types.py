91546a49912eaa2e9eb3aa6e4361a6db9e4e0b35
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-05-04 11:55:37 +0000

    Interval logic moved from type to widget

--- eggs/Naaya/eggs/Naaya/naaya/core/custom_types.py
+++ eggs/Naaya/eggs/Naaya/naaya/core/custom_types.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 
@@ -9,35 +10,14 @@ class Interval(tuple):
 
     security = ClassSecurityInfo()
 
-    def __new__(cls, start_date=None, start_time = None, end_date=None,
-                end_time = None, all_day=False):
-        if start_date is end_date is None:
-            pass
-        elif None in (start_date, end_date):
-            raise ValueError('Start time (date) and End time (date) '
-                             'must both have values')
-        elif all_day is False and None in (start_time, end_time):
-            raise ValueError('Start time (hh:mm) and End time (hh:mm) '
-                             'must both have values when all_day is False')
-        else:
-            def normalize_date(date, time, all_day, name):
-                try:
-                    ds = map(int, date.split("/"))
-                    if all_day is True:
-                        return datetime(ds[2], ds[1], ds[0])
-                    else:
-                        (h, m) = map(int, time.split(":"))
-                        return datetime(ds[2], ds[1], ds[0], h, m)
-                except Exception, e:
-                    raise ValueError('Bad value (%s, %s) for %s' % (repr(date),
-                                                                    repr(time),
-                                                                    name))
-            start_date = normalize_date(start_date, start_time, all_day,
-                                        'Start Time')
-            end_date = normalize_date(end_date, end_time, all_day, 'End Time')
-            if start_date > end_date:
-                raise ValueError('End time can not precede start time')
-
+    def __new__(cls, start_date=None, end_date=None, all_day=False):
+        if start_date is not None and start_date > end_date:
+            raise ValueError('End time can not precede start time')
+        if all_day:
+            # strip h:m since they are all_day
+            # could use start_date.date(), but we prefer to maintain data type
+            start_date = datetime(start_date.year, start_date.month, start_date.day)
+            end_date = datetime(end_date.year, end_date.month, end_date.day)
         return tuple.__new__(cls, (start_date, end_date, all_day))
 
 
@@ -57,6 +37,8 @@ class Interval(tuple):
         return self[2]
 
     def __repr__(self):
+        if self == Interval():
+            return "empty Interval()"
         data = {
             'start_date': self.start_date.strftime("%d/%m/%Y"),
             'end_date': self.end_date.strftime("%d/%m/%Y"),
--- eggs/Naaya/eggs/Naaya/naaya/core/tests/test_interval_type.py
+++ eggs/Naaya/eggs/Naaya/naaya/core/tests/test_interval_type.py
@@ -14,42 +14,29 @@ class IntervalTestCase(unittest.TestCase):
         self.assertTrue(i.all_day is False)
 
     def test_create(self):
-        i = Interval('23/03/2011', '10:00', '24/03/2011', '19:30', False)
+        start_date = datetime(2011, 3, 23, 10, 0)
+        end_date = datetime(2011, 3, 24, 19, 30)
+        i = Interval(start_date, end_date, False)
         self.assertTrue(isinstance(i.start_date, datetime))
         self.assertTrue(isinstance(i.end_date, datetime))
         self.assertTrue(isinstance(i.all_day, bool))
-        self.assertEqual(i.start_date, datetime(2011, 3, 23, 10, 0))
-        self.assertEqual(i.end_date, datetime(2011, 3, 24, 19, 30))
+        self.assertEqual(i.start_date, start_date)
+        self.assertEqual(i.end_date, end_date)
         self.assertEqual(i.all_day, False)
 
     def test_create_all_day(self):
         # hour:minutes must anyway be ignored
-        i = Interval('23/03/2011', '8:00', '24/03/2011', '19:30', True)
+        start_date = datetime(2011, 3, 23, 10, 0)
+        end_date = datetime(2011, 3, 24, 19, 30)
+        i = Interval(start_date, end_date, True)
         self.assertEqual(i.start_date, datetime(2011, 3, 23))
         self.assertEqual(i.end_date, datetime(2011, 3, 24))
         self.assertEqual(i.all_day, True)
 
-    def test_create_error(self):
-        # invalid date (format)
-        self.assertRaises(ValueError,
-             lambda: Interval('23//2011', '10:00', '24/03/2011', '19:30', True))
-        # invalid hour
-        self.assertRaises(ValueError,
-           lambda: Interval('23/03/2011', 'a:00', '24/03/2011', '29:30', False))
-        # invalid date (month)
-        self.assertRaises(ValueError,
-           lambda: Interval('03/23/2011', '10:00', '24/03/2011', '19:30', True))
-        # end time precedes start time
-        self.assertRaises(ValueError,
-           lambda: Interval('24/03/2011', '10:00', '24/03/2011', '9:30', False))
-        # shouldn't raise error, all day is True, hours ignored
-        Interval('24/03/2011', '10:00', '24/03/2011', '9:30', True)
-        # unspecified hours when all_day is False
-        self.assertRaises(ValueError,
-              lambda: Interval('23/03/2011', '', '24/03/2011', '', False))
-
     def test_repr(self):
-        i = Interval('22/03/2011', '10:00', '24/03/2011', '10:30', False)
+        start_date = datetime(2011, 3, 22, 10, 0)
+        end_date = datetime(2011, 3, 24, 10, 30)
+        i = Interval(start_date, end_date, False)
         self.assertEqual(repr(i), ("Interval:[22/03/2011, 10:00 - "
                                    "24/03/2011, 10:30; All day: False]"))
 
