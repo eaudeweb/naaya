af2a7a81f49bacfb92534a72c686b90c2a45d656
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-05-04 14:35:30 +0000

    Fixes #536 - Wrong redirects in copied sites
    Added a clone subscriber that checks __before_traverse__ value in a
    copied site and corrects the values in it.
    Added tests.
    Fixed a few imports.

--- eggs/Naaya/Products/Naaya/subscribers.py
+++ eggs/Naaya/Products/Naaya/subscribers.py
@@ -1,3 +1,5 @@
+from ZPublisher.BeforeTraverse import rewriteBeforeTraverse
+
 def update_maintopics_after_move(event):
     """ object was renamed; update maintopics """
     site = event.context.getSite()
@@ -15,3 +17,37 @@ def update_maintopics_after_move(event):
     if prefix + old_sp in mt:
         mt[mt.index(prefix + old_sp)] = prefix + new_sp
         site._p_changed = True
+
+def site_cloned(site, event):
+    """If a `INySite` was pasted check the `__before_traverse__` for
+    old object's values and delete them.
+
+    For example::
+
+        >>> portal.__before_traverse__
+            {
+                (99, 'Localizer'):
+                    <ZPublisher.BeforeTraverse.NameCaller>,
+                (99, 'Naaya Site/copy_of_portal'):
+                    <ZPublisher.BeforeTraverse.NameCaller>,
+                (99, 'Naaya Site/portal'):
+                    <ZPublisher.BeforeTraverse.NameCaller>
+            }
+
+    From the above example the last value (original object) in the dict should
+    be removed because the before_traverse hooks will use that instead of
+    new object's value.
+
+    """
+    btr = {}
+    for name, ob in site.__before_traverse__.items():
+        # Check if it is of the same content type
+        if site.meta_type in name[1]:
+            # Leave the current object and remove the old one
+            if ("%s/%s" % (site.meta_type, site.id) ==
+                name[1]):
+                btr[name] = ob
+        else:
+            btr[name] = ob
+    #Override __before_traverse__
+    rewriteBeforeTraverse(site, btr)
