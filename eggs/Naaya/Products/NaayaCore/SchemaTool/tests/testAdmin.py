6b7d7c2bea9143e5985a1c6dd4b7ca998d0f91b1
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-05-24 10:01:12 +0000

    Fix tests for admin schema

--- eggs/Naaya/Products/NaayaCore/SchemaTool/tests/testAdmin.py
+++ eggs/Naaya/Products/NaayaCore/SchemaTool/tests/testAdmin.py
@@ -1,10 +1,4 @@
-from unittest import TestSuite, makeSuite
-
-from DateTime import DateTime
-from Testing import ZopeTestCase
-from Products.Naaya.tests import NaayaTestCase, NaayaFunctionalTestCase
-
-from Products.NaayaCore.SchemaTool.Schema import Schema
+from Products.Naaya.tests import NaayaFunctionalTestCase
 
 class SchemaAdminTest(NaayaFunctionalTestCase.NaayaFunctionalTestCase):
     """ Functional TestCase for Naaya Schema Tool administration interface """
@@ -19,30 +13,26 @@ class SchemaAdminTest(NaayaFunctionalTestCase.NaayaFunctionalTestCase):
         self.browser.go('http://localhost/portal/portal_schemas/admin_html')
 
         html = self.browser.get_html()
-        self.failUnless('<h1>Manage content types</h1>' in html)
+        self.assertTrue('<h1>Manage content types</h1>' in html)
 
-        self.failUnless('HTML Document' in html)
-        self.failUnless('portal_schemas/NyDocument/admin_html' in html)
+        self.assertTrue('HTML Document' in html)
+        self.assertTrue('portal_schemas/NyDocument/admin_html' in html)
 
     def test_schema(self):
         self.browser.go('http://localhost/portal/portal_schemas/NyDocument/admin_html')
 
         html = self.browser.get_html()
-        self.failUnless('<h1>Manage content types - HTML Document</h1>' in html)
+        import pdb; pdb.set_trace()
+        self.assertTrue('<h1>Manage content types - HTML Document</h1>' in html)
 
-        self.failUnless('Title' in html)
-        self.failUnless('portal_schemas/NyDocument/title-property/admin_html' in html)
-        self.failUnless('Release date' in html)
-        self.failUnless('portal_schemas/NyDocument/releasedate-property/admin_html' in html)
+        self.assertTrue('Title' in html)
+        self.assertTrue('portal_schemas/NyDocument/title-property/admin_html' in html)
+        self.assertTrue('Release date' in html)
+        self.assertTrue('portal_schemas/NyDocument/releasedate-property/admin_html' in html)
 
     def test_schema_widget(self):
         self.browser.go('http://localhost/portal/portal_schemas/NyDocument/title-property/admin_html')
 
         html = self.browser.get_html()
-        self.failUnless('<h1>Manage content types - HTML Document - title</h1>' in html)
+        self.assertTrue('<h1>Manage content types - HTML Document - title</h1>' in html)
 
-
-def test_suite():
-    suite = TestSuite()
-    suite.addTest(makeSuite(SchemaAdminTest))
-    return suite
