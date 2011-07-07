dbbeae892835b8a5333ed5271b444874fc983a46
Alex Morega <alex@grep.ro>
2011-07-07 07:49:04 +0000

    (andredor) Tool page for full CIRCA migration
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/edw.circaimport@16794 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/edw.circaimport/edw/circaimport/__init__.py
+++ eggs/edw.circaimport/edw/circaimport/__init__.py
@@ -4,6 +4,8 @@ from zipfile import ZipFile
 
 import backupdata
 
+from naaya.core.zope2util import relative_object_path
+
 def demo():
     from actors import DemoActor
     actor = DemoActor()
@@ -60,3 +62,173 @@ def work_in_zope(context, name, root_path):
 
 def tsv2csv():
     backupdata.convert_index(sys.stdin, sys.stdout)
+
+def add_roles_from_circa_export(site, filepath, ldap_source_title):
+    from ldif_extract import get_user_and_group_mapping
+    user_2_role, group_2_role = get_user_and_group_mapping(filepath)
+
+    auth_tool = site.getAuthenticationTool()
+    for source in auth_tool.getSources():
+        if source.title == ldap_source_title:
+            ldap_source = source
+            break
+    else:
+        return 'Error: No user source named %s' % ldap_source_title
+
+    report = StringIO()
+    for userid, role in user_2_role.items():
+        if not ldap_source.has_user(userid):
+            print>>report, 'Error: no user named %s in source %s' % (userid, ldap_source_title)
+            continue
+
+        try:
+            ldap_source.addUserRoles(userid, [role])
+        except Exception, e:
+            print>>report, 'Error adding role %s for user %s: %s' % (role, userid, e)
+        else:
+            print>>report, 'Added role %s for user %s' % (role, userid)
+
+    for groupid, role in group_2_role.items():
+        try:
+            ldap_source.map_group_to_role(groupid, [role])
+        except Exception, e:
+            print>>report, 'Error adding role %s for group %s: %s' % (role, groupid, e)
+        else:
+            print>>report, 'Added role %s for group %s' % (role, groupid)
+
+    return report.getvalue()
+
+def add_notifications_from_circa_export(site, filepath):
+    from notifications_extract import get_notifications_mapping
+    dbfile = open(filepath, 'rb')
+    notifications, not_matched = get_notifications_mapping(dbfile)
+    dbfile.close()
+
+    notif_tool = site.getNotificationTool()
+    auth_tool = site.getAuthenticationTool()
+    report = StringIO()
+    for user_id, values in notifications.items():
+        user = auth_tool.get_user_with_userid(user_id)
+        if user is None:
+            print>>report, 'Error: user not found: %s' % user_id
+            continue
+
+        for val in values:
+            if val['notif_type'] == 3:
+                print>>report, 'Ignoring (turned off) subscription for user %s at location %s' % (user_id, val['path'])
+                continue
+
+            try:
+                ob = site.unrestrictedTraverse(val['path'].strip('/'))
+            except KeyError:
+                print>>report, "Couldn't find object at path: %s" % val['path']
+                continue
+
+            location = relative_object_path(ob, site)
+            try:
+                notif_tool.add_account_subscription(user_id,
+                                                    location,
+                                                    'instant', 'en')
+            except ValueError, msg:
+                print>>report, 'Error adding subscription for user %s at location %s: %s' % (user_id, val['path'], msg)
+                continue
+
+            print>>report, 'Added subscription for user %s at location %s' % (user_id, val['path'])
+
+    return report.getvalue()
+
+def add_acls_from_circa_export(site, filepath):
+    from AccessControl.Permissions import view
+    from AccessControl.Permission import Permission
+    from acl_extract import get_acl_mapping
+
+    auth_tool = site.getAuthenticationTool()
+    def set_acl_for_user(ob, user):
+        ldap_source_title = auth_tool.getUserSource(user)
+        location = relative_object_path(ob, site)
+        for source in auth_tool.getSources():
+            if source.title == ldap_source_title:
+                ldap_source = source
+                ldap_source.addUserRoles(str(user), ['Viewer'], location)
+                break
+
+    def set_acl_for_roles(ob, roles):
+        permission_object = Permission(view, (), ob)
+        current_roles = permission_object.getRoles()
+        is_tuple = isinstance(current_roles, tuple)
+        current_roles = list(current_roles)
+        new_roles = set(roles + current_roles)
+        if is_tuple:
+            new_roles = tuple(new_roles)
+        else:
+            new_roles = list(new_roles)
+        permission_object.setRoles(new_roles)
+
+    ROLES_MAPPING = {'0': 'Administrator',
+            '1': 'Viewer',
+            '2': 'Contributor',
+            '3': 'Viewer',
+            '4': 'Viewer'}
+    def compute_roles_mapping(acls):
+        non_userids = []
+        for values in acls.values():
+            non_userids.extend([val for val in values if not val.endswith('@circa')])
+        roles = [val[2:] for val in non_userids if val.startswith('__')]
+        roles = list(set(roles)) # remove duplicates
+        roles = map(int, roles)
+        roles.append(6)
+        max_role = max(roles)
+
+        ROLES_MAPPING[str(max_role)] = 'Authenticated'
+        ROLES_MAPPING[str(max_role - 1)] = 'Anonymous'
+        for i in xrange(5, max_role - 1):
+            ROLES_MAPPING[str(i)] = 'Viewer'
+
+    def get_role(circa_profile):
+        DEFAULT_ROLE = 'Viewer'
+
+        if circa_profile in ROLES_MAPPING:
+            return ROLES_MAPPING[circa_profile]
+        return DEFAULT_ROLE
+
+    fd = open(filepath, 'rb')
+    acls, not_matched = get_acl_mapping(fd)
+    fd.close()
+
+    report = StringIO()
+    compute_roles_mapping(acls)
+    for path, values in acls.items():
+        try:
+            ob = site.unrestrictedTraverse(path.strip('/'))
+        except KeyError:
+            print>>report, "Couldn't find object at path: %s" % path
+            continue
+
+        # add users acls
+        userids = [val[:-6] for val in values if val.endswith('@circa')]
+        non_userids = [val for val in values if not val.endswith('@circa')]
+        for user_id in userids:
+            user = auth_tool.get_user_with_userid(user_id)
+            if user is None:
+                print>>report, 'User not found: %s' % user_id
+                continue
+            set_acl_for_user(ob, user)
+            print>>report, 'Granted view on path %s for user %s' % (path, user_id)
+
+        # add roles acls
+        roles = [val[2:] for val in non_userids if val.startswith('__')]
+        roles = list(set(map(get_role, roles)))
+        if roles:
+            set_acl_for_roles(ob, roles)
+            print>>report, 'Granted view on path %s for roles %s' % (path, roles)
+
+        # not matched values
+        nonvals = [val for val in non_userids if not val.startswith('__')]
+        if nonvals:
+            print>>report, 'Not matched user or profile for %s' % nonvals
+
+    if not_matched:
+        print>>report, 'Not matched rows (from the exported file): %s' % not_matched
+
+    return report.getvalue()
+
--- eggs/edw.circaimport/edw/circaimport/ui.py
+++ eggs/edw.circaimport/edw/circaimport/ui.py
@@ -1,18 +1,53 @@
+import os.path
+
 from zope.publisher.browser import BrowserPage
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from AccessControl.Permissions import view
-from AccessControl.Permission import Permission
 
-from naaya.core.zope2util import relative_object_path
+from edw.circaimport import (work_in_zope, add_roles_from_circa_export,
+                             add_notifications_from_circa_export,
+                             add_acls_from_circa_export)
 
 upload_prefix = None
 
+import_all_zpt = PageTemplateFile('import_all.zpt', globals())
 import_files_zpt = PageTemplateFile('import_files.zpt', globals())
 import_files_result_zpt = PageTemplateFile('import_files_result.zpt', globals())
 import_roles_zpt = PageTemplateFile('import_roles.zpt', globals())
 import_notifications_zpt = PageTemplateFile('import_notifications.zpt',
-                                            globals())
+                                        globals())
 import_acls_zpt = PageTemplateFile('import_acls.zpt', globals())
+
+class ImportAllFromCirca(BrowserPage):
+    def __call__(self):
+        if upload_prefix is None:
+            return "upload prefix not configured"
+        ctx = self.context.aq_inner # because self subclasses from Explicit
+        if self.request['REQUEST_METHOD'] == 'GET':
+            return import_all_zpt.__of__(ctx)()
+
+        reports = []
+
+        filename_files = self.request.form['filename_files']
+        import_files_path = self.request.form['import_files_path']
+        if filename_files and import_files_path:
+            files_ctx = ctx.restrictedTraverse(import_files_path)
+            reports.append(work_in_zope(files_ctx, filename_files, upload_prefix))
+
+        filename_roles = self.request.form['filename_roles']
+        ldap_source_title = self.request.form['source_title']
+        if filename_roles and ldap_source_title:
+            reports.append(add_roles_from_circa_export(ctx, os.path.join(upload_prefix, filename_roles), ldap_source_title))
+
+        filename_notifications = self.request.form['filename_notifications']
+        if filename_notifications:
+            reports.append(add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, filename_notifications)))
+
+        filename_acls = self.request.form['filename_acls']
+        if filename_acls:
+            reports.append(add_acls_from_circa_export(ctx, os.path.join(upload_prefix, filename_acls)))
+
+        return import_all_zpt.__of__(ctx)(reports=reports)
+
 
 class ImportFilesFromCirca_html(BrowserPage):
     def __call__(self):
@@ -24,7 +59,6 @@ class ImportFilesFromCirca(BrowserPage):
         if upload_prefix is None:
             return "upload prefix not configured"
         ctx = self.context.aq_inner # because self subclasses from Explicit
-        from edw.circaimport import work_in_zope
         #name = ctx.REQUEST.get('name')
         name = self.request.form['filename']
         report = work_in_zope(ctx, name, upload_prefix)
@@ -40,29 +74,12 @@ class ImportRolesFromCirca(BrowserPage):
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_roles_zpt.__of__(ctx)()
 
-        from ldif_extract import get_user_and_group_mapping
         name = self.request.form['filename']
         ldap_source_title = self.request.form['source_title']
-        user_2_role, group_2_role = get_user_and_group_mapping(
-                                                upload_prefix + '/' + name)
 
-        auth_tool = ctx.getAuthenticationTool()
-        for source in auth_tool.getSources():
-            if source.title == ldap_source_title:
-                ldap_source = source
-                break
-        else:
-            report = 'No user source named %s' % ldap_source_title
-            return import_roles_zpt.__of__(ctx)(report=report)
+        report = add_roles_from_circa_export(ctx, os.path.join(upload_prefix, name), ldap_source_title)
 
-        for userid, role in user_2_role.items():
-            ldap_source.addUserRoles(userid, [role])
-
-        for groupid, role in group_2_role.items():
-            ldap_source.map_group_to_role(groupid, [role])
-
-        return import_roles_zpt.__of__(ctx)(user_2_role=user_2_role,
-                                            group_2_role=group_2_role)
+        return import_roles_zpt.__of__(ctx)(report=report)
 
 
 class ImportNotificationsFromCirca(BrowserPage):
@@ -74,53 +91,11 @@ class ImportNotificationsFromCirca(BrowserPage):
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_notifications_zpt.__of__(ctx)()
 
-        from notifications_extract import get_notifications_mapping
         name = self.request.form['filename']
 
-        dbfile = open(upload_prefix + '/' + name, 'rb')
-        notifications, not_matched = get_notifications_mapping(dbfile)
-        dbfile.close()
+        report = add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, name))
 
-        subscriptions = []
-        errors = []
-        ignored = {}
-        notif_tool = ctx.getNotificationTool()
-        auth_tool = ctx.getAuthenticationTool()
-        for user_id, values in notifications.items():
-            user = auth_tool.get_user_with_userid(user_id)
-            if user is None:
-                errors.append('User not found: %s' % user_id)
-                continue
-
-            for val in values:
-                if val['notif_type'] == 3:
-                    if user_id not in ignored:
-                        ignored[user_id] = []
-                    ignored[user_id].append(val['path'])
-                    continue
-
-                try:
-                    ob = ctx.getSite().unrestrictedTraverse(val['path'].strip('/'))
-                except KeyError:
-                    errors.append("Couldn't find object at path: %s" % val['path'])
-                    continue
-
-                location = relative_object_path(ob, ctx.getSite())
-                try:
-                    notif_tool.add_account_subscription(user_id,
-                                                        location,
-                                                        'instant', 'en')
-                except ValueError, msg:
-                    errors.append(msg)
-                    continue
-
-                subscriptions.append({'user_id': user_id,
-                                      'path': val['path']})
-
-        return import_notifications_zpt.__of__(ctx)(subscriptions=subscriptions,
-                                                    not_matched=not_matched,
-                                                    ignored=ignored,
-                                                    errors=errors)
+        return import_notifications_zpt.__of__(ctx)(report=report)
 
 
 class ImportACLsFromCirca(BrowserPage):
@@ -132,98 +107,8 @@ class ImportACLsFromCirca(BrowserPage):
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_acls_zpt.__of__(ctx)()
 
-        auth_tool = ctx.getAuthenticationTool()
-        def set_acl_for_user(ob, user):
-            ldap_source_title = auth_tool.getUserSource(user)
-            location = relative_object_path(ob, ctx.getSite())
-            for source in auth_tool.getSources():
-                if source.title == ldap_source_title:
-                    ldap_source = source
-                    ldap_source.addUserRoles(str(user), ['Viewer'], location)
-                    break
-
-        def set_acl_for_roles(ob, roles):
-            permission_object = Permission(view, (), ob)
-            current_roles = permission_object.getRoles()
-            is_tuple = isinstance(current_roles, tuple)
-            current_roles = list(current_roles)
-            new_roles = set(roles + current_roles)
-            if is_tuple:
-                new_roles = tuple(new_roles)
-            else:
-                new_roles = list(new_roles)
-            permission_object.setRoles(new_roles)
-
-        ROLES_MAPPING = {'0': 'Administrator',
-                '1': 'Viewer',
-                '2': 'Contributor',
-                '3': 'Viewer',
-                '4': 'Viewer'}
-        def compute_roles_mapping(acls):
-            non_userids = []
-            for values in acls.values():
-                non_userids.extend([val for val in values if not val.endswith('@circa')])
-            roles = [val[2:] for val in non_userids if val.startswith('__')]
-            roles = list(set(roles)) # remove duplicates
-            roles = map(int, roles)
-            roles.append(6)
-            max_role = max(roles)
-
-            ROLES_MAPPING[str(max_role)] = 'Authenticated'
-            ROLES_MAPPING[str(max_role - 1)] = 'Anonymous'
-            for i in xrange(5, max_role - 1):
-                ROLES_MAPPING[str(i)] = 'Viewer'
-
-        def get_role(circa_profile):
-            DEFAULT_ROLE = 'Viewer'
-
-            if circa_profile in ROLES_MAPPING:
-                return ROLES_MAPPING[circa_profile]
-            return DEFAULT_ROLE
-
-        from acl_extract import get_acl_mapping
         name = self.request.form['filename']
 
-        fd = open(upload_prefix + '/' + name, 'rb')
-        acls, not_matched = get_acl_mapping(fd)
-        fd.close()
+        report = add_acls_from_circa_export(ctx, os.path.join(upload_prefix, name))
 
-        users_map, roles_map = {}, {}
-        errors = []
-        compute_roles_mapping(acls)
-        for path, values in acls.items():
-            try:
-                ob = ctx.getSite().unrestrictedTraverse(path.strip('/'))
-            except KeyError:
-                errors.append("Couldn't find object at path: %s" % path)
-                continue
-
-            # add users acls
-            userids = [val[:-6] for val in values if val.endswith('@circa')]
-            non_userids = [val for val in values if not val.endswith('@circa')]
-            for user_id in userids:
-                user = auth_tool.get_user_with_userid(user_id)
-                if user is None:
-                    errors.append('User not found: %s' % user_id)
-                    continue
-                set_acl_for_user(ob, user)
-                if path not in users_map:
-                    users_map[path] = []
-                users_map[path].append(user_id)
-
-            # add roles acls
-            roles = [val[2:] for val in non_userids if val.startswith('__')]
-            roles = list(set(map(get_role, roles)))
-            if roles:
-                set_acl_for_roles(ob, roles)
-                roles_map[path] = roles
-
-            # not matched values
-            nonvals = [val for val in non_userids if not val.startswith('__')]
-            if nonvals:
-                errors.append('Not matched user or profile for %s' % nonvals)
-
-        return import_acls_zpt.__of__(ctx)(users_map=users_map,
-                                            roles_map=roles_map,
-                                            errors=errors,
-                                            not_matched=not_matched)
+        return import_acls_zpt.__of__(ctx)(report=report)
