e801b8acdc848a26feb1511263f51e37d46c31f8
Alex Morega <alex@grep.ro>
2011-07-07 08:05:27 +0000

    (andredor) Heading document types for viewer aggregator
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.ew-aoa-library@16892 a0171fae-0946-0410-966d-b1775066cd26

--- extra/viewer_aggregator/aggregate_vl_cf_viewers.py
+++ extra/viewer_aggregator/aggregate_vl_cf_viewers.py
@@ -13,4 +13,17 @@ for shadow in cf_shadows:
 for shadow in vl_shadows:
     group_by_document_type.setdefault(shadow.document_type, []).append(shadow)
 
-return group_by_document_type
+document_type_names = cf['w_type-document'].getChoices()
+heading_document_types = container.heading_document_types()
+
+ret = {}
+for heading, document_types in heading_document_types.items():
+    ret[heading] = {}
+    for dt in document_types:
+        dt_i = document_type_names.index(dt)
+        if dt_i in group_by_document_type:
+            ret[heading][dt_i] = group_by_document_type[dt_i]
+
+ret = list(ret.items())
+ret.sort()
+return ret
