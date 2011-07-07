96c05df1c06c91aeef56b4d130da43edd9aa75c4
Alex Morega <alex@grep.ro>
2011-07-07 07:59:26 +0000

    (andredor) use logging module for generating reports

--- eggs/edw.circaimport/edw/circaimport/ui.py
+++ eggs/edw.circaimport/edw/circaimport/ui.py
@@ -1,15 +1,20 @@
 import os.path
+import logging
+from StringIO import StringIO
 
 from zope.publisher.browser import BrowserPage
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 import transaction
 
-from middleware import (work_in_zope, get_acl_users_sources_titles,
+from middleware import (add_files_and_folders_from_circa_export,
+                             get_acl_users_sources_titles,
                              add_roles_from_circa_export,
                              add_notifications_from_circa_export,
                              add_acls_from_circa_export)
 
 upload_prefix = None
+logger = logging.getLogger('edw.circaimport.ui')
+logger.setLevel(logging.DEBUG)
 
 import_all_zpt = PageTemplateFile('zpt/import_all.zpt', globals())
 import_files_zpt = PageTemplateFile('zpt/import_files.zpt', globals())
@@ -20,6 +25,13 @@ import_notifications_zpt = PageTemplateFile('zpt/import_notifications.zpt',
                                         globals())
 import_acls_zpt = PageTemplateFile('zpt/import_acls.zpt', globals())
 
+def init_log_stream():
+    log = StringIO()
+    handler = logging.StreamHandler(log)
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return log
+
 class ImportAllFromCirca(BrowserPage):
     def __call__(self):
         if upload_prefix is None:
@@ -29,8 +41,7 @@ class ImportAllFromCirca(BrowserPage):
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_all_zpt.__of__(ctx)(sources=sources)
 
-        reports = []
-
+        log = init_log_stream()
         try:
             if 'report' in self.request.form:
                 savepoint = transaction.savepoint()
@@ -39,26 +50,26 @@ class ImportAllFromCirca(BrowserPage):
             import_files_path = self.request.form['import_files_path']
             if filename_files and import_files_path:
                 files_ctx = ctx.restrictedTraverse(import_files_path)
-                reports.append(work_in_zope(files_ctx, filename_files, upload_prefix))
+                add_files_and_folders_from_circa_export(files_ctx, filename_files, upload_prefix)
 
             filename_roles = self.request.form['filename_roles']
             ldap_source_title = self.request.form['source_title']
             if filename_roles and ldap_source_title:
-                reports.append(add_roles_from_circa_export(ctx, os.path.join(upload_prefix, filename_roles), ldap_source_title))
+                add_roles_from_circa_export(ctx, os.path.join(upload_prefix, filename_roles), ldap_source_title)
 
             filename_notifications = self.request.form['filename_notifications']
             if filename_notifications:
-                reports.append(add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, filename_notifications)))
+                add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, filename_notifications))
 
             filename_acls = self.request.form['filename_acls']
             if filename_acls:
-                reports.append(add_acls_from_circa_export(ctx, os.path.join(upload_prefix, filename_acls)))
+                add_acls_from_circa_export(ctx, os.path.join(upload_prefix, filename_acls))
 
         finally:
             if 'report' in self.request.form:
                 savepoint.rollback()
 
-        return import_all_zpt.__of__(ctx)(sources=sources, reports=reports)
+        return import_all_zpt.__of__(ctx)(sources=sources, report=log.getvalue())
 
 
 class ImportFilesFromCirca_html(BrowserPage):
@@ -73,8 +84,12 @@ class ImportFilesFromCirca(BrowserPage):
         ctx = self.context.aq_inner # because self subclasses from Explicit
         #name = ctx.REQUEST.get('name')
         name = self.request.form['filename']
-        report = work_in_zope(ctx, name, upload_prefix)
-        return import_files_result_zpt.__of__(ctx)(report=report)
+
+        log = init_log_stream()
+
+        add_files_and_folders_from_circa_export(ctx, name, upload_prefix)
+
+        return import_files_result_zpt.__of__(ctx)(report=log.getvalue())
 
 
 class ImportRolesFromCirca(BrowserPage):
@@ -90,9 +105,11 @@ class ImportRolesFromCirca(BrowserPage):
         name = self.request.form['filename']
         ldap_source_title = self.request.form['source_title']
 
-        report = add_roles_from_circa_export(ctx, os.path.join(upload_prefix, name), ldap_source_title)
+        log = init_log_stream()
 
-        return import_roles_zpt.__of__(ctx)(sources=sources, report=report)
+        add_roles_from_circa_export(ctx, os.path.join(upload_prefix, name), ldap_source_title)
+
+        return import_roles_zpt.__of__(ctx)(sources=sources, report=log.getvalue())
 
 
 class ImportNotificationsFromCirca(BrowserPage):
@@ -106,9 +123,11 @@ class ImportNotificationsFromCirca(BrowserPage):
 
         name = self.request.form['filename']
 
-        report = add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, name))
+        log = init_log_stream()
 
-        return import_notifications_zpt.__of__(ctx)(report=report)
+        add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, name))
+
+        return import_notifications_zpt.__of__(ctx)(report=log.getvalue())
 
 
 class ImportACLsFromCirca(BrowserPage):
@@ -122,6 +141,8 @@ class ImportACLsFromCirca(BrowserPage):
 
         name = self.request.form['filename']
 
-        report = add_acls_from_circa_export(ctx, os.path.join(upload_prefix, name))
+        log = init_log_stream()
 
-        return import_acls_zpt.__of__(ctx)(report=report)
+        add_acls_from_circa_export(ctx, os.path.join(upload_prefix, name))
+
+        return import_acls_zpt.__of__(ctx)(report=log.getvalue())
