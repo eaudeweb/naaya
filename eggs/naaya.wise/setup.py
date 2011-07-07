dfc95367f3001a29ac30faa06005c95ab6a5cab5
Alex Morega <alex@grep.ro>
2011-07-07 08:06:31 +0000

    (nituacor) #577 (Homepage changes for the BISE portal)
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.wise@16900 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.wise/setup.py
+++ eggs/naaya.wise/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.wise',
-    version='0.1dev',
+    version='0.3dev',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
