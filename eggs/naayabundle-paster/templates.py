5f28260a68eef29f6c16d6d2753e2603b179e39d
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-07-22 09:30:11 +0000

    Initial import

---
+++ eggs/naayabundle-paster/templates.py
@@ -0,0 +1,19 @@
+from paste.script.templates import Template, var
+from paste.util.template import paste_script_template_renderer
+
+class NaayaBundleTemplate(Template):
+    """A paster script template for creating naaya bundles."""
+
+    _template_dir = 'bundle-template'
+    summary = 'Create a bundle'
+    vars = [
+        var('version', 'Version (like 0.1)', default='0.1'),
+        var('description', 'One-line description', default='Naaya bundle'),
+        var('keywords', 'Space-separated keywords/tags', default='naaya bundle'),
+        var('author', 'Author name', default='Eau de Web'),
+        var('author_email', 'Author email', default='office@eaudeweb.ro'),
+        var('url', 'URL', default='http://naaya.eaudeweb.ro/'),
+        var('license_name', 'License name', default='MPL'),
+        var('parent', 'Parent naaya bundle. Ex: Naaya, CHM, Groupware', default='Naaya')
+    ]
+    template_renderer = staticmethod(paste_script_template_renderer)
