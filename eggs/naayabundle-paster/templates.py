32d78853262cee80a90c0d210ae76cdd6a18a151
Alex Plugaru <alex.plugaru@eaudeweb.ro>
2011-07-22 14:49:39 +0000

    Tweaked the template. Removed run.py. Added more documentation.

--- eggs/naayabundle-paster/templates.py
+++ eggs/naayabundle-paster/templates.py
@@ -7,13 +7,15 @@ class NaayaBundleTemplate(Template):
     _template_dir = 'bundle-template'
     summary = 'Create a bundle'
     vars = [
-        var('version', 'Version (like 0.1)', default='0.1'),
+        var('version', 'Version', default='0.1'),
         var('description', 'One-line description', default='Naaya bundle'),
-        var('keywords', 'Space-separated keywords/tags', default='naaya bundle'),
+        var('long_description', 'Multi-line description (in reST)'),
+        var('keywords', 'Space-separated keywords', default='naaya bundle'),
         var('author', 'Author name', default='Eau de Web'),
         var('author_email', 'Author email', default='office@eaudeweb.ro'),
         var('url', 'URL', default='http://naaya.eaudeweb.ro/'),
         var('license_name', 'License name', default='MPL'),
-        var('parent', 'Parent naaya bundle. Ex: Naaya, CHM, Groupware', default='Naaya')
+        var('parent', 'Parent naaya bundle. Ex: Naaya, CHM, Groupware',
+            default='Naaya')
     ]
     template_renderer = staticmethod(paste_script_template_renderer)
