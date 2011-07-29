0e4b7da76680fa7ebfb11508bc4702414a9618c7
Alex Morega <alex@grep.ro>
2011-07-29 16:11:39 +0000

    convert TestOrderFolder to unit test

--- eggs/Naaya/naaya/core/tests/test_folderutils.py
+++ eggs/Naaya/naaya/core/tests/test_folderutils.py
@@ -1,24 +1,27 @@
-from Products.Naaya.tests.NaayaTestCase import NaayaTestCase
+import unittest
+#from Products.Naaya.NyFolder import NyFolder, manage_addFolder
+from OFS.Folder import Folder, manage_addFolder
 from naaya.core import folderutils
 
-class TestOrderFolder(NaayaTestCase):
-    def afterSetUp(self):
-        self.portal.manage_addFolder('folder1')
-        self.portal.folder1.manage_addFolder('folder11')
-        self.portal.folder1.manage_addFolder('folder12')
-        self.portal.folder1.manage_addFolder('folder13')
+class TestOrderFolder(unittest.TestCase):
+
+    def setUp(self):
+        self.folder = Folder('folder1')
+        manage_addFolder(self.folder, 'folder11')
+        manage_addFolder(self.folder, 'folder12')
+        manage_addFolder(self.folder, 'folder13')
 
     def test_basic(self):
         """ Let's create a folder with a few objects and change their order """
         order_seq = ['folder11', 'folder13', 'folder12']
-        folderutils.sort_folder(self.portal.folder1, order_seq)
-        self.assertEqual(self.portal.folder1.objectIds(), order_seq)
+        folderutils.sort_folder(self.folder, order_seq)
+        self.assertEqual(self.folder.objectIds(), order_seq)
 
     def test_unchanged(self):
         """ Nothing changes """
-        order_seq = self.portal.folder1.objectIds()
-        folderutils.sort_folder(self.portal.folder1, order_seq)
-        self.assertEqual(self.portal.folder1.objectIds(), order_seq)
+        order_seq = self.folder.objectIds()
+        folderutils.sort_folder(self.folder, order_seq)
+        self.assertEqual(self.folder.objectIds(), order_seq)
 
     def test_incomplete(self):
         """ Some ids from the folder are missing in the call. This means that
@@ -26,13 +29,13 @@ class TestOrderFolder(NaayaTestCase):
         end."""
 
         order_seq = ['folder13', 'folder12']
-        folderutils.sort_folder(self.portal.folder1, order_seq)
-        self.assertEqual(self.portal.folder1.objectIds(),
-                order_seq + ['folder11'])
+        folderutils.sort_folder(self.folder, order_seq)
+        self.assertEqual(self.folder.objectIds(), order_seq + ['folder11'])
 
     def test_extra_id(self):
         """ An ID that is not inside the folder """
 
         order_seq = ['folder13', 'asdf']
-        folderutils.sort_folder(self.portal.folder1, order_seq)
-        self.assertEqual(['folder13', 'folder12', 'folder11'])
+        folderutils.sort_folder(self.folder, order_seq)
+        self.assertEqual(self.folder.objectIds(),
+                         ['folder13', 'folder11', 'folder12'])
