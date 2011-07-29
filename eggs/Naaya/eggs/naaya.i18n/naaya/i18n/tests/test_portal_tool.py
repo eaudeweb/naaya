a1e06d298c9574c698e55fe77f6ceb9b7c39c55a
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-07-29 09:50:33 +0000

    naaya.i18n langs ordering and small admin fixes

--- eggs/Naaya/eggs/naaya.i18n/naaya/i18n/tests/test_portal_tool.py
+++ eggs/Naaya/eggs/naaya.i18n/naaya/i18n/tests/test_portal_tool.py
@@ -53,7 +53,7 @@ class TestNySiteApi(NaayaTestCase):
 
     def test_get_languages(self):
         self.assertEqual(self.portal.gl_get_languages(),
-                         ('de', DEFAULT_PORTAL_LANGUAGE_CODE))
+                         (DEFAULT_PORTAL_LANGUAGE_CODE, 'de'))
 
     def test_get_languages_mapping(self):
         self.portal.gl_change_site_defaultlang(DEFAULT_PORTAL_LANGUAGE_CODE)
@@ -104,8 +104,9 @@ class TestNySiteApi(NaayaTestCase):
 
     def test_add_site_language(self):
         self.portal.gl_add_site_language('fr')
+        self.portal.gl_add_site_language('ar')
         self.assertEqual(self.portal.gl_get_languages(),
-                         ('de', DEFAULT_PORTAL_LANGUAGE_CODE, 'fr'))
+                         (DEFAULT_PORTAL_LANGUAGE_CODE, 'ar', 'de', 'fr'))
 
     def test_del_site_languages(self):
         self.portal.gl_add_site_language('fr')
