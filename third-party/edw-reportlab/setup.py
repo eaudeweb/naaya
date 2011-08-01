e3a3ce0b4cb63b3624ed0ceca2b1767c878b68fd
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-08-01 12:58:02 +0000

    edw-reportlab fixed on svn: namespace,  reserved word, egg-info

--- third-party/edw-reportlab/setup.py
+++ third-party/edw-reportlab/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.20'
+version = '1.20.1'
 
 setup(name='edw-reportlab',
       version=version,
@@ -18,7 +18,6 @@ setup(name='edw-reportlab',
       url='http://svn.plone.org/svn/collective/',
       license='MPL',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['edw'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
