bc4c171457fc22864ea7bd20a4a7bd4bf1753ebb
andredor <andredor@eaudeweb.ro>
2011-08-17 10:01:06 +0000

    Move Fix CHM terms glossary update script to the glossary product
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content-expnet@17156 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content-expnet/naaya/content/project/updates.py
+++ eggs/naaya.content-expnet/naaya/content/project/updates.py
@@ -80,16 +80,3 @@ class MainTopicsToChmTerms(UpdateScript):
         else:
             self.log.debug('Nothing to update')
             return True
-
-class FixTypo(UpdateScript):
-    title = 'Fix typo in CHM terms'
-    authors = ('Stanciu Gabriel', )
-    creation_date = 'Aug 9, 2011'
-
-    def _update(self, portal):
-        ob = portal.chm_terms._getOb('04')._getOb('04_10')
-        old_title = ob.title
-        if ob.title == 'Biodiversity and  tourism':
-            ob.title = ob.title.replace(' tourism', 'tourism')
-            self.log.debug('"%s" is now: "%s"'%(old_title,ob.title))
-        return True
