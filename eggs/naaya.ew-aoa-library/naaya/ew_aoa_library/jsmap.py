45a38a1ff62083cfb73d15e724076e2e3febac33
Alex Morega <alex@grep.ro>
2011-08-19 15:11:31 +0000

    load async map config, including all documents

--- eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/jsmap.py
+++ eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/jsmap.py
@@ -18,26 +18,26 @@ country_code = json.load(open(os.path.join(os.path.dirname(__file__),
                                            'country_code.json')))
 
 
-def catalog_filters_for_shadows(site):
-    return {
-        'meta_type': "Naaya EW_AOA Shadow Object",
-        'path': [
-            ofs_path(site['virtual-library-viewer']),
-            ofs_path(site['country-fiches-viewer']),
-        ],
-    }
-
-
 def shadow_to_dict(shadow):
+    pth = shadow.getPhysicalPath()
+    if 'virtual-library-viewer' in pth:
+        library = 'virtual-library'
+    elif 'country-fiches-viewer' in pth:
+        library = 'other-objects'
+    else:
+        library = None
+
     return {
         "title": shadow.viewer_title_en,
         "country": shadow.viewer_country,
         "region": shadow.viewer_region,
+        "geolevel": shadow.viewer_geolevel,
         "theme": shadow.viewer_main_theme,
         "document_type": shadow.viewer_document_type,
         "year": shadow.viewer_year,
         "author": shadow.viewer_author,
         "url": shadow.url,
+        "library": library,
     }
 
 
@@ -49,101 +49,29 @@ def all_documents(site):
         yield shadow_to_dict(shadow)
 
 
-def filter_documents(ctx, request):
-    site = ctx.getSite()
-    lang = site.gl_get_selected_language()
-
-    def get_field(name, default=None):
-        value = request.form.get(name, '').decode('utf-8').strip()
-        return value or default
-
-    country = request.form.get('country[]', [])
-    if isinstance(country, basestring):
-        country = [country]
-
-    filters = catalog_filters_for_shadows(site)
-    filters.update({
-        'viewer_country': {'query': country, 'operator': 'and'},
-        'viewer_main_theme': get_field('theme'),
-        'viewer_year': get_field('year'),
-        'viewer_title_'+lang: get_field('text', ''),
-    })
-
-    library = get_field('library')
-    if library == 'virtual-library':
-        filters['path'] = [ofs_path(site['virtual-library-viewer'])]
-    elif library == 'other-objects':
-        filters['path'] = [ofs_path(site['country-fiches-viewer'])]
-
-    for name in ['viewer_country', 'viewer_main_theme', 'viewer_year']:
-        if filters[name] in (None, []):
-            del filters[name]
-
-    for brain in site.getCatalogTool()(**filters):
-        yield shadow_to_dict(brain.getObject())
+def get_country_index(site):
+    from shadow import get_countries_mapping
+    cf_viewer = site['country-fiches-viewer']
+    cf_survey = cf_viewer.target_survey()
+    return dict((v,k) for k, v in get_countries_mapping(cf_survey).iteritems())
 
 
-def do_search(ctx, request):
+def get_map_async_config(site):
     t0 = time()
-    documents = list(filter_documents(ctx, request))
-
-    return json.dumps({
+    return {
+        'country_code': country_code,
+        'country_index': get_country_index(site),
+        'documents': list(all_documents(site)),
         'query-time': time() - t0,
-        'documents': documents,
-    })
+    }
 
 
 class SearchMapDocuments(BrowserPage):
     def __call__(self):
         aoa_devel_hook(__name__)
         aoa_devel_hook('naaya.ew_aoa_library.shadow')
-        return do_search(self.aq_parent, self.request)
-
-
-def documents_summary(site):
-    t0 = time()
-    for brain in site.getCatalogTool()(**catalog_filters_for_shadows(site)):
-        yield {
-            'countries': brain['viewer_country'],
-            'themes': brain['viewer_main_theme'],
-        }
-    #print 'documents_summary:', time() - t0
-
-
-def search_map_initial_data(site):
-    from shadow import get_countries_mapping
-    cf_viewer = site['country-fiches-viewer']
-    cf_survey = cf_viewer.target_survey()
-    country_index = dict((v,k) for k, v in
-                         get_countries_mapping(cf_survey).iteritems())
-
-    return {
-        'document_types': get_document_types(site),
-        'documents_summary': list(documents_summary(site)),
-        'country_code': country_code,
-        'country_index': country_index,
-    }
-
-
-class SearchMapInitial(BrowserPage):
-    def __call__(self):
-        aoa_devel_hook(__name__)
-        aoa_devel_hook('naaya.ew_aoa_library.shadow')
-        return json.dumps(search_map_initial_data(self.aq_parent.getSite()))
-
-
-def get_document_types(site):
-    vl_viewer = site['virtual-library-viewer']
-    vl_survey = vl_viewer.target_survey()
-    vl_document_types = vl_survey['w_type-document'].getChoices()[1:]
-
-    cf_viewer = site['country-fiches-viewer']
-    cf_survey = cf_viewer.target_survey()
-    cf_document_types = cf_survey['w_type-document'].getChoices()[1:]
-
-    document_types = set(force_to_unicode(t) for t in
-                         cf_document_types + vl_document_types)
-    return sorted(document_types)
+        site = self.aq_parent.getSite()
+        return json.dumps(get_map_async_config(site))
 
 
 tiles_url = getConfiguration().environment.get('AOA_MAP_TILES', '')
