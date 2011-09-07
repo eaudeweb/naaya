745910360e398f89220448c8d27376a527802f11
Alex Morega <alex@grep.ro>
2011-09-07 16:00:43 +0000

    change URL structure to support multiple languages

--- eggs/Naaya/eggs/eea.aoamap/eea/aoamap/browser/naaya_views.py
+++ eggs/Naaya/eggs/eea.aoamap/eea/aoamap/browser/naaya_views.py
@@ -11,6 +11,12 @@ class SearchMap(plone_views.AoaMap):
         site = self.aq_parent.getSite()
         return site.absolute_url() + '/jsmap_search_map_documents'
 
+    def _get_current_language(self):
+        return self.aq_parent.gl_get_selected_language()
+
+    def _get_root_url(self):
+        return ''
+
     def __call__(self):
         options = {
             'map_html': self.get_map_html(),
