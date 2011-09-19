3c765e67798cdd7047d6b3ffa600df5d001c69f5
Alex Morega <alex@grep.ro>
2011-09-19 15:34:40 +0000

    basic region info pages

--- eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/country_profile.py
+++ eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/country_profile.py
@@ -1,6 +1,6 @@
 from zope.publisher.browser import BrowserPage
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
-from jsmap import country_code
+from jsmap import country_code, region_code
 
 country_profile_zpt = PageTemplateFile('zpt/country_profile.zpt', globals())
 
@@ -13,3 +13,16 @@ class CountryProfileView(BrowserPage):
                                     key=lambda x: x.lstrip('the ')),
         }
         return country_profile_zpt.__of__(ctx)(**options)
+
+
+
+region_info_zpt = PageTemplateFile('zpt/region_info.zpt', globals())
+
+class RegionInfoView(BrowserPage):
+
+    def __call__(self):
+        ctx = self.aq_parent
+        options = {
+            'region_names': sorted(region_code),
+        }
+        return region_info_zpt.__of__(ctx)(**options)
