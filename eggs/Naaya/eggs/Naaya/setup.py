0700d3e435b0835a4e1238390396ecc9d52ef86e
andredor <andredor@eaudeweb.ro>
2011-09-23 16:46:44 +0000

    New versions and manifests for chm svn to eggs

--- eggs/Naaya/eggs/Naaya/setup.py
+++ eggs/Naaya/eggs/Naaya/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='2.11.03', # March 2011
+      version='2.11.05',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
