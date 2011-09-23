6aa7dacb2adf47bd6f3592dadc55830df23657eb
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-09-23 09:29:30 +0000

    Update translations script
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.updater@17335 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.updater/Products/naayaUpdater/updates/update_css.py
+++ eggs/naaya.updater/Products/naayaUpdater/updates/update_css.py
@@ -1,44 +1,13 @@
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# The Initial Owner of the Original Code is European Environment
-# Agency (EEA). Portions created by Eau de Web are
-# Copyright (C) European Environment Agency.  All
-# Rights Reserved.
-#
-# Authors:
-#
-# Alex Morega, Eau de Web
-
-
-#Python imports
 import os
-from os.path import join
 from StringIO import StringIO
 from urllib2 import urlopen
 from urllib import urlencode
-
 import simplejson as json
 
-#Zope imports
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from App.ImageFile import ImageFile
 
-#Naaya imports
 from Products.naayaUpdater.updates import UpdateScript
-
-from Products.Naaya import NySite as NySite_module
-from Products.NaayaCore.managers.utils import html_diff, normalize_template
-from Products.Naaya.managers.skel_parser import skel_parser
-from Products.naayaUpdater.utils import (convertLinesToList, convertToList,
-    get_template_content, readFile)
 
 
 default_service_url = 'http://speaker.edw.ro/css_diff?format=json'
