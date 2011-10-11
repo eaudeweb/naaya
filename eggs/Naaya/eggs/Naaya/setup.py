ec9ddac2ce4dc8abf44ab870ddb579fd2f997367
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2011-10-11 12:41:27 +0000

    released 2.12.4, new version 2.12.5

--- eggs/Naaya/eggs/Naaya/setup.py
+++ eggs/Naaya/eggs/Naaya/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='2.12.4',
+      version='2.12.5',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
