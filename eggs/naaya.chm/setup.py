51fa1004ee6ab1dd2cfe8701cf34eb906c79bd5f
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2011-10-11 08:00:25 +0000

    released 2.4.3, new version 2.4.4
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.chm@17463 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.chm/setup.py
+++ eggs/naaya.chm/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.chm',
-    version='2.4.3',
+    version='2.4.4',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
