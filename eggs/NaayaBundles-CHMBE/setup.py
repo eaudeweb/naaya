f7589a10de42f59ee058c4e4be230d14fb681f5f
andredor <andredor@eaudeweb.ro>
2011-10-14 14:51:06 +0000

    Depends on CHM2BE

--- eggs/NaayaBundles-CHMBE/setup.py
+++ eggs/NaayaBundles-CHMBE/setup.py
@@ -6,5 +6,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['naaya.chm'],
+    install_requires=['naaya.chm', 'Products.CHM2BE'],
 )
