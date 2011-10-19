80069a8defb768a647c5c35b86a713067be75dcd
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-10-19 10:08:33 +0000

    naaya.chm release 2.4.7

--- eggs/Naaya/eggs/naaya.chm/setup.py
+++ eggs/Naaya/eggs/naaya.chm/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.chm',
-    version='2.4.7',
+    version='2.4.8',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
