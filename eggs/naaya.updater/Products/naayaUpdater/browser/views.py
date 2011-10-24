3235e00f3ee1229c2caff6f0face83d722f0a99d
andredor <andredor@eaudeweb.ro>
2011-10-24 14:10:08 +0000

    all updates in a single table

--- eggs/naaya.updater/Products/naayaUpdater/browser/views.py
+++ eggs/naaya.updater/Products/naayaUpdater/browser/views.py
@@ -1,7 +1,5 @@
 from time import strptime
-from datetime import date
 import operator
-from zope.component import getUtility
 from zope.component import getUtilitiesFor
 from Products.Five.browser import BrowserView
 
@@ -27,19 +25,20 @@ class UpdateScriptsView(BrowserView):
 
     @property
     def table(self):
-        items = self.categories.items()
-        items.sort()
-        def sort_date(script):
-            try:
-                t = strptime(script.creation_date, '%b %d, %Y')
-                return date(t.tm_year, t.tm_mon, t.tm_mday)
-            except ValueError:
-                return date(2000, 1, 1)
+        ret = []
+        for product, scripts in self.categories.items():
+            for script in scripts:
+                ret.append((script, product))
 
-        for category, scripts in items:
-            scripts.sort(key=sort_date, reverse=True)
-            scripts.sort(key=operator.attrgetter('priority'))
-            yield category, scripts
+        def sort_date(item):
+            try:
+                return strptime(item[0].creation_date, '%b %d, %Y')
+            except ValueError:
+                return None
+        ret.sort(key=sort_date, reverse=True)
+        return ret
+
+
 
 class LogsView(BrowserView):
     """ Display logs """
