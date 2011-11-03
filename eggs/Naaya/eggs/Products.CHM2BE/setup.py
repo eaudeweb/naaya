13638b16539c0fa27183227aa7c5ac27978860d5
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2011-11-03 16:43:21 +0000

    increase version

--- eggs/Naaya/eggs/Products.CHM2BE/setup.py
+++ eggs/Naaya/eggs/Products.CHM2BE/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.CHM2BE',
-      version='1.0.11',
+      version='1.0.12',
       description="CHM2BE",
       long_description="CHM2BE",
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
