b8c25569b5916899e5936c9f59ed1d56a6393f0d
andredor <andredor@eaudeweb.ro>
2011-11-08 13:15:42 +0000

    location filter (only locations with roles)

--- eggs/Naaya/Products/NaayaCore/AuthenticationTool/www/admin.js
+++ eggs/Naaya/Products/NaayaCore/AuthenticationTool/www/admin.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
 	$('.delbutton').live('click', function(e){
 		return confirm(gettext("Are you sure you want to remove this?"));
 	});
+
+	function show_hide_filter_locations() {
+		if ($('#filter-roles option:selected').val() == 'noroles') {
+			$('#filter-locations').parent().hide();
+		} else {
+			$('#filter-locations').parent().show();
+		}
+	}
+
+	$('#filter-roles').change(show_hide_filter_locations);
+	show_hide_filter_locations();
 });
