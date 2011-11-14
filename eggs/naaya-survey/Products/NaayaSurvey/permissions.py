889bc7e359183c55b5ea18df0c0aae2c96eefb7f
andredor <andredor@eaudeweb.ro>
2011-11-14 14:53:49 +0000

    permission description update
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya-survey@17828 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya-survey/Products/NaayaSurvey/permissions.py
+++ eggs/naaya-survey/Products/NaayaSurvey/permissions.py
@@ -1,21 +1,4 @@
-# The contents of this file are subject to the Mozilla Public
-# License Version 1.1 (the "License"); you may not use this file
-# except in compliance with the License. You may obtain a copy of
-# the License at http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS
-# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
-# implied. See the License for the specific language governing
-# rights and limitations under the License.
-#
-# The Initial Owner of the Original Code is European Environment
-# Agency (EEA).  Portions created by Finsiel Romania and Eau de Web are
-# Copyright (C) European Environment Agency.  All
-# Rights Reserved.
-#
-# Authors:
-#
-# Alin Voinea, Eau de Web
+from Products.Naaya.permissions import _register_permissions
 
 PERMISSION_ADD_REPORT = 'Naaya - Add Naaya Survey Report'
 PERMISSION_ADD_ATTACHMENT = 'Naaya - Add Naaya Survey Attachment'
@@ -24,3 +7,48 @@ PERMISSION_VIEW_ANSWERS = 'Naaya - View Naaya Survey Answers'
 PERMISSION_EDIT_ANSWERS = 'Naaya - Edit Naaya Survey Answers'
 PERMISSION_VIEW_REPORTS = 'Naaya - View Naaya Survey Reports'
 PERMISSION_ADD_MEGASURVEY = 'Naaya - Add Naaya Mega Survey'
+
+permission_data = {
+    PERMISSION_ADD_REPORT: {
+        'title': 'Submit Survey Report objects',
+        'description': """
+            Create new survey reports.
+        """,
+    },
+    PERMISSION_ADD_ATTACHMENT: {
+        'title': 'Submit Survey Attachments',
+        'description': """
+            Attach files.
+        """,
+    },
+    PERMISSION_ADD_ANSWER: {
+        'title': 'Submit Survey Answer objects',
+        'description': """
+            Answer the survey.
+        """,
+    },
+    PERMISSION_VIEW_ANSWERS: {
+        'title': 'View survey answers',
+        'description': """
+        """,
+    },
+    PERMISSION_EDIT_ANSWERS: {
+        'title': 'Edit survey answers',
+        'description': """
+        """,
+    },
+    PERMISSION_VIEW_REPORTS: {
+        'title': 'View survey reports',
+        'description': """
+        """,
+    },
+    PERMISSION_ADD_MEGASURVEY: {
+        'title': 'Submit Survey objects',
+        'description': """
+            Create new surveys.
+        """,
+    },
+}
+
+def register_permissions():
+    _register_permissions(permission_data)
