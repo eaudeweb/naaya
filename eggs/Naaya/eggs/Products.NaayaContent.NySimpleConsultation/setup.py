d1a9b2cd905c53759ae9582f92006ce701a7fe29
andredor <andredor@eaudeweb.ro>
2011-11-14 15:32:15 +0000

    released 1.1.3, new version 1.1.4

--- eggs/Naaya/eggs/Products.NaayaContent.NySimpleConsultation/setup.py
+++ eggs/Naaya/eggs/Products.NaayaContent.NySimpleConsultation/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Products.NaayaContent.NySimpleConsultation',
-      version='1.1.3',
+      version='1.1.4',
       description="",
       long_description=open("README.txt").read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
