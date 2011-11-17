e972a5e93fb36e838f215fd29709808b9128fd24
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2011-11-17 17:34:51 +0000

    released 1.1.3, new version 1.1.4
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.NaayaContent.NyPublication@17876 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.NaayaContent.NyPublication/setup.py
+++ eggs/Products.NaayaContent.NyPublication/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='Products.NaayaContent.NyPublication',
-	version='1.1.3',
+	version='1.1.4',
 	description="NyPublication",
 	long_description=open("README.txt").read() + "\n" +
 					 open("CHANGELOG.rst").read(),
