a6e861a9d6e91104876e9fbe9f3f0827a0655b26
Alex Morega <alex@grep.ro>
2011-11-29 18:12:47 +0000

    released 1.2 (bugfix in setup.py), next is 1.3

--- eggs/Products.NaayaChatter/setup.py
+++ eggs/Products.NaayaChatter/setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='Products.NaayaChatter',
-    version='1.2',
+    version='1.3',
     author='EaudeWeb',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro/',
     license='MPL',
     include_package_data=True,
+    packages=find_packages(),
     zip_safe=False,
     install_requires=[
         'Naaya',
