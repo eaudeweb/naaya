5b7743d3d15b8165eae78df0d47cea115087a053
Alex Morega <alex@grep.ro>
2011-11-29 16:33:01 +0000

    import EW Applications, Installer from Zope repo

---
+++ eggs/Naaya/eggs/Products.EnviroWindowsInstaller/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Products.EnviroWindowsInstaller',
+    version='1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+)
