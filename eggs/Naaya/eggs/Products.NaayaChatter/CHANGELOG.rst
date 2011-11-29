a6fabdfa2ca8bb07b07d2b0ab061a826bf0950ff
Alex Morega <alex@grep.ro>
2011-11-29 16:41:44 +0000

    released version 1.1, next is 1.2

--- eggs/Naaya/eggs/Products.NaayaChatter/CHANGELOG.rst
+++ eggs/Naaya/eggs/Products.NaayaChatter/CHANGELOG.rst
@@ -1,5 +1,9 @@
-1.1 (unreleased)
+1.2 (unreleased)
 ----------------
+
+1.1 (2011-11-29)
+----------------
+* Various updates for changes in Naaya [moregale, simiamih, andredor]
 
 1.0 (2010-08-05)
 ----------------
--- eggs/Naaya/eggs/Products.NaayaChatter/setup.py
+++ eggs/Naaya/eggs/Products.NaayaChatter/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Products.NaayaChatter',
-    version='1.1',
+    version='1.2',
     author='EaudeWeb',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro/',
