a9a438940bdf67db91d8f805ad43b83999a7a98d
Cornel Nitu <cornel.nitu@eaudeweb.ro>
2011-12-09 08:40:55 +0000

    released 1.2.11, new version 1.2.12

--- eggs/naaya-survey/setup.py
+++ eggs/naaya-survey/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='naaya-survey',
-    version="1.2.11",
+    version="1.2.12",
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro/',
