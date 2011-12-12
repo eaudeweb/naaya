083251e6798ecf434cc791b02fb341843456f979
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-12-12 13:40:55 +0000

    proper animation on toggle in destinet.publisher userinfo

--- eggs/Naaya/eggs/destinet.publishing/destinet/publishing/www/destinet.publishing.js
+++ eggs/Naaya/eggs/destinet.publishing/destinet/publishing/www/destinet.publishing.js
@@ -3,7 +3,14 @@
     function init_userinfo(){
         jQuery(".userinfo_item h3").click(
                 function(){
-                    jQuery(".items", this.parentNode).toggle('blind', {}, 800);
+                    var h3 = jQuery(this);
+                    jQuery(".items", this.parentNode).toggle('blind',
+                                                             function(){
+                                                              if(jQuery(this).is(":visible")){
+                                                                h3.removeClass("close").addClass("open");
+                                                              } else h3.removeClass("open").addClass("close");
+                                                             }
+                                                             , 800);
                 });
     }
 
