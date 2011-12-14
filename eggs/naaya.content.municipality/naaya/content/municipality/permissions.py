6cbdbb5e0d84c502014c883481e8dcb2c201e0e4
andredor <andredor@eaudeweb.ro>
2011-12-14 13:42:20 +0000

    using global edit permission

--- eggs/naaya.content.municipality/naaya/content/municipality/permissions.py
+++ eggs/naaya.content.municipality/naaya/content/municipality/permissions.py
@@ -1,19 +1,12 @@
 from Products.Naaya.permissions import _register_permissions
 
 PERMISSION_ADD_MUNICIPALITY = 'Naaya - Add Naaya Municipality objects'
-PERMISSION_EDIT_MUNICIPALITY = 'Naaya - Edit Naaya Municipality objects'
 
 permission_data = {
     PERMISSION_ADD_MUNICIPALITY: {
         'title': 'Submit Municipality objects',
         'description': """
             Create new municipality objects.
-        """,
-    },
-    PERMISSION_EDIT_MUNICIPALITY: {
-        'title': 'Edit Municipality objects',
-        'description': """
-            Edit municipality objects.
         """,
     },
 }
