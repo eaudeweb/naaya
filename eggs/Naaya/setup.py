9d463b2ef175600f5213c472549d0571d783ac7a
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-12-16 16:12:21 +0000

    released Naaya 2.12.38

--- eggs/Naaya/setup.py
+++ eggs/Naaya/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='2.12.38',
+      version='2.12.39',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
