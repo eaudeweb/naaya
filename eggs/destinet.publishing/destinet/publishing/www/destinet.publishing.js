453c159aaaa1a2df8d1ffe6434562f2e9437c0be
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2011-12-20 15:33:14 +0000

    my contacts in destinet.publisher and realeasedate

--- eggs/destinet.publishing/destinet/publishing/www/destinet.publishing.js
+++ eggs/destinet.publishing/destinet/publishing/www/destinet.publishing.js
@@ -1,5 +1,5 @@
 (function(){
-    
+
     function init_userinfo(){
         jQuery(".userinfo_item h3").click(
                 function(){
@@ -12,12 +12,15 @@
                                                              }
                                                              , 800);
                 });
+        jQuery(".more_link").click(function(){
+           var jthis = jQuery(this);
+           jQuery(".more_list_item", this.parentNode).show('blind', {}, 800);
+           jthis.hide();
+        });
     }
 
-    
     jQuery(document).ready(function(){
         init_userinfo();
     });
-
 
 })();
