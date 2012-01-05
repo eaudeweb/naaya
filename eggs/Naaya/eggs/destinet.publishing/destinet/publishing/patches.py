31161be5a3ee57993bcce29de90619bdce15b09d
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-01-05 14:51:00 +0000

    destinet.publisher patches addNyContact (P2/2)

--- eggs/Naaya/eggs/destinet.publishing/destinet/publishing/patches.py
+++ eggs/Naaya/eggs/destinet.publishing/destinet/publishing/patches.py
@@ -1,18 +1,33 @@
-from Products.NaayaBase.NyContentType import NyContentType
-from naaya.content.contact.contact_item import NyContact
+from naaya.content.contact import contact_item
+from subscribers import get_category_location
+from Products.Naaya.NyFolder import NyFolder
 
 
-def nycontenttype_object_submitted_message():
-    original = NyContentType.object_submitted_message
-    def patched(self, REQUEST):
+def patch_addNyContact():
+    original = contact_item.addNyContact
+    def patched(self, id='', REQUEST=None, contributor=None, **kwargs):
         """
-        NyContacts shouldn't do RESPONSE redirect, because their location
-        can get changed in naaya.content.base.interfaces.INyContentObjectAddEvent
-        handler
+        addNyContact should place the contact in a location based on
+        geo_type (Category)
 
         """
-        if getattr(self.getSite(), 'destinet.publisher', False):
-            if isinstance(self, NyContact):
-                return None
-        return original(self, REQUEST)
-    NyContentType.object_submitted_message = patched
+        site = self.getSite()
+        if contributor is not None:
+            kwargs['contributor'] = contributor
+        if id is not None:
+            kwargs['id'] = id
+        if REQUEST is not None:
+            schema_raw_data = dict(REQUEST.form)
+            kwargs['REQUEST'] = REQUEST
+        else:
+            schema_raw_data = kwargs
+        if not getattr(site, 'destinet.publisher', False):
+            return original(self, **kwargs)
+        geo_type = schema_raw_data.get('geo_type', None)
+        new_parent = get_category_location(site, geo_type)
+        if not new_parent:
+            new_parent = self
+        return original(new_parent, **kwargs)
+
+    contact_item.addNyContact = patched
+    NyFolder.addNyContact = patched
