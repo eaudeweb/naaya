1580c15aa8b46550c5a194619d27abb3ca22a8dc
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2012-01-05 13:55:09 +0000

    #780 naaya.core.utils function replace_illegal_xml for stripping xml-illegal characters, used in NyBase.rss_item_for_object

--- eggs/Naaya/eggs/Naaya/naaya/core/utils.py
+++ eggs/Naaya/eggs/Naaya/naaya/core/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import re
+import sys
 import htmlentitydefs
 import warnings
 import tempfile
@@ -226,3 +227,26 @@ def set_default_socket_timeout_to_1min():
     This should be called when starting zope.
     """
     socket.setdefaulttimeout(60) # in seconds
+
+_illegal_unichrs = [
+    (0x00, 0x08), (0x0B, 0x1F), (0x7F, 0x84), (0x86, 0x9F),
+    (0xD800, 0xDFFF), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF),
+    (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
+    (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+    (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
+    (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+    (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
+    (0x10FFFE, 0x10FFFF),
+]
+
+_illegal_ranges = ["%s-%s" % (unichr(low), unichr(high))
+                   for (low, high) in _illegal_unichrs
+                   if low < sys.maxunicode]
+
+_illegal_xml_re = re.compile(u'[%s]' % u''.join(_illegal_ranges))
+
+def replace_illegal_xml(text, replacement=''):
+    try:
+        return _illegal_xml_re.sub(replacement, text)
+    except:
+        import pdb;pdb.set_trace()
