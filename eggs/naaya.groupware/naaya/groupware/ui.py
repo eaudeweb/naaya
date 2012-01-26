d359c84e09e76e3c0f2b221be421b0e6c9ecbbe0
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-01-26 13:04:18 +0000

    Refs #4795; Added link for Edit NRC members in nfp-eionet

--- eggs/naaya.groupware/naaya/groupware/ui.py
+++ eggs/naaya.groupware/naaya/groupware/ui.py
@@ -1,7 +1,13 @@
 from zope.publisher.browser import BrowserPage
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from profileoverview.profile import ProfileClient
+
+from App.config import getConfiguration
 
 local_users_zpt = PageTemplateFile('zpt/local_users.zpt', globals())
+
+CONFIG = getConfiguration()
+eionet_url = getattr(CONFIG, 'environment', {}).get('EIONET_LDAP_EXPLORER', '')
 
 class LocalUsersPage(BrowserPage):
     def __call__(self):
@@ -10,3 +16,26 @@ class LocalUsersPage(BrowserPage):
         for site in ctx.objectValues('Groupware site'):
             local_users[site] = site.getAuthenticationTool().getUsers()
         return local_users_zpt.__of__(ctx)(local_users=local_users)
+
+def nfp_admin_link(context, request):
+    """
+    Check if LDAP user is NFP and return the URL to 'Edit NRC members' section
+    """
+    nfp_url = ''
+
+    if context.REQUEST.AUTHENTICATED_USER:
+        user = context.REQUEST.AUTHENTICATED_USER
+        zope_app = context.unrestrictedTraverse('/')
+        client = ProfileClient(zope_app, user)
+        roles_list = client.roles_list_in_ldap()
+        leaf_roles_list = [ r for r in roles_list if not r['children'] ]
+
+        country = ''
+        for leaf in leaf_roles_list:
+            if 'eionet-nfp-' in leaf['id']:
+                country = leaf['id'].rsplit('-', 1)[-1]
+
+        if country:
+            nfp_url = eionet_url + '/nfp_nrc/nrcs?nfp=' + country
+
+    return nfp_url
