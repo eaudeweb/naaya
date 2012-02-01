26d5a9c3dc328cdb0cc78f713d67148403b20cb6
Alex Morega <alex@grep.ro>
2012-02-01 14:45:16 +0000

    release 1.0.11, next version is 1.0.12

--- eggs/Naaya/eggs/naaya.i18n/setup.py
+++ eggs/Naaya/eggs/naaya.i18n/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.i18n',
-      version='1.0.11',
+      version='1.0.12',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
