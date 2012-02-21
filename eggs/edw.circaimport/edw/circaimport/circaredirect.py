ab240219c26866be1c6fa0be6c33ce88137cf2c0
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-02-21 13:47:52 +0000

    edw.circaimport: circaredirect refactoring
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/edw.circaimport@18397 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/edw.circaimport/edw/circaimport/circaredirect.py
+++ eggs/edw.circaimport/edw/circaimport/circaredirect.py
@@ -14,16 +14,19 @@ for server in SERVERS:
     REDIRECTS.update({
     server:
         {
-            'home': 'http://%s.eionet.europa.eu/$1' % server,
-            'directory': 'http://%s.eionet.europa.eu/$1/member_search' % server,
-            'library': 'http://%s.eionet.europa.eu/$1/library$2' % server
+            'home': 'http://%s.eionet.europa.eu/%%(alias)s' % server,
+            'directory': 'http://%s.eionet.europa.eu/%%(alias)s/member_search' % server,
+            'library': 'http://%s.eionet.europa.eu/%%(alias)s/library%%(path)s' % server
         }
     })
 
-def load_config_file():
+def get_configuration():
     """
     Reads redirects.txt from var/circa_import folder and if successful,
     updates redirection rules.
+
+    Returns known redirection rules.
+    Raises ValueError if file not properly parsed.
 
     """
     if not ui.upload_prefix:
@@ -31,25 +34,28 @@ def load_config_file():
     file_path = os.path.join(ui.upload_prefix, "redirects.txt")
     modification_time = os.stat(file_path).st_mtime
     if modification_time == redirect_records['last-load']:
-        return None
-    file = open(file_path, "r")
+        return redirect_records['data']
+
     found = {}
-    for (i, line) in enumerate(file):
-        line = line.strip()
-        if not line or line.startswith('#'):
-            continue
-        tokens = line.split()
-        if len(tokens) == 1:
-            tokens.extend(['forum', ''])
-        elif len(tokens) == 2:
-            tokens.append('')
-        elif len(tokens) > 3:
-            file.close()
-            raise ValueError("Found more than 3 tokens on line #%d" % (i+1))
-        found[tokens[0]] = {'where': tokens[1], 'alias': tokens[2]}
-    redirect_records['data'] = deepcopy(found)
-    redirect_records['last-load'] = modification_time
-    file.close()
+    config_file = open(file_path, "r")
+    try:
+        for (i, line) in enumerate(config_file):
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            tokens = line.split()
+            if len(tokens) == 1:
+                tokens.extend(['forum', ''])
+            elif len(tokens) == 2:
+                tokens.append('')
+            elif len(tokens) > 3:
+                raise ValueError("Found more than 3 tokens on line #%d" % (i+1))
+            found[tokens[0]] = {'where': tokens[1], 'alias': tokens[2]}
+        redirect_records['data'] = deepcopy(found)
+        redirect_records['last-load'] = modification_time
+        return redirect_records['data']
+    finally:
+        config_file.close()
 
 def circa_redirect(context, request):
     """
@@ -60,17 +66,17 @@ def circa_redirect(context, request):
     - `path`, optional - only used in "inside library" redirects
 
     """
-    load_config_file()
     id = request.form.get('id', '')
     rtype = request.form.get('type', 'home')
     path = request.form.get('path', '')
 
-    redirect = redirect_records['data'].get(id, DEFAULT_REDIRECT)
+    redirects_config = get_configuration()
+    redirect = redirects_config.get(id, DEFAULT_REDIRECT)
     alias = id
     if redirect['alias']:
         alias = redirect['alias']
     new_path = REDIRECTS.get(redirect['where'], {}).get(rtype)
-    new_path = new_path.replace('$1', alias).replace('$2', path)
+    new_path = new_path % {'alias': alias, 'path': path}
     request.RESPONSE.redirect(new_path)
     request.RESPONSE.setStatus(301,
         reason="CIRCA Interest Groups migrated to eionet.europa.eu hosts")
@@ -78,7 +84,7 @@ def circa_redirect(context, request):
 
 def circa_redirect_inspector(context, request):
     """ Debugger when connecting to machine is not desirable """
-    load_config_file()
+    redirects_config = get_configuration()
     pt = PageTemplateFile("zpt/circaredirect/inspector.zpt", globals())
-    return pt.__of__(context)(redirects=redirect_records['data'],
+    return pt.__of__(context)(redirects=redirects_config,
                     last_modification=time.ctime(redirect_records['last-load']))
