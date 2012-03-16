e5c59eed175cb392d483e98aa7cc72de30d64910
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2012-03-16 13:18:54 +0000

    #834 Adapt keywords to work with standard folder listing

--- eggs/Naaya/eggs/destinet.extra/destinet/keywords/www/keywords.js
+++ eggs/Naaya/eggs/destinet.extra/destinet/keywords/www/keywords.js
@@ -1,62 +1,22 @@
-if (toggleSelect == undefined) {
-    var isSelected = false;
+$(document).ready(function(){
+	$('#allocate_keywords').click(function(e){
+		e.preventDefault();
+		fAllocateKeywords();
+	});
+		console.log($('.select-all'));
+	$('.keywords-select-all').click(function(e){
+		//Select toggle for all items
+		var checked = !$(this).attr('checked');
+		$('.checked_paths').attr('checked', !checked);
+	});
+});
 
-    var toggleSelect = function() {
-        var frm = document.frmAllocateKeywords;
-        if(frm == undefined){
-            var frm = document.objectItems;
-        }
-
-        if(frm != undefined){
-            var i;
-
-            if (isSelected == false) {
-                for(i=0; i<frm.elements.length; i++){
-                    if (frm.elements[i].type == "checkbox" && ((frm.elements[i].name == 'checked_paths:list') || (frm.elements[i].name == 'id:list'))) {
-                        frm.elements[i].checked = true;
-                    }
-                }
-                isSelected = true;
-            }else {
-                for(i=0; i<frm.elements.length; i++){
-                    if (frm.elements[i].type == "checkbox" && ((frm.elements[i].name == 'checked_paths:list') || (frm.elements[i].name == 'id:list'))) {
-                        frm.elements[i].checked = false;
-                    }
-                }
-                isSelected = false;
-            }
-        }
-    };
-}
-
-if (fCheckSelection == undefined) {
-    var fCheckSelection = function(){
-        var frm = document.frmAllocateKeywords;
-        if(frm == undefined){
-            var frm = document.objectItems;
-        }
-
-        if(frm != undefined){
-            var i;
-            check = false;
-
-            for(i=0; i<frm.elements.length; i++){
-                if (frm.elements[i].type == "checkbox" && ((frm.elements[i].name == 'checked_paths:list') || (frm.elements[i].name == 'id:list'))) {
-                    check = true; break;
-                }
-            }
-
-            return check;
-        }
-    };
-}
-
-
-function fAllocateKeywords() {
-    if (fCheckSelection()) {
+function fAllocateKeywords(){
+	if ($("#folderfile_list td.checkbox input[type=checkbox]:checked").length == 0){
+		mesg = gettext('Please select on or more items for which you want to allocate keywords');
+		alert(mesg);
+		return false;
+	}
 	document.objectItems.action="allocate_keywords_html";
 	document.objectItems.submit();
-    }else {
-	alert('Please select on or more items for which you want to allocate keywords');
-    }
 }
