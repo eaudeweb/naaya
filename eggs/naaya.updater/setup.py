d5da4aa8b2307453a2f10ab32254a77d589abb3d
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-04-06 07:59:21 +0000

    released naaya.updater 1.2.25
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.updater@18668 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.updater/setup.py
+++ eggs/naaya.updater/setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.updater',
-    version='1.2.25',
+    version='1.2.26',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    url='https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.updater/',
     install_requires=[
         'setuptools',
         'Naaya',
