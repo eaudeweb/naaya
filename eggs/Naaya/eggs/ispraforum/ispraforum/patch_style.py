f1659e928927b282974c18daa998e42e7597addd
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-04-11 10:57:34 +0000

    Refs #875; Remove right logo and and new left logo for IspraForum

--- eggs/Naaya/eggs/ispraforum/ispraforum/patch_style.py
+++ eggs/Naaya/eggs/ispraforum/ispraforum/patch_style.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 line_pattern = re.compile(r'tal:attributes="href [^"]*/additional_style_css"')
@@ -21,4 +22,25 @@ def patch_style_for_new_site(event):
     application = event.application
     site = application.unrestrictedTraverse(application.created_path)
     tmpl = site['portal_layout'].get_current_skin()['standard_template']
+    _update_logo(site, logo_id='right_logo.gif', delete=True)
+    _update_logo(site, 'left_logo.gif', 'left_logo.png', 'image/png',
+                 'Site left logo')
     patch_template(tmpl)
+
+def _update_logo(site, logo_id='', new_logo_file='', content_type='image/gif',
+                 title='', delete=False):
+    """
+    Updates logo file
+    """
+    layout_tool = site.getLayoutTool()
+    layout_tool.manage_delObjects([logo_id])
+
+    if not delete:
+        full_path = os.path.dirname(os.path.abspath(__file__))
+        logo_file = os.path.join(full_path, 'www/', new_logo_file)
+        logo_content = open(logo_file, 'rb')
+        layout_tool.manage_addImage(id=logo_id,
+                                   file=logo_content,
+                                   content_type=content_type,
+                                   title=title)
+        logo_content.close()
