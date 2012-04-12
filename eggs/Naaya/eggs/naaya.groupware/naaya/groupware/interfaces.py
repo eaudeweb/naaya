76e1ab4ff38c7caa020651288de81787513267a4
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-04-12 14:00:31 +0000

    Refs #855; JSON with all archived portals from archives.eionet.europa.eu

--- eggs/Naaya/eggs/naaya.groupware/naaya/groupware/interfaces.py
+++ eggs/Naaya/eggs/naaya.groupware/naaya/groupware/interfaces.py
@@ -5,6 +5,12 @@ from Products.Naaya.interfaces import INySite
 class IGroupwareApplication(Interface):
     """ This is a Zope App marker where groupware is installed """
 
+class IEionetForumApplication(IGroupwareApplication):
+    """ This is a Zope App marker where EIONET Forum is installed """
+
+class IArchivesForumApplication(IGroupwareApplication):
+    """ This is a Zope App marker where EIONET Forum is installed """
+
 class IGWSite(INySite):
     """ Interface for EnviroWindowsSite """
     pass
