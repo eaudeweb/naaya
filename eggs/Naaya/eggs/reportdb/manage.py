90f6d9ca1963aab113ddf6ab1b23950977ff3ebc
Alex Morega <alex@grep.ro>
2012-04-13 15:15:08 +0000

    database setup; save a new report

--- eggs/Naaya/eggs/reportdb/manage.py
+++ eggs/Naaya/eggs/reportdb/manage.py
@@ -5,23 +5,37 @@ import flaskext.script
 
 
 default_config = {
-    'DEBUG': True,
+    'DATABASE_URI': 'postgresql://localhost/reportdb',
+    'TESTING_DATABASE_URI': 'postgresql://localhost/reportdb_test',
 }
 
 
 def create_app():
     import views
+    import database
 
     app = flask.Flask(__name__, instance_relative_config=True)
     app.config.update(default_config)
     app.config.from_pyfile('settings.py', silent=True)
 
+    database.initialize_app(app)
     views.register_on(app)
 
     return app
 
 
 manager = flaskext.script.Manager(create_app)
+
+
+@manager.command
+def resetdb():
+    import database
+    database.get_session().drop_all()
+
+@manager.command
+def syncdb():
+    import database
+    database.get_session().create_all()
 
 
 if __name__ == '__main__':
---
+++ eggs/Naaya/eggs/reportdb/tests/common.py
@@ -0,0 +1,26 @@
+_testing_db_uri = None
+def _get_testing_db_uri():
+    global _testing_db_uri
+    from manage import create_app
+    if _testing_db_uri is None:
+        tmp_app = create_app()
+        _testing_db_uri = tmp_app.config['TESTING_DATABASE_URI']
+    return _testing_db_uri
+
+
+def create_mock_app():
+    from manage import create_app
+    import database
+    app = create_app()
+    app.config["TESTING"] = True
+    app.config["DATABASE_URI"] = _get_testing_db_uri()
+    database.initialize_app(app)
+
+    with app.test_request_context():
+        database.get_session().create_all()
+
+    def app_teardown():
+        with app.test_request_context():
+            database.get_session().drop_all()
+
+    return app, app_teardown
