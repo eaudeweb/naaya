5e8a1e12919370916f5f42fa988eeed90196d33d
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2012-04-17 11:16:42 +0000

    Seris review schema+template

--- eggs/reportdb/schema.py
+++ eggs/reportdb/schema.py
@@ -19,7 +19,7 @@ ReportSchema = fl.Dict.with_properties(widget="schema") \
     fl.String.named('country') \
              .using(label=u"Country"),
     fl.String.named('name') \
-             .using(label="Name of report"),
+             .using(label=u"Name of report"),
 
     CommonDict.named('details') \
               .using(label=u"REPORT DETAILS") \
@@ -35,7 +35,7 @@ ReportSchema = fl.Dict.with_properties(widget="schema") \
     ),
 
     CommonDict.named('format') \
-              .using(label="FORMAT & ACCESSIBILITY") \
+              .using(label=u"FORMAT & ACCESSIBILITY") \
               .of(
         CommonEnum.named('Format') \
                .valued(*sorted(report_formats.keys())),
@@ -69,23 +69,66 @@ ReportSchema = fl.Dict.with_properties(widget="schema") \
 )
 
 
-SerisReviewSchema = fl.Dict.with_properties(widget="group") \
+indicators_usage = _load_json("refdata/indicators_usage.json")
+
+SerisReviewSchema = fl.Dict.with_properties(widget="schema") \
                            .of(
 
-    fl.Integer.named('report_id'),
+    fl.Integer.named('report_id'), #TODO should be filled by default
 
-    fl.Dict.named('links') \
-           .of(
+    CommonDict.named('links') \
+              .using(label=u"LINKS TO OTHER SoE REPORTS") \
+              .of(
 
-        fl.Boolean.named('global'),
+        CommonBoolean.named('global') \
+                     .using(label=u"Global-level SOER's (e.g. UNEP GEO)?"),
 
-        fl.Boolean.named('european'),
+        CommonBoolean.named('european') \
+                     .using(label=u"European-level SOER's (e.g. EEA SOER)?"),
 
-        fl.Boolean.named('national'),
+        CommonBoolean.named('national') \
+                     .using(label=u"National-level SOER's (e.g. other country)?"),
+
+        CommonBoolean.named('sub_national') \
+                     .using(label=u"Sub-national-level SOER's (e.g. regional)?"),
+                     
+        CommonBoolean.named('key_findings') \
+                     .using(label=u"Reference/links to key findings of the country's previous SOER?"),
+    ),
+
+    CommonDict.named('structure') \
+              .using(label=u"STRUCTURE") \
+              .of(
+        CommonBoolean.named('basis') \
+                     .using(label=u"Basis for report structure?"),
+        CommonBoolean.named('referenced') \
+                     .using(label=u"Referenced (ie. keyword search)?"),
+        CommonBoolean.named('indicator_based') \
+                     .using(label=u"Indicator-based report?"),
+        CommonBoolean.named('eea_indicators') \
+                     .using(label=u"EEA indicators used?"),
+        CommonEnum.named('to_compare_countries') \
+                  .valued(*sorted(indicators_usage.keys())) \
+                  .using(label=u"To compare with other countries/EU?"),
+        CommonEnum.named('to_compare_subnational') \
+                  .valued(*sorted(indicators_usage.keys())) \
+                  .using(label=u"To compare with other countries/EU?"),
+        CommonEnum.named('to_assess_progress') \
+                  .valued(*sorted(indicators_usage.keys())) \
+                  .using(label=u"To compare with other countries/EU?"),
+        CommonEnum.named('to_evaluate') \
+                  .using(label=u"To compare with other countries/EU?"),
+        CommonBoolean.named('dedicated_chapter') \
+                  .using(label=u"Dedicated chapter"),
+        CommonBoolean.named('dedicated_method') \
+                     .using(label=u"Dedicated method"),
+        CommonBoolean.named('uncertainty') \
+                     .using(label=u"Uncertainty/multiple scenarios/error bars"),
 
     )
 
 )
+
 """
 class ReportSchema(_ReportSchema):
 
