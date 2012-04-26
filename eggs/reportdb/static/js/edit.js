0ae3feb0d59496113f9dc9f37a9c571fd091a844
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2012-04-26 10:56:34 +0000

    #856 Fixed radio button values
    Updated javascript for field relationships

--- eggs/reportdb/static/js/edit.js
+++ eggs/reportdb/static/js/edit.js
@@ -1,37 +1,46 @@
 $(document).ready(function() {
-	show_hide('f_format_availability_url', 1, 'f_format_availability_paper_or_web_web');
-	$('#f_format_availability_paper_or_web_web').change(function(){
-		show_hide('f_format_availability_url', 1, 'f_format_availability_paper_or_web_web');
-	});
-	$('#f_format_availability_paper_or_web_paperonly').change(function(){
-		show_hide('f_format_availability_url', 1, 'f_format_availability_paper_or_web_web');
-	});
-
-	show_hide('f_structure_eea_indicators_estimated_no', 2, 'f_structure_eea_indicators_Yes');
-	$('#f_structure_eea_indicators_Yes').change(function(){
-		show_hide('f_structure_eea_indicators_estimated_no', 2, 'f_structure_eea_indicators_Yes');
-	});
-	$('#f_structure_eea_indicators_No').change(function(){
-		show_hide('f_structure_eea_indicators_estimated_no', 2, 'f_structure_eea_indicators_Yes');
+	show_hide($('#f_format_availability_url').parent(),
+			  ['f_format_availability_paper_or_web_web']);
+	$('input[name="format_availability_paper_or_web"]').change(function(){
+		show_hide($('#f_format_availability_url').parent(),
+				  ['f_format_availability_paper_or_web_web']);
 	});
 
-	function show_hide(field_id, hide_parents, dependent_by){
-		if ($('#'+dependent_by+':checked').length > 0){
-			if (hide_parents == 1){
-				$('#'+field_id).parent().show();
+	show_hide($('select[name="structure_indicators_estimation"]').parent().parent(),
+			  ['f_structure_indicator_based_Yes']);
+	$('input[name="structure_indicator_based"]').change(function(){
+		show_hide($('select[name="structure_indicators_estimation"]').parent().parent(),
+				  ['f_structure_indicator_based_Yes']);
+	});
+
+	show_hide($('#f_structure_eea_indicators_estimated_no').parent().parent(),
+			  ['f_structure_eea_indicators_Yes']);
+	$('input[name="structure_eea_indicators"]').change(function(){
+		show_hide($('#f_structure_eea_indicators_estimated_no').parent().parent(),
+				  ['f_structure_eea_indicators_Yes']);
+	});
+
+	show_hide($('#f_structure_indicators_usage_evaluation_method').parent(),
+			  ['f_structure_indicators_usage_to_evaluate_Most50',
+			   'f_structure_indicators_usage_to_evaluate_Some50']);
+	$('input[name="structure_indicators_usage_to_evaluate"]').change(function(){
+		show_hide($('#f_structure_indicators_usage_evaluation_method').parent(),
+			  ['f_structure_indicators_usage_to_evaluate_Most50',
+			   'f_structure_indicators_usage_to_evaluate_Some50']);
+	});
+
+	function show_hide(field, dependent_by){
+		var found = false;
+		for (i=0; i<dependent_by.length;i++){
+			if ($('#'+dependent_by[i]+':checked').length > 0){
+				found = true;
 			}
-			else if (hide_parents == 2){
-				$('#'+field_id).parent().parent().show();
-			}
+		}
+		if (found){
+			$(field).show();
 		}
 		else{
-			$('#'+field_id).val("");
-			if (hide_parents == 1){
-				$('#'+field_id).parent().hide();
-			}
-			else if (hide_parents == 2){
-				$('#'+field_id).parent().parent().hide();
-			}
+			$(field).hide();
 		}
-}
+	}
 })
