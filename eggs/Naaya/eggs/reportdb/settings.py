c006d4dc28b04bf4e8df102305698e951609bac2
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2012-04-30 10:18:01 +0000

    delete settings.py from root-dir

--- eggs/Naaya/eggs/reportdb/settings.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-SECRET_KEY = "234asdirew"
-ACCOUNTS = [("mihai.dinca@eaudeweb.ro", "edw")]
-DATABASE_URI = "postgresql://edw:edw@localhost/reportdb"
-TESTING_DATABASE_URI = "postgresql://edw:edw@localhost/reportdb_test"
-ZOPE_TEMPLATE_PATH = 'http://forum.eionet.europa.eu/nrc-state-environment/report_templates/'
-
