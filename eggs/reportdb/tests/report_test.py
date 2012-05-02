52e9f76b6372b1b74191fd82033dc6407b1d1fb4
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2012-05-02 15:42:13 +0000

    use random name for report in test_add

--- eggs/reportdb/tests/report_test.py
+++ eggs/reportdb/tests/report_test.py
@@ -1,4 +1,6 @@
 import unittest2 as unittest
+import random 
+import string
 
 
 class ReportCrudTest(unittest.TestCase):
@@ -7,6 +9,8 @@ class ReportCrudTest(unittest.TestCase):
         from common import create_mock_app
         self.app, app_teardown = create_mock_app()
         self.addCleanup(app_teardown)
+        self.report_name=''.join(random.choice(string.letters + string.digits) 
+                                 for i in xrange(30))
 
     def test_add(self):
         client = self.app.test_client()
@@ -14,11 +18,11 @@ class ReportCrudTest(unittest.TestCase):
             'format_availability_paper_or_web': 'paper only',
             'header_uploader': 'Report Guru',
             'format_lang_of_pub': 'ro',
-            'details_original_name': 'Teh new report',
+            'details_original_name': self.report_name,
             'format_availability_costs': 'free',
             'header_country': 'Romania',
             'details_original_language': 'ro',
             'global_level': 'on',
         })
         list_response = client.get('/reports/')
-        self.assertIn("Teh new report", list_response.data)
+        self.assertIn(self.report_name, list_response.data)
