7d4ff592a6c548d294d69f502bc89d560c723f3b
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2012-05-02 12:14:02 +0000

    working report test_add

--- eggs/reportdb/tests/report_test.py
+++ eggs/reportdb/tests/report_test.py
@@ -10,9 +10,15 @@ class ReportCrudTest(unittest.TestCase):
 
     def test_add(self):
         client = self.app.test_client()
-        post_response = client.post('/reports', data={
-            'title': u"Teh new report",
+        post_response = client.post('/reports/new/', data={
+            'format_availability_paper_or_web': 'paper only',
+            'header_uploader': 'Report Guru',
+            'format_lang_of_pub': 'ro',
+            'details_original_name': 'Teh new report',
+            'format_availability_costs': 'free',
+            'header_country': 'Romania',
+            'details_original_language': 'ro',
+            'global_level': 'on',
         })
-
-        list_response = client.get('/reports')
+        list_response = client.get('/reports/')
         self.assertIn("Teh new report", list_response.data)
