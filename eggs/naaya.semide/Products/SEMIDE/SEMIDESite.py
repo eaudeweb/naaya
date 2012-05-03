7d6893d50398ab2d67d0692f6c317585257a5855
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-05-03 06:53:22 +0000

    SEMIDE: AttributeError bug fix
    - Fixed AttributeError in search form
    
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.semide@18947 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.semide/Products/SEMIDE/SEMIDESite.py
+++ eggs/naaya.semide/Products/SEMIDE/SEMIDESite.py
@@ -394,6 +394,10 @@ class SEMIDESite(NySite, ProfileMeta, export_pdf, SemideZip, Cacheable):
         except: pass
         try:    self.getCatalogTool().addIndex('resource_end_date', 'DateIndex')
         except: pass
+        try:    self.getCatalogTool().addIndex('start_date', 'DateIndex')
+        except: pass
+        try:    self.getCatalogTool().addIndex('end_date', 'DateIndex')
+        except: pass
         class DateRangeIndexExtraParams: # 100% stupid code needed for DateRangeIndex
             def __init__(self, since_field, until_field):
                 self.since_field = since_field
