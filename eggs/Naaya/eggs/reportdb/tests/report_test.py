9ae4ecc751c0ae57f61502e5f7cc4b0632e42ec5
Mihai Dinca <mihai.dinca@eaudeweb.ro>
2012-05-03 12:42:24 +0000

    tests - compile a full report+seris dict in setUp function

--- eggs/Naaya/eggs/reportdb/tests/report_test.py
+++ eggs/Naaya/eggs/reportdb/tests/report_test.py
@@ -2,6 +2,8 @@ import unittest2 as unittest
 import random 
 import string
 import database
+import schema
+import re
 
 
 class ReportCrudTest(unittest.TestCase):
@@ -10,46 +12,45 @@ class ReportCrudTest(unittest.TestCase):
         from common import create_mock_app
         self.app, app_teardown = create_mock_app()
         self.addCleanup(app_teardown)
-        # unique random report name
-        self.report_name = "asds3923x.@"
+        self.report_data = {}
+        self.report_data.update(schema.ReportSchema.from_defaults().flatten())
+        self.report_data.update(schema.SerisReviewSchema.from_defaults().flatten())
+        data = {
+            u'format_availability_paper_or_web': u'paper only',
+            u'format_lang_of_pub': u'ro',
+            u'format_availability_costs': u'free',
+            u'details_original_name': u'asds3923x.@',
+            u'details_original_language': u'ro',
+            u'header_country': u'Romania',
+            u'header_uploader': u'Report Guru',
+            u'links_reference_global_level': u'on'
+        }
+        self.report_data.update(data)
+
 
     def test_create(self):
         client = self.app.test_client()
-        post_response = client.post('/reports/new/', data={
-            'format_availability_paper_or_web': 'paper only',
-            'header_uploader': 'Report Guru',
-            'format_lang_of_pub': 'ro',
-            'details_original_name': self.report_name,
-            'format_availability_costs': 'free',
-            'header_country': 'Romania',
-            'details_original_language': 'ro',
-            'links_reference_global_level': 'on',
-        }, follow_redirects=True)
+        post_response = client.post('/reports/new/', 
+                                    data= self.report_data,
+                                    follow_redirects=True)
         self.assertIn('Report saved.', post_response.data)
         list_response = client.get('/reports/')
-        self.assertIn(self.report_name, list_response.data)
+        self.assertIn(self.report_data[u'details_original_name'], list_response.data)
 
         
     def test_update(self):
-        #NOTE global_level is part of seris not the master report
+        #NOTE this tests both report and seris
 
         client = self.app.test_client()
         with self.app.test_request_context():
             session = database.get_session()
             row = database.ReportRow()
-            data = {
-                    u'format_availability_paper_or_web': u'paper only',
-                    u'format_lang_of_pub': u'ro',
-                    u'format_availability_costs': u'free',
-                    u'details_original_name': self.report_name,
-                    u'details_original_language': u'ro',
-                    u'header_country': u'Romania',
-                    u'header_uploader': u'Report Guru',
-                    u'links_reference_global_level': u'on'
-                   }
-            row.update(data)
+            row.update(self.report_data)
             session.save(row)
             session.commit()
+
+            # clone self.report_data dict
+            data = dict(self.report_data)
 
             #add additional info
             data.update({u'format_no_of_pages': u'2303445'})
@@ -74,7 +75,6 @@ class ReportCrudTest(unittest.TestCase):
             self.assertIn("Jerry Seinfeld", edit_response.data)
 
             # checking now if the checkbox has changed to No
-            import re
             regx = re.compile('(?<=Global-level SOER)'     # positive lookbehind
                               '.+s?>\s+<td>\s+No\s+</td>', # check change to No
                               re.IGNORECASE)
@@ -85,11 +85,11 @@ class ReportCrudTest(unittest.TestCase):
         client = self.app.test_client()
         with self.app.test_request_context():
             session = database.get_session()
-            row = database.ReportRow(name="very_nice_and_unique_name")
+            row = database.ReportRow(name="v323x,3240543#%")
             session.save(row)
             session.commit()
 
         post_response = client.post("reports/%s/delete/" %row.id)
         list_response = client.get('/reports/')
-        self.assertFalse( "very_nice_and_unique_name" in list_response.data)
+        self.assertFalse( "v323x,3240543#%" in list_response.data)
         self.assertIn("Report deleted.", list_response.data)
