6393e4dfbd614672a8f401a55bab257d557da159
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-05-08 09:11:22 +0000

    better make use of INyCatalogAware

--- eggs/Naaya/Products/NaayaCore/AuthenticationTool/subscribers.py
+++ eggs/Naaya/Products/NaayaCore/AuthenticationTool/subscribers.py
@@ -1,8 +1,8 @@
-from Products.Naaya.interfaces import INySite
+from Products.NaayaCore.CatalogTool.interfaces import INyCatalogAware
 
 def auto_catalog_object(event):
     obj = event.context
-    if not INySite.providedBy(obj):
+    if INyCatalogAware.providedBy(obj):
         catalog = obj.getSite().getCatalogTool()
         try:
             catalog.recatalogNyObject(obj)
