ba6a23fac7a808659edb13d963b1767623a74c3d
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-06-21 12:19:31 +0000

    Refs #908; edw.circaimport.dearchive basic methods and tests
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/edw.circaimport@19121 a0171fae-0946-0410-966d-b1775066cd26

---
+++ eggs/edw.circaimport/edw/circaimport/dearchive.py
@@ -0,0 +1,36 @@
+"""
+This is not exactly a CIRCA related feature, but it's specific to EIONET
+Forum setup - depends on Archives, Projects and Forum instances on same
+machine and specific configs.
+
+"""
+import re
+import transaction
+
+
+def manage_main_catch_path(self, REQUEST, manage_tabs_message, title):
+    """
+    Unfortunately ObjectManager.manage_exportObject does not return a value.
+    We have to pretend we have request and mock the manage_main call
+    to get the path of the zexp on server.
+
+    """
+    pattern = r'<em>(.*?)</em> successfully exported to <em>(.*?)</em>'
+    path = re.match(pattern, manage_tabs_message)
+    if not path:
+        raise ValueError("Can not deduct zexp path from ZMI message")
+    return path.groups()[1]
+
+def write_zexp(ob):
+    """ Generates corresponding zexp file of `ob` and any objects under it """
+    sp = transaction.savepoint()
+    try:
+        ob.manage_main = manage_main_catch_path
+        path_on_disk = ob.manage_exportObject(REQUEST=True)
+    finally:
+        sp.rollback()
+    return path_on_disk
+
+def load_zexp(zexp_path, destination):
+    """ Given the zexp file, it loads the data in the given OFS destination """
+    destination._importObjectFromFile(zexp_path, verify=0, set_owner=0)
