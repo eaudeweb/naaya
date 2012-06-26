85eaf08e214fcb2103dbe4455d6d503f01dddf77
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-06-26 07:53:36 +0000

    more meaningful module name: dearchive -> zexpcopy

--- eggs/Naaya/eggs/edw.circaimport/edw/circaimport/tests/test_zexpcopy.py
+++ eggs/Naaya/eggs/edw.circaimport/edw/circaimport/tests/test_zexpcopy.py
@@ -5,32 +5,32 @@ import tempfile
 from App.config import getConfiguration
 
 from Products.Naaya.tests.NaayaTestCase import NaayaTestCase
-from edw.circaimport import dearchive
+from edw.circaimport import zexpcopy
 
 
-class DearchiveTestSuite(unittest.TestCase):
+class ZexpCopyTestSuite(unittest.TestCase):
 
     def test_catch_zexp_path(self):
         path = '/var/local/forum/zope-instance/feedback-report.zexp'
         message = '<em>some-id</em> successfully exported to <em>%s</em>' % path
-        self.assertEqual(dearchive.manage_main_catch_path('', '', message, ''),
+        self.assertEqual(zexpcopy.manage_main_catch_path('', '', message, ''),
                          path)
 
 
-class DearchiveNaayaTestSuite(NaayaTestCase):
+class ZexpCopyNaayaTestSuite(NaayaTestCase):
 
     def test_write_zexp(self):
         config = getConfiguration()
         ob = self.portal.info
-        path = dearchive.write_zexp(ob)
+        path = zexpcopy.write_zexp(ob)
         self.assertEqual(path, os.path.join(config.clienthome,
                                             '%s.zexp' % ob.getId()))
         self.assertTrue(os.path.isfile(path))
 
     def test_write_load_zexp(self):
         ob = self.portal.info
-        path = dearchive.write_zexp(ob)
+        path = zexpcopy.write_zexp(ob)
         object_ids = ob.objectIds()
         self.portal._delObject('info')
-        dearchive.load_zexp(path, self.portal)
+        zexpcopy.load_zexp(path, self.portal)
         self.assertEqual(self.portal.info.objectIds(), object_ids)
