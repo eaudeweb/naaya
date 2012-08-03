879b402476fb4a55c864bdc47f0445e8ae56bf31
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-08-03 14:25:51 +0000

    Refs #961; added user groups in destinet registration
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/destinet.extra@19269 a0171fae-0946-0410-966d-b1775066cd26

--- destinet/registration/constants.py
+++ destinet/registration/constants.py
@@ -3,3 +3,6 @@ WIDGET_NAMES = ['geo_location', 'postaladdress', 'geo_type', 'coverage',
 # these are the normal field names in register form for an EW Site
 EW_REGISTER_FIELD_NAMES = ('firstname', 'lastname', 'email', 'password',
                            'organisation', 'comments', 'location')
+USER_GROUPS = {
+    'european-ecotourism-network': 'who-who/civil-society-ngos/european-ecotourism-network/new-applicants',
+}
--- destinet/registration/core.py
+++ destinet/registration/core.py
@@ -1,11 +1,13 @@
 """ Core methods for dealing with registration in Destinet """
 from Products.NaayaCore.SchemaTool.widgets.geo import Geo
 from Products.NaayaCore.SchemaTool.widgets.Widget import WidgetError
+from naaya.core.zope2util import path_in_site
+from naaya.content.pointer.pointer_item import addNyPointer
 
-from destinet.registration.constants import WIDGET_NAMES
+from destinet.registration.constants import WIDGET_NAMES, USER_GROUPS
 
 
-def validate_widgets(schema, form):
+def validate_widgets(contact_schema, registration_schema, form):
     """
     This is almost the same code and logic from
     :meth:`~Products.NaayaCore.SchemaTool.Schema.Schema.processForm`
@@ -13,7 +15,8 @@ def validate_widgets(schema, form):
     """
     form_data = {}
     form_errors = {}
-    widgets = [schema["%s-property" % w] for w in WIDGET_NAMES ]
+    widgets = [contact_schema["%s-property" % w] for w in WIDGET_NAMES ]
+    widgets.extend(registration_schema.objectValues())
     for widget in widgets:
         field_name = widget.prop_name()
 
@@ -50,7 +53,8 @@ def validate_widgets(schema, form):
             form_errors[field_name] = errors
     return form_data, form_errors
 
-def prepare_error_response(context, schema, form_errors, req_form):
+def prepare_error_response(context, contact_schema, register_schema,
+                           form_errors, req_form):
     """
     Almost the same as
     :meth:`~Products.NaayaBase.NyContentType.NyContentType._prepare_error_response`
@@ -66,6 +70,66 @@ def prepare_error_response(context, schema, form_errors, req_form):
     for prop_name in WIDGET_NAMES:
         for key in req_form:
             if key == prop_name or key.startswith(prop_name+'.'):
-                widget = schema.getWidget(prop_name)
+                widget = contact_schema.getWidget(prop_name)
                 value = widget.convert_to_session(req_form[key])
                 context.setSession(key, value)
+
+    for widget in register_schema.objectValues():
+        prop_name = widget.prop_name()
+        for key in req_form:
+            if key == prop_name or key.startswith(prop_name+'.'):
+                value = widget.convert_to_session(req_form[key])
+                context.setSession(key, value)
+
+def place_pointer_to_contact(ob, pointer_parent):
+    """
+    Very much like :func:~destinet.publishing.subscribers.place_pointers:
+
+    """
+    props = {
+        'title': ob.title,
+        'description': getattr(ob, 'description', ''),
+        'topics': ob.__dict__.get('topics', []),
+        'target-groups': ob.__dict__.get('target-groups', []),
+        'geo_location.lat': '',
+        'geo_location.lon': '',
+        'geo_location.address': '',
+        'geo_type': getattr(ob, 'geo_type', ''),
+        'coverage': ob.__dict__.get('coverage', ''),
+        'keywords': ob.__dict__.get('keywords', ''),
+        'sortorder': getattr(ob, 'sortorder', ''),
+        'redirect': True,
+        'pointer': path_in_site(ob)
+    }
+    if ob.geo_location:
+        if ob.geo_location.lat:
+            props['geo_location.lat'] = unicode(ob.geo_location.lat)
+        if ob.geo_location.lon:
+            props['geo_location.lon'] = unicode(ob.geo_location.lon)
+        if ob.geo_location.address:
+            props['geo_location.address'] = ob.geo_location.address
+    if not props['sortorder']:
+        props['sortorder'] = '200'
+
+    p_id = addNyPointer(pointer_parent, ob.getId(), contributor=ob.contributor, **props)
+    pointer = pointer_parent._getOb(p_id, None)
+    if pointer:
+        if ob.approved:
+            pointer.approveThis(1, ob.contributor)
+        else:
+            pointer.approveThis(0, None)
+        return pointer
+
+def handle_groups(ob, req_form):
+    """
+    If user selected a group to be member/user of, then a pointer must
+    be created to his profile (hist contact object) in folder of each
+    selected group.
+
+    """
+    site = ob.getSite()
+    groups = req_form.get('groups', [])
+    for group in groups:
+        if group in USER_GROUPS:
+            pointer_location = site.unrestrictedTraverse(USER_GROUPS[group])
+            place_pointer_to_contact(ob, pointer_location)
