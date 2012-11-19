8e6401eacf5396cb535e952824ee4448bb0c511a
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2012-11-19 12:27:30 +0000

    naaya.cache stronger install_requires
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.cache@19500 a0171fae-0946-0410-966d-b1775066cd26

--- setup.py
+++ setup.py
@@ -2,18 +2,15 @@ import sys
 from setuptools import setup, find_packages
 
 
-if sys.version_info >= (2, 6):
-    install_requires = ['plone.memoize >= 1.1.1', ]
-else:
-    # actually Zope 2.10 branch
-    install_requires = ['zope.component', 'zope.interface', 'zope.ramcache', ]
-
-
 setup(name='naaya.cache',
       version='1.1',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=install_requires,
+      install_requires=[
+        'plone.memoize >= 1.1.1',
+        # this should have been in setup.py of zope.ramcache 1.0
+        'zope.location >= 3.5.4',
+        ],
       zip_safe=False)
