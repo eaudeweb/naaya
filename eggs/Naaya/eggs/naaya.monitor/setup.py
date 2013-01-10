c4708338231c96f59170825361852476f48a87cf
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2013-01-10 13:30:35 +0000

    Refs #12677; First fully working version of naaya.monitor

---
+++ eggs/Naaya/eggs/naaya.monitor/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='naaya.monitor',
+      version='1.0',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False
+)
