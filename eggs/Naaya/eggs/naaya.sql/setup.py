dfa4ef211dadd834da89049d26259b2f69650416
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2013-02-08 12:42:15 +0000

    released naaya.sql 1.2

--- eggs/Naaya/eggs/naaya.sql/setup.py
+++ eggs/Naaya/eggs/naaya.sql/setup.py
@@ -8,7 +8,7 @@ if version_info < (2, 5):
     requires.append('pysqlite')
 
 setup(name='naaya.sql',
-      version='1.0.1',
+      version='1.2',
       description="",
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
