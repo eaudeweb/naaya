d8cad35c4a042324a3bc3509e36bcdaff8295446
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-02-26 10:22:56 +0000

    #13981 eea.tach initial import

---
+++ eggs/Naaya/eggs/eea.tach/tach/settings_local.py
@@ -0,0 +1,12 @@
+HOSTNAME = "http://localhost:8000"
+DATABASES = {
+
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'tach',
+    'USER': 'edw',
+    'PASSWORD': 'edw',
+    'HOST': '',
+    'PORT': ''
+    }
+}
