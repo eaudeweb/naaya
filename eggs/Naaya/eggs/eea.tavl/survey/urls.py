1ccfdf47d4b82038f3d48fd5dbbb29fcdfbb4b9a
Dragos Catarahia <dragos.catarahia@eaudeweb.ro>
2013-03-06 10:14:06 +0000

    section_b

--- eggs/Naaya/eggs/eea.tavl/survey/urls.py
+++ eggs/Naaya/eggs/eea.tavl/survey/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from survey import views
 
 urlpatterns = patterns('',
-    url(r'^section_a/(?P<category_id>[\d]+)/view/(?P<survey_id>[\d]+)$', views.SectionA.as_view(), name='view'),
-    url(r'^section_a/(?P<category_id>[\d]+)/edit$', views.SectionAEdit.as_view(), name='edit'),
+    url(r'^section_a/(?P<category_id>[\d]+)/view/(?P<survey_id>[\d]+)$', views.View.as_view(), name='view'),
+    url(r'^section_a/(?P<category_id>[\d]+)/edit$', views.Edit.as_view(), name='edit'),
 )
