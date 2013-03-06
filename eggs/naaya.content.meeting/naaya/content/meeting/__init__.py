315c540f1509afb7edb98fb7879e993485fd62e5
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2013-03-06 11:06:22 +0000

    WIP - fixing edit rights Last quickfix was to grant Admin to contributor of the meeting which is wrong because he/she implicitly becomes a participant. I tried to grant permissions to owner role, but this doesn't seem to work
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.meeting@19861 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content.meeting/naaya/content/meeting/__init__.py
+++ eggs/naaya.content.meeting/naaya/content/meeting/__init__.py
@@ -4,5 +4,6 @@ WAITING_ROLE = 'Meeting Waiting List'
 PARTICIPANT_ROLE = 'Meeting Participant'
 ADMINISTRATOR_ROLE = 'Administrator'
 MANAGER_ROLE = 'Manager'
+OWNER_ROLE = 'Owner'
 
 from meeting import NyMeeting
