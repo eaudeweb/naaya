6eae30fd6c927706c43e3f3b43e7957a9a96a03a
Dragos Catarahia <dragos.catarahia@eaudeweb.ro>
2013-03-06 10:21:26 +0000

    change url to sections

--- eggs/Naaya/eggs/eea.tavl/survey/urls.py
+++ eggs/Naaya/eggs/eea.tavl/survey/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from survey import views
 
 urlpatterns = patterns('',
-    url(r'^section_a/(?P<category_id>[\d]+)/view/(?P<survey_id>[\d]+)$', views.View.as_view(), name='view'),
-    url(r'^section_a/(?P<category_id>[\d]+)/edit$', views.Edit.as_view(), name='edit'),
+    url(r'^section/(?P<category_id>[\d]+)/view/(?P<survey_id>[\d]+)$', views.View.as_view(), name='view'),
+    url(r'^section/(?P<category_id>[\d]+)/edit$', views.Edit.as_view(), name='edit'),
 )
