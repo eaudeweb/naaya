70ede5ffcc624af18d27d5c5d3568bdcab9267e8
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2013-03-07 14:11:48 +0000

    use member_search in frameservice, if available

--- eggs/Naaya/naaya/core/frameservice.py
+++ eggs/Naaya/naaya/core/frameservice.py
@@ -5,13 +5,17 @@ def frame_view(context, request):
     # TODO unit tests
     # TODO documentation
     user = request.AUTHENTICATED_USER
-    users_tool = context.getSite().acl_users
+    site = context.getSite()
+    users_tool = site.acl_users
     first_name = ''
     last_name = ''
     email = ''
     phone_number = ''
     try:
-        user_ob = users_tool.search_users(user.getId(), all_users=True)[0]
+        if hasattr(site, "member_search"):
+            user_ob = site.member_search._search_users(user.getId())[0]
+        else:
+            user_ob = users_tool.search_users(user.getId(), all_users=True)[0]
         first_name = user_ob.first_name
         last_name = user_ob.last_name
         phone_number = user_ob.phone_number
