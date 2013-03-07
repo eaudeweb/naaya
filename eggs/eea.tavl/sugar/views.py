a8a7833718ce8076386a661bfb5929662fa6ccfd
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2013-03-07 15:16:07 +0000

    redirect to login

--- eggs/eea.tavl/sugar/views.py
+++ eggs/eea.tavl/sugar/views.py
@@ -1,13 +1,15 @@
 from functools import wraps
 from django.shortcuts import render, redirect
 
+from tach.settings import HOSTNAME
 
 def auth_required(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         request = args[0]
         if not request.user:
-            return render(request, 'restricted.html')
+            login_page = "/login/login_form?disable_cookie_login__=1&came_from=%s" % HOSTNAME
+            return redirect(login_page)
         return func(*args, **kwargs)
     return wrapper
 
