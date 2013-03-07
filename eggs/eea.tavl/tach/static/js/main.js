088ca6eb54c0d36db29f572e045ccd0d4964a876
Dragos Catarahia <dragos.catarahia@eaudeweb.ro>
2013-03-07 17:10:47 +0000

    close open

--- eggs/eea.tavl/tach/static/js/main.js
+++ eggs/eea.tavl/tach/static/js/main.js
@@ -11,7 +11,17 @@ $(function () {
     var parent = $(this).parents('.answers-container');
     var target = $(this).data('target');
     var multiple = $(this).data('multiple');
+    var closetext = $(this).data('closetext');
+    var close = $(this).data('close');
 
+    if(close) {
+      closeContainer(parent.find('.add-container'));
+      $(this).text($(this).data('opentext'));
+      $(this).data('close', '');
+      return
+    }
+    $(this).text($(this).data('closetext'));
+    $(this).data('close', true);
     $.get($(this).data('href'), function (data) {
       var add_container = parent.find('.add-container');
       add_container.html(data);
@@ -25,6 +35,10 @@ $(function () {
 
   $('.add-container').on('click', '.cancel', function () {
 
+    var parent = $(this).parents('.answers-container');
+    var add = parent.find('.add');
+    add.text(add.data('opentext'));
+    add.data('close', '');
     closeContainer($(this).parents('.add-container'));
 
   });
@@ -36,11 +50,14 @@ $(function () {
     var parent = $(this).parents('.answers-container');
     var target = $(this).data('target');
     var multiple = $(this).data('multiple');
+    var add = parent.find('.add');
 
     $.post($(this).attr('action'), $(this).serialize(), function (data) {
       if(data.status == 'success') {
         $(target).append(data.html);
         closeContainer(parent.find('.add-container'));
+        add.text(add.data('opentext'));
+        add.data('close', '');
         if(!multiple) {
           parent.find('.add').remove();
         }
