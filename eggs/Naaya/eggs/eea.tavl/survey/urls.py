309a4fffd8f93e58b3c71ca05d3de5ae005f525e
Dragos Catarahia <dragos.catarahia@eaudeweb.ro>
2013-03-11 12:00:38 +0000

    section a edit

--- eggs/Naaya/eggs/eea.tavl/survey/urls.py
+++ eggs/Naaya/eggs/eea.tavl/survey/urls.py
@@ -4,4 +4,5 @@ from survey import views
 urlpatterns = patterns('',
     url(r'^section/(?P<category_id>[\d]+)/view/(?P<survey_id>[\d]+)/$', views.View.as_view(), name='view'),
     url(r'^section/(?P<category_id>[\d]+)/edit/$', views.Edit.as_view(), name='edit'),
+    url(r'^section/(?P<category_id>[\d]+)/edit/(?P<survey_id>[\d]+)/$', views.Edit.as_view(), name='edit'),
 )
