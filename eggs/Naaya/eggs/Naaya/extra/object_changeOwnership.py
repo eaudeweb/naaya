e8258b4036fc028fe9d29e34edd6549ccd665fb8
Mihnea Simian <mihnea.simian@eaudeweb.ro>
2013-03-12 12:46:53 +0000

    more generic change ownership script

--- eggs/Naaya/eggs/Naaya/extra/object_changeOwnership.py
+++ eggs/Naaya/eggs/Naaya/extra/object_changeOwnership.py
@@ -1,12 +1,9 @@
 """ Change ownership for a specific object """
 
-def change_ownership_for_object(username):
+def change_ownership_for_object(ob, username):
     """ Example of changing ownsership for a specific object """
 
-    path_to_my_object = "/eea-2012-cc-iva-report-authors/library/"
-                        "mihai-tabara-folder/survey-test")
-    myobject = app.unrestrictedTraverse(path_to_my_object)
-    acl_users = myobject.getSite().getAuthenticationTool()
+    acl_users = ob.getSite().getAuthenticationTool()
     new_owner = acl_users.getUser(username)
-    myobject.changeOwnership(user=new_owner)
+    ob.changeOwnership(user=new_owner)
 
