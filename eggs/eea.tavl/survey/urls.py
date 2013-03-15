be67f3372fac391b90b11fa25e12a172b41b5e9d
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-03-15 17:02:20 +0000

    eea.tavl moved to https://svn.eionet.europa.eu/repositories/Python
    Second step deletion...

--- eggs/eea.tavl/survey/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from survey import views
-
-urlpatterns = patterns('',
-    url(r'^section/(?P<category_id>[\d]+)/view/(?P<survey_id>[\d]+)/$', views.View.as_view(), name='view'),
-    url(r'^section/(?P<category_id>[\d]+)/edit/$', views.Edit.as_view(), name='edit'),
-    url(r'^section/(?P<category_id>[\d]+)/edit/(?P<survey_id>[\d]+)/$', views.Edit.as_view(), name='edit'),
-)
--- eggs/eea.tavl/tach/template/context_processor.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-
-def context(request):
-    return {
-        'account': request.account,
-        'HOSTNAME': settings.HOSTNAME,
-    }
