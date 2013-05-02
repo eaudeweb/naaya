7b262a68e77ae6d656277b92bda8dc9feb25ce86
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-02 10:45:55 +0000

    Released 1.3.6, new version 1.3.7
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.groupware@20104 a0171fae-0946-0410-966d-b1775066cd26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.3.6',
+      version='1.3.7',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
