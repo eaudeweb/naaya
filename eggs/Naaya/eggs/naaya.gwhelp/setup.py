ff95a291c2f2ee42342b190565c5f5d55aa151f4
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-07 14:52:21 +0000

    #14432 naaya.gwhelp initial import

---
+++ eggs/Naaya/eggs/naaya.gwhelp/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='naaya.gwhelp',
+      version='0.0.1',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      url='http://naaya.eaudeweb.ro',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'naaya.groupware',
+      ]
+)
