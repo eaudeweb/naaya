cf05c743eea627de78c5014f3135a214a3210f58
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-08 12:21:22 +0000

    #14432 Naaya Folder detailed help
    Navigation
    
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.gwhelp@20108 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.gwhelp/naaya/gwhelp/gwhelp.py
+++ eggs/naaya.gwhelp/naaya/gwhelp/gwhelp.py
@@ -1,5 +1,7 @@
 from OFS.Folder import Folder
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from AccessControl import ClassSecurityInfo
+from AccessControl.Permissions import view, view_management_screens
 
 manage_addGWHelp_html = PageTemplateFile('zpt/manage_add', globals())
 
@@ -12,32 +14,41 @@ class GWHelp(Folder):
         self.id = id
         self.title = title
 
-    _index_html = PageTemplateFile('zpt/index', globals())
+    security = ClassSecurityInfo()
+
+    security.declareProtected(view, 'help_index_html')
+    help_index_html = PageTemplateFile('zpt/index', globals())
+    security.declareProtected(view, 'index_html')
     def index_html(self):
         """ Main Help Page """
-        return self._index_html()
+        return self.help_index_html()
 
     _main_features_html = PageTemplateFile('zpt/main_features', globals())
+    security.declareProtected(view, 'main_features')
     def main_features(self):
         """ Main features page """
         return self._main_features_html()
 
     _content_html = PageTemplateFile('zpt/content', globals())
+    security.declareProtected(view, 'content')
     def content(self):
         """ Content page """
         return self._content_html()
 
     _content_folder_html = PageTemplateFile('zpt/content_folder', globals())
+    security.declareProtected(view, 'content_folder')
     def content_folder(self):
         """ Content type: folder page """
         return self._content_folder_html()
 
     _ig_admin_html = PageTemplateFile('zpt/ig_admin', globals())
+    security.declareProtected(view, 'ig_admin')
     def ig_admin(self):
         """ IG-Admin page """
         return self._ig_admin_html()
 
     _user_roles_html = PageTemplateFile('zpt/user_roles', globals())
+    security.declareProtected(view, 'user_roles')
     def user_roles(self):
         """ User roles page """
         return self._user_roles_html()
