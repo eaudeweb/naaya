c0af93b5c46266c5ce041a33d414feab952697f1
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-20 14:52:19 +0000

    Released 1.3.9, new version 1.3.10

--- eggs/naaya.groupware/CHANGELOG.rst
+++ eggs/naaya.groupware/CHANGELOG.rst
@@ -1,4 +1,7 @@
-1.3.9 (unreleased)
+1.3.10 (unreleased)
+-------------------
+
+1.3.9 (2013-05-20)
 -------------------
 * support for reCAPTCHA keys from buildout [dumitval]
 
--- eggs/naaya.groupware/setup.py
+++ eggs/naaya.groupware/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.3.9',
+      version='1.3.10',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
