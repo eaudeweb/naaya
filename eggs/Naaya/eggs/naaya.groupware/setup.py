1d500b996ab4a44afe80ad7cb7448e8129094adf
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-20 16:24:53 +0000

    Released 1.3.10, new version 1.3.11

--- eggs/Naaya/eggs/naaya.groupware/setup.py
+++ eggs/Naaya/eggs/naaya.groupware/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.3.10',
+      version='1.3.11',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
