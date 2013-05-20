2a38dd8236c7116cb498c28ef37fa5fc1d8e17d8
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-20 16:01:44 +0000

    Released 3.0.22, new version 3.0.23

--- eggs/naaya.chm/setup.py
+++ eggs/naaya.chm/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.chm',
-    version='3.0.22',
+    version='3.0.23',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
