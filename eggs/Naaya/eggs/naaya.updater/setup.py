ec0d5fd8c196ac17b900917742f49a14dee5f01e
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-27 11:26:04 +0000

    Released 1.2.29, new version 1.2.30

--- eggs/Naaya/eggs/naaya.updater/setup.py
+++ eggs/Naaya/eggs/naaya.updater/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.updater',
-    version='1.2.28',
+    version='1.2.30',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     packages=find_packages(),
