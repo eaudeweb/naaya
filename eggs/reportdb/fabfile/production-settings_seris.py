125b4619d73e0c0bc72bd91cab108ab7e00c43b3
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-05-28 08:37:18 +0000

    #14638 Updated production settings to forum.eionet.europa.eu

--- eggs/reportdb/fabfile/production-settings_seris.py
+++ eggs/reportdb/fabfile/production-settings_seris.py
@@ -8,7 +8,7 @@ if secret_key_path.isfile():
 
 DATABASE_URI = "postgresql://edw:edw@localhost/seris"
 TESTING_DATABASE_URI = "postgresql://edw:edw@localhost/seris_test"
-FRAME_URL = 'http://projects.eionet.europa.eu/seris-revision/report_templates/frame'
+FRAME_URL = 'http://forum.eionet.europa.eu/nrc-state-environment/seris_frame/frame'
 HTTP_PROXIED = True
 ZOPE_TEMPLATE_CACHE = True
 FRAME_COOKIES = ['__ac', '_ZopeId']
