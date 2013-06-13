8446a64bde98350edd45afa6daf25cf05de5b46e
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-06-13 08:40:57 +0000

    Released 1.2.18, new version 1.2.19

--- eggs/Naaya/eggs/Products.NaayaForum/setup.py
+++ eggs/Naaya/eggs/Products.NaayaForum/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Products.NaayaForum',
-    version='1.2.18',
+    version='1.2.19',
     description="Naaya Forum",
     long_description=open("README.rst").read() + "\n" +
                 open("CHANGELOG.rst").read(),
