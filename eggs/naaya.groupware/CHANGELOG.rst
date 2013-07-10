d07db0512e962b11e46e52c6a7a14e09a1062481
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-07-10 09:18:44 +0000

    Released 1.4.2, new version 1.4.3

--- eggs/naaya.groupware/CHANGELOG.rst
+++ eggs/naaya.groupware/CHANGELOG.rst
@@ -1,4 +1,7 @@
-1.4.2 (unreleased)
+1.4.3 (unreleased)
+-------------------
+
+1.4.2 (2013-07-10)
 -------------------
 * links to eionet user profiles from folder listing [dumitval]
 
--- eggs/naaya.groupware/setup.py
+++ eggs/naaya.groupware/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.4.2',
+      version='1.4.3',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
