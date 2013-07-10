4568bd347f468725ef41aaa4e10ca309c51723fe
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-07-10 09:11:46 +0000

    #14478 Recent changes depend on Naaya 3.3.0
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.bfile@20269 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content.bfile/setup.py
+++ eggs/naaya.content.bfile/setup.py
@@ -9,7 +9,7 @@ setup(name='naaya.content.bfile',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'Naaya >= 3.1.7',
+          'Naaya >= 3.3.0',
           'ZODB3 >= 3.8',
           'zope.proxy >= 3.4',
       ]
