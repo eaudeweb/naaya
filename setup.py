136e528c7ffd49cb84e27fb80e1257a77c6676e9
dumitval <dumitval@a0171fae-0946-0410-966d-b1775066cd26>
2013-07-26 09:00:18 +0000

    Released 1.1.13 new version 1.1.14
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.envirowindows@20325 a0171fae-0946-0410-966d-b1775066cd26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.envirowindows',
-    version='1.1.13',
+    version='1.1.14',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro',
