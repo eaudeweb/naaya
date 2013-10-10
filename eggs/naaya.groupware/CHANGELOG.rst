c4c7dcc26897b0028b5de44283f3a13514fa01f1
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-10-10 13:23:17 +0000

    Released 1.4.7 new version 1.4.8
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.groupware@20388 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.groupware/CHANGELOG.rst
+++ eggs/naaya.groupware/CHANGELOG.rst
@@ -1,4 +1,7 @@
-1.4.7 (unreleased)
+1.4.8 (unreleased)
+-------------------
+
+1.4.7 (2013-10-10)
 -------------------
 * added link to nfp organisations [dumitval]
 
--- eggs/naaya.groupware/setup.py
+++ eggs/naaya.groupware/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.4.7',
+      version='1.4.8',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
