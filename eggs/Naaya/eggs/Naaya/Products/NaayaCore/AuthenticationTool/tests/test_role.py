e7bd09e32d5b467d133b72df71596e3b8659efe5
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-11-06 13:18:56 +0000

    #17368 Mark new users in the admin_assignroles page (except in EEA sites)
    + tests

--- eggs/Naaya/eggs/Naaya/Products/NaayaCore/AuthenticationTool/tests/test_role.py
+++ eggs/Naaya/eggs/Naaya/Products/NaayaCore/AuthenticationTool/tests/test_role.py
@@ -1,3 +1,4 @@
+from mock import patch
 from unittest import TestSuite, makeSuite
 import transaction
 from Products.Naaya.tests.NaayaFunctionalTestCase import NaayaFunctionalTestCase
@@ -58,3 +59,35 @@ class RoleTest(NaayaFunctionalTestCase):
 
         (p._Naaya___Add_Naaya_URL_objects_Permission,
          p._Naaya___Edit_content_Permission) = orig_perm
+
+    @patch('Products.NaayaCore.AuthenticationTool.AuthenticationTool.get_zope_env')
+    @patch('naaya.core.zope2util.get_zope_env')
+    def test_mark_new_users(self, get_zope_env_atool, get_zope_env_zope2util):
+        get_zope_env_atool.return_value = 'noteionet'
+        get_zope_env_zope2util.return_value = 'noteionet'
+        p = self.portal
+
+        self.browser_do_login('admin', '')
+
+        self.browser.go('http://localhost/portal/admin_assignroles_html')
+
+        assert ("Assign roles to local users" in self.browser.get_html())
+        assert ("User One (user1) *" in self.browser.get_html())
+
+        self.browser_do_logout()
+
+    @patch('Products.NaayaCore.AuthenticationTool.AuthenticationTool.get_zope_env')
+    @patch('naaya.core.zope2util.get_zope_env')
+    def test_mark_new_users_off(self, get_zope_env_atool, get_zope_env_zope2util):
+        get_zope_env_atool.return_value = 'eionet'
+        get_zope_env_zope2util.return_value = 'eionet'
+        p = self.portal
+
+        self.browser_do_login('admin', '')
+
+        self.browser.go('http://localhost/portal/admin_assignroles_html')
+
+        assert ("Assign roles to local users" in self.browser.get_html())
+        assert ("User One (user1) *" not in self.browser.get_html())
+
+        self.browser_do_logout()
