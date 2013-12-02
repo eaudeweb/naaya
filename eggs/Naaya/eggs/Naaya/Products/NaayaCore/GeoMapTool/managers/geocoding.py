b1367d59a83ecf61b41a4164597beaf9cfb90964
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2013-12-02 15:13:37 +0000

    Refs #16938 Forum; fix method used by Geo.py

--- eggs/Naaya/eggs/Naaya/Products/NaayaCore/GeoMapTool/managers/geocoding.py
+++ eggs/Naaya/eggs/Naaya/Products/NaayaCore/GeoMapTool/managers/geocoding.py
@@ -3,16 +3,22 @@
 
 from naaya.core import ggeocoding
 
-def location_geocode(address):
-    return ggeocoding.geocode(address)
 
-def geocode(portal_map, address):
-    address = location_geocode(address)
+def location_geocode(address):
+    """ Returns a tuple of geo coordinates for the address
+    """
+    address = ggeocoding.geocode(address)
 
     # Sometimes google doesn't return proper address on first request
     if address == None:
-        address = location_geocode(address)
+        address = ggeocoding.geocode(address)
 
     lat, lon = address
-
     return (str(lat), str(lon))
+
+
+def geocode(portal_map, address):
+    """ Returns a tuple of geo coordinates for the address
+    """
+
+    return location_geocode(address)
