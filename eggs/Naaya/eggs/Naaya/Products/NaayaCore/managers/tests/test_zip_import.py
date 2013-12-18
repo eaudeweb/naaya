6aa83a8dbaf30cf5b6cd5da6ed218c787a0bdb05
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2013-12-18 17:04:21 +0000

    Updated some tests to work with new CC field in diverted_mail

--- eggs/Naaya/eggs/Naaya/Products/NaayaCore/managers/tests/test_zip_import.py
+++ eggs/Naaya/eggs/Naaya/Products/NaayaCore/managers/tests/test_zip_import.py
@@ -155,6 +155,6 @@ class NyZipImport(NaayaTestCase):
         self.assertEqual(expected_recipients[0], diverted_mail[0][1][0])
         self.assertEqual(expected_recipients[1], diverted_mail[1][1][0])
         self.assertEqual(expected_recipients[2], diverted_mail[2][1][0])
-        self.assertEqual(expected_sender, mail[2])
-        self.assertEqual(expected_subject, mail[3])
+        self.assertEqual(expected_sender, mail[3])
+        self.assertEqual(expected_subject, mail[4])
         EmailTool.divert_mail(False)
