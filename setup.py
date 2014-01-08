e1f02a837d2bd843f39c31c89823f53d109a18ff
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2014-01-08 12:54:49 +0000

    Refs #17641 Destinet; bump version to dev
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/destinet.extra@20618 a0171fae-0946-0410-966d-b1775066cd26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='destinet.extra',
-      version='1.2.14',
+      version='1.2.15-dev',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
