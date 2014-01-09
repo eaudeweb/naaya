4c766a6f9dee5660bcec51fe7d34a89553d7f8a4
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2014-01-09 09:19:09 +0000

    Bump version to devel

--- eggs/Naaya/eggs/destinet.extra/setup.py
+++ eggs/Naaya/eggs/destinet.extra/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='destinet.extra',
-      version='1.2.15',
+      version='1.2.16-dev',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
