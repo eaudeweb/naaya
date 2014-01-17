10c4a7d703ebaa06785eaaad3ab7d740c00b485f
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-01-17 13:50:31 +0000

    released 1.4.31, new version 1.4.32

--- eggs/naaya.content.talkback/setup.py
+++ eggs/naaya.content.talkback/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.content.talkback',
-      version='1.4.31',
+      version='1.4.32',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
