b037ad6227f16ea3f4fb5b437910e52460def9a6
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-02-07 15:28:50 +0000

    released 3.3.36, new version 3.3.37

--- eggs/Naaya/trunk/setup.py
+++ eggs/Naaya/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='3.3.36',
+      version='3.3.37',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
