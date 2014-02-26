05580d0286673c92d93f3651289b7993cbf817ac
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-02-26 14:32:08 +0000

    released 1.0.2, new version 1.0.3

--- eggs/naaya.ew-aoa-library/setup.py
+++ eggs/naaya.ew-aoa-library/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.ew-aoa-library',
-      version='1.0.2',
+      version='1.0.3',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
