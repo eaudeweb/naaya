28cb8ea00c4457dbdbdbc8e78fedb9ea8cedf1bb
baragdan <baragdan@a0171fae-0946-0410-966d-b1775066cd26>
2014-04-07 13:22:11 +0000

    Released eggs for select what emails to export to xcel.
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.talkback@20839 a0171fae-0946-0410-966d-b1775066cd26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.content.talkback',
-      version='1.4.32',
+      version='1.4.33',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
@@ -9,7 +9,7 @@ setup(name='naaya.content.talkback',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'Naaya >= 3.3.26',
+          'Naaya >= 3.3.45',
           'BeautifulSoup >= 3.0.7a',
           'scrubber >= 1.4.2',
           'xlrd'
