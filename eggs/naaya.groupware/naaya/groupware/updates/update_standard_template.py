f6656a84fa3e6ae61c94914375e1bbec02a519a0
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-04-10 09:52:12 +0000

    #4150 bugfix in the update script

--- eggs/naaya.groupware/naaya/groupware/updates/update_standard_template.py
+++ eggs/naaya.groupware/naaya/groupware/updates/update_standard_template.py
@@ -7,20 +7,39 @@ class LogoutDirectlyInStandardTemplate(UpdateScript):
     creation_date = 'Apr 10, 2014'
 
     def _update(self, portal):
-        old_logout = ('<a tal:condition="python:username != \'Anonymous User\'" '
+        old_logout = [
+                    ('<a tal:condition="python:username != \'Anonymous User\'" '
+                    'tal:attributes="href string:${site_url}/login_html" '
+                    'i18n:translate="">Logout <tal:block tal:content="string:'
+                    '(${username})" i18n:name="username" /></a>'),
+                    ('<a tal:condition="python:username != \'Anonymous User\'" '
+                    'tal:attributes="href string:${site_url}/login_html" '
+                    'i18n:translate="">Logout <tal:block tal:content="string:'
+                    '(${username})" i18n:name="username"/></a>'),
+                    ('<a tal:condition="python:username != \'Anonymous User\'" '
+                     'tal:attributes="href string:${site_url}/login/logout" '
+                     'i18n:translate="">Logout</a> <a tal:condition="python:'
+                     'username != \'Anonymous User\'" tal:define="'
+                     'user_full_name python:here.getAuthenticationTool().'
+                     'name_from_userid(username) or username" tal:attributes="'
+                     'href string:${site_url}/login_html" tal:content="string:'
+                     '(${user_full_name})" />'),
+                    ('<a tal:condition="python:username != \'Anonymous User\'" '
                     'tal:attributes="href string:${site_url}/login/logout" '
-                    'i18n:translate="">Logout</a> <a tal:condition="python:username != '
-                    '\'Anonymous User\'" tal:define="user_full_name python:'
-                    'here.getAuthenticationTool().name_from_userid(username) '
-                    'or username" tal:attributes="href string:${site_url}/'
-                    'login_html" tal:content="string:(${user_full_name})" />')
+                    'i18n:translate="">Logout</a> <a tal:condition="'
+                    'python:username != \'Anonymous User\'" tal:define="'
+                    'user_full_name python:here.getAuthenticationTool().'
+                    'name_from_userid(username) or username" tal:attributes="'
+                    'href string:${site_url}/member_search?${username}" '
+                    'tal:content="string:(${user_full_name})" />')
+                    ]
         new_logout = ('<a tal:condition="python:username != \'Anonymous User\'" '
                     'tal:attributes="href string:${site_url}/login/logout" '
                     'i18n:translate="">Logout</a> <a tal:condition="python:username != '
                     '\'Anonymous User\'" tal:define="user_full_name python:'
                     'here.getAuthenticationTool().name_from_userid(username) '
                     'or username" tal:attributes="href string:${site_url}/'
-                    'member_search?${username}" '
+                    'member_search?search_string=${username}" '
                     'tal:content="string:(${user_full_name})" />')
 
         standard_template = get_standard_template(portal)
@@ -29,9 +48,10 @@ class LogoutDirectlyInStandardTemplate(UpdateScript):
             self.log.debug('Standard_template already updated')
         else:
             changed = False
-            if old_logout in tal:
-                tal = tal.replace(old_logout, new_logout)
-                changed = True
+            for tal_code in old_logout:
+                if tal_code in tal:
+                    tal = tal.replace(tal_code, new_logout)
+                    changed = True
             if changed:
                 standard_template.write(tal)
                 self.log.debug('Standard_template updated')
