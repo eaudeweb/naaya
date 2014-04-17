ed0764db543014a60963d0bb2c61e4e37ce668fe
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-04-17 10:58:26 +0000

    released 3.3.47, new version 3.3.48

--- eggs/Naaya/eggs/Naaya/trunk/CHANGELOG.rst
+++ eggs/Naaya/eggs/Naaya/trunk/CHANGELOG.rst
@@ -1,4 +1,7 @@
-3.3.47 (unreleased)
+3.3.48 (unreleased)
+-------------------
+
+3.3.47 (2014-04-17)
 -------------------
 * re-added an import (SubscriptionContainer) for backwards-compatibility [dumitval]
 
--- eggs/Naaya/eggs/Naaya/trunk/setup.py
+++ eggs/Naaya/eggs/Naaya/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='3.3.47',
+      version='3.3.48',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
