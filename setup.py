f943086db43616862da198374c1886bbfd797c6b
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-04-23 12:50:42 +0000

    released 1.4.21, new version 1.4.22
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.groupware@20869 a0171fae-0946-0410-966d-b1775066cd26

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.4.20',
+      version='1.4.21',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
