9919dbbd183e95f2e21581e3cd232f81b936ba72
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-07-10 14:21:28 +0000

    released 3.0.26, new version 3.0.27

--- eggs/Naaya/eggs/naaya.chm/setup.py
+++ eggs/Naaya/eggs/naaya.chm/setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.chm',
-    version='3.0.26',
-    author='Eau de Web',
-    author_email='office@eaudeweb.ro',
-    url='http://naaya.eaudeweb.ro',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'Naaya >= 3.2.4',
-        'naaya.photoarchive',
-        'Products.NaayaLinkChecker',
-        'Products.NaayaNetRepository',
-        'Products.NaayaGlossary',
-        'Products.NaayaCalendar',
-        'Products.NaayaForum'
-    ]
-)
+      version='3.0.27',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      url='http://naaya.eaudeweb.ro',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'Naaya >= 3.2.4',
+          'naaya.photoarchive',
+          'Products.NaayaLinkChecker',
+          'Products.NaayaNetRepository',
+          'Products.NaayaGlossary',
+          'Products.NaayaCalendar',
+          'Products.NaayaForum'
+      ]
+      )
