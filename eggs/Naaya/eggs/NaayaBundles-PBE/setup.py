c78db962cc561af0eb4f04877419b90a88a557aa
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2014-07-22 15:10:29 +0000

    Refs #19736 Forum; work in progress on a simple object exporter

--- eggs/Naaya/eggs/NaayaBundles-PBE/setup.py
+++ eggs/Naaya/eggs/NaayaBundles-PBE/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NaayaBundles-PBE',
+    name='naayabundles_pbe',
     description = "Server-specific bundles, PBE at EEA",
     packages=find_packages(),
     include_package_data=True,
