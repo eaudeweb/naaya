8bd6ebc066ef3108dc9395a5906667c3fdc78def
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-09-29 15:01:00 +0000

    #21088 bugfix when importing all from circa with notifications disabled

--- eggs/edw.circaimport/edw/circaimport/ui.py
+++ eggs/edw.circaimport/edw/circaimport/ui.py
@@ -11,11 +11,11 @@ import transaction
 from Products.Naaya.constants import METATYPE_NYSITE
 from naaya.groupware.constants import METATYPE_GROUPWARESITE
 
-from edw.circaimport.middleware import (add_files_and_folders_from_circa_export,
-                                        get_acl_users_sources_titles,
-                                        add_roles_from_circa_export,
-                                        add_notifications_from_circa_export,
-                                        add_acls_from_circa_export)
+from edw.circaimport.middleware import add_files_and_folders_from_circa_export
+from edw.circaimport.middleware import get_acl_users_sources_titles
+from edw.circaimport.middleware import add_roles_from_circa_export
+from edw.circaimport.middleware import add_notifications_from_circa_export
+from edw.circaimport.middleware import add_acls_from_circa_export
 from edw.circaimport import zexpcopy
 
 
@@ -26,15 +26,17 @@ logger.setLevel(logging.DEBUG)
 import_all_zpt = PageTemplateFile('zpt/import_all.zpt', globals())
 import_files_zpt = PageTemplateFile('zpt/import_files.zpt', globals())
 import_files_result_zpt = PageTemplateFile('zpt/import_files_result.zpt',
-                                        globals())
+                                           globals())
 import_roles_zpt = PageTemplateFile('zpt/import_roles.zpt', globals())
 import_notifications_zpt = PageTemplateFile('zpt/import_notifications.zpt',
-                                        globals())
+                                            globals())
 import_acls_zpt = PageTemplateFile('zpt/import_acls.zpt', globals())
 zexpcopy_export_zpt = PageTemplateFile('zpt/zexpcopy/export.zpt', globals())
 zexpcopy_import_zpt = PageTemplateFile('zpt/zexpcopy/import.zpt', globals())
-zexpcopy_tree_ajax_zpt = PageTemplateFile('zpt/zexpcopy/tree_ajax.zpt', globals())
+zexpcopy_tree_ajax_zpt = PageTemplateFile('zpt/zexpcopy/tree_ajax.zpt',
+                                          globals())
 tpl_macros = PageTemplateFile('zpt/zexpcopy/macros.zpt', globals()).macros
+
 
 def init_log_stream():
     log = StringIO()
@@ -44,26 +46,29 @@ def init_log_stream():
     logger.addHandler(handler)
     return log
 
+
 class UIFormatter(logging.Formatter):
     color_codes = {
-            'DEBUG': 'blue',
-            'INFO': 'green',
-            'WARNING': 'orange',
-            'ERROR': 'orangered',
-            'CRITICAL': 'red'
-            }
+        'DEBUG': 'blue',
+        'INFO': 'green',
+        'WARNING': 'orange',
+        'ERROR': 'orangered',
+        'CRITICAL': 'red'
+        }
     html = """<span style="color: %s">%s: %s</span>"""
+
     def format(self, record):
         message = record.msg % record.args
         return self.html % (self.color_codes[record.levelname],
                             record.levelname,
                             message)
 
+
 class ImportAllFromCirca(BrowserPage):
     def __call__(self):
         if upload_prefix is None:
             return "upload prefix not configured"
-        ctx = self.context.aq_inner # because self subclasses from Explicit
+        ctx = self.context.aq_inner  # because self subclasses from Explicit
         sources = get_acl_users_sources_titles(ctx)
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_all_zpt.__of__(ctx)(sources=sources)
@@ -78,7 +83,8 @@ class ImportAllFromCirca(BrowserPage):
             if filename_files and import_files_path:
                 try:
                     files_ctx = ctx.restrictedTraverse(import_files_path)
-                    add_files_and_folders_from_circa_export(files_ctx, filename_files, upload_prefix)
+                    add_files_and_folders_from_circa_export(
+                        files_ctx, filename_files, upload_prefix)
                 except:
                     logger.critical(traceback.format_exc())
 
@@ -86,21 +92,27 @@ class ImportAllFromCirca(BrowserPage):
             ldap_source_title = self.request.form['source_title']
             if filename_roles and ldap_source_title:
                 try:
-                    add_roles_from_circa_export(ctx, os.path.join(upload_prefix, filename_roles), ldap_source_title)
+                    add_roles_from_circa_export(
+                        ctx, os.path.join(upload_prefix, filename_roles),
+                        ldap_source_title)
                 except:
                     logger.critical(traceback.format_exc())
 
-            filename_notifications = self.request.form['filename_notifications']
+            filename_notifications = self.request.form.get(
+                'filename_notifications')
             if filename_notifications:
                 try:
-                    add_notifications_from_circa_export(ctx, os.path.join(upload_prefix, filename_notifications))
+                    add_notifications_from_circa_export(
+                        ctx, os.path.join(upload_prefix,
+                                          filename_notifications))
                 except:
                     logger.critical(traceback.format_exc())
 
             filename_acls = self.request.form['filename_acls']
             if filename_acls:
                 try:
-                    add_acls_from_circa_export(ctx, os.path.join(upload_prefix, filename_acls))
+                    add_acls_from_circa_export(
+                        ctx, os.path.join(upload_prefix, filename_acls))
                 except:
                     logger.critical(traceback.format_exc())
 
@@ -108,20 +120,22 @@ class ImportAllFromCirca(BrowserPage):
             if 'report' in self.request.form:
                 savepoint.rollback()
 
-        return import_all_zpt.__of__(ctx)(sources=sources, report=log.getvalue())
+        return import_all_zpt.__of__(ctx)(sources=sources,
+                                          report=log.getvalue())
 
 
 class ImportFilesFromCirca_html(BrowserPage):
     def __call__(self):
-        ctx = self.context.aq_inner # because self subclasses from Explicit
+        ctx = self.context.aq_inner  # because self subclasses from Explicit
         return import_files_zpt.__of__(ctx)()
+
 
 class ImportFilesFromCirca(BrowserPage):
     def __call__(self):
         if upload_prefix is None:
             return "upload prefix not configured"
-        ctx = self.context.aq_inner # because self subclasses from Explicit
-        #name = ctx.REQUEST.get('name')
+        ctx = self.context.aq_inner  # because self subclasses from Explicit
+        # name = ctx.REQUEST.get('name')
         name = self.request.form['filename']
 
         log = init_log_stream()
@@ -139,7 +153,7 @@ class ImportRolesFromCirca(BrowserPage):
         if upload_prefix is None:
             return "upload prefix not configured"
 
-        ctx = self.context.aq_inner # because self subclasses from Explicit
+        ctx = self.context.aq_inner  # because self subclasses from Explicit
         sources = get_acl_users_sources_titles(ctx)
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_roles_zpt.__of__(ctx)(sources=sources)
@@ -150,11 +164,13 @@ class ImportRolesFromCirca(BrowserPage):
         log = init_log_stream()
 
         try:
-            add_roles_from_circa_export(ctx, os.path.join(upload_prefix, name), ldap_source_title)
+            add_roles_from_circa_export(
+                ctx, os.path.join(upload_prefix, name), ldap_source_title)
         except:
             logger.critical(traceback.format_exc())
 
-        return import_roles_zpt.__of__(ctx)(sources=sources, report=log.getvalue())
+        return import_roles_zpt.__of__(ctx)(sources=sources,
+                                            report=log.getvalue())
 
 
 class ImportNotificationsFromCirca(BrowserPage):
@@ -162,7 +178,7 @@ class ImportNotificationsFromCirca(BrowserPage):
         if upload_prefix is None:
             return "upload prefix not configured"
 
-        ctx = self.context.aq_inner # because self subclasses from Explicit
+        ctx = self.context.aq_inner  # because self subclasses from Explicit
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_notifications_zpt.__of__(ctx)()
 
@@ -172,8 +188,8 @@ class ImportNotificationsFromCirca(BrowserPage):
         log = init_log_stream()
 
         try:
-            add_notifications_from_circa_export(ctx,
-                    os.path.join(upload_prefix, filename), notif_type)
+            add_notifications_from_circa_export(
+                ctx, os.path.join(upload_prefix, filename), notif_type)
         except:
             logger.critical(traceback.format_exc())
 
@@ -185,7 +201,7 @@ class ImportACLsFromCirca(BrowserPage):
         if upload_prefix is None:
             return "upload prefix not configured"
 
-        ctx = self.context.aq_inner # because self subclasses from Explicit
+        ctx = self.context.aq_inner  # because self subclasses from Explicit
         if self.request['REQUEST_METHOD'] == 'GET':
             return import_acls_zpt.__of__(ctx)()
 
@@ -199,6 +215,7 @@ class ImportACLsFromCirca(BrowserPage):
             logger.critical(traceback.format_exc())
 
         return import_acls_zpt.__of__(ctx)(report=log.getvalue())
+
 
 class ZExportData(BrowserPage):
     def __call__(self):
@@ -218,13 +235,14 @@ class ZExportData(BrowserPage):
                 zexp_path = zexpcopy.write_zexp(ob)
             except Exception, e:
                 zexp_path = ''
-                error = "Error while exporting %s IG Data: %s" % (ig_id, e.args)
+                error = "Error while exporting %s IG Data: %s" % (ig_id,
+                                                                  e.args)
                 logger.exception(error)
                 subject = 'Error exporting IG Data'
                 zexpcopy.send_action_completed_mail(error, sender, to, subject)
             else:
-                info = zexpcopy.export_completed_message_zpt.__of__(ctx)(zexp_path=zexp_path,
-                                                                    ob_url=ob.absolute_url())
+                info = zexpcopy.export_completed_message_zpt.__of__(ctx)(
+                    zexp_path=zexp_path, ob_url=ob.absolute_url())
                 subject = 'IG Data exported successfully'
                 zexpcopy.send_action_completed_mail(info, sender, to, subject)
             options = {
@@ -240,6 +258,7 @@ class ZExportData(BrowserPage):
             return zexpcopy_export_zpt.__of__(ctx)(performed=False,
                                                    igs=sites,
                                                    macros=tpl_macros)
+
 
 class ZImportData(BrowserPage):
     def __call__(self):
@@ -286,6 +305,7 @@ class ZImportData(BrowserPage):
             return zexpcopy_import_zpt.__of__(ctx)(performed=False,
                                                    igs=sites,
                                                    macros=tpl_macros)
+
 
 class ZExpcopyTree(BrowserPage):
     def __call__(self):
