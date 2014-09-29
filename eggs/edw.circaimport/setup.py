8e501eac6fcf015119437bca35aad47bc52b9174
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-09-29 15:01:14 +0000

    released 1.3.1, new version 1.3.2
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/edw.circaimport@21095 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/edw.circaimport/setup.py
+++ eggs/edw.circaimport/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='edw.circaimport',
-    version='1.3.1',
-    author='Eau de Web',
-    author_email='office@eaudeweb.ro',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={'console_scripts': ['circaimport = edw.circaimport:demo',
-                                      'tsv2csv = edw.circaimport:tsv2csv']},
-)
+      version='1.3.2',
+      author='Eau de Web',
+      author_email='office@eaudeweb.ro',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={'console_scripts': ['circaimport = edw.circaimport:demo',
+                                        'tsv2csv = edw.circaimport:tsv2csv']},
+      )
