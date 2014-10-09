77ceedf47b544af73a6cd378ff1302bdc4b48718
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-10-09 11:55:07 +0000

    released 1.3.3, new version 1.3.4

--- eggs/edw.circaimport/setup.py
+++ eggs/edw.circaimport/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='edw.circaimport',
-      version='1.3.3',
+      version='1.3.4',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       packages=find_packages(),
