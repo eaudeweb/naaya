3a295bad6d0ea0dcb163fa18a1e093608e33e326
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-10-24 17:47:22 +0000

    fix for map zoom on load

--- eggs/Naaya/eggs/Naaya/trunk/Products/NaayaCore/GeoMapTool/engine_google/__init__.py
+++ eggs/Naaya/eggs/Naaya/trunk/Products/NaayaCore/GeoMapTool/engine_google/__init__.py
@@ -11,7 +11,7 @@ from Products.NaayaCore.GeoMapTool.GeoMapTool import register_map_engine
 
 # restricted to eea.europa.eu and eionet.europa.eu, for other domains
 # configure your own from Administration, Map Management.
-#DEFAULT_API_KEY = 'AIzaSyCV-BfmY6xxmO0V3JAvRbvB30fwKHjaa9s'
+# DEFAULT_API_KEY = 'AIzaSyCV-BfmY6xxmO0V3JAvRbvB30fwKHjaa9s'
 
 
 class GoogleMapEngine(SimpleItem):
@@ -27,13 +27,14 @@ class GoogleMapEngine(SimpleItem):
     def __init__(self, id):
         super(GoogleMapEngine, self).__init__()
         self._setId(id)
-        #self.api_keys = DEFAULT_API_KEY
+        # self.api_keys = DEFAULT_API_KEY
         self.base_layer = 'map'
         self.allow_mouse_scroll = True
         self.portal_map_zoom = None
 
     _html_setup = PageTemplateFile('setup', globals())
     security.declarePrivate('html_setup')
+
     def html_setup(self, request, global_config):
         js_config = {
             'base_layer': self.base_layer,
@@ -42,25 +43,27 @@ class GoogleMapEngine(SimpleItem):
         }
         js_config.update(global_config)
         options = {
-            #'apikey': pick_api_key(self.api_keys, request),
+            #  'apikey': pick_api_key(self.api_keys, request),
             'js_config': json.dumps(js_config),
         }
         return self._html_setup(**options)
 
     _config_html = PageTemplateFile('configure', globals())
     security.declarePrivate('config_html')
+
     def config_html(self):
         return self._config_html(all_layers=[
-                {'name': 'map', 'label': "Streets"},
-                {'name': 'hybrid', 'label': "Hybrid"},
-                {'name': 'physical', 'label': "Physical"},
-                {'name': 'satellite', 'label': "Satellite"}])
+            {'name': 'map', 'label': "Streets"},
+            {'name': 'hybrid', 'label': "Hybrid"},
+            {'name': 'physical', 'label': "Physical"},
+            {'name': 'satellite', 'label': "Satellite"}])
 
     security.declarePrivate('save_config')
+
     def save_config(self, form_data):
-        #self.api_keys = form_data['google_api_keys']
+        # self.api_keys = form_data['google_api_keys']
         self.allow_mouse_scroll = form_data.get(
-                    'google_allow_mouse_scroll', False)
+            'google_allow_mouse_scroll', False)
         self.base_layer = form_data['google_base_layer']
         try:
             zoom = int(form_data.get('initial_zoom', None))
@@ -70,6 +73,7 @@ class GoogleMapEngine(SimpleItem):
 
     security.declareProtected(view, 'naaya_google_js')
     naaya_google_js = ImageFile('naaya_google.js', globals())
+
 
 def pick_api_key(key_data, request):
     """Return the API key for the current domain"""
@@ -85,6 +89,7 @@ def pick_api_key(key_data, request):
                 return key
     else:
         return ''
+
 
 def register():
     register_map_engine(GoogleMapEngine)
