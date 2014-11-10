86a62b2dc582ec9f4d4e50fa9216f3b7091f7688
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2014-11-10 14:17:41 +0000

    released 1.0.4, new version 1.0.5
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.gwhelp@21241 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.gwhelp/setup.py
+++ eggs/naaya.gwhelp/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.gwhelp',
-      version='1.0.4',
+      version='1.0.5',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
