bbecb0549311d744e366c61199d145304477b054
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2014-11-24 11:30:13 +0000

    Bump egg to development
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.NaayaContent.NyPublication@21288 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.NaayaContent.NyPublication/setup.py
+++ eggs/Products.NaayaContent.NyPublication/setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='Products.NaayaContent.NyPublication',
-	version='1.1.7',
-	description="NyPublication",
-	long_description=open("README.txt").read() + "\n" +
-					 open("CHANGELOG.rst").read(),
-	# Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	classifiers=[
-		"Programming Language :: Python",
-	],
-	author='Eau de Web',
-	author_email='office@eaudeweb.ro',
-	license='MPL',
-	packages=find_packages(exclude=['ez_setup']),
-	namespace_packages=['Products'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		'Naaya'
-	],
+    name='Products.NaayaContent.NyPublication',
+    version='1.1.8',
+    description="NyPublication",
+    long_description=open("README.txt").read() + "\n" +
+                     open("CHANGELOG.rst").read(),
+    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author='Eau de Web',
+    author_email='office@eaudeweb.ro',
+    license='MPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Naaya'
+    ],
 )
