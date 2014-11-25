c24a58abbf0655d4e7f9ed0cfcf7f20a2b030b0b
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2014-11-25 12:45:55 +0000

    Bump egg to development

--- eggs/Naaya/eggs/naaya.content.bfile/trunk/setup.py
+++ eggs/Naaya/eggs/naaya.content.bfile/trunk/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.content.bfile',
-      version='1.4.16',
+      version='1.4.17',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
