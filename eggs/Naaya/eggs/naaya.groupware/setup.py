92744bffcea6d643372fafe29eff0abf75075436
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2015-01-27 11:28:53 +0000

    Refs #10124 Forum; avoid error on HEAD request on <zoperoot>/@@index_html

--- eggs/Naaya/eggs/naaya.groupware/setup.py
+++ eggs/Naaya/eggs/naaya.groupware/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.groupware',
-      version='1.4.32',
+      version='1.4.33',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
