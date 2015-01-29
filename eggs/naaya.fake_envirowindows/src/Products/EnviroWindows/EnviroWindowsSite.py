618becd327b50af86a589f8560bc7fda6333d019
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2015-01-29 12:36:32 +0000

    Added naaya.fake_envirowindows code

---
+++ eggs/naaya.fake_envirowindows/src/Products/EnviroWindows/EnviroWindowsSite.py
@@ -0,0 +1,17 @@
+from Globals import InitializeClass
+from App.ImageFile import ImageFile
+from Products.Naaya.NySite import NySite
+
+class EnviroWindowsSite(NySite):
+    """ """
+    meta_type = "Naaya Site"
+
+    ew_common_css = ImageFile('www/ew_common.css', globals())
+    ew_print_css = ImageFile('www/ew_print.css', globals())
+    ew_style_css = ImageFile('www/ew_style.css', globals())
+
+    manage_options = (
+        NySite.manage_options
+    )
+
+InitializeClass(EnviroWindowsSite)
