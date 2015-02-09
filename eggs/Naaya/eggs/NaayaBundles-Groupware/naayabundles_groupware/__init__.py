dfcd234f404e6da10d2ffe94a39f9f87ef2fb0c1
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-02-09 16:00:51 +0000

    deleted unused Groupware bundle

--- eggs/Naaya/eggs/NaayaBundles-Groupware/naayabundles_groupware/__init__.py
+++
@@ -1,8 +0,0 @@
-PARENT_BUNDLE = 'Groupware'
-BUNDLE_NAME_PREFIX = 'Groupware-'
-
-def load_bundles():
-    import os.path
-    from naaya.core.fsbundles import auto_bundle_package
-    bundles_dir = os.path.dirname(__file__)
-    auto_bundle_package(bundles_dir, BUNDLE_NAME_PREFIX, PARENT_BUNDLE)
