1765d4293cc0bb0878cb74a3d5fd48121f31d8ac
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2015-02-12 14:38:03 +0000

    Refs #19736 Forum; added stripAllHTMLtags method to fake envirowindows
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.fake_envirowindows@21375 a0171fae-0946-0410-966d-b1775066cd26

--- src/Products/EnviroWindows/EnviroWindowsSite.py
+++ src/Products/EnviroWindows/EnviroWindowsSite.py
@@ -1,11 +1,14 @@
-from Globals import InitializeClass
+from AccessControl import ClassSecurityInfo
 from App.ImageFile import ImageFile
-#from Products.Naaya.NySite import NySite
+from Globals import InitializeClass
 from naaya.groupware.groupware_site import GroupwareSite
+from Products.NaayaCore.managers.utils import utils
+
 
 class EnviroWindowsSite(GroupwareSite):	# NySite):
     """ """
     meta_type = "Groupware site"
+    security = ClassSecurityInfo()
 
     ew_common_css = ImageFile('www/ew_common.css', globals())
     ew_print_css = ImageFile('www/ew_print.css', globals())
@@ -14,5 +17,10 @@ class EnviroWindowsSite(GroupwareSite):	# NySite):
     manage_options = (
         GroupwareSite.manage_options
     )
+
+    security.declarePublic('stripAllHtmlTags')
+    def stripAllHtmlTags(self, p_text):
+        """ """
+        return utils().utStripAllHtmlTags(p_text)
 
 InitializeClass(EnviroWindowsSite)
