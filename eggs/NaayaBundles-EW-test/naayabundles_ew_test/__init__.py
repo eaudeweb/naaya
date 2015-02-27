586daeb47438c669eb5ba6d5ec6bd272f72218bc
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-02-27 08:25:15 +0000

    Deleted old EW bundles (migrated sites have now the PBE bundle)

--- eggs/NaayaBundles-EW-test/naayabundles_ew_test/__init__.py
+++
@@ -1,8 +0,0 @@
-PARENT_BUNDLE = 'EW'
-BUNDLE_NAME_PREFIX = 'EW-test-'
-
-def load_bundles():
-    import os.path
-    from naaya.core.fsbundles import auto_bundle_package
-    bundles_dir = os.path.dirname(__file__)
-    auto_bundle_package(bundles_dir, BUNDLE_NAME_PREFIX, PARENT_BUNDLE)
