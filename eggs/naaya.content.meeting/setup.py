edbd637acc264f9fe45fba5cc470facbb7d6f126
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-03-10 14:28:05 +0000

    released 1.2.67, new version 1.2.68

--- eggs/naaya.content.meeting/setup.py
+++ eggs/naaya.content.meeting/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.content.meeting',
-      version='1.2.66',
+      version='1.2.68',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
