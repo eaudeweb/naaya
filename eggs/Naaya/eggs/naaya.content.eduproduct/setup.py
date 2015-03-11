30b3e7ee70729bea18e3bd6e13c6da184925e1a0
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-03-11 14:38:23 +0000

    #23786 reCaptcha 2.0 compatibility

--- eggs/Naaya/eggs/naaya.content.eduproduct/setup.py
+++ eggs/Naaya/eggs/naaya.content.eduproduct/setup.py
@@ -9,6 +9,6 @@ setup(name='naaya.content.eduproduct',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'Naaya',
+          'Naaya >= 3.4.15',
       ],
-)
+      )
