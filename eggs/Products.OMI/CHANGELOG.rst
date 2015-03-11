6710556da9d4bd4e7d8500539e7b344a2a31b153
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-03-11 14:20:16 +0000

    #23786 reCaptcha 2.0 compatibility
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.OMI@21431 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.OMI/CHANGELOG.rst
+++ eggs/Products.OMI/CHANGELOG.rst
@@ -1,5 +1,6 @@
 1.2 (unreleased)
 ----------------
+* reCaptcha 2.0 compatibility [dumitval]
 
 1.1 (2011-11-29)
 ----------------
--- eggs/Products.OMI/setup.py
+++ eggs/Products.OMI/setup.py
@@ -6,5 +6,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[],
+    install_requires=['Naaya >= 3.4.15'],
 )
