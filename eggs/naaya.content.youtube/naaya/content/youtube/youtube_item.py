691c8475c1f64d9b42868d3f7cae4c4be10fbfd8
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-03-12 10:41:58 +0000

    #23786 reCaptcha 2.0 compatibility bugfix
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.youtube@21441 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/naaya.content.youtube/naaya/content/youtube/youtube_item.py
+++ eggs/naaya.content.youtube/naaya/content/youtube/youtube_item.py
@@ -181,8 +181,8 @@ def addNyYoutube(self, id='', REQUEST=None, contributor=None, **kwargs):
     # redirect if case
     if REQUEST is not None:
         l_referer = REQUEST['HTTP_REFERER'].split('/')[-1]
-        if l_referer == ('youtube_manage_add' or
-                         l_referer.find('youtube_manage_add') != -1):
+        if (l_referer == 'youtube_manage_add' or
+                l_referer.find('youtube_manage_add') != -1):
             return self.manage_main(self, REQUEST, update_menu=1)
         elif l_referer == 'youtube_add_html':
             self.setSession('referer', self.absolute_url())
