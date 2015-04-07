e1bcd1987bb982980695965dc02b293c1a42713c
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-04-07 09:00:38 +0000

    #24581 added method to import non-local libraries

--- eggs/Naaya/eggs/Naaya/trunk/Products/NaayaCore/managers/utils.py
+++ eggs/Naaya/eggs/Naaya/trunk/Products/NaayaCore/managers/utils.py
@@ -1440,3 +1440,17 @@ def rss_channel_for_channel(channel, lang):
         {'{%s}about' % rdf_namespace: s.absolute_url()}
         )
     return channel
+
+
+def import_non_local(name, custom_name=None):
+    import imp
+    import sys
+
+    custom_name = custom_name or name
+
+    f, pathname, desc = imp.find_module(name, sys.path[1:])
+    module = imp.load_module(custom_name, f, pathname, desc)
+    if f:
+        f.close()
+
+    return module
