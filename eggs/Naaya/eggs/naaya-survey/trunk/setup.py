0af953adf2f4fba145821d065ca8daf2265d8bdc
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-05-20 13:29:41 +0000

    default media type print

--- eggs/Naaya/eggs/naaya-survey/trunk/setup.py
+++ eggs/Naaya/eggs/naaya-survey/trunk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='naaya-survey',
-    version="1.2.54",
+    version="1.2.55",
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     url='http://naaya.eaudeweb.ro/',
