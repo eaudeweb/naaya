8efaba942e4f35e0448a951876cd954bf9b99212
dumitval <dumitval@a0171fae-0946-0410-966d-b1775066cd26>
2015-06-16 15:07:13 +0000

    pep8
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/Products.NaayaGlossary@21577 a0171fae-0946-0410-966d-b1775066cd26

--- eggs/Products.NaayaGlossary/Products/NaayaGlossary/NyGlossaryElement.py
+++ eggs/Products.NaayaGlossary/Products/NaayaGlossary/NyGlossaryElement.py
@@ -16,6 +16,7 @@ from events import ItemTranslationChanged
 # constants
 LABEL_OBJECT = 'Glossary element'
 
+
 class ElementBasic:
     """ define the basic properties for NyGlossaryElement """
 
@@ -26,22 +27,25 @@ class ElementBasic:
         self.contributor = contributor
 
 
-manage_addGlossaryElement_html = PageTemplateFile('zpt/NaayaGlossaryElement/add',
-        globals())
+manage_addGlossaryElement_html = PageTemplateFile(
+    'zpt/NaayaGlossaryElement/add', globals())
+
 
 def manage_addGlossaryElement(self, id='', title='', source='', subjects=[],
-        contributor='', approved=1, REQUEST=None):
+                              contributor='', approved=1, REQUEST=None):
     """ adds a new NyGlossaryElement object """
     ob = NyGlossaryElement(id, title, source, subjects, contributor, approved)
     self._setObject(id, ob)
     element_obj = self._getOb(id)
     element_obj.subjects = self.get_subject_by_codes(subjects)
     element_obj.load_translations_list()
-    #imported here to avoid cross-import errors
+    # imported here to avoid cross-import errors
     from NyGlossary import set_default_translation
     set_default_translation(element_obj)
 
-    if REQUEST: return self.manage_main(self, REQUEST, update_menu=1)
+    if REQUEST:
+        return self.manage_main(self, REQUEST, update_menu=1)
+
 
 class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
     """ NyGlossaryElement """
@@ -53,28 +57,31 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
     icon = 'misc_/NaayaGlossary/element.gif'
 
     manage_options = (
-        {'label':'Translations',    'action':'translations_html'},
-        {'label':'Properties',      'action':'properties_html'},
-        {'label':"View",            'action':'index_html'},
-        {'label':'Undo',            'action':'manage_UndoForm'},)
+        {'label': 'Translations',    'action': 'translations_html'},
+        {'label': 'Properties',      'action': 'properties_html'},
+        {'label': "View",            'action': 'index_html'},
+        {'label': 'Undo',            'action': 'manage_UndoForm'},)
 
     security = ClassSecurityInfo()
 
     def __init__(self, id, title, source, subjects, contributor, approved):
         """ constructor """
-        self.id =           id
-        self.subjects =     subjects
-        self.approved =     approved
+        self.id = id
+        self.subjects = subjects
+        self.approved = approved
         ElementBasic.__dict__['__init__'](self, title, source, contributor)
 
-    def is_published(self): return self.approved
+    def is_published(self):
+        return self.approved
 
     #####################
     #  BASIC PROPERTIES #
     #####################
-    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'manageBasicProperties')
-    def manageBasicProperties(self, title='', source='', subjects=[], contributor='',
-                              approved=0, REQUEST=None):
+    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY,
+                              'manageBasicProperties')
+
+    def manageBasicProperties(self, title='', source='', subjects=[],
+                              contributor='', approved=0, REQUEST=None):
         """ manage basic properties for NyGlossaryElement """
         self.title = title
         self.source = source
@@ -83,9 +90,12 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
         self.approved = approved
         self._p_changed = 1
         self.cu_recatalog_object(self)
-        if REQUEST: return REQUEST.RESPONSE.redirect('properties_html?save=ok')
+        if REQUEST:
+            return REQUEST.RESPONSE.redirect('properties_html?save=ok')
 
-    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'approveElement')
+    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY,
+                              'approveElement')
+
     def approveElement(self, REQUEST=None):
         """ used for approval link in basket of approvals"""
         self.approved = 1
@@ -108,12 +118,14 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
         return self.subjects
 
     security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'set_subjects')
+
     def set_subjects(self, code, name):
         """ set the languages """
         append = self.subjects.append
-        append({'code':code, 'name':name})
+        append({'code': code, 'name': name})
 
     security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'del_subject')
+
     def del_subject(self, code):
         """ remove a language from list """
         for subj_info in self.subjects:
@@ -144,7 +156,9 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
                 return 1
         return 0
 
-    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'set_translations_list')
+    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY,
+                              'set_translations_list')
+
     def set_translations_list(self, language, translation):
         """ set the languages """
         real_self = self.aq_base
@@ -158,16 +172,20 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
             setattr(real_self, language, translation)
         event.notify(ItemTranslationChanged(self, language, translation))
 
-    def load_translations_list (self):
+    def load_translations_list(self):
         """ load languages """
         for lang in self.get_english_names():
             self.set_translations_list(lang, '')
 
-    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'manageNameTranslations')
-    def manageNameTranslations(self, lang_code='', translation='', REQUEST=None):
+    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY,
+                              'manageNameTranslations')
+
+    def manageNameTranslations(self, lang_code='', translation='',
+                               REQUEST=None):
         """ save translation for a language """
         self.set_translations_list(lang_code, translation)
-        if REQUEST: return REQUEST.RESPONSE.redirect('translations_html?tab=0')
+        if REQUEST:
+            sreturn REQUEST.RESPONSE.redirect('translations_html?tab=0')
 
     #######################################
     #  DEFINITION TRANSLATIONS FUNCTIONS  #
@@ -183,7 +201,9 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
                 return 1
         return 0
 
-    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'set_def_trans_list')
+    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY,
+                              'set_def_trans_list')
+
     def set_def_trans_list(self, language, translation):
         """ set the languages """
         self.set_translations_list(self.definition_lang(language), translation)
@@ -193,39 +213,51 @@ class NyGlossaryElement(SimpleItem, ElementBasic, utils, catalog_utils):
         for lang in self.get_english_names():
             self.set_translations_list(self.definition_lang(lang), '')
 
-    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY, 'manageDefinitionTranslations')
-    def manageDefinitionTranslations(self, lang_code='', translation='', REQUEST=None):
+    security.declareProtected(PERMISSION_MANAGE_NAAYAGLOSSARY,
+                              'manageDefinitionTranslations')
+
+    def manageDefinitionTranslations(self, lang_code='', translation='',
+                                     REQUEST=None):
         """ save translation for a language """
         self.set_def_trans_list(lang_code, translation)
-        if REQUEST: return REQUEST.RESPONSE.redirect('translations_html?tab=1')
+        if REQUEST:
+            return REQUEST.RESPONSE.redirect('translations_html?tab=1')
 
     #####################
     #   MANAGEMENT TABS #
     #####################
     security.declareProtected(view_management_screens, 'translations_html')
-    translations_html =     PageTemplateFile("zpt/NaayaGlossaryElement/translations", globals())
+    translations_html = PageTemplateFile(
+        "zpt/NaayaGlossaryElement/translations", globals())
 
     security.declareProtected(view_management_screens, 'name_trans_html')
-    name_trans_html =     PageTemplateFile("zpt/NaayaGlossaryElement/name_trans", globals())
+    name_trans_html = PageTemplateFile("zpt/NaayaGlossaryElement/name_trans",
+                                       globals())
 
     security.declareProtected(view_management_screens, 'definition_trans_html')
-    definition_trans_html =     PageTemplateFile("zpt/NaayaGlossaryElement/definition_trans", globals())
+    definition_trans_html = PageTemplateFile(
+        "zpt/NaayaGlossaryElement/definition_trans", globals())
 
     security.declareProtected(view_management_screens, 'properties_html')
-    properties_html =       PageTemplateFile("zpt/NaayaGlossaryElement/properties", globals())
+    properties_html = PageTemplateFile("zpt/NaayaGlossaryElement/properties",
+                                       globals())
 
-    view_elements_html =    PageTemplateFile("zpt/NaayaGlossaryElement/view_elements", globals())
-    index_html =            NaayaPageTemplateFile("zpt/NaayaGlossaryElement/index", globals(), 'glossary_element_index')
+    view_elements_html = PageTemplateFile(
+        "zpt/NaayaGlossaryElement/view_elements", globals())
+    index_html = NaayaPageTemplateFile("zpt/NaayaGlossaryElement/index",
+                                       globals(), 'glossary_element_index')
 
     #################
     #   SITE MAP    #
     #################
     security.declarePublic('getGlossaryObTree')
+
     def getGlossaryObTree(self):
         """ """
         return None
 
     security.declareProtected(view_management_screens, 'manage_tabs')
+
     def manage_tabs(self):
         # we override manage_tabs to insert warning about synchronized glossary
         if self.sync_remote_url:
