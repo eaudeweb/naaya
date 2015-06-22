c55427926a2ecbcf0e7896c55a4b1999caef7cce
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-06-22 09:15:20 +0000

    #22522 created and cleaned up map icons for the new meeting types
    git-svn-id: https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/naaya.content.meeting@21599 a0171fae-0946-0410-966d-b1775066cd26

--- naaya/content/meeting/meeting.py
+++ naaya/content/meeting/meeting.py
@@ -187,13 +187,12 @@ def meeting_on_install(site):
     # add new map symbols for the meeting
     portal_map = site.getGeoMapTool()
     if portal_map is not None:
-        new_map_symbols = [('eionet_meeting.png', 'Eionet meeting', 10),
+        new_map_symbols = [('conference.png', 'Conference', 10),
                            ('nrc_meeting.png', 'NRC meeting', 20),
                            ('nrc_webinar.png', 'NRC webinar', 30),
                            ('nfp_meeting.png', 'NFP meeting', 40),
-                           ('meeting.png', 'Meeting', 100),
-                           ('conference.png', 'Conference', 110),
-                           ('workshop.png', 'Workshop', 120)]
+                           ('nfp_webinar.png', 'NFP webinar', 50),
+                           ('workshop.png', 'Workshop', 60)]
         for i in range(len(new_map_symbols)):
             new_map_symbols[i] = (os.path.join(os.path.dirname(__file__),
                                                'www', 'map_symbols',
