283d3c83f2426743bb52597992834bbeb8e8dc11
Alex Eftimie <alex@eftimie.ro>
2015-08-03 16:51:18 +0200

    Added submodule: NaayaBundles-CHMBE-cop10

--- eggs/Naaya/contrib/submodule-generic.sh
+++ eggs/Naaya/contrib/submodule-generic.sh
@@ -8,14 +8,16 @@ REPO_PATH=/Users/alex/Projects/bun3/
 
 echo "Please ensure http://github.com/eaudeweb/$GIT_REPO exists, prior to running this script"
 
+echo "Source clone..."
 git svn clone https://svn.eionet.europa.eu/repositories/Naaya/$SVN_FOLDER/$NAME -A authors-transform.txt 
 cd $NAME
 git remote add origin git@github.com:eaudeweb/$GIT_REPO 
 git push -u origin master
 
+echo "Submodule..."
 cd $REPO_PATH/$REPO_FOLDER
 git rm -r $NAME
-git submodule add $GIT_REPO $NAME
+git submodule add git@github.com:eaudeweb/$GIT_REPO $NAME
 git config -f ../.gitmodules submodule.$REPO_FOLDER/$NAME.branch master
 
 git commit -am "Added submodule: $NAME"
