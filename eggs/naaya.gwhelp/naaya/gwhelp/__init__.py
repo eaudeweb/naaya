9683c8a0380ae4c491b0e4c18291b7c7fb3d57d7
Alex Eftimie <alex@eftimie.ro>
2015-08-04 09:27:54 +0200

    Added submodule: naaya.gwhelp

--- eggs/naaya.gwhelp/naaya/gwhelp/__init__.py
+++
@@ -1,8 +0,0 @@
-from gwhelp import manage_addGWHelp_html, manage_addGWHelp
-from gwhelp import GWHelp
-
-def initialize(context):
-    context.registerClass(GWHelp, constructors=(
-        ('manage_addGWHelp_html', manage_addGWHelp_html),
-        ('manage_addGWHelp', manage_addGWHelp),
-    ))
