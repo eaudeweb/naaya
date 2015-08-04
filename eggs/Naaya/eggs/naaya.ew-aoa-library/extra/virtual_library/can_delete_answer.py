8a54eefa2a2ee27be2fb81b711e9ba9adf1dfac4
Alex Eftimie <alex@eftimie.ro>
2015-08-04 09:32:10 +0200

    Added submodule: naaya.ew-aoa-library

--- eggs/Naaya/eggs/naaya.ew-aoa-library/extra/virtual_library/can_delete_answer.py
+++
@@ -1,3 +0,0 @@
-site = container.getSite()
-rt_library_ids = site['review-template-viewer'].get_library_ids()
-return answer_id not in rt_library_ids
--- eggs/Naaya/eggs/naaya.ew-aoa-library/naaya/ew_aoa_library/__init__.py
+++
@@ -1,8 +0,0 @@
-from viewer import manage_addViewer_html, manage_addViewer
-from viewer import AoALibraryViewer
-
-def initialize(context):
-    context.registerClass(AoALibraryViewer, constructors=(
-        ('manage_addViewer_html', manage_addViewer_html),
-        ('manage_addViewer', manage_addViewer),
-    ))
