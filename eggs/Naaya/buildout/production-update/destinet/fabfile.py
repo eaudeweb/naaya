974a8a735e6496d5e400ea634efc0c36f60b0a46
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-08-19 20:02:33 +0300

    fabfile for github integration

--- eggs/Naaya/buildout/production-update/destinet/fabfile.py
+++ eggs/Naaya/buildout/production-update/destinet/fabfile.py
@@ -1,12 +1,17 @@
 from fabric.api import *
+from fabric.api import env
 from fabric.contrib.files import *
+from path import path as ppath
 
 app = env.app = {
-    'buildout_repo': 'https://svn.eionet.europa.eu/repositories/Naaya/buildout/destinet',
-    'bundle_repo': 'https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/NaayaBundles-DESTINET/',
+    'buildout_repo': 'https://github.com/eaudeweb/naaya.buildout.destinet.git',
+    'bundle_repo':
+        'https://github.com/eaudeweb/naaya.bundles.NaayaBundles-DESTINET.git',
 }
 
-from localcfg import *
+env.hosts = ['edw@destinet.eu']
+
+app['repo'] = ppath('/var/local/destinet/buildout')
 
 app.update({
     'buildout_var': app['repo']/'src/buildout-destinet',
@@ -19,20 +24,22 @@ def ssh():
     open_shell("cd '%(repo)s'" % app)
 
 
-def _svn_repo(repo_path, origin_url, update=True):
-    if not exists(repo_path/'.svn'):
+def _git_repo(repo_path, origin_url, update=True):
+    if not exists(repo_path/'.git'):
         run("mkdir -p '%s'" % repo_path)
         with cd(repo_path):
-            run("svn co '%s' ." % origin_url)
+            run("git clone '%s' buildout-destinet" % origin_url)
 
     elif update:
         with cd(repo_path):
-            run("svn up")
+            run("git pull")
+
 
 @task
 def _update_repos():
-    _svn_repo(app['buildout_var'], app['buildout_repo'], update=True)
-    _svn_repo(app['bundle_var'], app['bundle_repo'], update=True)
+    _git_repo(app['buildout_var'], app['buildout_repo'], update=True)
+    _git_repo(app['bundle_var'], app['bundle_repo'], update=True)
+
 
 @task
 def restart_destinet():
@@ -57,10 +64,12 @@ def restart_destinet():
     run("sleep 0.5")
     run("%(repo)s/bin/zope-instance-translations-test start" % app)
 
+
 @task
 def update_destinet():
     execute('_update_repos')
     execute('restart_destinet')
+
 
 @task
 def shutdown_destinet():
@@ -76,6 +85,7 @@ def shutdown_destinet():
     run("%(repo)s/bin/zeo-server stop" % app)
     run("sleep 0.5")
     run("%(repo)s/bin/zope-instance-translations-test stop" % app)
+
 
 @task
 def start_destinet():
