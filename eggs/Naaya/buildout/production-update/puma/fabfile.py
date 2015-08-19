b4c06899b8ee97505c8c5aa091cb931fda62fd76
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-08-19 19:49:09 +0300

    fabfile for puma, github repos

--- eggs/Naaya/buildout/production-update/puma/fabfile.py
+++ eggs/Naaya/buildout/production-update/puma/fabfile.py
@@ -4,8 +4,9 @@ from fabric.contrib.files import *
 from path import path as ppath
 
 app = env.app = {
-    'buildout_repo': 'https://svn.eionet.europa.eu/repositories/Naaya/buildout/chm-eu/',
-    'bundle_repo': 'https://svn.eionet.europa.eu/repositories/Naaya/trunk/eggs/NaayaBundles-CHMEU/',
+    'buildout_repo': 'https://github.com/eaudeweb/naaya.buildout.chm-eu.git',
+    'bundle_repo':
+        'https://github.com/eaudeweb/naaya.bundles.NaayaBundles-CHMEU.git',
 }
 
 
@@ -24,20 +25,22 @@ def ssh():
     open_shell("cd '%(repo)s'" % app)
 
 
-def _svn_repo(repo_path, origin_url, update=True):
-    if not exists(repo_path/'.svn'):
+def _git_repo(repo_path, origin_url, update=True):
+    if not exists(repo_path/'.git'):
         run("mkdir -p '%s'" % repo_path)
         with cd(repo_path):
-            run("svn co '%s' ." % origin_url)
+            run("git clone '%s' buildout-chm-eu" % origin_url)
 
     elif update:
         with cd(repo_path):
-            run("svn up")
+            run("git pull")
+
 
 @task
 def _update_repos():
-    _svn_repo(app['buildout_var'], app['buildout_repo'], update=True)
-    _svn_repo(app['bundle_var'], app['bundle_repo'], update=True)
+    _git_repo(app['buildout_var'], app['buildout_repo'], update=True)
+    _git_repo(app['bundle_var'], app['bundle_repo'], update=True)
+
 
 @task
 def restart_puma():
@@ -58,10 +61,12 @@ def restart_puma():
     run("sleep 0.5")
     run("%(repo)s/bin/zope-instance-3 start" % app)
 
+
 @task
 def update_puma():
     execute('_update_repos')
     execute('restart_puma')
+
 
 @task
 def shutdown_puma():
@@ -75,6 +80,7 @@ def shutdown_puma():
     run("%(repo)s/bin/zope-instance-3 stop" % app)
     run("sleep 0.5")
     run("%(repo)s/bin/zeo-server stop" % app)
+
 
 @task
 def start_puma():
