f15dacb1ff633d7974897ddf05a3318c95e23b8e
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-10-02 18:48:30 +0300

    released 1.2.9, new version 1.2.10

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.content.municipality',
-      version='1.2.9',
+      version='1.2.10',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
