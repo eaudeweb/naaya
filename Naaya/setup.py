ad58fd138f4e25d5464bcdc911a0d142ad49d842
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-10-16 18:49:20 +0300

    released 4.0.13, new version 4.0.14

--- Naaya/setup.py
+++ Naaya/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Naaya',
-      version='4.0.13',
+      version='4.0.14',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
