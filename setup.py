478a393af77397d0bc8ae9995d4c41696123020c
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2015-10-26 18:06:03 +0200

    released 3.0.29, new version 3.0.30

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.chm',
-      version='3.0.29',
+      version='3.0.30',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
