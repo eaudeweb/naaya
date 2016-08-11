04f31829337a4f75b9c85fc9c1308a92d273910c
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2016-08-11 19:10:09 +0300

    added support for the verbose edw version of validate_email

--- naaya/content/talkback/www/archive_check_emails.js
+++ naaya/content/talkback/www/archive_check_emails.js
@@ -2,7 +2,7 @@ $(function () {
 	function markDomElement(e, text) {
 		e.removeAttr("href");
 		e[0].style.color = "red";
-		e.attr("title", text + " " + check_emails.invalid_email_text);
+		e.attr("title", text);
 	}
 	check_emails.resolve("td.recipients-cell>a", markDomElement, 2);
 });
