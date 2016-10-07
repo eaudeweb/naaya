15083808d14e2ba207b04b2fb3ba2591d0516b7f
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2016-10-07 12:23:52 +0300

    released 1.1.7, new version 1.1.8

--- eggs/naaya.content.apncbphoto/setup.py
+++ eggs/naaya.content.apncbphoto/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='naaya.content.apncbphoto',
-      version='1.1.7',
+      version='1.1.8',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
