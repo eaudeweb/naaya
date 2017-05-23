ac6445161df7590f28c90bbf296e791413e400d1
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2017-05-23 12:52:09 +0300

    released 1.2.23, new version 1.2.24

--- eggs/destinet.extra/setup.py
+++ eggs/destinet.extra/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='destinet.extra',
-      version='1.2.23',
+      version='1.2.24',
       author='Eau de Web',
       author_email='office@eaudeweb.ro',
       url='http://naaya.eaudeweb.ro',
